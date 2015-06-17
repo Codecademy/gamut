53d669da4d7a767479aefbdca7fb021abd1a14fc
Jake Hiller <jakemhiller@gmail.com>
2015-06-17 16:34:40 -0400

    JS variables are camelcase, scss is kebab

--- cc-identity/build.js
+++ cc-identity/build.js
@@ -1,9 +1,11 @@
 
 var fs = require('fs');
+var _ = require('lodash');
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var jsonSass = require('gulp-json-sass');
 var dir = require('../build-utils/dir')(__dirname);
+var normalize = require('normalize-object');
 
 var taskName = 'cc-identity';
 
@@ -11,12 +13,16 @@ var ccData = require('./src');
 
 gulp.task('build-json', function(callback) {
   var ccDataJSON = JSON.stringify(ccData);
-  fs.writeFile(dir('/cc-identity.json'), ccDataJSON, callback);
+  var ccDataJS = JSON.stringify(normalize(ccData));
+  // JS variables are camel case for dot-notation
+  fs.writeFile(dir('/cc-identity.json'), ccDataJS, _.noop);
+  // TMP file used to create the SCSS variables (kebab case)
+  fs.writeFile(dir('/tmp/cc-identity.json'), ccDataJSON, callback);
 });
 
 gulp.task(taskName, ['build-json'], function() {
   return gulp
-    .src(dir('/cc-identity.json'))
+    .src(dir('/tmp/cc-identity.json'))
     .pipe(jsonSass())
     .pipe(concat('cc-identity.scss'))
     .pipe(gulp.dest(dir('/')));
--- cc-identity/src/indexJSON.js
+++ cc-identity/src/indexJSON.js
@@ -1,7 +1,6 @@
 var _ = require('lodash');
 var base = require('./colors/base');
 var lp = require('./colors/lp');
-var normalize = require('normalize-object');
 
 var output = {
   color: _.extend({}, base, lp)
