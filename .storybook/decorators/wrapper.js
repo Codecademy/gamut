4ebb60c5185429fe66b320e627fa9ebaf9f5ea9e
Jake Hiller <jakemhiller@gmail.com>
2016-10-12 16:57:36 -0400

    updated storybook styles

--- .storybook/decorators/wrapper.js
+++ .storybook/decorators/wrapper.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Container } from 'src/FlexBox';
-import 'src/styles/core';
+import './wrapper-styles';
 
 export const wrapper = (story) => (
   <Container justify="stretch" column style={{ minHeight: '100vh', padding: '1rem' }}>
