dc42e08cdbb284d85d0238b46c2928dcada71420
Lydia Stepanek <lydia.stepanek@gmail.com>
2017-03-09 17:59:50 -0500

    final styles

--- stories/Color-story.js
+++ stories/Color-story.js
@@ -1,28 +1,69 @@
 import React from 'react';
 import { storiesOf, action } from '@kadira/storybook';
-import { Container } from 'src/FlexBox';
+import { Container, Item } from 'src/FlexBox';
 import Button, { presetThemes } from 'src/Button';
 import id from 'identity';
 
-let renderBaseColors = () => {
-  return Object.keys(id.color).map(function (colorName, idx) {
-    let style = {
-      'display': 'inline-block',
-      'background-color': id.color[colorName]
-    };
-    return <div key={`base-color-${idx}`} style={style}>{colorName}</div>;
-  });
+let formatCamelCase = (string) => {
+  return string.replace(/([a-zA-Z])(?=[A-Z])/g, '$1-').toLowerCase();
 };
 
-let renderSwatchColors = () => {
-  return Object.keys(id.swatches).map(function (swatchName) {
-    return Object.keys(id.swatches[swatchName]).map(function (colorLevel, idx) {
-      let style = {
-        'display': 'inline-block',
-        'background-color': id.swatches[swatchName][colorLevel]
-      };
-      return <div key={`base-color-${idx}`} style={style}>{swatchName} {colorLevel}</div>;
-    });
+let hexToRgb = (hex) => {
+  let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result ? {
+    r: parseInt(result[1], 16),
+    g: parseInt(result[2], 16),
+    b: parseInt(result[3], 16)
+  } : null;
+}
+
+let contrastColor = (hex) => {
+  let rgb = hexToRgb(hex);
+  let red = rgb['r'];
+  let green = rgb['g'];
+  let blue = rgb['b'];
+  if ((red*0.299 + green*0.587 + blue*0.114) > 176) {
+    return '#000000';
+  }
+  return '#ffffff';
+}
+
+let containerStyles = {
+  display: 'inline-block',
+  marginBottom: '1rem',
+  marginRight: '1rem'
+};
+
+let renderSwatchRow = (data, swatchName) => {
+  return Object.keys(data[swatchName]).map(function (colorSuffix) {
+    let swatchStyles = {
+      color: contrastColor(data[swatchName][colorSuffix]),
+      backgroundColor: data[swatchName][colorSuffix],
+      height: '222px',
+      margin: '10px 0',
+      width: '222px'
+    };
+    let colorName = swatchName === 'standard' ? colorSuffix : formatCamelCase(swatchName) + '-' + colorSuffix;
+    return (
+      <div style={containerStyles} key={colorName}>
+        <Container style={swatchStyles} center>
+          {data[swatchName][colorSuffix]}
+        </Container>
+        <span>{colorName}</span>
+      </div>
+    );
+  });
+}
+
+let renderSwatchRows = (data) => {
+  return Object.keys(data).map(function (swatchName) {
+    if (['code', 'basic'].includes(swatchName)) return null;
+    return (
+      <div key={swatchName}>
+        <h2>{formatCamelCase(swatchName)}</h2>
+        {renderSwatchRow(data, swatchName)}
+      </div>
+    );
   });
 };
 
@@ -30,10 +71,8 @@ storiesOf('Colors', module)
   .add('with text', () => (
     <Container column>
       <div>
-        <h3>Standard colors</h3>
-        { renderBaseColors() }
-        <h3>Swatch colors</h3>
-        { renderSwatchColors() }
+        {renderSwatchRows({ 'standard': id.color })}
+        {renderSwatchRows(id.swatches)}
       </div>
     </Container>
   ));
