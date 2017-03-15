bb0f74fb6750b761763ebb3d58661668c7271214
Lydia Stepanek <lydia.stepanek@gmail.com>
2017-03-15 14:29:38 -0400

    use const not let

--- stories/Color-story.js
+++ stories/Color-story.js
@@ -3,18 +3,18 @@ import { storiesOf } from '@kadira/storybook';
 import { Container } from 'src/FlexBox';
 import id from 'identity';
 
-let infoOptions = {
+const infoOptions = {
   inline: true,
   source: false,
   propTables: false
 };
 
-let parseCamelCase = (string) => {
+const parseCamelCase = (string) => {
   return string.replace(/([a-zA-Z])(?=[A-Z0-9])/g, '$1-').toLowerCase();
 };
 
-let hexToRgb = (hex) => {
-  let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+const hexToRgb = (hex) => {
+  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
   return result ? {
     r: parseInt(result[1], 16),
     g: parseInt(result[2], 16),
@@ -22,40 +22,40 @@ let hexToRgb = (hex) => {
   } : null;
 };
 
-let contrastColor = (hex) => {
-  let rgb = hexToRgb(hex);
-  let red = rgb.r;
-  let green = rgb.g;
-  let blue = rgb.b;
+const contrastColor = (hex) => {
+  const rgb = hexToRgb(hex);
+  const red = rgb.r;
+  const green = rgb.g;
+  const blue = rgb.b;
   if (((red * 0.299) + (green * 0.587) + (blue * 0.114)) > 176) {
     return '#000000';
   }
   return '#ffffff';
 };
 
-let containerStyles = {
+const containerStyles = {
   display: 'inline-block',
   marginBottom: '1rem',
   marginRight: '1rem'
 };
 
-let getSassVariableName = (variablePrefix, variableSuffix) => {
+const getSassVariableName = (variablePrefix, variableSuffix) => {
   if (variablePrefix) {
     return `$swatches-${parseCamelCase(variablePrefix)}-${parseCamelCase(variableSuffix)}`;
   }
   return `$color-${parseCamelCase(variableSuffix)}`;
 };
 
-let renderSwatch = (data, variablePrefix) => {
+const renderSwatch = (data, variablePrefix) => {
   return Object.keys(data).map((variableSuffix) => {
-    let swatchStyles = {
+    const swatchStyles = {
       color: contrastColor(data[variableSuffix]),
       backgroundColor: data[variableSuffix],
       height: '160px',
       margin: '10px 0',
       width: '160px'
     };
-    let sassVariableName = getSassVariableName(variablePrefix, variableSuffix);
+    const sassVariableName = getSassVariableName(variablePrefix, variableSuffix);
     return (
       <div style={containerStyles} key={sassVariableName}>
         <Container style={swatchStyles} center>
@@ -67,7 +67,7 @@ let renderSwatch = (data, variablePrefix) => {
   });
 };
 
-let stories = storiesOf('Colors', module)
+const stories = storiesOf('Colors', module)
   .addWithInfo(
     'standard',
     () => (
