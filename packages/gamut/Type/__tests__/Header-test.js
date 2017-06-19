d55778faabece84bbd9b7197f4e322c391835110
Jon Samp <jon@codecademy.com>
2017-06-18 21:46:32 -0400

    Adds test for <Header>

--- packages/gamut/Type/__tests__/Header-test.js
+++ packages/gamut/Type/__tests__/Header-test.js
@@ -1,1 +1,22 @@
-// WIP
+import React from 'react';
+import { shallow } from 'enzyme';
+import Header from '../Header';
+
+describe('<Header>', () => {
+
+  const wrapper = shallow(
+    <Header />
+  );
+
+  const wrapperWithSize = shallow(
+    <Header size={2} />
+  );
+
+  it('renders h1 when no size provided', () => {
+    expect(wrapper.find('h1').length).toBe(1);
+  });
+
+  it('renders h2 when size 2 is provided', () => {
+    expect(wrapperWithSize.find('h2').length).toBe(1);
+  });
+});
