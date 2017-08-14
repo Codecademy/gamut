b014f3c9b2b6df550201b98f2ed4cff15c5cb003
Jon Samp <jon@codecademy.com>
2017-08-13 20:25:59 -0400

    working version, still wip

--- packages/gamut/Form/Checkbox.js
+++ packages/gamut/Form/Checkbox.js
@@ -4,13 +4,18 @@ import cx from 'classnames';
 import s from './styles/Checkbox.scss';
 
 const Checkbox = ({ className, label, htmlFor, ...inputProps }) => {
-  const classNames = cx(s.Checkbox, className);
+  const classNames = cx(s.labelCbx, className);
 
   return (
-    <label htmlFor={htmlFor} className={s.labelCbx}>
-      <div className={classNames}>
-        <input id={htmlFor} type="checkbox" className={s.invisible} />
-        <div className={s.checkbox}>
+    <label htmlFor={htmlFor} className={classNames}>
+      <input
+        id={htmlFor}
+        type="checkbox"
+        className={s.invisible}
+        {...inputProps}
+      />
+      <div className={s.checkboxWrapper}>
+        <div className={s.checkboxSquare}>
           <svg width="24px" height="24px" viewBox="0 0 20 20">
             <path d="M3,1 L17,1 L17,1 C18.1045695,1 19,1.8954305 19,3 L19,17 L19,17 C19,18.1045695 18.1045695,19 17,19 L3,19 L3,19 C1.8954305,19 1,18.1045695 1,17 L1,3 L1,3 C1,1.8954305 1.8954305,1 3,1 Z" />
             <polyline points="4 11 8 15 16 6" />
