c461fad01dcd3e09a220b9a3acdacaabe3f57557
Jon Samp <jon@codecademy.com>
2017-08-14 21:53:26 -0400

    format it back to the original

--- packages/gamut-storybook/stories/Form-story.js
+++ packages/gamut-storybook/stories/Form-story.js
@@ -1,35 +1,30 @@
 import React from 'react';
 import { storiesOf } from '@kadira/storybook';
 import { Button } from '@codecademy/gamut';
-import {
-  Checkbox,
-  RadioGroup,
-  Radio,
-  Input,
-  TextArea,
-  Select,
-  FormGroup,
-  FormGroupLabel,
-  FormGroupDescription,
-  Form
-} from '@codecademy/gamut/Form';
+import { Checkbox, RadioGroup, Radio, Input, TextArea, Select, FormGroup, FormGroupLabel, FormGroupDescription, Form } from '@codecademy/gamut/Form';
 
 storiesOf('Form', module)
   .addWithInfo(
     'Form',
-    () =>
+    () => (
       <Form>
         <FormGroup
           htmlFor="firstName"
           label="First Name"
           description="Type your first name"
         >
-          <Input htmlFor="firstName" placeholder="First Name" />
+          <Input
+            htmlFor="firstName"
+            placeholder="First Name"
+          />
         </FormGroup>
 
         <FormGroup>
           <FormGroupLabel htmlFor="lastName">Last Name</FormGroupLabel>
-          <Input htmlFor="lastName" placeholder="Last Name" />
+          <Input
+            htmlFor="lastName"
+            placeholder="Last Name"
+          />
         </FormGroup>
 
         <FormGroup
@@ -37,14 +32,16 @@ storiesOf('Form', module)
           label="About"
           description="Write about your self in Haiku form."
         >
-          <TextArea htmlFor="about" placeholder="About you" rows="8" />
+          <TextArea
+            htmlFor="about"
+            placeholder="About you"
+            rows="8"
+          />
         </FormGroup>
 
         <FormGroup>
           <FormGroupLabel>Development Goal</FormGroupLabel>
-          <FormGroupDescription>
-            Which goal do you identify with?
-          </FormGroupDescription>
+          <FormGroupDescription>Which goal do you identify with?</FormGroupDescription>
           <RadioGroup
             htmlForPrefix="why-are-you-learning"
             name="why-are-you-learning"
@@ -70,48 +67,57 @@ storiesOf('Form', module)
 
         <FormGroup>
           <FormGroupLabel>Terms of Service</FormGroupLabel>
-          <Checkbox htmlFor="tos" label="I agree" />
+          <Checkbox
+            htmlFor="tos"
+            label="I agree"
+          />
         </FormGroup>
-        <Button href="/" type="submit" caps>
-          Submit
-        </Button>
-      </Form>,
-  {
-    inline: true
-  }
+        <Button href="/" type="submit" caps>Submit</Button>
+      </Form>
+    ), {
+      inline: true
+    }
   )
   .addWithInfo(
     'FormGroupLabel',
-    () => <FormGroupLabel>Password</FormGroupLabel>,
-  {
-    inline: true
-  }
+    () => (
+      <FormGroupLabel>Password</FormGroupLabel>
+    ), {
+      inline: true
+    }
   )
   .addWithInfo(
     'FormGroupDescription',
-    () =>
-      <FormGroupDescription>
-        Write about your self in Haiku form.
-      </FormGroupDescription>,
-  {
-    inline: true
-  }
+    () => (
+      <FormGroupDescription>Write about your self in Haiku form.</FormGroupDescription>
+    ), {
+      inline: true
+    }
   )
   .addWithInfo(
     'Checkbox',
-    () =>
+    () => (
       <div>
-        <Checkbox htmlFor="html-css" label="HTML & CSS" />
-        <Checkbox htmlFor="javascript" label="JavaScript" />
-        <Checkbox htmlFor="ruby" label="Ruby" />
-      </div>,
-  {
-    inline: true
-  }
+        <Checkbox
+          htmlFor="html-css"
+          label="HTML & CSS"
+        />
+        <Checkbox
+          htmlFor="javascript"
+          label="JavaScript"
+        />
+        <Checkbox
+          htmlFor="ruby"
+          label="Ruby"
+        />
+      </div>
+    ), {
+      inline: true
+    }
   )
   .addWithInfo(
     'RadioGroup',
-    () =>
+    () => (
       <RadioGroup
         htmlForPrefix="why-are-you-learning"
         name="why-are-you-learning"
@@ -124,47 +130,67 @@ storiesOf('Form', module)
           label="A better understanding of web development in general"
           value="understanding"
         />
-      </RadioGroup>,
-  {
-    inline: true
-  }
+      </RadioGroup>
+    ), {
+      inline: true
+    }
   )
   .addWithInfo(
     'Input',
-    () => <Input htmlFor="name" placeholder="First Name" />,
-  {
-    inline: true
-  }
+    () => (
+      <Input
+        htmlFor="name"
+        placeholder="First Name"
+      />
+    ), {
+      inline: true
+    }
   )
   .addWithInfo(
     'Input error state',
-    () => <Input htmlFor="name" placeholder="First Name" error />,
-  {
-    inline: true
-  }
+    () => (
+      <Input
+        htmlFor="name"
+        placeholder="First Name"
+        error
+      />
+    ), {
+      inline: true
+    }
   )
   .addWithInfo(
     'TextArea',
-    () => <TextArea htmlFor="about" placeholder="About you" rows="8" />,
-  {
-    inline: true
-  }
+    () => (
+      <TextArea
+        htmlFor="about"
+        placeholder="About you"
+        rows="8"
+      />
+    ), {
+      inline: true
+    }
   )
   .addWithInfo(
     'TextArea error state',
-    () => <TextArea htmlFor="about" placeholder="About you" rows="8" error />,
-  {
-    inline: true
-  }
+    () => (
+      <TextArea
+        htmlFor="about"
+        placeholder="About you"
+        rows="8"
+        error
+      />
+    ), {
+      inline: true
+    }
   )
   .addWithInfo(
     'Select',
-    () =>
+    () => (
       <Select
         htmlFor="select"
         options={['one', 'two', 'three', 'option four', 'five']}
-      />,
-  {
-    inline: true
-  }
+      />
+    ), {
+      inline: true
+    }
   );
