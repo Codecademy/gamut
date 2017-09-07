1c938e47af32c0fed1dca4e2743dccd091f627c0
Alex Holachek <aholachek@gmail.com>
2017-09-06 20:27:50 -0400

    adding tabs to gamut

---
+++ packages/gamut/Tabs/__tests__/index.js
@@ -0,0 +1,103 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+
+import Tabs from '../index';
+
+describe('Accessible Tabs', () => {
+
+  function generateTabConfig(num, isDefault) {
+    return [...Array(num).keys()].map(ind => {
+      const displayInd = ind + 1;
+      return {
+        text: 'Tab Number ' + displayInd,
+        default: isDefault === ind
+      };
+    });
+  }
+
+  it('allows prop config to specify default tab and show appropriate tab panel', () => {
+
+    const wrapper = shallow(
+      <Tabs config={generateTabConfig(2)}>
+        <div>Tab 1</div>
+        <div>Tab 2</div>
+      </Tabs>
+    );
+
+    expect(wrapper.find('.tab').at(0).props().active).toBe(true);
+    expect(wrapper.find('.tabPanel').at(0).props().active).toBe(true);
+    expect(wrapper.find('.tab').at(1).props().active).toBe(false);
+    expect(wrapper.find('.tabPanel').at(1).props().active).toBe(false);
+
+    const wrapper2 = shallow(
+      <Tabs config={generateTabConfig(2, 1)}>
+        <div>Tab 1</div>
+        <div>Tab 2</div>
+      </Tabs>
+    );
+
+    expect(wrapper2.find('.tab').at(0).props().active).toBe(false);
+    expect(wrapper2.find('.tabPanel').at(0).props().active).toBe(false);
+    expect(wrapper2.find('.tab').at(1).props().active).toBe(true);
+    expect(wrapper2.find('.tabPanel').at(1).props().active).toBe(true);
+
+  });
+
+  it('does not render the contents of hidden tabs', () => {
+
+    const wrapper = shallow(
+      <Tabs config={generateTabConfig(2)}>
+        <div>Tab 1</div>
+        <div>Tab 2</div>
+      </Tabs>
+    );
+
+    expect(wrapper.find('.tabPanel').at(0).children().html()).toBe('<div>Tab 1</div>');
+    expect(wrapper.find('.tabPanel').at(1).children().html()).toBe('<div></div>');
+  });
+
+  it('Shows the correct tab panel on click, with proper tab highlighted via animated underline', () => {
+
+    const wrapper = mount(
+      <Tabs config={generateTabConfig(2)}>
+        <div>Tab 1</div>
+        <div>Tab 2</div>
+      </Tabs>
+    );
+
+    // highlight first tab + show first tab panel initially
+    expect(wrapper.find('.tab').at(0).props()['aria-selected']).toBe(true);
+    expect(wrapper.find('.tabPanel').at(0).props().style.display).toBe(undefined);
+    expect(wrapper.find('.tab').at(1).props()['aria-selected']).toBe(false);
+    expect(wrapper.find('.tabPanel').at(1).props().style.display).toBe('none');
+    expect(wrapper.find('.tabIndicator').props().style).toEqual({left: '0%', width: '50%'});
+
+    // now the second tab is active
+    wrapper.find('.tab').at(1).simulate('focus');
+
+    expect(wrapper.find('.tab').at(0).props()['aria-selected']).toBe(false);
+    expect(wrapper.find('.tabPanel').at(0).props().style.display).toBe('none');
+    expect(wrapper.find('.tab').at(1).props()['aria-selected']).toBe(true);
+    expect(wrapper.find('.tabPanel').at(1).props().style.display).toBe(undefined);
+    expect(wrapper.find('.tabIndicator').props().style).toEqual({left: '50%', width: '50%'});
+
+  });
+
+
+  it('calls the onChange function provided in props when tabs change', () => {
+    const onChange = jest.fn();
+    const wrapper = mount(
+      <Tabs
+        config={generateTabConfig(2)}
+        onChange={onChange}
+      >
+        <div>Tab 1</div>
+        <div>Tab 2</div>
+      </Tabs>
+    );
+    wrapper.find('.tab').at(1).simulate('focus');
+
+    expect(onChange.mock.calls.length).toBe(1);
+  });
+
+});
---
+++ packages/gamut/Tabs/index.js
@@ -0,0 +1,114 @@
+import React, { Component, Children } from 'react';
+import PropTypes from 'prop-types';
+import { TabList, Tab, TabPanel, Wrapper } from 'react-aria-tabpanel';
+
+import s from './styles';
+
+export default class Tabs extends Component {
+  static propTypes = {
+    config: PropTypes.arrayOf(
+      PropTypes.shape({
+        text: PropTypes.string.isRequired,
+        default: PropTypes.bool
+      })
+    ).isRequired,
+    onChange: PropTypes.func,
+    children: PropTypes.arrayOf(PropTypes.node).isRequired
+  };
+
+  state = { activeTabId: undefined };
+
+  componentWillMount() {
+    // for unique tab ids (in case there are multiple
+    // tab widgets on the page)
+    this._idPrefix = Math.random();
+  }
+
+  createId(index) {
+    return `${this._idPrefix}-${index}`;
+  }
+
+  renderTabList = activeTabId => {
+    // leftPercent determines where the animated underline should be positioned
+    const leftPercent =
+      (this.props.config.findIndex((c, i) => this.createId(i) === activeTabId) /
+      this.props.config.length) *
+      100;
+
+    return (
+      <TabList className={s.tabList}>
+        {this.props.config.map((c, i) => {
+          const key = this.createId(i);
+          return (
+            <Tab
+              id={key}
+              key={key}
+              className={s.tab}
+              active={key === activeTabId}
+            >
+              {c.text}
+            </Tab>
+          );
+        })}
+        <div
+          className={s.tabIndicator}
+          style={{
+            left: `${leftPercent}%`,
+            width: `${100 / this.props.config.length}%`
+          }}
+          aria-hidden
+        />
+      </TabList>
+    );
+  };
+
+  renderTabPanels = activeTabId => {
+    // render all tab panels, but only active tab panel contains anything
+    return (
+      <div className={s.tabPanelContainer}>
+        {this.props.config.map((c, i) => {
+          const key = this.createId(i);
+          const isActive = key === activeTabId;
+          return (
+            <TabPanel
+              tabId={key}
+              key={key}
+              active={isActive}
+              className={s.tabPanel}
+            >
+              {isActive ? Children.toArray(this.props.children)[i] : <div />}
+            </TabPanel>
+          );
+        })}
+      </div>
+    );
+  };
+
+  render() {
+    let activeTabId;
+    // which tab should be shown right now?
+    if (this.state.activeTabId) {
+      activeTabId = this.state.activeTabId;
+    } else if (this.props.config.findIndex(c => c.default) !== -1) {
+      activeTabId = this.createId(this.props.config.findIndex(c => c.default));
+    } else {
+      activeTabId = this.createId(0);
+    }
+
+    return (
+      <Wrapper
+        onChange={this.onChange}
+        activeTabId={activeTabId}
+        className={s.tabContainer}
+      >
+        {this.renderTabList(activeTabId)}
+        {this.renderTabPanels(activeTabId)}
+      </Wrapper>
+    );
+  }
+
+  onChange = id => {
+    if (this.props.onChange) this.props.onChange(id);
+    this.setState(() => ({ activeTabId: id }));
+  };
+}
