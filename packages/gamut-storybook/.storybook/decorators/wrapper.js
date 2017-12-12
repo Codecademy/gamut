75e6b6d2302b319fd2febce4adb204dd1770c097
Jake Hiller <jakemhiller@gmail.com>
2017-12-12 15:08:18 -0500

    update config, run prettier last

--- packages/gamut-storybook/.storybook/decorators/wrapper.js
+++ packages/gamut-storybook/.storybook/decorators/wrapper.js
@@ -3,7 +3,11 @@ import { Container } from '@codecademy/gamut/FlexBox';
 import './wrapper-styles';
 
 export const wrapper = story => (
-  <Container justify="stretch" column style={{ minHeight: '100vh', padding: '1rem' }}>
+  <Container
+    justify="stretch"
+    column
+    style={{ minHeight: '100vh', padding: '1rem' }}
+  >
     {story()}
   </Container>
 );
