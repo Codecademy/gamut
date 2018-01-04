43a6d334e874e8b039080853c7b6ffbf66046fee
Alex Holachek <aholachek@gmail.com>
2018-01-04 11:25:47 -0500

    rename new tabsand deprecated tabs

--- packages/gamut/Tabs/NewTab/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Tab } from './Tab';
-export { default as TabList } from './TabList';
-export { default as TabPanel } from './TabPanel';
-export { default as Tabs } from './Tabs';
