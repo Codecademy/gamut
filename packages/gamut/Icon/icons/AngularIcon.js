aa5a9b0e6fe8418a5db56fa6ec12636f4f44d6b9
Alex Hannan <ahannan@codecademy.com>
2018-01-12 17:17:50 -0500

    Push fill definition into parent icon

--- packages/gamut/Icon/icons/AngularIcon.js
+++ packages/gamut/Icon/icons/AngularIcon.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function AngularIcon(props) {
   return (
-    <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
+    <svg viewBox="0 0 24 24" {...props}>
       <title>AngularJS Icon</title>
       <path d="M12 2L2.6 5.413l1.42 12.184L12 22l7.978-4.402 1.42-12.183L12 2zm6.96 14.912L12 20.75l-6.962-3.838-1.25-10.767L12 3.16l8.215 2.985-1.255 10.768z" />
       <path d="M6.963 16.42h1.372l1.137-2.75h5.343l1.138 2.75h1.372L12.143 4.943 6.963 16.42zm2.967-3.852l2.213-5.343 2.207 5.343H9.93z" />
