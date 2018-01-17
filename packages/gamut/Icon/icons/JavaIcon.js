31c5cb5a9e8e443949f04f1706f439111c49eb5e
Alex Hannan <ahannan@codecademy.com>
2018-01-16 19:44:04 -0500

    Fix a couple icon names

--- packages/gamut/Icon/icons/JavaIcon.js
+++ packages/gamut/Icon/icons/JavaIcon.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function HTMLIcon(props) {
+export default function JavaIcon(props) {
   return (
     <svg viewBox="0 0 24 24" {...props}>
       <title>Java Icon</title>
