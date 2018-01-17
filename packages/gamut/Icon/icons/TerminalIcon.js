cc2b52b05247af8ad3a851c90f186e6916c1fa02
Alex Hannan <ahannan@codecademy.com>
2018-01-16 19:16:29 -0500

    Update icons with newest SVGs

--- packages/gamut/Icon/icons/TerminalIcon.js
+++ packages/gamut/Icon/icons/TerminalIcon.js
@@ -4,8 +4,8 @@ export default function TerminalIcon(props) {
   return (
     <svg viewBox="0 0 24 24" {...props}>
       <title>Terminal Icon</title>
-      <path d="M3 5v14.694h17.96V5H3zm16.979 13.715h-16V7.939h16v10.776zm-16-11.755v-.981h16v.979l-16 .002z" />
-      <path d="M5.612 9.735h.979v1.96h-.98z" />
+      <path d="M2 4v16h20V4H2zm19 15H3V7h18v12zM3 6V5h18v.998L3 6z" />
+      <path d="M4 8h1v2H4z" />
     </svg>
   );
 }
