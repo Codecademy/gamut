c4a1d9f9400a7efd310dd518ff90531328372bd7
Alex Hannan <ahannan@codecademy.com>
2018-01-16 19:16:29 -0500

    Update icons with newest SVGs

--- packages/gamut/Icon/icons/NumPyIcon.js
+++ packages/gamut/Icon/icons/NumPyIcon.js
@@ -4,7 +4,7 @@ export default function NumPyIcon(props) {
   return (
     <svg viewBox="0 0 24 24" {...props}>
       <title>NumPy Icon</title>
-      <path d="M3 16.154h4.385v4.154H3v-4.154zm0-4.385h4.385v4.154H3V11.77zm0-4.384h4.385v4.153H3V7.385zM3 3h4.385v4.154H3V3zm4.615 4.385H12v4.153H7.615V7.385zM12 11.769h4.385v4.154H12V11.77zm4.615 4.385H21v4.154h-4.385v-4.154zm0-4.385H21v4.154h-4.385V11.77zm0-4.384H21v4.153h-4.385V7.385zm0-4.385H21v4.154h-4.385V3z" />
+      <path d="M3 17h4v4H3zM3 12h4v4H3zM3 7h4v4H3zM3 2h4v4H3zM8 7h4v4H8zM12 12h4v4h-4zM17 17h4v4h-4zM17 12h4v4h-4zM17 7h4v4h-4zM17 2h4v4h-4z" />
     </svg>
   );
 }
