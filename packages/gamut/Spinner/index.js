4418bdd30385ed148ce8849971d5902656b12607
Alex Hannan <ahannan@codecademy.com>
2018-01-26 16:26:38 -0500

    Clean up code a bit

--- packages/gamut/Spinner/index.js
+++ packages/gamut/Spinner/index.js
@@ -3,22 +3,9 @@ import PropTypes from 'prop-types';
 import cx from 'classnames';
 import s from './styles';
 import SpinnerIcon from './SpinnerIcon';
-import SpinnerIconV2 from './SpinnerIconV2';
 
-const Spinner = ({ size, className, fill, v2 }) => {
+const Spinner = ({ size, className, fill }) => {
   const classNames = cx(s.spinner, className);
-  // const classNames = cx(className);
-
-  if (v2) {
-    return (
-      <SpinnerIconV2
-        className={classNames}
-        fill={fill}
-        height={size}
-        width={size}
-      />
-    );
-  }
 
   return (
     <SpinnerIcon
@@ -39,7 +26,6 @@ Spinner.propTypes = {
   size: PropTypes.string,
   className: PropTypes.string,
   fill: PropTypes.string,
-  v2: PropTypes.bool,
 };
 
 export default Spinner;
