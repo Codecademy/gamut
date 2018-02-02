15ae2cf078da15d4b814a14aebfdb5f3baa39390
Alex Hannan <ahannan@codecademy.com>
2018-02-02 14:48:30 -0500

    Add a Finite loading spinner

--- packages/gamut-storybook/stories/Spinner-story.js
+++ packages/gamut-storybook/stories/Spinner-story.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Spinner from '@codecademy/gamut/Spinner';
-import { text } from '@storybook/addon-knobs';
+import { text, select } from '@storybook/addon-knobs';
 
 storiesOf('Component/Spinner', module).add(
   'Spinner',
-  () => <Spinner size={text('size', '1rem')} />,
+  () => (
+    <Spinner
+      size={text('size', '1rem')}
+      type={select('type', ['infinite', 'finite'], 'infinite')}
+      duration={text('duration', '1s')}
+    />
+  ),
   {
     inline: true,
     propTables: false,
