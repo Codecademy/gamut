e6d623252f55667f7230bbfabd6c8647927a36a1
Alex Hannan <ahannan@codecademy.com>
2018-02-06 16:41:50 -0500

    Refactor spinner to be infinite only

--- packages/gamut-storybook/stories/Spinner-story.js
+++ packages/gamut-storybook/stories/Spinner-story.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Spinner from '@codecademy/gamut/Spinner';
-import { text, select } from '@storybook/addon-knobs';
+import { text } from '@storybook/addon-knobs';
 
 storiesOf('Component/Spinner', module).add(
   'Spinner',
-  () => (
-    <Spinner
-      size={text('size', '1rem')}
-      type={select('type', ['infinite', 'finite'], 'infinite')}
-      duration={text('duration', '1s')}
-    />
-  ),
+  () => <Spinner size={text('size', '1rem')} />,
   {
     inline: true,
     propTables: false,
