c7930fdb87d7aa450988e0e5d405b7ba7d0ac19d
Alex Hannan <ahannan@codecademy.com>
2018-02-06 16:41:50 -0500

    Refactor spinner to be infinite only

--- packages/gamut/Spinner/index.js
+++ packages/gamut/Spinner/index.js
@@ -1,25 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Infinite from './Infinite';
-import Finite from './Finite';
 
 const propTypes = {
   size: PropTypes.string,
   className: PropTypes.string,
-  type: PropTypes.oneOf(['infinite', 'finite']),
-  duration: PropTypes.string,
 };
 
 const defaultProps = {
   size: '16px',
-  type: 'infinite',
-  duration: '1s',
 };
 
-function Spinner({ size, className, type, duration }) {
-  const Tag = type === 'infinite' ? Infinite : Finite;
-
-  return <Tag size={size} className={className} duration={duration} />;
+function Spinner({ size, className }) {
+  return (
+    <svg viewBox="0 0 100 100" height={size} width={size} className={className}>
+      <defs>
+        <linearGradient id="spinner-stroke" x1="0" y1="0" x2="1" y2="1">
+          <stop offset="0" stopColor="currentColor" stopOpacity="0" />
+          <stop offset="1" stopColor="currentColor" />
+        </linearGradient>
+      </defs>
+      <path
+        d="M50 10 a 40 40 0 0 1 40 40"
+        stroke="url(#spinner-stroke)"
+        strokeWidth="10"
+        strokeLinecap="round"
+        fill="none"
+      />
+      <animateTransform
+        attributeName="transform"
+        type="rotate"
+        from="0"
+        to="360"
+        dur="1s"
+        begin="0"
+        repeatCount="indefinite"
+      />
+    </svg>
+  );
 }
 
 Spinner.defaultProps = defaultProps;
