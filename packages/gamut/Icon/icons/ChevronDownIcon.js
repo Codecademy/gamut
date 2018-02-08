57337606df7f6daba8f38d079f03ff25abc3d7e0
Bana Malik <bandanakm@gmail.com>
2018-02-07 19:11:46 -0500

    add icons to iconMap

---
+++ packages/gamut/Icon/icons/ChevronDownIcon.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function ChevronDownIcon(props) {
+  return (
+    <svg width="24" height="24" {...props}>
+      <title>Chevron Down Icon</title>
+      <path
+        d="M20.293 7.293l1.414 1.414L12 18.414 2.293 8.707l1.414-1.414L12 15.586z"
+        fill="#000"
+        fillRule="nonzero"
+      />
+    </svg>
+  );
+}
---
+++ packages/gamut/Icon/icons/ChevronUpIcon.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function ChevronUpIcon(props) {
+  return (
+    <svg width="24" height="24" {...props}>
+      <title>Chevron Up Icon</title>
+      <path
+        d="M20.293 18.414L21.707 17 12 7.293 2.293 17l1.414 1.414L12 10.121z"
+        fill="#000"
+        fillRule="nonzero"
+      />
+    </svg>
+  );
+}
