565d141b51f73bf5450beb3821138b208cd3a75f
Bana Malik <bandanakm@gmail.com>
2018-02-08 14:38:46 -0500

    incorporate feedback from review

--- packages/gamut/Icon/icons/ChevronLeftIcon.js
+++ packages/gamut/Icon/icons/ChevronLeftIcon.js
@@ -1,19 +1,10 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 export default function ChevronLeftIcon(props) {
   return (
-    <svg {...props}>
+    <svg viewBox="0 0 24 24" {...props}>
       <title>Chevron Left Icon</title>
-      <path
-        d="M9.475 12.06l7.293 7.294-1.414 1.414-8.708-8.707 8.708-8.707 1.414 1.414z"
-        fill={props.currentColor}
-        fillRule="nonzero"
-      />
+      <path d="M9.475 12.06l7.293 7.294-1.414 1.414-8.708-8.707 8.708-8.707 1.414 1.414z" />
     </svg>
   );
 }
-
-ChevronLeftIcon.propTypes = {
-  currentColor: PropTypes.string,
-};
