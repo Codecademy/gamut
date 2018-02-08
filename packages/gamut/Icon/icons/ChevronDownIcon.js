04861cdb6a3054623761c0fd250b82413e074398
Bana Malik <bandanakm@gmail.com>
2018-02-07 22:32:23 -0500

    pass in height width as props

--- packages/gamut/Icon/icons/ChevronDownIcon.js
+++ packages/gamut/Icon/icons/ChevronDownIcon.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 export default function ChevronDownIcon(props) {
   return (
-    <svg width="24" height="24" {...props}>
+    <svg {...props}>
       <title>Chevron Down Icon</title>
       <path
         d="M20.293 7.293l1.414 1.414L12 18.414 2.293 8.707l1.414-1.414L12 15.586z"
