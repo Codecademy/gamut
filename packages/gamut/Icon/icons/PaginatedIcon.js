45cffde71edac642babea387f36b82673ce7998f
Bana Malik <bandanakm@gmail.com>
2018-03-07 01:31:38 -0500

    fix paginated icon

--- packages/gamut/Icon/icons/PaginatedIcon.js
+++ packages/gamut/Icon/icons/PaginatedIcon.js
@@ -2,9 +2,14 @@ import React from 'react';
 
 export default function PaginatedIcon(props) {
   return (
-    <svg viewBox="0 0 24 24" {...props}>
+    <svg viewBox="-5 -15 45 45" {...props}>
       <title>Paginated Icon</title>
-      <g transform="translate(1 8)" fill="none" fillRule="evenodd">
+      <g
+        transform="translate(1 8)"
+        stroke="#000"
+        fill="none"
+        fillRule="evenodd"
+      >
         <circle cx="4" cy="4" r="3.5" />
         <circle cx="16" cy="4" r="3.5" />
         <circle cx="28" cy="4" r="3.5" />
