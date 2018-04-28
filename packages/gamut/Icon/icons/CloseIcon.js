6779a6eef7c4ae03366efd84105d454deb65abb4
Bana Malik <bandanakm@gmail.com>
2018-04-28 16:30:25 -0400

    change component name

--- packages/gamut/Icon/icons/CloseIcon.js
+++ packages/gamut/Icon/icons/CloseIcon.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function CertificateIcon(props) {
+export default function CloseIcon(props) {
   return (
     <svg viewBox="0 0 24 24" {...props}>
       <title>Close Icon</title>
