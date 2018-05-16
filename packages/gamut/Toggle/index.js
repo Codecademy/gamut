a8aeb34b463a678a64b6852e85d1c18e7c015eb3
Emily G <e.m.giurleo@gmail.com>
2018-05-16 09:00:35 -0400

    Add disabled state to Toggle component (#108)
    * add disabled state to toggle

--- packages/gamut/Toggle/index.js
+++ packages/gamut/Toggle/index.js
@@ -8,14 +8,18 @@ class Toggle extends Component {
     checked: PropTypes.bool,
     onClick: PropTypes.func,
     label: PropTypes.string,
+    disabled: PropTypes.bool,
   };
 
   render() {
-    const { checked, onClick, label } = this.props;
+    const { checked, onClick, label, disabled } = this.props;
 
     return (
       <label
-        className={cx(s.toggleButton, { [s.toggled]: checked })}
+        className={cx(s.toggleButton, {
+          [s.toggled]: checked,
+          [s.disabled]: disabled,
+        })}
         onClick={onClick}
         htmlFor={label}
       >
@@ -24,6 +28,7 @@ class Toggle extends Component {
           checked={checked}
           className={s.invisible}
           id={label}
+          disabled={disabled}
         />
         <span className={s.invisible}>{label}</span>
         <div className={s.track} />
