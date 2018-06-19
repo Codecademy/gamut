25c0b0250f9b347a8570f89cf0c9e0cb85d7c053
Jake Hiller <jakemhiller@gmail.com>
2018-06-19 13:53:25 -0400

    upgrade stuff to react 16

--- script/jest/base-setup.js
+++ script/jest/base-setup.js
@@ -3,3 +3,12 @@
  */
 
 require('babel-polyfill');
+const Enzyme = require('enzyme');
+const Adapter = require('enzyme-adapter-react-16');
+
+Enzyme.configure({ adapter: new Adapter() });
+
+// make window.location.assign modify the jsdom url
+const shimLocationAssign = require('./helpers/shimLocationAssign');
+
+shimLocationAssign();
--- workspaces/gamut-storybook/.storybook/config.js
+++ workspaces/gamut-storybook/.storybook/config.js
@@ -1,16 +1,17 @@
 import { configure, addDecorator, setAddon } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
 import { withKnobs } from '@storybook/addon-knobs';
 import { wrapper } from './decorators/wrapper';
 import { setDefaults } from '@storybook/addon-info';
 
+addDecorator(withInfo);
 addDecorator(withKnobs);
 addDecorator(wrapper);
 
-// Require all files that match `stories/*-story.js`
-const req = require.context('../', true, /stories\/.*-story\.js$/);
-
-// And load them
 function loadStories() {
+  // Require all files that match `stories/*-story.js`
+  const req = require.context('../', true, /stories\/.*-story\.js$/);
+  // And load them
   req.keys().forEach(req);
 }
 
