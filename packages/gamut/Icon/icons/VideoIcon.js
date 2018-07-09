4461f83a14e237f871ad4695342ce7ce5d94f8c9
Blobb Ered <blobbered@gmail.com>
2018-07-09 17:23:31 -0400

    add video icon for syllabus (#136)
    type(icon): feat
    
    Adding video as an icon.

---
+++ packages/gamut/Icon/icons/VideoIcon.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function VideoIcon(props) {
+  return (
+    <svg viewBox="0 0 24 24" {...props}>
+      <title>Video Icon</title>
+      <path d="M8 5l-8 5V0z" fill="#000" fillRule="nonzero" />
+    </svg>
+  );
+}
