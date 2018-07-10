b2e74fc3c5df7ec692de9a8f0f3f8f74ad17431c
Blobb Ered <blobbered@gmail.com>
2018-07-10 10:44:41 -0400

    update video svg to be cropped and centered (#137)
    type(scope): fix
    
    fix the video svg to be centered and cropped

--- packages/gamut/Icon/icons/VideoIcon.js
+++ packages/gamut/Icon/icons/VideoIcon.js
@@ -4,7 +4,7 @@ export default function VideoIcon(props) {
   return (
     <svg viewBox="0 0 24 24" {...props}>
       <title>Video Icon</title>
-      <path d="M8 5l-8 5V0z" fill="#000" fillRule="nonzero" />
+      <path d="M18 12L6 20V4z" fill="currentColor" fillRule="evenodd" />
     </svg>
   );
 }
