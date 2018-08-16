d47a448592fb2da78a6ad03ba29f5f6c14579c3d
Jack Ratner <jackratner@gmail.com>
2018-08-16 17:10:43 -0400

    success and invalid icons

--- packages/gamut/Form/Input.js
+++ packages/gamut/Form/Input.js
@@ -3,19 +3,36 @@ import PropTypes from 'prop-types';
 import cx from 'classnames';
 import s from './styles/Input.scss';
 
-const Input = ({ error, htmlFor, className, ...rest }) => {
+const Input = ({ error, success, htmlFor, className, showIcons, ...rest }) => {
   const classNames = cx(
     s.Input,
     {
       [s.error]: error,
+      [s.success]: success,
     },
     className
   );
-  return <input {...rest} id={htmlFor} className={classNames} />;
+  const input = <input {...rest} id={htmlFor} className={classNames} />;
+
+  let icon;
+  if (success) {
+    icon = '❌';
+  } else if (error) {
+    icon = '✅';
+  }
+
+  if (!showIcons || !icon) return input;
+  return (
+    <span className={s.wrapper}>
+      {input}
+      <span className={s.icon}>{icon}</span>
+    </span>
+  );
 };
 
 Input.defaultProps = {
   type: 'text',
+  showIcons: true,
 };
 
 Input.propTypes = {
@@ -26,6 +43,8 @@ Input.propTypes = {
   required: PropTypes.bool,
   type: PropTypes.string,
   error: PropTypes.bool,
+  success: PropTypes.bool,
+  showIcons: PropTypes.bool,
 };
 
 export default Input;
