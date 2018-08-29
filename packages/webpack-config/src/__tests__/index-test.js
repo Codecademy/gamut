eac413b74edd3fe79c88e31c5016cc461d9c39d2
Jake Hiller <jakemhiller@gmail.com>
2018-08-29 16:17:17 -0400

    test cleanup

--- packages/webpack-config/src/__tests__/index-test.js
+++ packages/webpack-config/src/__tests__/index-test.js
@@ -5,7 +5,7 @@ describe('createConfig', () => {
   it('merges custom config', () => {
     const testConfig = createConfig()
       .common({
-        context: __dirname,
+        context: './',
       })
       .merge({
         entry: 'my-file.js',
@@ -13,65 +13,5 @@ describe('createConfig', () => {
       .toConfig();
 
     expect(testConfig.entry).toEqual('my-file.js');
-  });
-
-  it('merges loaders', () => {
-    const testConfig = createConfig()
-      .common({
-        context: __dirname,
-      })
-      .if(false, config =>
-        config.merge({
-          notInTheSnapshot: true,
-        })
-      )
-      .mergeLoader({
-        test: /\.js?$/,
-        loader: 'babel-loader',
-        include: [__dirname],
-      })
-      .toConfig();
-
-    expect(testConfig).toMatchSnapshot();
-  });
-  it('merges loaders without mergeLoader', () => {
-    const testConfig = createConfig()
-      .common({
-        context: __dirname,
-      })
-      .merge({
-        module: {
-          rules: [
-            {
-              test: /\.js$/,
-              use: {
-                loader: 'babel-loader',
-                options: {
-                  presets: ['@babel/preset-env', 'codecademy'],
-                  plugins: ['@babel/plugin-transform-runtime'],
-                },
-              },
-            },
-          ],
-        },
-      })
-      .merge({
-        module: {
-          rules: [
-            {
-              test: /\.js$/,
-              use: {
-                loader: 'babel-loader',
-                options: {
-                  presets: ['codecademy'],
-                },
-              },
-            },
-          ],
-        },
-      })
-      .toConfig();
-    console.log(JSON.stringify(testConfig.module.rules, null, 2));
-    expect(testConfig).toMatchSnapshot();
   });
 });
