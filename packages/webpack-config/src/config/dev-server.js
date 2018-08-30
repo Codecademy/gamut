b3b29b0858bab1a80ee02b0d035e1773c073a8fd
Jake Hiller <jakemhiller@gmail.com>
2018-08-30 15:54:52 -0400

    feat: remove hot-loader as a dependency (#146)
    * remove hot-loader as a dependency, clients should configure this locally
    
    * remove default babel options, should be set in babel config

--- packages/webpack-config/src/config/dev-server.js
+++ packages/webpack-config/src/config/dev-server.js
@@ -1,46 +1,38 @@
 const webpack = require('webpack');
 const merge = require('webpack-merge');
-const babelConfig = require('./babel');
 
 const devServerConfig = options => {
   const { port = 3808, publicPath, ...serveOptions } = options;
 
-  return merge.smart(
-    {
-      output: {
-        publicPath: publicPath || `http://localhost:${port}/dist/`,
-      },
-
-      serve: merge(
-        {
-          port,
-          devMiddleware: {
-            publicPath: publicPath || `http://localhost:${port}/dist/`,
-            headers: { 'Access-Control-Allow-Origin': '*' },
-            stats: {
-              chunkGroups: true,
-            },
-          },
-          hotClient: {
-            hmr: true,
-            allEntries: true,
-          },
-          clipboard: false,
-        },
-        serveOptions
-      ),
-
-      plugins: [
-        new webpack.NoEmitOnErrorsPlugin(),
-        new webpack.NamedModulesPlugin(),
-      ],
+  return merge.smart({
+    output: {
+      publicPath: publicPath || `http://localhost:${port}/dist/`,
     },
-    babelConfig({
-      options: {
-        plugins: [['react-hot-loader/babel']],
+
+    serve: merge(
+      {
+        port,
+        devMiddleware: {
+          publicPath: publicPath || `http://localhost:${port}/dist/`,
+          headers: { 'Access-Control-Allow-Origin': '*' },
+          stats: {
+            chunkGroups: true,
+          },
+        },
+        hotClient: {
+          hmr: true,
+          allEntries: true,
+        },
+        clipboard: false,
       },
-    })
-  );
+      serveOptions
+    ),
+
+    plugins: [
+      new webpack.NoEmitOnErrorsPlugin(),
+      new webpack.NamedModulesPlugin(),
+    ],
+  });
 };
 
 module.exports = devServerConfig;
