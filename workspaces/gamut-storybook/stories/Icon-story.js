2c8b56bfe79804f985a913f85bb62fedd5b0c81b
Jake Hiller <jakemhiller@gmail.com>
2018-10-04 11:37:20 -0400

    ci: run prettier and add CircleCI check (#148)

--- workspaces/gamut-storybook/stories/Icon-story.js
+++ workspaces/gamut-storybook/stories/Icon-story.js
@@ -31,36 +31,32 @@ storiesOf('Component/Icon', module)
     'Editable Icon',
     withInfo({
       ...options,
-    })(
-      () => (
-        <div>
-          <span className={s.iconWrapper}>
-            {select('name', iconNames, iconNames[0])}
-            <Icon
-              name={select('name', iconNames, iconNames[0])}
-              width={number('width', 128)}
-              height={number('height', 128)}
-              style={{
-                color: select('color', selectableGamutColors, '#000'),
-                backgroundColor: select(
-                  'backgroundColor',
-                  selectableGamutColors,
-                  '#fff'
-                ),
-              }}
-            />
-          </span>
-          <p className={s.note}>
-            <em>
-              Note: Both color and backgroundColor should be adjusted with CSS
-              classes rather than inline styles.
-            </em>
-          </p>
-        </div>
-      ),
-      {
-        inline: true,
-        propTables: false,
-      }
-    )
+      inline: true,
+      propTables: false,
+    })(() => (
+      <div>
+        <span className={s.iconWrapper}>
+          {select('name', iconNames, iconNames[0])}
+          <Icon
+            name={select('name', iconNames, iconNames[0])}
+            width={number('width', 128)}
+            height={number('height', 128)}
+            style={{
+              color: select('color', selectableGamutColors, '#000'),
+              backgroundColor: select(
+                'backgroundColor',
+                selectableGamutColors,
+                '#fff'
+              ),
+            }}
+          />
+        </span>
+        <p className={s.note}>
+          <em>
+            Note: Both color and backgroundColor should be adjusted with CSS
+            classes rather than inline styles.
+          </em>
+        </p>
+      </div>
+    ))
   );
