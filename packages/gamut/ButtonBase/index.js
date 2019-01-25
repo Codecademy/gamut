270954fb8a6672eb8cfa1d460604832352ad2920
Jake Hiller <jakemhiller@gmail.com>
2019-01-25 11:08:38 -0500

    fully reset ButtonBase

--- packages/gamut/ButtonBase/index.js
+++ packages/gamut/ButtonBase/index.js
@@ -5,14 +5,14 @@ import omitProps from '../utils/omitProps';
 import styles from './styles';
 
 const propTypes = {
-  children: PropTypes.node.isRequired,
+  children: PropTypes.node,
   href: PropTypes.string,
   link: PropTypes.bool,
   className: PropTypes.string,
 };
 
 const ButtonBase = props => {
-  const { children, href, className, link } = props;
+  const { href, className, link } = props;
   const propsToTransfer = omitProps(propTypes, props);
 
   const BaseTag = href ? 'a' : 'button';
@@ -21,9 +21,7 @@ const ButtonBase = props => {
   });
 
   return (
-    <BaseTag data-btn {...propsToTransfer} className={classes} href={href}>
-      {children}
-    </BaseTag>
+    <BaseTag data-btn {...propsToTransfer} className={classes} href={href} />
   );
 };
 
