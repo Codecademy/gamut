046fdc5a4f28c521bb0a2fe5ae42d967e2e93542
Bana Malik <bandanakm@gmail.com>
2019-01-28 17:36:28 -0500

    add icon files

---
+++ packages/gamut/Icon/icons/ForumsIcons.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function ForumsIcon(props) {
+  return (
+    <svg viewBox="0 0 24 24" {...props}>
+      <title>ForumsIcon</title>
+      <path
+        d="M18.4 4H5.6A1.6 1.6 0 0 0 4 5.6V20l3.2-3.2h11.2a1.6 1.6 0 0 0 1.6-1.6V5.6A1.6 1.6 0 0 0 18.4 4m0 11.2H7.2l-1.6 1.6V5.6h12.8"
+        fill="#000"
+        fillRule="nonzero"
+      />
+    </svg>
+  );
+}
