e332bc0946b37f87d44e6fc745e90c19f326202c
Jake Hiller <jakemhiller@gmail.com>
2019-01-29 13:56:52 -0500

    fix eslint override

--- packages/gamut/Markdown/overrides/Img/index.js
+++ packages/gamut/Markdown/overrides/Img/index.js
@@ -1,5 +1,3 @@
-/* eslint-disable jsx-a11y/alt-text */
-
 import React from 'react';
 import PropTypes from 'prop-types';
 
@@ -11,6 +9,8 @@ const propTypes = {
  * Overrides image elements to prevent errors from images with closing tags
  * ie: <img src=""></img>
  */
+
+// eslint-disable-next-line jsx-a11y/alt-text
 const Img = ({ children, ...props }) => <img {...props} />;
 
 Img.propTypes = propTypes;
