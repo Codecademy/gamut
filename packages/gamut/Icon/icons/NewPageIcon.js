220111246319c5cc3ed2d693cef75c309927baed
Emily Giurleo <e.m.giurleo@gmail.com>
2019-01-30 11:56:42 -0500

    changed title and removed fill from icon (#174)

--- packages/gamut/Icon/icons/NewPageIcon.js
+++ packages/gamut/Icon/icons/NewPageIcon.js
@@ -1,12 +1,11 @@
 import React from 'react';
 
-export default function ForumsIcon(props) {
+export default function NewPageIcon(props) {
   return (
     <svg viewBox="0 0 24 24" {...props}>
-      <title>ForumsIcon</title>
+      <title>NewPageIcon</title>
       <path
         d="M13.778 4v1.778h3.19l-8.737 8.738 1.253 1.253 8.738-8.738v3.191H20V4h-6.222zm4.444 14.222H5.778V5.778H12V4H5.778C4.79 4 4 4.8 4 5.778v12.444C4 19.204 4.796 20 5.778 20h12.444c.982 0 1.778-.796 1.778-1.778V12h-1.778v6.222z"
-        fill="#000"
         fillRule="nonzero"
       />
     </svg>
