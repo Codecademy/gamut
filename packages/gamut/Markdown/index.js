5162872b2c0367e8dee768585b3432e612dd4906
Jake Hiller <jakemhiller@gmail.com>
2019-02-01 17:57:15 -0500

    fix: Markdown version 2.0 (#176)

--- packages/gamut/Markdown/index.js
+++ packages/gamut/Markdown/index.js
@@ -1,42 +1,67 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
-import MarkdownJSX from 'markdown-to-jsx';
+import marked from 'marked';
+import HtmlToReact from 'html-to-react';
+import insane from 'insane';
 import omitProps from '../utils/omitProps';
+import { createTagOverride, createCodeBlockOverride } from './libs/overrides';
 import s from './styles';
 
 import Iframe from './overrides/Iframe';
-import Pre from './overrides/Pre';
-import Code from './overrides/Code';
-import Img from './overrides/Img';
 
-const CODE_BLOCK_FENCED = /(`{3,}|~{3,}) *(\S+)? *\n([\s\S]+?)\s*\1 *(?:\n *)*/gim;
-// Matches html tags and self closing tags that aren't inline
-// Adapted from https://github.com/probablyup/markdown-to-jsx/blob/a1fb781bc51445f5a226bf3944bed0436d0d69d2/index.js#L154
-const BLOCK_HTML_TAGS = /^(?:\n| )*<\/?([a-z][a-z0-9:]*)?(?:\s+((?:<.*?>|[^>])*))?\/?>/gi;
-const EXTRA_NEWLINES = /\n{2,}/g;
+const htmlToReactParser = new HtmlToReact.Parser();
+const processNodeDefinitions = new HtmlToReact.ProcessNodeDefinitions();
 
-/**
- * 1. Add extra newlines before and after block-level html tags (tags with preceding newlines)
- *    - This fixes a rendering issue where HTML without
- *      the correct whitespace will not render correctly
- * 2. Add newlines before and after fenced code blocks
- *    - This fixes a rendering issue when a fenced codeblock doesn't
- *      have the required starting and ending newlines
- * 3. Remove excessive newlines and replaces it with 2 newlines
- *    - This fixes a rendering issue inside of html code blocks
- *      where markdown inside of a codeblock was treated as html
- */
-const cleanupMarkdownFormatting = str =>
-  str
-    .replace(BLOCK_HTML_TAGS, '\n\n$&\n\n')
-    .replace(CODE_BLOCK_FENCED, '\n$&\n')
-    .replace(EXTRA_NEWLINES, '\n\n');
+const sanitizationConfig = {
+  allowedAttributes: {
+    source: ['src', 'type'],
+    video: ['width', 'height', 'align', 'style', 'controls'],
+    span: ['class'],
+    code: ['class'],
+    pre: ['class'],
+    iframe: [
+      'src',
+      'width',
+      'height',
+      'frameborder',
+      'gesture',
+      'allow',
+      'allowfullscreen',
+    ],
+  },
+  allowedTags: [
+    ...insane.defaults.allowedTags,
+    'video',
+    'source',
+    'pre',
+    'code',
+    'br',
+    'iframe',
+    'codeblock',
+  ],
+};
+
+const isValidNode = function() {
+  return true;
+};
 
 class Markdown extends PureComponent {
   static propTypes = {
     spacing: PropTypes.oneOf(['loose', 'tight', 'none']),
-    overrides: PropTypes.object,
+    overrides: PropTypes.objectOf(
+      PropTypes.shape({
+        component: PropTypes.oneOfType([
+          PropTypes.func,
+          PropTypes.shape({
+            render: PropTypes.func.isRequired,
+          }),
+        ]),
+        props: PropTypes.object,
+        shouldProcessNode: PropTypes.func,
+        processNode: PropTypes.func,
+      })
+    ),
     className: PropTypes.string,
     inline: PropTypes.bool,
     text: PropTypes.string,
@@ -47,48 +72,59 @@ class Markdown extends PureComponent {
       spacing = 'tight',
       text = '',
       className,
-      inline = false,
       overrides: userOverrides = {},
+      inline = false,
     } = this.props;
 
     const spacingStyles = s[`spacing-${spacing}`];
     const classes = cx(spacingStyles, className);
 
-    const defaultOverrides = {
-      iframe: Iframe,
-      pre: {
-        component: Pre,
-        props: {
-          // User overrides are used in pre to determine
-          // how to render Code blocks
-          overrides: userOverrides,
-        },
-      },
-      img: {
-        component: Img,
-      },
-      code: Code,
-    };
-
-    const options = {
-      overrides: {
-        ...defaultOverrides,
-        ...userOverrides,
-      },
-      forceBlock: !inline,
-      forceInline: inline,
-    };
-
     const Wrapper = inline ? 'span' : 'div';
+
+    const overrides = Object.keys(userOverrides).map(tagName => {
+      if (tagName === 'CodeBlock') {
+        return createCodeBlockOverride(tagName, userOverrides[tagName]);
+      }
+      return createTagOverride(tagName, userOverrides[tagName]);
+    });
+
+    const processingInstructions = [
+      createTagOverride('iframe', {
+        component: Iframe,
+      }),
+      ...overrides,
+      {
+        shouldProcessNode() {
+          return true;
+        },
+        processNode: processNodeDefinitions.processDefaultNode,
+      },
+    ];
+
+    // Render markdown to html
+    const rawHtml = inline ? marked.inlineLexer(text, []) : marked(text);
+
+    const html = insane(rawHtml, {
+      ...sanitizationConfig,
+      allowedTags: [
+        ...sanitizationConfig.allowedTags,
+        ...Object.keys(userOverrides).map(key => key.toLowerCase()),
+      ],
+    });
+
+    // Render html to a react tree
+    const react = htmlToReactParser.parseWithInstructions(
+      html,
+      isValidNode,
+      processingInstructions
+    );
 
     return (
       <Wrapper
         {...omitProps(Object.keys(this.props), this.props)}
         className={classes}
       >
-        <MarkdownJSX options={options}>
-          {cleanupMarkdownFormatting(text)}
-        </MarkdownJSX>
+        {react}
       </Wrapper>
     );
   }
