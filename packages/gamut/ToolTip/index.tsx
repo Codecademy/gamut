2940ac8334929b76ca3d76979c06fcca690dee46
Jon Samp <jon@codecademy.com>
2019-02-26 15:30:04 -0500

    pure CSS implementation

--- packages/gamut/ToolTip/index.tsx
+++ packages/gamut/ToolTip/index.tsx
@@ -1,4 +1,5 @@
 import React, { ReactNode, Component } from 'react';
+import cx from 'classnames';
 import { CardShell, CardBody } from '../Card';
 import s from './styles/index.scss';
 
@@ -9,77 +10,25 @@ type ToolTipProps = {
   id: string;
 };
 
-const defaultProps = {
-  position: 'top-right',
-};
-
 class ToolTip extends Component<ToolTipProps> {
-  private tipElementRef = React.createRef<HTMLButtonElement>();
-
-  constructor(props) {
-    super(props);
-    this.tipElementRef = React.createRef();
-  }
-
-  static defaultProps = defaultProps;
-
-  state = {
-    tipElementHeight: 0,
-  };
-
-  componentDidMount() {
-    const tipElementDimensions: {
-      height: number;
-    } = this.tipElementRef.current.getBoundingClientRect();
-
-    this.setState({
-      tipElementHeight: tipElementDimensions.height,
-    });
-  }
-
-  getToolTipPosition = position => {
-    const { tipElementHeight } = this.state;
-
-    const translateX = 0;
-    const translateY = tipElementHeight;
-
-    const positions = {
-      'top-left': {
-        bottom: translateY,
-        right: translateX,
-      },
-      'top-right': {
-        bottom: translateY,
-        left: translateX,
-      },
-      'bottom-left': {
-        top: translateY,
-        right: translateX,
-      },
-      'bottom-right': {
-        top: translateY,
-        left: translateX,
-      },
-    };
-
-    return positions[position];
+  static defaultProps = {
+    position: 'top-right',
   };
 
   render() {
     const { children, tipElement, position, id } = this.props;
+
     return (
       <div className={s.toolTipWrapper}>
         <button
           aria-labelledby={id}
           type="button"
-          ref={this.tipElementRef}
           className={s.tipElementContainer}
         >
           {tipElement}
         </button>
         <CardShell
-          className={s.toolTipContainer}
-          style={this.getToolTipPosition(position)}
+          className={cx(s.toolTipContainer, s[position])}
           role="tooltip"
           id={id}
         >
