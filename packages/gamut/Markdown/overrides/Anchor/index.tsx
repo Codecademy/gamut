10dbc29b3582bacf394268448970554002d76e8a
Jake Hiller <jakemhiller@gmail.com>
2019-02-25 19:05:30 -0500

    exclude in SSR

--- packages/gamut/Markdown/overrides/Anchor/index.tsx
+++ packages/gamut/Markdown/overrides/Anchor/index.tsx
@@ -2,6 +2,8 @@
 import React, { FunctionComponent, HTMLAttributes } from 'react';
 import PropTypes from 'prop-types';
 
+declare var __SSR__: boolean;
+
 export interface AnchorProps extends HTMLAttributes<HTMLAnchorElement> {
   href: string;
   title?: string;
@@ -18,7 +20,7 @@ const Anchor: FunctionComponent<AnchorProps> = props => {
     rel: 'noopener noreferrer',
   };
 
-  if (typeof window !== 'undefined' && window.URL) {
+  if (!__SSR__) {
     try {
       const url = new window.URL(props.href, window.location.href);
       // remove noopener/noreferrer on same origin urls
