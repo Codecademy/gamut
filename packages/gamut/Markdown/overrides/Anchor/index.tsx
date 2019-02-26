db849801030e1c2e0c00796b14c2a2fef2259319
Jake Hiller <jakemhiller@gmail.com>
2019-02-25 19:09:10 -0500

    just switch to something that won't work on the server

--- packages/gamut/Markdown/overrides/Anchor/index.tsx
+++ packages/gamut/Markdown/overrides/Anchor/index.tsx
@@ -2,8 +2,6 @@
 import React, { FunctionComponent, HTMLAttributes } from 'react';
 import PropTypes from 'prop-types';
 
-declare var __SSR__: boolean;
-
 export interface AnchorProps extends HTMLAttributes<HTMLAnchorElement> {
   href: string;
   title?: string;
@@ -20,16 +18,12 @@ const Anchor: FunctionComponent<AnchorProps> = props => {
     rel: 'noopener noreferrer',
   };
 
-  if (!__SSR__) {
-    try {
-      const url = new window.URL(props.href, window.location.href);
-      // remove noopener/noreferrer on same origin urls
-      if (url.origin === window.location.origin) {
-        anchorProps.rel = '';
-        anchorProps.target = '';
-      }
-    } catch (e) {
-      // Failed to parse url, fall through to default
+  if (typeof URL !== 'undefined') {
+    const url = new URL(props.href, window.location.href);
+    // remove noopener/noreferrer on same origin urls
+    if (url.origin === window.location.origin) {
+      anchorProps.rel = '';
+      anchorProps.target = '';
     }
   }
 
