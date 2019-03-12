bc0c252f9cc5e16ba6724c68931e23a6d6f517ea
Jake Hiller <jakemhiller@gmail.com>
2019-03-12 12:19:01 -0400

    add menu icon, adjust close icon

---
+++ packages/gamut/Icon/icons/MenuIcon.tsx
@@ -0,0 +1,11 @@
+import React, { SVGProps } from 'react';
+
+export default function MenuIcon(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg viewBox="0 0 24 24" {...props}>
+      <g fill-rule="nonzero">
+        <path d="M20 11v2H4v-2zM20 17v2H4v-2zM20 5v2H4V5z" />
+      </g>
+    </svg>
+  );
+}
