e9a01ea4eecd7598210dbb45f1bcf817640c49ac
Jake Hiller <jakemhiller@gmail.com>
2019-03-12 12:24:34 -0400

    fix prop name

--- packages/gamut/Icon/icons/MenuIcon.tsx
+++ packages/gamut/Icon/icons/MenuIcon.tsx
@@ -3,7 +3,7 @@ import React, { SVGProps } from 'react';
 export default function MenuIcon(props: SVGProps<SVGSVGElement>) {
   return (
     <svg viewBox="0 0 24 24" {...props}>
-      <g fill-rule="nonzero">
+      <g fillRule="nonzero">
         <path d="M20 11v2H4v-2zM20 17v2H4v-2zM20 5v2H4V5z" />
       </g>
     </svg>
