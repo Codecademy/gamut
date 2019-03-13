057e0d942df08047bf89c561280d37341d3412bd
Jon Samp <jon@codecademy.com>
2019-03-13 12:40:29 -0400

    feat: Adds mar 2019 colors, deprecates colors (#211)

--- workspaces/gamut-storybook/stories/helpers.js
+++ workspaces/gamut-storybook/stories/helpers.js
@@ -1,4 +1,4 @@
-import { gamutColors } from '@codecademy/gamut-styles/utils/variables';
+import { deprecatedGamutColors } from '@codecademy/gamut-styles/utils/variables';
 
 function convertCamelToSpinel(str) {
   return str.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
@@ -7,9 +7,9 @@ function convertCamelToSpinel(str) {
 // remapping of gamut base colors that can be placed directly into the `select` knob function
 const selectableGamutColors = {};
 
-for (const color in gamutColors.base) {
+for (const color in deprecatedGamutColors.base) {
   selectableGamutColors[
-    gamutColors.base[color]
+    deprecatedGamutColors.base[color]
   ] = `gamut-${convertCamelToSpinel(color)}`;
 }
 
