a0fb58f82f21a5726b6e03fe86630a4e03f5c3e3
joshuakgoldberg <josh@codecademy.com>
2019-04-01 11:49:47 -0400

    Added react.d.ts typings

---
+++ packages/gamut/typings/react.d.ts
@@ -0,0 +1,9 @@
+import React, { Attributes, ReactHTML } from 'react';
+
+/**
+ * Simple tag string, component class, or functional component for a render prop.
+ */
+export type ChildComponentDescriptor =
+  | string
+  | React.ComponentClass
+  | React.FC;
