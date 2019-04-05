2f46c5e1511ca1ef4b5d96ba364d8c4a82963242
cmackenz <colin@codecademy.com>
2019-04-05 11:33:35 -0400

    clean yarn install

---
+++ packages/enzyme-helpers/src/main.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+export class EnzymeHelpers {
+  static testId = id => `[data-testid="${id}"]`;
+
+  constructor(Component, opts = {}, props = {}) {
+    this.Component = Component;
+    this.opts = opts;
+    this.baseProps = props;
+  }
+
+  setup = (props = {}) => {
+    const {
+      Component,
+      opts: { render },
+    } = this;
+    const wrapper = render(<Component {...this.baseProps} {...props} />);
+    this.wrapper = wrapper;
+
+    // sanity checks
+    expect(wrapper).toExist();
+    expect(wrapper.isEmptyRender()).toBeFalsy();
+
+    return { wrapper };
+  };
+
+  // eslint-disable-next-line no-console
+  debug = () => console.log(this.wrapper.debug());
+
+  // actions
+  click = selector => this.wrapper.find(selector).simulate('click');
+
+  clickTestId = testId => this.click(EnzymeHelpers.testId(testId));
+
+  // expectations
+  expectCount = (selector, n = 1) =>
+    expect(this.wrapper.find(selector).length).toBe(n);
+
+  expectTestIdCount = (selector, n = 1) =>
+    this.expectCount(EnzymeHelpers.testId(selector), n);
+  ka;
+
+  expectAllTestIds = ids => ids.forEach(id => this.expectTestIdCount(id, 1));
+}
+
+export const mockStore = data => ({
+  getState: () => data,
+  dispatch: jest.fn(),
+  subscribe: jest.fn(),
+});
