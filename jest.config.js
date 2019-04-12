22ef5cbce7d5a2452a800374788379580f8e6c6a
Jake Hiller <jakemhiller@gmail.com>
2019-04-12 14:18:59 -0400

    feat(Markdown): add default table override component (#307)

--- jest.config.js
+++ jest.config.js
@@ -16,7 +16,7 @@ module.exports = {
     'tsx',
   ],
   moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|md)$':
       '<rootDir>/script/jest/fileMock',
     '\\.(css|scss)$': '<rootDir>/script/jest/styleMock',
   },
