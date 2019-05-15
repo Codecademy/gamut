257a8603033c5de341d2222931b6fd312e743f41
joshuakgoldberg <joshuakgoldberg@outlook.com>
2019-05-15 14:12:49 -0400

    Heh, icon titles

--- packages/gamut/Icon/icons/DownArrowIcon.tsx
+++ packages/gamut/Icon/icons/DownArrowIcon.tsx
@@ -5,7 +5,7 @@ import { defaultIconProps } from '../defaultIconProps';
 export default function DownArrowIcon(props: SVGProps<SVGSVGElement>) {
   return (
     <svg transform="rotate(90)" {...props}>
-      <title>Left Arrow Icon</title>
+      <title>Down Arrow Icon</title>
       <path
         d="M4 10.92v2h12l-5.5 5.5 1.42 1.42 7.92-7.92L11.92 4 10.5 5.42l5.5 5.5z"
         fillRule="nonzero"
