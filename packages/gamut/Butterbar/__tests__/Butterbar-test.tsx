9a31cfa6a686384b1efd4b8c34719f95d4f607e9
joshuakgoldberg <joshuakgoldberg@outlook.com>
2019-05-16 15:15:52 -0400

    Added Butterbar component
    Visually equivalent to the existing components/Butterbar/index.js in the monolith. This one also has the option to switch to a visualization with a border-bottom, as that's the desired design for scenarios where the content just below the butterbar has a white background and/or makes the butterbar inside a visual container.
    
    I verified this looks the same when swapped out in the monolith's SkillPathsButterbar component with this linked. Once this PR goes in I'll mark the original component as deprecated and apply that switch.

---
+++ packages/gamut/Butterbar/__tests__/Butterbar-test.tsx
@@ -0,0 +1,76 @@
+import { mount } from 'enzyme';
+import React from 'react';
+
+import Butterbar from '..';
+import Button from '../../Button';
+
+const createStubStorage = (key = 'stub-key') => {
+  const items = new Map<string, string | null>();
+
+  return {
+    key,
+    local: {
+      getItem(key: string) {
+        return items.get(key);
+      },
+      setItem(key: string, value: string) {
+        items.set(key, value);
+      },
+    },
+  };
+};
+
+describe('Butterbar', () => {
+  it('renders children when closing has not been requested', () => {
+    const children = <span className="test" />;
+    const component = mount(<Butterbar>{children}</Butterbar>);
+
+    expect(component.find('span.test')).toHaveLength(1);
+  });
+
+  it('does not render a button when no storage is provided', () => {
+    const component = mount(<Butterbar />);
+    component.update();
+
+    expect(component.find(Button)).toHaveLength(0);
+  });
+
+  it('renders a button when storage is provided', () => {
+    const component = mount(<Butterbar storage={createStubStorage()} />);
+    component.update();
+
+    expect(component.find(Button)).toHaveLength(1);
+  });
+
+  it('re-renders as null when the button is clicked', () => {
+    const component = mount(<Butterbar storage={createStubStorage()} />);
+
+    component.find(Button).simulate('click');
+    component.update();
+
+    expect(component.html()).toBe(null);
+  });
+
+  it('does not render the container when closing has previously been requested', () => {
+    const sharedStorage = createStubStorage();
+    const firstComponent = mount(<Butterbar storage={sharedStorage} />);
+    firstComponent.find(Button).simulate('click');
+
+    const secondComponent = mount(<Butterbar storage={sharedStorage} />);
+
+    expect(secondComponent.html()).toBe(null);
+  });
+
+  it('renders the container when closing on a different keyed butterbar has previously been requested', () => {
+    const firstComponent = mount(
+      <Butterbar storage={createStubStorage('first-key')} />
+    );
+    firstComponent.find(Button).simulate('click');
+
+    const secondComponent = mount(
+      <Butterbar storage={createStubStorage('second-key')} />
+    );
+
+    expect(secondComponent.html()).not.toBe(null);
+  });
+});
