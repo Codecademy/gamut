d1d41a8d93b0d77f21c41aa5d72b742580eb4aa3
Josh Goldberg <joshuakgoldberg@outlook.com>
2019-06-24 09:44:36 -0500

    Added SplitInterstitial component (#377)
    * Added SplitInterstitial component
    
    We'll be using this as part of the credit card trial uwork as a somewhat-equivalent-replacement for the trial welcome screen.
    
    * CSS and text touchups for fun
    
    * Force added svg typings; removed storybook test changes
    
    * A few git deletions...
    
    * ...and some readds

---
+++ packages/gamut/SplitInterstitial/index.tsx
@@ -0,0 +1,50 @@
+import cx from 'classnames';
+import React, { ReactNode } from 'react';
+
+import blueCurve from './assets/blueCurve.svg';
+import purpleCurveBottomRight from './assets/purpleCurveBottomRight.svg';
+import purpleCurveTopLeft from './assets/purpleCurveTopLeft.svg';
+import styles from './styles.scss';
+
+type SplitInterstitialSide = {
+  children?: ReactNode;
+  className?: string;
+};
+
+export type SplitInterstitialProps = {
+  className?: string;
+  left?: SplitInterstitialSide;
+  right?: SplitInterstitialSide;
+};
+
+export const SplitInterstitial: React.FC<SplitInterstitialProps> = ({
+  className,
+  left = {},
+  right = {},
+}) => {
+  return (
+    <div className={cx(styles.splitInterstitial, className)}>
+      <div className={cx(styles.side, styles.left, className)}>
+        <img
+          alt=""
+          className={cx(styles.sideImage, styles.purpleCurveTopLeft)}
+          src={purpleCurveTopLeft}
+        />
+        <img
+          alt=""
+          className={cx(styles.sideImage, styles.blueCurve)}
+          src={blueCurve}
+        />
+        <img
+          alt=""
+          className={cx(styles.sideImage, styles.purpleCurveBottomRight)}
+          src={purpleCurveBottomRight}
+        />
+        {left.children}
+      </div>
+      <div className={cx(styles.side, className)}>{right.children}</div>
+    </div>
+  );
+};
+
+export default SplitInterstitial;
