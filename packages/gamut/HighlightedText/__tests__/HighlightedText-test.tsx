51377898b846a7e048ba55e281a7552159190374
Josh Goldberg <joshuakgoldberg@outlook.com>
2019-06-24 09:45:36 -0500

    Added HighlightedText component (#378)
    * Added HighlightedText component
    
    It's been coming up more often in designs, including the upcoming credit card trial interstitial.
    
    * Lint disable; added story

---
+++ packages/gamut/HighlightedText/__tests__/HighlightedText-test.tsx
@@ -0,0 +1,14 @@
+import { mount } from 'enzyme';
+import React from 'react';
+
+import HighlightedText from '..';
+
+describe('HighlightedText', () => {
+  it('preserves text children with whitespace', () => {
+    const text = 'abc    def \t ghi\njkl';
+
+    const wrapped = mount(<HighlightedText>{text}</HighlightedText>);
+
+    expect(wrapped.text()).toEqual(text);
+  });
+});
---
+++ packages/gamut/HighlightedText/index.tsx
@@ -0,0 +1,34 @@
+import cx from 'classnames';
+import React, { HTMLAttributes } from 'react';
+
+import styles from './styles.scss';
+
+export type HighlightedTextProps = {
+  children?: string;
+  wordProps?: HTMLAttributes<HTMLSpanElement>;
+  textProps?: HTMLAttributes<HTMLElement>;
+};
+
+export const HighlightedText: React.FC<HighlightedTextProps> = ({
+  children = '',
+  textProps = {},
+}) => {
+  const combinedProps = {
+    ...textProps,
+    className: cx(styles.highlightedText, textProps.className),
+  };
+
+  const words = children
+    .split(/(\s+)(\S+)/g)
+    .filter(Boolean)
+    .map((word, i) => (
+      // eslint-disable-next-line react/no-array-index-key
+      <span className={styles.word} key={i}>
+        {word}
+      </span>
+    ));
+
+  return <em {...combinedProps}>{words}</em>;
+};
+
+export default HighlightedText;
