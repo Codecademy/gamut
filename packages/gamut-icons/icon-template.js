6e0fcfc27768c9496f8e79a465c8adcd50ffd8f7
Jake Hiller <jake@codecademy.com>
2019-07-26 18:10:06 -0400

    feat: New Gamut Icons Package (#402)

---
+++ packages/gamut-icons/icon-template.js
@@ -0,0 +1,20 @@
+function iconTemplate(
+  { template },
+  opts,
+  { componentName, jsx /* imports, props, exports */ }
+) {
+  const typeScriptTpl = template.smart({ plugins: ['typescript'] });
+
+  return typeScriptTpl.ast`
+    import * as React from 'react';
+    export interface GamutIconProps extends React.SVGProps<SVGSVGElement> {
+      size?: number;
+      title?: string;
+      color?: string;
+    }
+    const ${componentName} = ({title, size, color, width, height, ...props}: GamutIconProps) => ${jsx};
+    export default ${componentName};
+  `;
+}
+
+module.exports = iconTemplate;
