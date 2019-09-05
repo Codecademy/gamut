2eb46b570fe9737618a88bb03fdb76f985acc6ab
codecaaron <52927224+codecaaron@users.noreply.github.com>
2019-09-05 15:48:10 -0400

    feat: Adds dynamic tag name for NotificationItem

---
+++ packages/gamut/NotificationList/__tests__/NotificationItem-test.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import NotificationItem from '../NotificationItem';
+
+const linkedNotification = {
+  text: 'notification 2',
+  id: '2',
+  date: '2019-08-29T15:27:22Z',
+  link: '/#',
+};
+
+const noLinkNotification = {
+  text: 'notification 1',
+  id: '1',
+  date: '2019-08-29T15:27:22Z',
+};
+
+describe('NotificationItem', () => {
+  it('renders an anchor tag if href is specified', () => {
+    const renderedNotification = shallow(
+      <NotificationItem notification={linkedNotification} />
+    );
+
+    expect(renderedNotification.name()).toBe('a');
+  });
+
+  it('renders a div if no href is specified', () => {
+    const renderedNotification = shallow(
+      <NotificationItem notification={noLinkNotification} />
+    );
+
+    expect(renderedNotification.name()).toBe('div');
+  });
+});
