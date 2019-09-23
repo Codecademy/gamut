6fde5f97e5f3b161e3983c55139912f61e4224bb
Bana Malik <bana@codecademy.com>
2019-09-23 09:45:05 -0500

    Bm pass images into split interstitial (#485)
    * add ability to pass in svg

--- workspaces/gamut-storybook/stories/SplitInterstitial-story.js
+++ workspaces/gamut-storybook/stories/SplitInterstitial-story.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { SplitInterstitial } from '@codecademy/gamut';
+import Triangle from '../assets/Triangle.svg';
+import Stairs from '../assets/Stairs.svg';
+import styles from './SplitInterstitial-story.scss';
 
-storiesOf('Component/SplitInterstitial', module).add(
-  'SplitInterstitial',
-  () => (
+storiesOf('Component/SplitInterstitial', module)
+  .add('SplitInterstitial', () => (
     <SplitInterstitial
       left={{
         children: (
@@ -23,5 +25,31 @@ storiesOf('Component/SplitInterstitial', module).add(
         ),
       }}
     />
-  )
-);
+  ))
+  .add('SplitInterstitial with Custom Images', () => (
+    <SplitInterstitial
+      left={{
+        children: (
+          <div>
+            <h1>Important stuff!</h1>
+            <p>Some sort of information here, perhaps?</p>
+          </div>
+        ),
+      }}
+      right={{
+        children: (
+          <div>
+            <h2>Less important...</h2>
+            <p>Some more information here, maybe?</p>
+          </div>
+        ),
+      }}
+      topLeftImage={{
+        src: Triangle,
+      }}
+      bottomRightImage={{
+        src: Stairs,
+        className: styles.bottomRightImgStyles,
+      }}
+    />
+  ));
