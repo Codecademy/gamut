3ef1bcaa5d4ad2c84860df5a01a52a350876647a
Bana Malik <bana@codecademy.com>
2019-10-14 15:27:13 -0500

    pass event data into track notification function (#510)
    * dist
    
    * update notification list test
    
    * linter errors
    
    * pass object
    
    * pass object into func
    
    * update tests
    
    * fix linter error
    
    * pass notification object into function
    
    * update tests

--- packages/gamut/src/NotificationList/__tests__/NotificationList-test.js
+++ packages/gamut/src/NotificationList/__tests__/NotificationList-test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-
 import NotificationList from '..';
 import Notification from '../NotificationItem';
 
@@ -42,43 +41,44 @@ const notifications = [
     campaign: 'new campaign',
   },
 ];
-
 describe('NotificationList', () => {
   it('renders a message when no notifications are passed in', () => {
-    const wrapper = shallow(<NotificationList notifications={[]} />);
+    const wrapper = shallow(
+      React.createElement(NotificationList, { notifications: [] })
+    );
     expect(wrapper.text()).toEqual(
       "No new notifications.You're all caught up!"
     );
   });
-
   it('renders up to five notifications', () => {
-    const wrapper = shallow(<NotificationList notifications={notifications} />);
+    const wrapper = shallow(
+      React.createElement(NotificationList, { notifications: notifications })
+    );
     expect(wrapper.find(Notification).length).toEqual(5);
   });
-
   it('renders the most recent notifications first', () => {
-    const wrapper = shallow(<NotificationList notifications={notifications} />);
+    const wrapper = shallow(
+      React.createElement(NotificationList, { notifications: notifications })
+    );
     const renderedNotifications = wrapper.find(Notification);
-
     const expectedIds = ['6', '5', '4', '3', '2'];
     const receivedIds = renderedNotifications.map(
       notifComponent => notifComponent.prop('notification').id
     );
-
     expect(receivedIds).toEqual(expectedIds);
   });
-
   it('renders an unread notification', () => {
     const unreadNotification = {
       text: 'notification 1',
       id: '1',
       date: 'Fri Mar 15 2019 09:00:00 GMT-0400',
       unread: true,
-      campaign: 'new campaign',
+      campaign: 'new campaign 1',
     };
-
     const wrapper = shallow(
-      <NotificationList notifications={[unreadNotification]} />
+      React.createElement(NotificationList, {
+        notifications: [unreadNotification],
+      })
     );
     expect(
       wrapper
@@ -87,16 +87,14 @@ describe('NotificationList', () => {
         .prop('notification').unread
     ).toEqual(true);
   });
-
   it('passes down the onNotificationClick function', () => {
     const mockCallBack = jest.fn();
     const wrapper = shallow(
-      <NotificationList
-        notifications={notifications}
-        onNotificationClick={mockCallBack}
-      />
+      React.createElement(NotificationList, {
+        notifications: notifications,
+        onNotificationClick: mockCallBack,
+      })
     );
-
     wrapper
       .find(Notification)
       .first()
@@ -105,3 +103,5 @@ describe('NotificationList', () => {
     expect(mockCallBack.mock.calls[0][0]).toStrictEqual(notifications[0]);
   });
 });
+
+// #sourceMappingURL=NotificationList-test.js.map
