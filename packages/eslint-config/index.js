3a706684d9259a26478c1b35c55f6122e18b4847
joshuakgoldberg <joshuakgoldberg@outlook.com>
2019-10-23 09:21:22 -0400

    Oh, also generic-type-naming

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -50,6 +50,7 @@ module.exports = {
     '@typescript-eslint/ban-types': 'off',
     '@typescript-eslint/camelcase': 'off',
     '@typescript-eslint/explicit-member-accessibility': 'off',
+    '@typescript-eslint/generic-type-naming': 'off',
     '@typescript-eslint/member-ordering': 'off',
     '@typescript-eslint/no-empty-function': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
