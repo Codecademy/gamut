5ad5a822f02f52f63315b8857c56facbf21cd05b
joshuakgoldberg <joshuakgoldberg@outlook.com>
2019-11-14 15:55:56 -0500

    Whoops, removed skipUndeclared lint override

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -36,6 +36,13 @@ module.exports = {
   rules: {
     // These off-by-default or configurable rules are good and we like having them on
     'no-only-tests/no-only-tests': 'error',
+    'react/prop-types': [
+      'error',
+      {
+        ignore: ['children'],
+        skipUndeclared: true,
+      },
+    ],
     'react-hooks/exhaustive-deps': 'error',
     'react-hooks/rules-of-hooks': 'error',
 
