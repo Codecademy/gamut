fd9ec11d9b83721f671defc0db33e8a6714a657a
Josh Goldberg <joshuakgoldberg@outlook.com>
2019-12-03 14:00:18 -0500

    UI updates and dark mode for the Tooltip component (#556)
    * UI updates and dark mode for the Tooltip component
    
    Added a nice little arrow to the bottom of it per designs. Also added an as-of-yet unused DarkMode theme for when we use it in the LE
    
    * Font size and leading adjustments; made width max out at 250
    
    * Consolidated styles yippee
    
    * Also exported new things
    
    * Added wrapperClassName on top of existing tipClassName
    
    * Added some tests
    
    * Aha, linting
    
    * DOM nesting and CSS hovering fixes
    
    * Added focusable prop to ToolTip
    
    * Added mention of focus-ness in storybook story

---
+++ packages/gamut/src/ToolTip/__tests__/ToolTip-test.tsx
@@ -0,0 +1,32 @@
+import { mount } from 'enzyme';
+import React from 'react';
+
+import { VisualTheme } from '../../theming/VisualTheme';
+import { ToolTip } from '..';
+import styles from '../styles.scss';
+
+describe('ToolTip', () => {
+  it('adds the dark class to the container name when its theme is dark', () => {
+    const wrapper = mount(
+      <ToolTip id="test-id" theme={VisualTheme.DarkMode}></ToolTip>
+    );
+
+    const containerClassName = wrapper
+      .find({ role: 'tooltip' })
+      .prop('className');
+
+    expect(containerClassName).toContain(styles.toolTipContainerDark);
+  });
+
+  it('adds the light class to the container name when its theme is light', () => {
+    const wrapper = mount(
+      <ToolTip id="test-id" theme={VisualTheme.LightMode}></ToolTip>
+    );
+
+    const containerClassName = wrapper
+      .find({ role: 'tooltip' })
+      .prop('className');
+
+    expect(containerClassName).toContain(styles.toolTipContainerLight);
+  });
+});
--- workspaces/gamut-storybook/stories/ToolTip.stories.js
+++ workspaces/gamut-storybook/stories/ToolTip.stories.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { ToolTip } from 'gamut';
+import { ToolTip, Button } from 'gamut';
 import { select } from '@storybook/addon-knobs';
 import Icon from 'gamut/Icon';
+import { VisualTheme } from 'gamut/theming/VisualTheme';
 
 export default {
   component: ToolTip,
@@ -9,21 +10,122 @@ export default {
 };
 
 export const toolTip = () => (
-  <div style={{ margin: '7rem 0' }}>
-    <p>Hover the icon at the end of the sentence below to view a tooltip.</p>
-    Your billing cycle is each antantwig.
-    <ToolTip
-      id="tooltip"
-      target={<Icon name="informational" size={24} />}
-      position={select(
-        'position',
-        ['top-left', 'top-right', 'bottom-left', 'bottom-right'],
-        'top-right'
-      )}
-    >
-      Like the term <em>fortnight</em>, a term for a two week period,{' '}
-      <em>antantwig</em> refers to a three week period.
-    </ToolTip>
+  <div>
+    <div style={{ marginTop: '3rem' }}>
+      <div>
+        Hover the icon at the end of the sentence below to view a tooltip.
+      </div>
+      Your billing cycle is each antantwig.
+      <ToolTip
+        focusable
+        id="tooltip"
+        target={<Icon name="informational" size={24} />}
+        position={select(
+          'position',
+          ['top-left', 'top-right', 'bottom-left', 'bottom-right'],
+          'top-right'
+        )}
+      >
+        Like the term <em>fortnight</em>, a term for a two week period,{' '}
+        <em>antantwig</em> refers to a three week period.
+      </ToolTip>
+    </div>
+    <div style={{ margin: '2rem 0' }}>
+      <div>
+        It even supports a fancy dark mode too!
+        <ToolTip
+          focusable
+          id="tooltip-dark"
+          target={<Icon name="informational" size={24} />}
+          theme={VisualTheme.DarkMode}
+          position={select(
+            'position',
+            ['top-left', 'top-right', 'bottom-left', 'bottom-right'],
+            'bottom-left'
+          )}
+        >
+          Dark mode has black background and white text.
+        </ToolTip>
+      </div>
+    </div>
+    <div>
+      <div>
+        Top left
+        <ToolTip
+          focusable
+          id="tooltip-top-left"
+          target={<Icon name="informational" size={24} />}
+          position="top-left"
+        >
+          One fish...
+        </ToolTip>
+      </div>
+      <div>
+        Top right
+        <ToolTip
+          focusable
+          id="tooltip-top-right"
+          target={<Icon name="informational" size={24} />}
+          position="top-right"
+        >
+          Two fish; red fish; blue fish...
+        </ToolTip>
+      </div>
+      <div>
+        Bottom left
+        <ToolTip
+          focusable
+          id="tooltip-bottom-left"
+          target={<Icon name="informational" size={24} />}
+          position="bottom-left"
+        >
+          One fish...
+        </ToolTip>
+      </div>
+      <div>
+        Bottom right
+        <ToolTip
+          focusable
+          id="tooltip-bottom-right"
+          target={<Icon name="informational" size={24} />}
+          position="bottom-right"
+        >
+          Two fish; red fish; blue fish...
+        </ToolTip>
+      </div>
+    </div>
+    <div style={{ padding: '1rem 0 7rem' }}>
+      <div>
+        Non-Focusable
+        <ToolTip
+          id="tooltip-overlapping"
+          target={<Button>Hi!</Button>}
+          position="top-right"
+        >
+          If the contents of the tooltip are themselves focusable, such as with
+          buttons, do not pass the <code>focusable</code> prop.
+        </ToolTip>
+      </div>
+      <div>
+        Overlapping content
+        <ToolTip
+          focusable
+          id="tooltip-overlapping"
+          target={
+            <Icon
+              name="informational"
+              size={30}
+              style={{ margin: '1.5rem 0 -1.5rem' }}
+            />
+          }
+          position="bottom-right"
+        >
+          Hovering the mouse over a tooltip keeps it visible even if focus is
+          moved away from its target. That is particularly necessary for
+          tooltips that overlap content.
+        </ToolTip>
+      </div>
+    </div>
   </div>
 );
 
