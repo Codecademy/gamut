8dd74b7e2e5f01f9f42c4b9a59680fd4bc041ae2
Jake Hiller <jakemhiller@gmail.com>
2019-12-03 10:58:51 -0500

    remove name from prod chunks, just ends up breaking the cache more often

--- packages/webpack-config/src/config/common.js
+++ packages/webpack-config/src/config/common.js
@@ -25,8 +25,8 @@ const commonConfig = (options = {}) => {
     entry: path.resolve(options.context, 'src/main.js'),
 
     output: {
-      filename: DEV ? '[name].js' : '[name].[contenthash].js',
-      chunkFilename: DEV ? '[name].chunk.js' : '[name].[contenthash].chunk.js',
+      filename: DEV ? '[name].js' : '[contenthash].js',
+      chunkFilename: DEV ? '[name].chunk.js' : '[contenthash].chunk.js',
       path: path.resolve(options.context, 'dist'),
     },
 
