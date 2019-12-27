59dae65f57bc11ec9af036d111d3e23a5ae37dc6
Jake Hiller <jakemhiller@gmail.com>
2019-12-27 15:04:07 -0500

    fix default title and title id

--- packages/gamut-icons/icon-template.js
+++ packages/gamut-icons/icon-template.js
@@ -1,6 +1,11 @@
+const path = require('path');
+const { startCase } = require('lodash');
+
 function iconTemplate(api, opts, { jsx /* imports, props, exports */ }) {
   const template = api.template.smart({ plugins: ['typescript'] });
-  const { componentName } = opts.state;
+  const { componentName, filePath } = opts.state;
+  const title = startCase(path.basename(filePath, '.svg'));
+  const titleId = `gamut-icon-${Buffer.from(componentName).toString('base64')}`;
   const TypeIdentifier = `${componentName}Props`;
   const ComponentIdentifier = `${componentName}: React.FC<${TypeIdentifier}>`;
   return template.ast`
@@ -11,7 +16,7 @@ function iconTemplate(api, opts, { jsx /* imports, props, exports */ }) {
       title?: string;
       color?: string;
     };
-    const ${ComponentIdentifier} = React.forwardRef(({ref: svgRef, title, titleId, size, color, width, height, ...props}) => ${jsx});
+    const ${ComponentIdentifier} = React.forwardRef(({ref: svgRef, title = "${title}", titleId = "${titleId}", size, color, width, height, ...props}) => ${jsx});
     export default ${componentName};
   `;
 }
