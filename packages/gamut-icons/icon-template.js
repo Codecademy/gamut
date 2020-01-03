3c3b35563170595d4331879a3045fe0fa6f23ba6
Jake Hiller <jakemhiller@gmail.com>
2020-01-03 13:37:34 -0500

    feat: various icon build updates(#584)
    * update SVGR to simplify build process
    * allow forwarding refs
    * improve icon accessibility with preconfigured titles

--- packages/gamut-icons/icon-template.js
+++ packages/gamut-icons/icon-template.js
@@ -1,20 +1,20 @@
-function iconTemplate(
-  { template },
-  opts,
-  { componentName, jsx /* imports, props, exports */ }
-) {
-  const typeScriptTpl = template.smart({ plugins: ['typescript'] });
+const path = require('path');
+const { startCase } = require('lodash');
 
-  return typeScriptTpl.ast`
+function iconTemplate(api, opts, { jsx /* imports, props, exports */ }) {
+  const template = api.template.smart({ plugins: ['typescript'] });
+  const { componentName, filePath } = opts.state;
+  const title = startCase(path.basename(filePath, '.svg'));
+  const titleId = `gamut-icon-${Buffer.from(componentName).toString('base64')}`;
+  const ComponentIdentifier = `${componentName}: React.FC<GamutIconProps>`;
+  const componentNameWithRef = `${componentName}Ref`;
+  const RefComponent = `<${componentName} svgRef={ref} {...props} />`;
+  return template.ast`
     import * as React from 'react';
-    export interface GamutIconProps extends React.SVGProps<SVGSVGElement> {
-      size?: number | string;
-      title?: string;
-      color?: string;
-    }
-    const ${componentName} = ({title, size, color, width, height, ...props}: GamutIconProps) => ${jsx};
-    export default ${componentName};
+    import { GamutIconProps } from '../types';
+    const ${ComponentIdentifier} = ({ title = "${title}", titleId = "${titleId}", svgRef, size, color, width, height, ...props}) => ${jsx};
+    const ${componentNameWithRef} = React.forwardRef((props: GamutIconProps, ref?: React.Ref<SVGSVGElement>) => ${RefComponent});
+    export default ${componentNameWithRef};
   `;
 }
-
 module.exports = iconTemplate;
