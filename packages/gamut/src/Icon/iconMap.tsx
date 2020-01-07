196afb733dd2282f4f9ecfda6dd8e9ff06735110
Jake Hiller <jakemhiller@gmail.com>
2020-01-07 13:07:01 -0500

    replace gamut/icon iconMap with gamut-icons imports

--- packages/gamut/src/Icon/iconMap.tsx
+++ packages/gamut/src/Icon/iconMap.tsx
@@ -1,155 +1,83 @@
-import AccessibilityIcon from './icons/AccessibilityIcon';
-import AddIcon from './icons/AddIcon';
-import AdvisorIcon from './icons/AdvisorIcon';
-import AdvisorChatIcon from './icons/AdvisorChatIcon';
-import AlertIcon from './icons/AlertIcon';
-import AlexaIcon from './icons/AlexaIcon';
-import AngularIcon from './icons/AngularIcon';
-import ArticleIcon from './icons/ArticleIcon';
-import BellIcon from './icons/BellIcon';
-import BayesIcon from './icons/BayesIcon';
-import BulbIcon from './icons/BulbIcon';
-import CSSIcon from './icons/CSSIcon';
-import CertificateIcon from './icons/CertificateIcon';
-import CheckmarkIcon from './icons/CheckmarkIcon';
-import CheckFilledIcon from './icons/CheckFilledIcon';
-import ChevronDownIcon from './icons/ChevronDownIcon';
-import ChevronLeftIcon from './icons/ChevronLeftIcon';
-import ChevronRightIcon from './icons/ChevronRightIcon';
-import ChevronUpIcon from './icons/ChevronUpIcon';
-import ClockIcon from './icons/ClockIcon';
-import CloseIcon from './icons/CloseIcon';
-import ColorsIcon from './icons/ColorsIcon';
-import CopyIcon from './icons/CopyIcon';
-import CommunityIcon from './icons/CommunityIcon';
-import DAWIcon from './icons/DAWIcon';
-import DownArrowIcon from './icons/DownArrowIcon';
-import EditIcon from './icons/EditIcon';
-import FloatIcon from './icons/FloatIcon';
-import ForumsIcon from './icons/ForumsIcon';
-import ForumsChatIcon from './icons/ForumsChatIcon';
-import FullstackIcon from './icons/FullstackIcon';
-import GitIcon from './icons/GitIcon';
-import GlassesIcon from './icons/GlassesIcon';
-import HTMLIcon from './icons/HTMLIcon';
-import InformationalIcon from './icons/InformationalIcon';
-import JavaIcon from './icons/JavaIcon';
-import JavascriptIcon from './icons/JavascriptIcon';
-import LearnIcon from './icons/LearnIcon';
-import LeftArrowIcon from './icons/LeftArrowIcon';
-import LessonIcon from './icons/LessonIcon';
-import MAWIcon from './icons/MAWIcon';
-import MaintenanceIcon from './icons/MaintenanceIcon';
-import MatplotLibIcon from './icons/MatplotLibIcon';
-import MenuIcon from './icons/MenuIcon';
-import NewPageIcon from './icons/NewPageIcon';
-import NumPyIcon from './icons/NumPyIcon';
-import PHPIcon from './icons/PHPIcon';
-import PandasIcon from './icons/PandasIcon';
-import PathIcon from './icons/PathIcon';
-import PhoneIcon from './icons/PhoneIcon';
-import PlayIcon from './icons/PlayIcon';
-import PracticeIcon from './icons/PracticeIcon';
-import ProjectIcon from './icons/ProjectIcon';
-import PythonIcon from './icons/PythonIcon';
-import QuizIcon from './icons/QuizIcon';
-import RailsIcon from './icons/RailsIcon';
-import ReactIcon from './icons/ReactIcon';
-import ResetIcon from './icons/ResetIcon';
-import ResponsiveIcon from './icons/ResponsiveIcon';
-import RightArrowIcon from './icons/RightArrowIcon';
-import RubyIcon from './icons/RubyIcon';
-import SQLIcon from './icons/SQLIcon';
-import SassIcon from './icons/SassIcon';
-import SciPyIcon from './icons/SciPyIcon';
-import SearchIcon from './icons/SearchIcon';
-import TDDIcon from './icons/TDDIcon';
-import TerminalIcon from './icons/TerminalIcon';
-import TrophyIcon from './icons/TrophyIcon';
-import TypeIcon from './icons/TypeIcon';
-import WatsonIcon from './icons/WatsonIcon';
-import UpArrowIcon from './icons/UpArrowIcon';
-import VideoIcon from './icons/VideoIcon';
-import jQueryIcon from './icons/jQueryIcon';
+import * as Icons from '@codecademy/gamut-icons';
 
 /**
- * @deprecated Directly import icons from @codecademy/gamut instead.
+ * @deprecated Directly import icons from @codecademy/gamut-icons instead.
  */
 const iconMap = {
-  accessibility: AccessibilityIcon,
-  add: AddIcon,
-  advisor: AdvisorIcon,
-  advisorChat: AdvisorChatIcon,
-  alert: AlertIcon,
-  alexa: AlexaIcon,
-  angular: AngularIcon,
-  article: ArticleIcon,
-  bayes: BayesIcon,
-  bell: BellIcon,
-  bulbIcon: BulbIcon,
-  certificate: CertificateIcon,
-  checkmark: CheckmarkIcon,
-  checkFilledIcon: CheckFilledIcon,
-  chevronDown: ChevronDownIcon,
-  chevronLeft: ChevronLeftIcon,
-  chevronRight: ChevronRightIcon,
-  chevronUp: ChevronUpIcon,
-  clock: ClockIcon,
-  close: CloseIcon,
-  colors: ColorsIcon,
-  community: CommunityIcon,
-  copy: CopyIcon,
-  css: CSSIcon,
-  daw: DAWIcon,
-  downArrow: DownArrowIcon,
-  edit: EditIcon,
-  floatIcon: FloatIcon,
-  forums: ForumsIcon,
-  forumsChat: ForumsChatIcon,
-  fullstack: FullstackIcon,
-  git: GitIcon,
-  glasses: GlassesIcon,
-  html: HTMLIcon,
-  informational: InformationalIcon,
-  jQuery: jQueryIcon,
-  java: JavaIcon,
-  javascript: JavascriptIcon,
-  learn: LearnIcon,
-  leftArrow: LeftArrowIcon,
-  lesson: LessonIcon,
-  maintenance: MaintenanceIcon,
-  matplotLib: MatplotLibIcon,
-  maw: MAWIcon,
-  menu: MenuIcon,
-  newPageIcon: NewPageIcon,
-  numPy: NumPyIcon,
-  pandas: PandasIcon,
-  path: PathIcon,
-  phone: PhoneIcon,
-  php: PHPIcon,
-  play: PlayIcon,
-  practice: PracticeIcon,
-  project: ProjectIcon,
-  python: PythonIcon,
-  quiz: QuizIcon,
-  rails: RailsIcon,
-  react: ReactIcon,
-  reset: ResetIcon,
-  responsive: ResponsiveIcon,
-  rightArrow: RightArrowIcon,
-  ruby: RubyIcon,
-  sass: SassIcon,
-  sciPy: SciPyIcon,
-  search: SearchIcon,
-  sql: SQLIcon,
-  syllabus: LessonIcon,
-  tdd: TDDIcon,
-  terminal: TerminalIcon,
-  trophy: TrophyIcon,
-  type: TypeIcon,
-  watson: WatsonIcon,
-  upArrow: UpArrowIcon,
-  video: VideoIcon,
+  // alexa: Icons.AlexaIcon,
+  // angular: Icons.AngularIcon,
+  // bayes: Icons.BayesIcon,
+  // jQuery: Icons.jQueryIcon,
+  // java: Icons.JavaIcon,
+  // javascript: Icons.JavascriptIcon,
+  // css: Icons.CSSIcon,
+  // daw: Icons.DAWIcon,
+  // fullstack: Icons.FullstackIcon,
+  // html: Icons.HTMLIcon,
+  // git: Icons.GitIcon,
+  // matplotLib: Icons.MatplotLibIcon,
+  // maw: Icons.MAWIcon,
+  // php: Icons.PHPIcon,
+  // ruby: Icons.RubyIcon,
+  // sass: Icons.SassIcon,
+  // sciPy: Icons.SciPyIcon,
+  // tdd: Icons.TDDIcon,
+  // watson: Icons.WatsonIcon,
+  // sql: Icons.SQLIcon,
+  // numPy: Icons.NumPyIcon,
+  // pandas: Icons.PandasIcon,
+  // python: Icons.PythonIcon,
+  // rails: Icons.RailsIcon,
+  // react: Icons.ReactIcon,
+  accessibility: Icons.AccessibilityIcon,
+  add: Icons.AddIcon,
+  advisor: Icons.PersonIcon,
+  advisorChat: Icons.PersonChatIcon,
+  alert: Icons.AlertIcon,
+  article: Icons.ArticleIcon,
+  bell: Icons.BellIcon,
+  bulbIcon: Icons.BulbIcon,
+  certificate: Icons.CertificateIcon,
+  checkmark: Icons.CheckIcon,
+  checkFilledIcon: Icons.CheckFilledIcon,
+  chevronDown: Icons.ArrowChevronDownIcon,
+  chevronLeft: Icons.ArrowChevronLeftIcon,
+  chevronRight: Icons.ArrowChevronRightIcon,
+  chevronUp: Icons.ArrowChevronUpIcon,
+  clock: Icons.ClockIcon,
+  close: Icons.CloseIcon,
+  colors: Icons.IntersectionIcon,
+  community: Icons.ChatIcon,
+  copy: Icons.CopyIcon,
+  downArrow: Icons.ArrowDownIcon,
+  edit: Icons.EditIcon,
+  floatIcon: Icons.LifePreserverIcon,
+  forums: Icons.PeopleIcon,
+  forumsChat: Icons.ForumsChatIcon,
+  glasses: Icons.GlassesIcon,
+  informational: Icons.InfoIcon,
+  learn: Icons.LearnIcon,
+  leftArrow: Icons.ArrowLeftIcon,
+  lesson: Icons.LessonIcon,
+  maintenance: Icons.MaintenanceIcon,
+  menu: Icons.MenuIcon,
+  newPageIcon: Icons.OpenIcon,
+  path: Icons.PathIcon,
+  phone: Icons.PhoneIcon,
+  play: Icons.PlayIcon,
+  practice: Icons.PracticeIcon,
+  project: Icons.ProjectIcon,
+  quiz: Icons.QuizIcon,
+  reset: Icons.ResetIcon,
+  responsive: Icons.ResponsiveIcon,
+  rightArrow: Icons.ArrowRightIcon,
+  search: Icons.SearchIcon,
+  syllabus: Icons.LessonIcon,
+  terminal: Icons.TerminalIcon,
+  trophy: Icons.TrophyIcon,
+  type: Icons.TypeIcon,
+  upArrow: Icons.ArrowUpIcon,
+  video: Icons.PlayIcon,
 };
 
 /**
--- packages/gamut/src/Icon/index.tsx
+++ packages/gamut/src/Icon/index.tsx
@@ -1,35 +1,26 @@
-import React, { SVGProps, HTMLAttributes } from 'react';
+import React from 'react';
 import iconMap from './iconMap';
 
 /**
  * @deprecated Directly import icons from @codecademy/gamut instead.
  */
-export type IconPropsDeprecated = HTMLAttributes<SVGElement> &
-  SVGProps<SVGSVGElement> & {
-    height?: number;
-    label?: string;
-    name: keyof typeof iconMap;
-    size?: number;
-    width?: number;
-  };
+export type IconPropsDeprecated = React.SVGProps<SVGSVGElement> & {
+  height?: number;
+  label?: string;
+  name: keyof typeof iconMap;
+  size?: number;
+  width?: number;
+  ref?: React.Ref<SVGSVGElement>;
+};
 
 /**
  * @deprecated Directly import icons from @codecademy/gamut instead.
  */
-export function Icon({ name, size, ...props }: IconPropsDeprecated) {
+export function Icon({ name, ...props }: IconPropsDeprecated) {
   const MappedIcon = iconMap[name];
   const { label, ...iconProps } = props;
 
-  if (label) {
-    iconProps['aria-label'] = label;
-  }
-
-  if (size) {
-    iconProps.width = size;
-    iconProps.height = size;
-  }
-
-  return <MappedIcon {...iconProps} />;
+  return <MappedIcon {...iconProps} title={label} />;
 }
 
 export default Icon;
