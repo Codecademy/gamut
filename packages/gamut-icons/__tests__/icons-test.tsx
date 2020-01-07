81debf8e6c93586be250d8e5c05ad263e98b41a6
Jake Hiller <jakemhiller@gmail.com>
2020-01-06 23:41:38 -0500

    Fix gamut-icons typescript types, add tests (#590)
    * fix types, add tests
    
    * test naming

---
+++ packages/gamut-icons/__tests__/icons-test.tsx
@@ -0,0 +1,63 @@
+import { mount } from 'enzyme';
+import React from 'react';
+
+import { AccessibilityIcon, GamutIconProps } from '../dist/index';
+
+describe('Compiled gamut-icons:', () => {
+  it('Converts size to equal width and height', () => {
+    const expectedSize = 76;
+    const iconProps: GamutIconProps = {
+      size: expectedSize,
+    };
+
+    const wrapper = mount(<AccessibilityIcon {...iconProps} />);
+    const svgEl = wrapper.find('svg');
+
+    expect(svgEl.props().width).toEqual(expectedSize);
+    expect(svgEl.props().height).toEqual(expectedSize);
+  });
+
+  it('Sets a title and id automatically and uses the appropriate aria label', () => {
+    const wrapper = mount(<AccessibilityIcon size={1} />);
+
+    const svgEl = wrapper.find('svg');
+    const titleEl = wrapper.find('title');
+    expect(svgEl.props()['aria-labelledby']).toEqual(titleEl.props().id);
+    expect(titleEl.text()).toEqual('Accessibility Icon');
+  });
+
+  it('Allows passing a custom title', () => {
+    const wrapper = mount(<AccessibilityIcon title="Accessible" />);
+
+    const svgEl = wrapper.find('svg');
+    const titleEl = wrapper.find('title');
+    expect(titleEl.text()).toEqual('Accessible');
+  });
+
+  it('Sets a default fill of currentColor', () => {
+    const wrapper = mount(<AccessibilityIcon />);
+
+    const svgEl = wrapper.find('svg');
+    expect(svgEl.props().fill).toEqual('currentColor');
+  });
+
+  it('Allows passing a custom color', () => {
+    const wrapper = mount(<AccessibilityIcon color="red" />);
+
+    const svgEl = wrapper.find('svg');
+    expect(svgEl.props().fill).toEqual('red');
+  });
+
+  it('Allows passing a ref', () => {
+    let ref: React.MutableRefObject<SVGSVGElement>;
+    const RefIcon: React.FC<any> = () => {
+      ref = React.useRef<SVGSVGElement>(null);
+
+      return <AccessibilityIcon ref={ref} />;
+    };
+
+    mount(<RefIcon color="red" />);
+
+    expect(ref.current).toBeTruthy();
+  });
+});
