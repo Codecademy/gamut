03335a0575ac85c45fa4fcf1455084151551659f
Prith Pal <prithpal87@gmail.com>
2020-01-08 16:58:30 -0500

    TS updates for Row and Col (#595)
    * Allow optional props to be optional instead of required
    
    * Standardize RowProps as ColProps

--- packages/gamut/src/FlexGrid/Row.tsx
+++ packages/gamut/src/FlexGrid/Row.tsx
@@ -52,25 +52,23 @@ const modificatorKeys = [
 
 type ModificatorType = 'xs' | 'sm' | 'md' | 'lg';
 
-export type RowProps<TElement extends HTMLElement = HTMLElement> = {
-  around: ModificatorType;
-  between: ModificatorType;
-  bottom: ModificatorType;
-  center: ModificatorType;
+export type RowProps = {
+  around?: ModificatorType;
+  between?: ModificatorType;
+  bottom?: ModificatorType;
+  center?: ModificatorType;
   className?: string;
-  end: ModificatorType;
-  first: ModificatorType;
-  last: ModificatorType;
-  middle: ModificatorType;
+  end?: ModificatorType;
+  first?: ModificatorType;
+  last?: ModificatorType;
+  middle?: ModificatorType;
   reverse?: boolean;
-  start: ModificatorType;
-  tagName?: TElement['tagName'];
-  top: ModificatorType;
+  start?: ModificatorType;
+  tagName?: string;
+  top?: ModificatorType;
 };
 
-export const Row = <TElement extends HTMLElement = HTMLDivElement>(
-  props: RowProps<TElement>
-) => {
+export const Row: React.FC<RowProps> = props => {
   return React.createElement(
     props.tagName || 'div',
     omitProps(propKeys, { ...props, className: getClassNames(props) })
