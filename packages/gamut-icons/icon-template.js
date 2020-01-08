670a7f2a1e37a06d62577ca7f92183aa177dfcc5
Jake Hiller <jakemhiller@gmail.com>
2020-01-08 13:20:30 -0500

    fix: re-enable icon title props (#594)

--- packages/gamut-icons/icon-template.js
+++ packages/gamut-icons/icon-template.js
@@ -1,10 +1,19 @@
+const path = require('path');
+const { startCase } = require('lodash');
+
 function iconTemplate(api, opts, { jsx /* imports, props, exports */ }) {
   const template = api.template.smart({ plugins: ['typescript'] });
-  const { componentName } = opts.state;
+  const { componentName, filePath } = opts.state;
+  const title = startCase(path.basename(filePath, '.svg'));
+  const baseTitleId = `gamut-icon-${Buffer.from(componentName).toString(
+    'base64'
+  )}`;
   return template.ast`
     import * as React from 'react';
     import { GamutIconProps } from '../types';
     const ${componentName} = React.forwardRef<SVGSVGElement, GamutIconProps>(({
+      title = "${title}",
+      titleId = '',
       size,
       color,
       width,
@@ -12,7 +21,10 @@ function iconTemplate(api, opts, { jsx /* imports, props, exports */ }) {
       ...props
     },
       svgRef
-    ) => ${jsx});
+    ) => {
+      titleId = "${baseTitleId}" + titleId;
+      return ${jsx};
+    });
     export default ${componentName};
   `;
 }
