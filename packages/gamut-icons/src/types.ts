222126217c7f84e002a03d44dd3dc4842ccc37ba
Jake Hiller <jakemhiller@gmail.com>
2020-01-08 13:20:30 -0500

    fix: re-enable icon title props (#594)

--- packages/gamut-icons/src/types.ts
+++ packages/gamut-icons/src/types.ts
@@ -1,7 +1,12 @@
 import * as React from 'react';
 
 export type GamutIconProps = {
+  /**
+   * A suffix added to the end of the unique generated ID for the icon. This is useful if you have multiple of the same icon on the page and need to pass accessibility guidelines.
+   */
+  titleId?: string;
   size?: number | string;
+  title?: string;
   color?: string;
   ref?: React.Ref<SVGSVGElement>;
 } & React.SVGProps<SVGSVGElement>;
