4d039d0e95def5dfc7e130f0f34fb70f783d62fd
Jake Hiller <jakemhiller@gmail.com>
2020-01-08 11:11:22 -0500

    fix: add missing narrative icon (#593)
    Add missing narrative icon, clean up some gamut-icons build settings

--- packages/gamut-icons/src/types.ts
+++ packages/gamut-icons/src/types.ts
@@ -1,9 +1,7 @@
 import * as React from 'react';
 
 export type GamutIconProps = {
-  titleId?: string;
   size?: number | string;
-  title?: string;
   color?: string;
   ref?: React.Ref<SVGSVGElement>;
 } & React.SVGProps<SVGSVGElement>;
