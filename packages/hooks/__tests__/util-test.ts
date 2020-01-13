dab90445c37d355d8c89d856b025b701ae0e3d23
Jake Hiller <jakemhiller@gmail.com>
2020-01-13 14:29:02 -0500

    add tests for isServer

---
+++ packages/hooks/__tests__/util-test.ts
@@ -0,0 +1,35 @@
+/**
+ * @jest-environment node
+ */
+
+import { isServer } from '../src/util';
+
+describe('isServer', () => {
+  beforeEach(() => {
+    delete process.env.__SSR__;
+    delete global['window'];
+  });
+
+  it('Returns false when window exists and the ENV variable is undefined', () => {
+    global['window'] = {};
+    expect(process.env.__SSR__).toBeUndefined();
+    expect(isServer()).toBe(false);
+  });
+
+  it('Returns true when window exists and the ENV variable is true', () => {
+    global['window'] = {};
+    process.env.__SSR__ = 'true';
+    expect(typeof window).toBe('object');
+    expect(isServer()).toBe(true);
+  });
+
+  it('Returns true when window does not exist and the ENV variable is true', () => {
+    process.env.__SSR__ = 'true';
+    expect(isServer()).toBe(true);
+  });
+
+  it('Returns true when window does not exist and the ENV variable is undefined', () => {
+    expect(process.env.__SSR__).toBeUndefined();
+    expect(isServer()).toBe(true);
+  });
+});
