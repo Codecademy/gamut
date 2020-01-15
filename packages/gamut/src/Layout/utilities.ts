1fb04392d46f1ed4b7a4ac9e38dfd95a18636ea4
Aaron Robb <aaron@codecademy.com>
2020-01-15 15:07:01 -0500

    Fix up class comp method

--- packages/gamut/src/Layout/utilities.ts
+++ packages/gamut/src/Layout/utilities.ts
@@ -2,15 +2,12 @@
  * Takes a prop/style configuration and templates the classes
  * to match the styleMaps classes returning a concatenated classname string
  */
-export type ClassList = (string | Record<string, unknown>)[];
 
 export const createClassnames = (
   config: Record<string, string | number | Record<string, string | number>>,
   styleMap: Record<string, string>
-): ClassList => {
-  const classNames: ClassList = [];
-
-  Object.entries(config).map(([propName, propValue]) => {
+): (string | string[])[] => {
+  return Object.entries(config).map(([propName, propValue]) => {
     switch (typeof propValue) {
       case 'number':
       case 'string': {
@@ -19,12 +16,10 @@ export const createClassnames = (
       case 'object': {
         return Object.entries(propValue).map(
           ([mediaSize, mediaPropValue]) =>
-            styleMap[`${propName}_${mediaPropValue}__${mediaSize}Screen`],
-          {} as Record<string, string>
+            styleMap[`${propName}_${mediaPropValue}__${mediaSize}Screen`]
         );
       }
       default:
     }
   });
-  return classNames;
 };
