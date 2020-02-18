e6e743121ed83927b41a0123970f44c005a82eba
Josh Goldberg <josh@codecademy.com>
2020-02-18 11:52:57 -0500

    Updated test name

--- packages/gamut/src/SkipToContent/__tests__/SkipToContent-test.tsx
+++ packages/gamut/src/SkipToContent/__tests__/SkipToContent-test.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import SkipToContent from '..';
 
 describe('SkipToContent', () => {
-  it('navigates to the corresponding content ID', () => {
+  it('has a link to the corresponding content ID', () => {
     const contentId = 'main-area';
     const wrapped = mount(<SkipToContent contentId={contentId} />);
 
