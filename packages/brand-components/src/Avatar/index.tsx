6888050e0842c39e92df661e88301cea2f75cb9e
Roger Vera <roger@codecademy.com>
2020-02-20 11:30:55 -0500

    feat: create branded avatar component
    * initial commit for BrandedAvatar
    
    * create Brand avatar component and create story
    
    * add typings
    
    * explicit alt prop
    
    * create index.ts
    
    * initial changes from PR
    
    * refactor imageProps and add aria-labelledby
    
    * refactor and add tests
    
    * pass alt prop
    
    * Update packages/brand-components/src/Avatar/__tests__/avatar-test.tsx
    
    Co-Authored-By: Josh Goldberg <joshuakgoldberg@outlook.com>
    
    * remove cannot test and rename types
    
    * clarify disabling of alt-text rule
    
    * add light/dark theme testing
    
    * change into toHaveLength
    
    Co-authored-by: Josh Goldberg <me@joshuakgoldberg.com>

---
+++ packages/brand-components/src/Avatar/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import s from './styles.module.scss';
+import cx from 'classnames';
+import { VisualTheme } from '../../../gamut/src/theming/VisualTheme';
+
+type AvatarImageProps =
+  | {
+      alt: string;
+      'aria-labelledby'?: never;
+    }
+  | { alt?: never; 'aria-labelledby': string };
+
+export type AvatarBaseProps = {
+  src: string;
+  size?: 'regular' | 'large';
+  theme?: VisualTheme;
+};
+
+type AvatarProps = AvatarBaseProps & AvatarImageProps;
+
+export const Avatar: React.FC<AvatarProps> = ({
+  size = 'regular',
+  theme = VisualTheme.LightMode,
+  ...avatarImageProps
+}) => {
+  return (
+    <div
+      className={cx(
+        s.container,
+        s[`${size}Container`],
+        theme === VisualTheme.DarkMode ? s.darkContainer : s.lightContainer
+      )}
+    >
+      {/*  The current rules for alt-text don't allow images with aria-labelledby to have no alt. So, we need to disable the rule for that line. https://github.com/evcohen/eslint-plugin-jsx-a11y/issues/411#issue-306995775 */}
+      {/* eslint-disable-next-line jsx-a11y/alt-text */}
+      <img className={cx(s.image, s[`${size}Image`])} {...avatarImageProps} />
+    </div>
+  );
+};
+
+export default Avatar;
---
+++ packages/styleguide/stories/Avatar.stories.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Avatar } from '../../brand-components/src/Avatar/';
+import { VisualTheme } from '../../gamut/src/theming/VisualTheme';
+import { withKnobs, select } from '@storybook/addon-knobs';
+
+export default {
+  component: Avatar,
+  title: 'Brand/Avatar',
+  decorators: [withKnobs],
+};
+
+export const avatar = () => (
+  <Avatar
+    src="https://content.codecademy.com/courses/free/boba.svg"
+    alt="testy"
+    size={select('size', ['regular', 'large'], 'regular')}
+    theme={select(
+      'theme',
+      [VisualTheme.LightMode, VisualTheme.DarkMode],
+      VisualTheme.LightMode
+    )}
+  />
+);
+
+avatar.story = {
+  name: 'Avatar used in Brand components',
+};
