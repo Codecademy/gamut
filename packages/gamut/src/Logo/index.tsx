ed566e1a1d0d19eb1c4ffb931e5b4a81ea987102
Mike Boyle <mikeboyle@users.noreply.github.com>
2020-02-24 15:00:44 -0500

    [PREM-38] Update nav bar logos in gamut (#638)
    The upper left nav logos on our site do not use gamut! This PR will change that!
    
    - Replace the gamut Pro logo with an updated one (Pro in purple, not mint)
    - Add a mono-color Pro logo. The immediate goal is to have an all-white Pro logo to use in dark mode LE.
    - Add a premium logo.
    
    Mike K and Yassine have looked at these pretty thoroughly. This is not a YOLO change. We've also done some VQA on my local with these logos rendered in the nav bar in the site, and the initial response is good.

--- packages/gamut/src/Logo/index.tsx
+++ packages/gamut/src/Logo/index.tsx
@@ -4,6 +4,8 @@ import CodecademyProLogo from './CodecademyProLogo';
 import CodecademyProAltLogo from './CodecademyProAltLogo';
 import CodecademyProgramLogo from './CodecademyProgramLogo';
 import CodecademyProLockupLogo from './CodecademyProLockupLogo';
+import CodecademyProMonoLogo from './CodecademyProMonoLogo';
+import CodecademyPremiumLogo from './CodecademyPremiumLogo';
 
 const defaultProps = {
   height: 32,
@@ -14,14 +16,25 @@ const logos = {
   pro: CodecademyProLogo,
   proAlt: CodecademyProAltLogo,
   proLockup: CodecademyProLockupLogo,
+  proMono: CodecademyProMonoLogo,
   program: CodecademyProgramLogo,
+  premium: CodecademyPremiumLogo,
   default: CodecademyLogo,
 };
+
+export type LogoType =
+  | 'default'
+  | 'pro'
+  | 'proAlt'
+  | 'proLockup'
+  | 'program'
+  | 'proMono'
+  | 'premium';
 
 export type LogoProps = SVGProps<SVGSVGElement> & {
   height?: number;
   width?: number;
-  type: 'default' | 'pro' | 'proAlt' | 'proLockup' | 'program';
+  type: LogoType;
 };
 
 export function Logo({ type, ...props }: LogoProps) {
