5716e03fd546aedbe1f4687580671bc4ad525a83
rogervera <roger@codecademy.com>
2020-02-24 11:49:03 -0500

    add test

---
+++ packages/brand-components/src/Testimonial/__tests__/Testimonial-test.tsx
@@ -0,0 +1,50 @@
+import { mount } from 'enzyme';
+import React from 'react';
+import { VisualTheme } from '@codecademy/gamut';
+
+import { Testimonial } from '..';
+import styles from '../../styles.module.scss';
+
+const exampleTestmonial = {
+  name: 'Frank Reynolds',
+  occupation: "Co-Owner @ Paddy's Pub",
+  imageUrl: '',
+  quote:
+    "I don't know how many years on this Earth I got left, I'm gonna get real weird with it.",
+};
+
+describe('Testimonial', () => {
+  it('adds the light class to the container name when its theme is light', () => {
+    const wrapper = mount(
+      <Testimonial
+        testimonial={exampleTestmonial}
+        size="small"
+        theme={VisualTheme.LightMode}
+      />
+    );
+
+    const containerClassName = wrapper
+      .find(`div`)
+      .first()
+      .prop('className');
+
+    expect(containerClassName).toContain(styles.lightContainer);
+  });
+
+  it('adds the dark class to the container name when its theme is dark', () => {
+    const wrapper = mount(
+      <Testimonial
+        testimonial={exampleTestmonial}
+        size="small"
+        theme={VisualTheme.DarkMode}
+      />
+    );
+
+    const containerClassName = wrapper
+      .find(`div`)
+      .first()
+      .prop('className');
+
+    expect(containerClassName).toContain(styles.darkContainer);
+  });
+});
