bed9174d855aeed84a94b5dfc8d7e3e8c953b213
Josh Goldberg <josh@codecademy.com>
2020-02-26 12:56:49 -0500

    Better explained GridForm in story

--- packages/styleguide/stories/GridForm.stories.js
+++ packages/styleguide/stories/GridForm.stories.js
@@ -2,63 +2,89 @@ import React from 'react';
 import { GridForm } from '../../gamut-templates';
 
 export default {
-  component: 'GridForm',
+  component: GridForm,
   title: 'Templates/GridForm',
 };
 
 export const gridForm = () => (
-  <GridForm
-    fields={[
-      {
-        label: 'Simple text',
-        name: 'simple-text',
-        sizing: {
-          xs: 3,
+  <>
+    <p>
+      This <code>GridForm</code> component takes in plain JSON-like props and
+      usess them to automatically formulate a magical combination of:
+      <ul>
+        <li>
+          {' '}
+          <a href="https://react-hook-form.com">
+            <code>react-hook-form</code>
+          </a>
+        </li>
+        <li>
+          <a href="/?path=/story/layout-grid-system--fixed-grid-default">
+            Our existing <code>Grid</code> layout system
+          </a>
+        </li>
+        <li>
+          <a href="/?path=/story/component-form--form">
+            Our existing <code>Form</code> primitives
+          </a>
+        </li>
+      </ul>
+      Unlike <code>Form</code> and its primitives, you should only ever directly
+      use <code>GridForm</code> to create your form.
+    </p>
+    <GridForm
+      fields={[
+        {
+          label: 'Simple text',
+          name: 'simple-text',
+          sizing: {
+            xs: 3,
+          },
+          type: 'text',
         },
-        type: 'text',
-      },
-      {
-        defaultValue: 'yeet',
-        label: 'Text with default value',
-        name: 'text-with-default',
-        sizing: {
-          xs: 4,
+        {
+          defaultValue: 'yeet',
+          label: 'Text with default value',
+          name: 'text-with-default',
+          sizing: {
+            xs: 4,
+          },
+          type: 'text',
         },
-        type: 'text',
-      },
-      {
-        label: 'Simple select',
-        name: 'simple-select',
-        options: ['One fish', 'Two fish', 'Red fish', 'Blue fish'],
-        sizing: {
-          xs: 5,
+        {
+          label: 'Simple select',
+          name: 'simple-select',
+          options: ['One fish', 'Two fish', 'Red fish', 'Blue fish'],
+          sizing: {
+            xs: 5,
+          },
+          type: 'select',
         },
-        type: 'select',
-      },
-      {
-        label:
-          "Validated, required text that must contain the word 'swag' twice",
-        name: 'validated-required-text',
-        sizing: {
-          xs: 12,
-        },
-        type: 'text',
-        validation: {
-          required: true,
-          pattern: {
-            value: /swag(.*)swag/,
-            message: 'Not enough swag',
+        {
+          label:
+            "Validated, required text that must contain the word 'swag' twice",
+          name: 'validated-required-text',
+          sizing: {
+            xs: 12,
+          },
+          type: 'text',
+          validation: {
+            required: true,
+            pattern: {
+              value: /swag(.*)swag/,
+              message: 'Not enough swag',
+            },
           },
         },
-      },
-    ]}
-    onSubmit={async values => {
-      alert(`Submitted: ${JSON.stringify(values)}`);
-    }}
-    submit={{
-      children: 'Submit Me!?',
-    }}
-  />
+      ]}
+      onSubmit={async values => {
+        alert(`Submitted: ${JSON.stringify(values)}`);
+      }}
+      submit={{
+        children: 'Submit Me!?',
+      }}
+    />
+  </>
 );
 
 gridForm.story = {
