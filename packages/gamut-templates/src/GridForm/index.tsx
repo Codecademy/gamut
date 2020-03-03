390ada37908f972dd69e05327e4c6be20a3e7735
Josh Goldberg <joshuakgoldberg@outlook.com>
2020-03-03 13:14:43 -0500

    feat: added GridForm component (#605)
    * stub GridForm template
    
    * stub story
    
    * Casing
    
    * WIP: basic structure
    
    * Fleshed out grid
    
    * Classic
    
    * Add missing register back in
    
    * Added unit tests, mostly
    
    * Corrected import path
    
    * getValues() is redundant
    
    * Aha, got the unit tests to work: by updating test libs to latest
    
    * Remove unnecessary setup-after-env
    
    * Also an unused styles.module.scss
    
    * Deduplicated types/react
    
    * Added a newly necessary cast, why not
    
    * Bumped TypeScript to 3.7
    
    * Updated Prettier to latest
    
    * I think this is the last of the updates...
    
    * FINALLY, Prettier changes
    
    * Remove addon-storyshots from root package.json
    
    * Exported ower level components
    
    * Better explained GridForm in story
    
    * Updated submit children name to be contents
    
    * Quick little compile error
    
    Co-authored-by: Roger Vera <roger@codecademy.com>

---
+++ packages/gamut-templates/src/GridForm/index.tsx
@@ -0,0 +1,62 @@
+import { Form, Grid, Row } from '@codecademy/gamut';
+import React, { useEffect } from 'react';
+import { useForm, FieldError } from 'react-hook-form';
+
+import GridFormInputGroup from './GridFormInputGroup';
+import GridFormSubmit, { GridFormSubmitProps } from './GridFormSubmit';
+import { GridFormField } from './types';
+
+export type GridFormProps<Values extends {}> = {
+  fields: GridFormField[];
+  onSubmit: (values: Values) => Promise<void>;
+  submit: GridFormSubmitProps;
+};
+
+export function GridForm<Values extends Record<string, string | undefined>>({
+  fields,
+  submit,
+  onSubmit,
+}: GridFormProps<Values>) {
+  const { errors, handleSubmit, register, setValue } = useForm<Values>({
+    defaultValues: fields.reduce(
+      (defaultValues, field) => ({
+        ...defaultValues,
+        [field.name]: field.defaultValue,
+      }),
+      {}
+    ),
+  });
+
+  useEffect(() => {
+    for (const field of fields) {
+      register({ name: field.name });
+    }
+  }, [fields, register]);
+
+  return (
+    <Form onSubmit={handleSubmit(onSubmit)}>
+      <Grid>
+        <Row>
+          {fields.map(field => {
+            const errorMessage = (errors[field.name] as FieldError)?.message;
+
+            return (
+              <GridFormInputGroup
+                error={errorMessage}
+                field={field}
+                key={field.name}
+                register={register}
+                setValue={value =>
+                  setValue(field.name, value as Values[string])
+                }
+              />
+            );
+          })}
+          <GridFormSubmit contents={submit.contents} />
+        </Row>
+      </Grid>
+    </Form>
+  );
+}
+
+export default GridForm;
