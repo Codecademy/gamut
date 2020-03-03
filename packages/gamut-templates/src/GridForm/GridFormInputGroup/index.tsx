253acadc5d0775386f48d9290bda77d112c6aa54
Josh Goldberg <joshuakgoldberg@outlook.com>
2020-03-03 13:14:43 -0500

    feat: added GridForm component (#605)
    * stub GridForm template
    
    * stub story
    
    * Casing
    
    * WIP: basic structure
    
    * Fleshed out grid
    
    * Classic
    
    * Add missing register back in
    
    * Added unit tests, mostly
    
    * Corrected import path
    
    * getValues() is redundant
    
    * Aha, got the unit tests to work: by updating test libs to latest
    
    * Remove unnecessary setup-after-env
    
    * Also an unused styles.module.scss
    
    * Deduplicated types/react
    
    * Added a newly necessary cast, why not
    
    * Bumped TypeScript to 3.7
    
    * Updated Prettier to latest
    
    * I think this is the last of the updates...
    
    * FINALLY, Prettier changes
    
    * Remove addon-storyshots from root package.json
    
    * Exported ower level components
    
    * Better explained GridForm in story
    
    * Updated submit children name to be contents
    
    * Quick little compile error
    
    Co-authored-by: Roger Vera <roger@codecademy.com>

---
+++ packages/gamut-templates/src/GridForm/GridFormInputGroup/index.tsx
@@ -0,0 +1,61 @@
+import {
+  Col,
+  FormError,
+  FormGroup,
+  FormGroupLabel,
+  Item,
+} from '@codecademy/gamut';
+import React from 'react';
+import { FormContextValues } from 'react-hook-form';
+
+import { GridFormField } from '../types';
+import GridFormTextInput from './GridFormTextInput';
+import GridFormSelectInput from './GridFormSelectInput';
+import styles from './styles.module.scss';
+
+export type GridFormInputGroupProps = {
+  error?: string;
+  field: GridFormField;
+  register: FormContextValues['register'];
+  setValue: (value: string) => void;
+};
+
+export const GridFormInputGroup: React.FC<GridFormInputGroupProps> = ({
+  error,
+  field,
+  register,
+  setValue,
+}) => {
+  const input =
+    field.type === 'select' ? (
+      <GridFormSelectInput
+        className={styles.gridFormInput}
+        field={field}
+        register={register}
+        setValue={setValue}
+      />
+    ) : (
+      <GridFormTextInput
+        className={styles.gridFormInput}
+        field={field}
+        register={register}
+        setValue={setValue}
+      />
+    );
+
+  return (
+    <Col {...field.sizing}>
+      <Item>
+        <FormGroup>
+          <FormGroupLabel className={styles.formGroupLabel}>
+            {field.label}
+          </FormGroupLabel>
+          {error && <FormError>{error}</FormError>}
+          {input}
+        </FormGroup>
+      </Item>
+    </Col>
+  );
+};
+
+export default GridFormInputGroup;
--- packages/gamut/src/Menus/MenuItem/__tests__/MenuItem-test.tsx
+++ packages/gamut/src/Menus/MenuItem/__tests__/MenuItem-test.tsx
@@ -5,12 +5,12 @@ import MenuItem from '../index';
 import s from '../styles.module.scss';
 
 describe('MenuItem', () => {
-  test.each([[false, false], [true, true]])(
-    'selected class is %d when selected is %d',
-    (selected, hasClass) => {
-      const component = mount(<MenuItem selected={selected} />);
+  test.each([
+    [false, false],
+    [true, true],
+  ])('selected class is %d when selected is %d', (selected, hasClass) => {
+    const component = mount(<MenuItem selected={selected} />);
 
-      expect(component.find('li').hasClass(s.selected)).toBe(hasClass);
-    }
-  );
+    expect(component.find('li').hasClass(s.selected)).toBe(hasClass);
+  });
 });
