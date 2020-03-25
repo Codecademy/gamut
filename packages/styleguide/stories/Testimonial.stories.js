64f211d71c8e1b933355f72151b3ffe2427d1e58
Roger Vera <roger@codecademy.com>
2020-03-25 10:57:21 -0400

    feat: add medium and large sizes to testimonial
    * use our first real learner testimonial as example
    
    * use auto instead of max-content for responsiveness
    
    * update component and story
    
    * create medium size component
    
    * create large component and styling for >large views
    
    * add background shape to large container
    
    * have large and medium classNames for large testimonial responsiveness
    
    * responsive styling for avatar
    
    * add additional tests
    
    * replace child selectors with descendant selectors
    
    * use selector list
    
    * pass custom classname to avatar for large sizing
    
    * fix test

--- packages/styleguide/stories/Testimonial.stories.js
+++ packages/styleguide/stories/Testimonial.stories.js
@@ -9,18 +9,32 @@ export default {
   decorators: [withKnobs],
 };
 
-const testimonial = {
-  name: 'Angela Davis',
-  occupation: 'Activist, Scholar, Writer',
+const laceyTestimonialProps = {
+  name: 'Lacey Bathala',
+  occupation: 'Data Analyst',
+  short_quote: "Coding isn't rocket science, it’s just falsely intimidating.",
+  long_quote:
+    'If it weren’t for Codecademy, I’d probably be in my old job. Codecademy enabled me to learn quick lessons between calls, on my lunch break, while multitasking. It was fun and easy and I went from 0 coding skills to a promotion within 6 months.',
   imageUrl: 'https://content.codecademy.com/courses/free/boba.svg',
-  quote:
-    'I am no longer accepting the things I cannot change. I am changing the things I cannot accept.',
 };
 
-export const testimonialSmall = () => (
+const josephineTestimonialProps = {
+  name: 'Josephine Anderson-Weber',
+  occupation: 'Junior Back-End Developer',
+  short_quote:
+    'Codecademy’s small lessons really help a person who has trouble focusing on long lessons.',
+  long_quote:
+    'The Codecademy quizzes and projects were so helpful. The quizzes test your knowledge and the projects reinforce what you learned by making you apply that knowledge to real world situations.',
+  imageUrl: 'https://content.codecademy.com/courses/free/boba.svg',
+};
+
+export const testimonialSmallWithAvatar = () => (
   <Testimonial
-    testimonial={testimonial}
-    size={select('size', ['small'], 'small')}
+    name={laceyTestimonialProps.name}
+    occupation={laceyTestimonialProps.occupation}
+    quote={laceyTestimonialProps.short_quote}
+    imageUrl={laceyTestimonialProps.imageUrl}
+    size="small"
     theme={select(
       'theme',
       [VisualTheme.DarkMode, VisualTheme.LightMode],
@@ -29,6 +43,98 @@ export const testimonialSmall = () => (
   />
 );
 
-testimonialSmall.story = {
-  name: 'Testimonial',
+testimonialSmallWithAvatar.story = {
+  name: 'Testimonial in size small (with Avatar)',
+};
+
+export const testimonialSmallWithoutAvatar = () => (
+  <Testimonial
+    name={laceyTestimonialProps.name}
+    occupation={laceyTestimonialProps.occupation}
+    quote={laceyTestimonialProps.short_quote}
+    size="small"
+    theme={select(
+      'theme',
+      [VisualTheme.DarkMode, VisualTheme.LightMode],
+      VisualTheme.DarkMode
+    )}
+  />
+);
+
+testimonialSmallWithoutAvatar.story = {
+  name: 'Testimonial in size small (without Avatar)',
+};
+
+export const testimonialMediumWithAvatar = () => (
+  <Testimonial
+    name={laceyTestimonialProps.name}
+    occupation={laceyTestimonialProps.occupation}
+    quote={laceyTestimonialProps.long_quote}
+    imageUrl={laceyTestimonialProps.imageUrl}
+    size="medium"
+    theme={select(
+      'theme',
+      [VisualTheme.DarkMode, VisualTheme.LightMode],
+      VisualTheme.DarkMode
+    )}
+  />
+);
+
+testimonialMediumWithAvatar.story = {
+  name: 'Testimonial in size medium (with Avatar)',
+};
+
+export const testimonialMediumWithoutAvatar = () => (
+  <Testimonial
+    name={laceyTestimonialProps.name}
+    occupation={laceyTestimonialProps.occupation}
+    quote={laceyTestimonialProps.long_quote}
+    size="medium"
+    theme={select(
+      'theme',
+      [VisualTheme.DarkMode, VisualTheme.LightMode],
+      VisualTheme.DarkMode
+    )}
+  />
+);
+
+testimonialMediumWithoutAvatar.story = {
+  name: 'Testimonial in size medium (without Avatar)',
+};
+
+export const testimonialLargeWithAvatar = () => (
+  <Testimonial
+    name={josephineTestimonialProps.name}
+    occupation={josephineTestimonialProps.occupation}
+    quote={josephineTestimonialProps.long_quote}
+    imageUrl={josephineTestimonialProps.imageUrl}
+    size="large"
+    theme={select(
+      'theme',
+      [VisualTheme.DarkMode, VisualTheme.LightMode],
+      VisualTheme.DarkMode
+    )}
+  />
+);
+
+testimonialLargeWithAvatar.story = {
+  name: 'Testimonial in size large (with Avatar)',
+};
+
+export const testimonialLargeWithoutAvatar = () => (
+  <Testimonial
+    name={josephineTestimonialProps.name}
+    occupation={josephineTestimonialProps.occupation}
+    quote={josephineTestimonialProps.long_quote}
+    size="large"
+    theme={select(
+      'theme',
+      [VisualTheme.DarkMode, VisualTheme.LightMode],
+      VisualTheme.DarkMode
+    )}
+  />
+);
+
+testimonialLargeWithoutAvatar.story = {
+  name: 'Testimonial in size large (without Avatar)',
 };
