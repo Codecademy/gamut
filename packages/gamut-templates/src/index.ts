003f074493e9e1004376c8ec16824f6da9f7e302
Josh Goldberg <joshuakgoldberg@outlook.com>
2020-04-02 15:29:33 -0400

    Also export all as in non-Gamut packages (#689)

--- packages/gamut-templates/src/index.ts
+++ packages/gamut-templates/src/index.ts
@@ -1,3 +1,3 @@
-export { default as Interstitial } from './Interstitial';
-export { default as GridForm } from './GridForm';
-export { default as SplitInterstitial } from './SplitInterstitial';
+export * from './Interstitial';
+export * from './GridForm';
+export * from './SplitInterstitial';
