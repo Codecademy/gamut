40d0cedaeb7eff722cf4f97c55e7c53be6fb9bb3
Josh Goldberg <joshuakgoldberg@outlook.com>
2020-04-02 15:29:33 -0400

    Also export all as in non-Gamut packages (#689)

--- packages/brand-components/src/index.ts
+++ packages/brand-components/src/index.ts
@@ -1,4 +1,4 @@
-export { default as Avatar } from './Avatar';
-export { default as Byline } from './Byline';
-export { default as Quote } from './Quote';
-export { default as Testimonial } from './Testimonial';
+export * from './Avatar';
+export * from './Byline';
+export * from './Quote';
+export * from './Testimonial';
