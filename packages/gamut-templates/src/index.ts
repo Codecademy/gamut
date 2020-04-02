55cb0514876766876cc9606a12623b4a84cceae8
Jasmine English <jasmine@codecademy.com>
2020-04-02 18:26:39 -0400

    changes from another branch
    just doing it here for easy merging later

--- packages/gamut-templates/src/index.ts
+++ packages/gamut-templates/src/index.ts
@@ -1,4 +1,4 @@
-export { default as Interstitial } from './Interstitial';
-export { default as GridForm } from './GridForm';
-export { default as SplitInterstitial } from './SplitInterstitial';
-export { default as Modal } from './Modal';
+export * from './GridForm';
+export * from './Interstitial';
+export * from './Modal';
+export * from './SplitInterstitial';
