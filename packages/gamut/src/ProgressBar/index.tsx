74ae5ae469d32edc1a177322353b27040f67d7c0
Josh Goldberg <joshuakgoldberg@outlook.com>
2020-04-10 10:53:01 -0400

    Switched ProgressBar's displayLabel prop name to be large (#706)

--- packages/gamut/src/ProgressBar/index.tsx
+++ packages/gamut/src/ProgressBar/index.tsx
@@ -9,7 +9,7 @@ export type ProgressBarProps = {
   /**
    * Whether to increase size and display the percentage as text.
    */
-  displayLabel?: boolean;
+  large?: boolean;
 
   /**
    * How much of the bar to fill in, as a number in [0, 100].
@@ -27,12 +27,12 @@ export type ProgressBarStyle = {
 
 export const ProgressBar: React.FC<ProgressBarProps> = ({
   className,
-  displayLabel,
+  large,
   percent,
   style,
 }) => {
   const { backgroundColor, barColor, fontColor } = style;
-  const height = displayLabel ? 36 : 6;
+  const height = large ? 36 : 6;
   const radius = `${height / 2}px`;
   const visualPercent = `${percent}%`;
 
@@ -53,13 +53,13 @@ export const ProgressBar: React.FC<ProgressBarProps> = ({
         style={{
           background: barColor,
           width: visualPercent,
-          ...(displayLabel && {
+          ...(large && {
             borderTopRightRadius: radius,
             borderBottomRightRadius: radius,
           }),
         }}
       >
-        {displayLabel && (
+        {large && (
           <span className={styles.displayedPercent}>{visualPercent}</span>
         )}
       </div>
