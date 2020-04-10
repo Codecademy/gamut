761279da9942507513a5f68284d42361d56ab616
Mike Boyle <mikeboyle@users.noreply.github.com>
2020-04-09 22:44:34 -0400

    feat: Add file input and text area to GridForm (#709)
    * [RRT-89] Add file input to GridForm
    
    * [RRT-89] Add textarea to GridForm
    
    * [RRT-89] Fix textarea typescript issue
    
    * [RRT-89] Address remaining tsc errors in my code
    
    * [RRT-89] Enhance GridFormSelect option types to match Select
    
    * [RRT-89] Remove import from /src
    
    * [RRT-89] Allow GridForm select to validate for required input
    
    * [RRT-89] Resolve prepub tsc issues
    
    * Oops, sorry, missed an import
    
    * Aaaand another
    
    * [RRT-89] Add GridForm unit tests
    
    Co-authored-by: Josh Goldberg <josh@codecademy.com>

--- packages/gamut/src/GridForm/__tests__/stubs.ts
+++ packages/gamut/src/GridForm/__tests__/stubs.ts
@@ -1,7 +1,9 @@
 import {
   GridFormCheckboxField,
+  GridFormFileField,
   GridFormSelectField,
   GridFormTextField,
+  GridFormTextAreaField,
 } from '../types';
 
 export const stubCheckboxField: GridFormCheckboxField = {
@@ -11,15 +13,28 @@ export const stubCheckboxField: GridFormCheckboxField = {
   type: 'checkbox',
 };
 
+export const stubSelectOptions: string[] = ['aaa', 'bbb'];
+
 export const stubSelectField: GridFormSelectField = {
   label: 'Stub Select',
-  options: ['aaa', 'bbb'],
+  options: stubSelectOptions,
   name: 'stub-select',
   type: 'select',
 };
-
 export const stubTextField: GridFormTextField = {
   label: 'Stub Text',
   name: 'stub-text',
   type: 'text',
+};
+
+export const stubFileField: GridFormFileField = {
+  label: 'Stub File',
+  name: 'stub-file',
+  type: 'file',
+};
+
+export const stubTextareaField: GridFormTextAreaField = {
+  label: 'Stub Textarea',
+  name: 'stub-textarea',
+  type: 'textarea',
 };
