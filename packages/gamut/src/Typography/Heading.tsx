08a6d114789deddc3fb9c4e7fc5e12f70ad3606b
Aaron Robb <aaron@codecademy.com>
2020-04-13 18:14:11 -0400

    Updates usages and adds prop comments

--- packages/gamut/src/Typography/Heading.tsx
+++ packages/gamut/src/Typography/Heading.tsx
@@ -10,40 +10,37 @@ export type HeadingTags = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
 export type FontSizes = 'xxs' | 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'xxl';
 export type AllowedStyles = { color?: string };
 
-type HeadingProps = {
+export type HeadingProps = {
+  /** Heading tags h1 - h6 */
   as: HeadingTags;
+  /** A font-size/font-family pair */
   fontSize: ResponsiveProperty<FontSizes>;
+  /** Allows you to pass color attributes directly to the tag */
   style?: AllowedStyles;
+  /** Toggle default heading margin */
   hideMargin?: boolean;
 } & ContainerElementProps;
 
-export const Heading: React.FC<HeadingProps> = React.memo(
-  ({
-    children,
-    as: Element,
-    className,
-    fontSize = 'md',
-    style,
-    testId,
-    hideMargin = false,
-  }) => {
-    return (
-      <Element
-        className={cx(
-          s.heading,
-          className,
-          generateClassnames({ fontSize }, s),
-          {
-            [s.hideMargin]: hideMargin,
-          }
-        )}
-        data-testid={testId}
-        style={style}
-      >
-        {children}
-      </Element>
-    );
-  }
-);
+export const Heading: React.FC<HeadingProps> = ({
+  children,
+  as: Element,
+  className,
+  fontSize = 'md',
+  style,
+  testId,
+  hideMargin = false,
+}) => {
+  return (
+    <Element
+      className={cx(s.heading, className, generateClassnames({ fontSize }, s), {
+        [s.hideMargin]: hideMargin,
+      })}
+      data-testid={testId}
+      style={style}
+    >
+      {children}
+    </Element>
+  );
+};
 
 export default Heading;
--- packages/styleguide/stories/Atoms/Heading.stories.tsx
+++ packages/styleguide/stories/Atoms/Heading.stories.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
 import {
-  Heading,
   FontSizes,
   HeadingTags,
   LayoutGrid,
   Column,
 } from '@codecademy/gamut/src';
 
+import { Heading } from '../../../gamut/src/Typography/Heading';
 import {
   decoratedStory,
   StoryDescription,
@@ -15,7 +15,7 @@ import {
   StoryTemplate,
 } from '../Templating';
 
-export default decoratedStory('Atoms', 'Heading');
+export default decoratedStory('Atoms', Heading);
 
 const tags: HeadingTags[] = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
 const sizes: FontSizes[] = ['xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs'];
@@ -25,7 +25,22 @@ export const heading = () => (
     <Column size={12}>
       <StoryTemplate status={StoryStatus.InProgress}>
         <StoryDescription>
-          Do whatever you like with these badass new Headings
+          <p>
+            This is a single primitive to handle all <strong>h1-h6</strong> tags
+            with defined size intervals to allow for semantic html that also
+            matches the designs.
+          </p>
+          <p>
+            Note: Please only{' '}
+            <a
+              href="https://dequeuniversity.com/rules/axe/3.0/heading-order"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              increase heading levels by 1
+            </a>{' '}
+            (and picking the appropriate size to match the design).
+          </p>
         </StoryDescription>
       </StoryTemplate>
     </Column>
