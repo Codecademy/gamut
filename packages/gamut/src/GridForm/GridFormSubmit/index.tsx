effdcdbbf31cee839171af975c9ee679781d41e7
Mike Boyle <mboyle@codecademy.com>
2020-04-14 14:08:19 -0400

    [RRT-89] Remove disabled from GridForm submit

--- packages/gamut/src/GridForm/GridFormSubmit/index.tsx
+++ packages/gamut/src/GridForm/GridFormSubmit/index.tsx
@@ -5,18 +5,16 @@ import { Column, ColumnSizes, ResponsiveProperty } from '../../Layout';
 
 export type GridFormSubmitProps = {
   contents: React.ReactNode;
-  disabled?: boolean;
   size?: ResponsiveProperty<ColumnSizes>;
 };
 
 export const GridFormSubmit: React.FC<GridFormSubmitProps> = ({
   contents,
-  disabled,
   size,
 }) => {
   return (
     <Column size={size}>
-      <Button disabled={disabled} theme="brand-purple" type="submit">
+      <Button theme="brand-purple" type="submit">
         {contents}
       </Button>
     </Column>
