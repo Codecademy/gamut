6b0f2a803ace62b0e581ba4ebf5d632cb5addb02
Aaron Robb <52927224+codecaaron@users.noreply.github.com>
2020-04-21 10:38:13 -0400

    Exports responsive types from the root (#742)

--- jest.config.js
+++ jest.config.js
@@ -15,6 +15,7 @@ module.exports = {
     'scss',
     'ts',
     'tsx',
+    'd.ts',
   ],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|md)$':
