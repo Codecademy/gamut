d6efbbc1a87430642b6effbe2f82c598d0421c9e
Aaron Robb <52927224+codecaaron@users.noreply.github.com>
2020-04-21 17:47:42 -0400

    feat(Styleguide): DecoratedStories Props and better Typography Docs (#746)
    * Add decoratedStories dynamic nesting
    
    * Reorganize stories for more docgen.

--- packages/styleguide/stories/Foundations/Typography/Heading.stories.tsx
+++ packages/styleguide/stories/Foundations/Typography/Heading.stories.tsx
@@ -5,26 +5,22 @@ import {
   HeadingTags,
   LayoutGrid,
   Column,
-  Text,
   Heading,
 } from '@codecademy/gamut/src';
-
 import {
-  decoratedStory,
-  StoryDescription,
-  StoryStatus,
-  StoryTemplate,
   decoratedStories,
-} from '../Templating';
-
-export default decoratedStories('Foundations', 'Typography');
+  decoratedStory,
+  StoryTemplate,
+  StoryStatus,
+  StoryDescription,
+} from '../../Templating';
 
 const headingTags: HeadingTags[] = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
 const headingSizes: FontSizes[] = ['xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs'];
-const textTags: ('p' | 'span')[] = ['p', 'span'];
-const textSizes: ('sm' | 'lg')[] = ['lg', 'sm'];
 
-export const baseTypography = () => (
+export default decoratedStories('Foundations', 'Typography', Heading);
+
+export const headingSizePairings = decoratedStory(() => (
   <StoryTemplate status={StoryStatus.InProgress}>
     <StoryDescription>
       Responsive Typography primitives for all your textual needs.
@@ -56,35 +52,9 @@ export const baseTypography = () => (
           </Heading>
         ))}
       </Column>
-      <Column size={12}>
-        <Heading as="h2" fontSize="lg">
-          Text
-        </Heading>
-        <p>
-          This is a single primitive to handle all <strong>{'<p>'}</strong> and{' '}
-          <strong>{'<span>'}</strong> tags with defined size intervals
-        </p>
-        {textSizes.map(size => (
-          <React.Fragment key={size}>
-            <Heading fontSize="md" as="h3">
-              Size: {size}
-            </Heading>
-            <Text key={size} fontSize={size} as={textTags[0]}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </Text>
-            <br />
-          </React.Fragment>
-        ))}
-      </Column>
     </LayoutGrid>
   </StoryTemplate>
-);
+));
 
 export const colorCustomization = decoratedStory(() => (
   <StoryTemplate status={StoryStatus.InProgress}>
@@ -96,9 +66,6 @@ export const colorCustomization = decoratedStory(() => (
         <Heading fontSize="md" as="h3" style={{ color: colors.royalBlue }}>
           Royal Blue
         </Heading>
-        <Text as="p" style={{ color: colors.royalBlue }}>
-          Royal Blue
-        </Text>
       </Column>
     </LayoutGrid>
   </StoryTemplate>
@@ -112,13 +79,13 @@ export const collapsableMargin = decoratedStory(() => (
         <Heading fontSize="md" as="h3">
           With Margin
         </Heading>
-        <Text as="p">I am body text</Text>
+        <p>Random text</p>
       </Column>
       <Column size={12}>
         <Heading fontSize="md" as="h3" hideMargin>
           Sans margin
         </Heading>
-        <Text as="p">I am body text</Text>
+        <p>Random text</p>
       </Column>
     </LayoutGrid>
   </StoryTemplate>
