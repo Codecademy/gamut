5a4b7a30ceab4117eb83c4a915dd456b6d7df8eb
rogervera <roger@codecademy.com>
2020-04-29 11:30:45 -0400

    implement PR feedback

--- packages/styleguide/stories/Brand/RobotoMono/RobotoMono.stories.tsx
+++ packages/styleguide/stories/Brand/RobotoMono/RobotoMono.stories.tsx
@@ -15,21 +15,16 @@ import {
 
 export default decoratedStories('Brand', RobotoMono);
 
-export const quote = decoratedStory(() => (
+export const robotoMono = decoratedStory(() => (
   <StoryTemplate status={StoryStatus.Ready}>
     <StoryDescription>
       A wrapper component that will style text with the
-      <RobotoMono>
-        <span> Robot Mono </span>
-      </RobotoMono>
+      <RobotoMono> Robot Mono</RobotoMono>
       font. Currently, Codecademy uses
       <span className={styles.monacoFont}> Monaco </span> as our default
       monospaced font. However, there are a few brand related instances where we
       are experimenting with using
-      <RobotoMono>
-        <span> Roboto Mono</span>
-      </RobotoMono>
-      .
+      <RobotoMono> Roboto Mono</RobotoMono>.
     </StoryDescription>
     <RobotoMono>
       <Heading fontSize="xl" as="h1">
