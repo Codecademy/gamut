3f41124ef04e911eb68f91a6979a4b5ad71e1aa2
Aaron Robb <aaron@codecademy.com>
2020-05-11 17:09:45 -0400

    Merge difficulties and add standalone formatting

--- packages/styleguide/.storybook/preview.js
+++ packages/styleguide/.storybook/preview.js
@@ -1,5 +1,9 @@
 import { addParameters } from '@storybook/react';
 import { create } from '@storybook/theming';
+import prettier from 'prettier/standalone';
+import prettierConfig from '@codecademy/prettier-config';
+import parserTypescript from 'prettier/parser-typescript';
+
 import './decorators/wrapper';
 
 const gamutTheme = create({
@@ -32,5 +36,17 @@ addParameters({
     config: {},
     options: {},
     manual: true,
+  },
+  docs: {
+    /** Currently this is the way to do this with MDX
+     * https://github.com/storybookjs/storybook/issues/8078#issuecomment-605430645
+     */
+    transformSource: (src, storyId) => {
+      return prettier.format(src, {
+        ...prettierConfig,
+        parser: 'typescript',
+        plugins: [parserTypescript],
+      });
+    },
   },
 });
