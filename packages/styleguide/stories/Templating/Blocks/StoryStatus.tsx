2cc76fc49c037d2fbe8e2385c309821068b47f01
Aaron Robb <aaron@codecademy.com>
2020-05-11 11:55:20 -0400

    Use stoplight for usage

--- packages/styleguide/stories/Templating/Blocks/StoryStatus.tsx
+++ packages/styleguide/stories/Templating/Blocks/StoryStatus.tsx
@@ -1,32 +1,54 @@
 import React from 'react';
 import { styled } from '@storybook/theming';
+import { colors } from '@codecademy/gamut-styles/utils/variables';
+
+const Statuses = {
+  ready: { message: 'Production Ready', color: colors.green[500] },
+  inProgress: { message: 'Use with Caution (WIP)', color: colors.yellow[500] },
+  deprecated: { message: 'Do Not Use (Deprecated)', color: colors.red[500] },
+};
 
 const StatusWrapper = styled.div`
   margin: 1rem 0;
   font-size: 1.4rem;
+  display: grid;
+  grid-template-columns: max-content max-content 1fr;
+  grid-gap: 0.5rem;
+  align-items: center;
 `;
 
-const StatusMessage = styled.span`
+const StatusIndicator = styled.div<{ color: string }>`
+  display: inline-block;
+  position: relative;
   font-size: 1.2rem;
   font-weight: 400;
+  width: 1rem;
+  height: 1rem;
+  border-radius: 50%;
+  margin-top: 0.2rem;
+  background-color: ${({ color }) => color};
+
+  &:hover {
+    & + * {
+      opacity: 1;
+    }
+  }
 `;
 
-const Statuses = {
-  ready: { message: 'Production Ready', icon: '✅' },
-  inProgress: { message: 'Not Production Ready', icon: '❌' },
-  deprecated: { message: 'Deprecated, do not use', icon: '💀' },
-};
+const StatusMessage = styled.div`
+  opacity: 0;
+  transition: 0.25s opacity;
+  font-size: 1rem;
+`;
 
 export const StoryStatus: React.FC<{ status: keyof typeof Statuses }> = ({
   status,
 }) => {
-  const { message, icon } = Statuses[status];
+  const { message, color } = Statuses[status];
   return (
     <StatusWrapper>
-      Usage Status:{' '}
-      <StatusMessage>
-        {icon} - {message}
-      </StatusMessage>
+      Usage - <StatusIndicator color={color} />
+      <StatusMessage>{message}</StatusMessage>
     </StatusWrapper>
   );
 };
