089dcbac7a88e3c3141ab45e749481b73de119ac
Aaron Robb <52927224+codecaaron@users.noreply.github.com>
2020-05-13 11:31:36 -0400

    feat(Styleguide): Convert All Stories to MDX (#788)
    * Converts all CSF to MDX with the exception of (Tabs which is currently incompatible with MDX).
    
    * Updated Stories: Added new discrete stories for specific components
    
    * Config Cleanup: Cleans up storybook config adding baseline decorators and removing unused options.
    
    * File Structure: Introduces Storybook Category / Folder Structure.
    
    * Storybook Blocks: Converts existing Storybook Native components to Blocks.
    
    Co-authored-by: cstatro <45748761+cstatro@users.noreply.github.com>
    Co-authored-by: Josh Goldberg <joshuakgoldberg@outlook.com>

---
+++ packages/styleguide/stories/Core/Foundations/Typography/constants.tsx
@@ -0,0 +1,11 @@
+import { FontSizes, TextSizes } from '@codecademy/gamut/src';
+export const headingSizes: FontSizes[] = [
+  'xxl',
+  'xl',
+  'lg',
+  'md',
+  'sm',
+  'xs',
+  'xxs',
+];
+export const textSizes: TextSizes[] = ['lg', 'md', 'sm'];
