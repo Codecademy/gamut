fdf1a0fc510c53c750ad86d360d5c017bcadfbbf
Neil Daftary <ndaftary@codecademy.com>
2020-05-19 18:44:14 -0400

    Fix lint.

--- packages/gamut/src/utils/pickDataProps.ts
+++ packages/gamut/src/utils/pickDataProps.ts
@@ -1,4 +1,4 @@
 import { pickBy } from 'lodash';
 
 export const pickDataProps = (props: {}) =>
-  pickBy(props, (value, key) => /^data-/.test(key));
+  pickBy(props, (value, key: string) => key.startsWith('data-'));
