65102c7627e9475a1701d0afc66d1fc14e89e9b7
Jake Hiller <jakemhiller@gmail.com>
2020-05-19 00:18:01 -0400

    add rtl tests for modal and overlay

--- packages/gamut/src/Overlay/__tests__/Overlay-test.tsx
+++ packages/gamut/src/Overlay/__tests__/Overlay-test.tsx
@@ -1,33 +1,76 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { render, fireEvent, screen } from '@testing-library/react';
 
-import Overlay from '..';
+import Overlay, { OverlayProps } from '..';
+
+const renderOverlay = (props?: Partial<OverlayProps>) => {
+  return render(
+    <Overlay {...props}>
+      <div data-testid="overlay-content">
+        Howdy!
+        <button type="button" />
+      </div>
+    </Overlay>
+  );
+};
+
+const overlayIsRendered = () => {
+  return Boolean(screen.queryByTestId('overlay-content'));
+};
 
 describe('Overlay', () => {
   it('renders null when isOpen is not true', () => {
-    const wrapper = mount(
-      <Overlay>
-        <div>
-          Howdy!
-          <button type="button" />
-        </div>
-      </Overlay>
-    );
+    renderOverlay();
 
-    expect(wrapper.isEmptyRender()).toBe(true);
+    expect(overlayIsRendered()).toBeFalsy();
   });
 
   it('renders children when isOpen is true', () => {
-    const children = 'Howdy!';
-    const wrapper = mount(
-      <Overlay isOpen>
-        <div>
-          {children}
-          <button type="button" />
-        </div>
-      </Overlay>
-    );
+    renderOverlay({ isOpen: true });
+    expect(overlayIsRendered()).toBeTruthy();
+  });
 
-    expect(wrapper.text()).toEqual(children);
+  it('triggers onRequestClose callback when escape key is triggered and escapeCloses is true', () => {
+    const onRequestClose = jest.fn();
+    const { baseElement } = renderOverlay({
+      isOpen: true,
+      onRequestClose,
+    });
+    fireEvent.keyDown(baseElement, { key: 'Escape', code: 'Escape' });
+    expect(onRequestClose.mock.calls.length).toBe(1);
+  });
+
+  it('does not trigger onRequestClose callback when escape key is triggered and escapeCloses is false', () => {
+    const onRequestClose = jest.fn();
+    const { baseElement } = renderOverlay({
+      isOpen: true,
+      escapeCloses: false,
+      onRequestClose,
+    });
+    fireEvent.keyDown(baseElement, { key: 'Escape', code: 'Escape' });
+    expect(onRequestClose.mock.calls.length).toBe(0);
+  });
+
+  it('triggers onRequestClose callback when clicking outside', () => {
+    const onRequestClose = jest.fn();
+    const { baseElement } = renderOverlay({
+      isOpen: true,
+      clickOutsideCloses: true,
+      onRequestClose,
+    });
+    // focus-trap listens to mouseDown, not click
+    fireEvent.mouseDown(baseElement);
+    expect(onRequestClose.mock.calls.length).toBe(1);
+  });
+
+  it('does not trigger onRequestClose callback when clicking inside', () => {
+    const onRequestClose = jest.fn();
+    renderOverlay({
+      isOpen: true,
+      clickOutsideCloses: true,
+      onRequestClose,
+    });
+    fireEvent.mouseDown(screen.queryByTestId('overlay-content'));
+    expect(onRequestClose.mock.calls.length).toBe(0);
   });
 });
