afa8b2f030f248be0a0cb29525bb3e5eb01d9a4e
Jake Hiller <jakemhiller@gmail.com>
2020-05-20 11:27:06 -0400

    chore: Add RTL tests for modal and overlay (#808)
    * add rtl tests for modal and overlay
    
    * update tests to match new props
    
    * cleanup types

--- packages/gamut/src/Modal/__tests__/Modal-test.tsx
+++ packages/gamut/src/Modal/__tests__/Modal-test.tsx
@@ -1,60 +1,75 @@
 import React from 'react';
-import { mount } from 'enzyme';
-import { Overlay } from '../../Overlay';
-import Modal from '..';
+import { render, fireEvent, screen } from '@testing-library/react';
+import Modal, { ModalProps } from '..';
+
+const renderModal = (props?: Partial<ModalProps>) => {
+  return render(
+    <Modal isOpen onRequestClose={() => {}} {...props}>
+      <div data-testid="modal-content">{props?.children}</div>
+    </Modal>
+  );
+};
 
 describe('Modal', () => {
   it('renders children when isOpen is true', () => {
     const children = 'Hey';
-    const wrapper = mount(<Modal isOpen>{children}</Modal>);
-
-    expect(wrapper.text()).toContain(children);
+    const { baseElement } = renderModal({ children });
+    expect(baseElement).toHaveTextContent(children);
   });
 
   it('does not render when isOpen is false', () => {
-    const wrapper = mount(<Modal isOpen={false}>Hey</Modal>);
-
-    expect(wrapper.isEmptyRender()).toBe(true);
+    const children = 'Hey';
+    const { baseElement } = renderModal({ isOpen: false, children });
+    expect(baseElement).not.toHaveTextContent(children);
   });
 
   it('does not render its close button if hideDefaultCloseButton is true', () => {
-    const wrapper = mount(
-      <Modal isOpen hideDefaultCloseButton>
-        I got my own close button
-        <button type="button" />
-      </Modal>
-    );
+    renderModal({
+      hideDefaultCloseButton: true,
+      children: <button type="button">Click Me</button>,
+    });
 
     expect(
-      wrapper.find(`[data-testid="modal-default-close-button"]`)
-    ).toHaveLength(0);
+      screen.queryByTestId('modal-default-close-button')
+    ).not.toBeInTheDocument();
   });
 
   it('renders its close button if hideDefaultCloseButton is false', () => {
-    const wrapper = mount(<Modal isOpen>I use the default close button</Modal>);
+    renderModal();
 
     expect(
-      wrapper.find(`[data-testid="modal-default-close-button"]`)
-    ).toHaveLength(1);
+      screen.queryByTestId('modal-default-close-button')
+    ).toBeInTheDocument();
   });
 
-  it('passes truthy clickOutsideCloses option to Overlay', () => {
-    const wrapper = mount(
-      <Modal clickOutsideCloses isOpen>
-        When you click the overlay I will close!
-      </Modal>
-    );
-
-    expect(wrapper.find(Overlay).props().clickOutsideCloses).toBeTruthy();
+  it('triggers onRequestClose callback when escape key is triggered', () => {
+    const onRequestClose = jest.fn();
+    const { baseElement } = renderModal({
+      isOpen: true,
+      onRequestClose,
+    });
+    fireEvent.keyDown(baseElement, { key: 'Escape', code: 'Escape' });
+    expect(onRequestClose.mock.calls.length).toBe(1);
   });
 
-  it('passes falsy clickOutsideCloses option to Overlay', () => {
-    const wrapper = mount(
-      <Modal clickOutsideCloses={false} isOpen>
-        When you click the overlay I will not close!
-      </Modal>
-    );
+  it('triggers onRequestClose callback when clicking outside when clickOutsideCloses is true', () => {
+    const onRequestClose = jest.fn();
+    const { baseElement } = renderModal({
+      isOpen: true,
+      onRequestClose,
+    });
+    // focus-trap listens to mouseDown, not click
+    fireEvent.mouseDown(baseElement);
+    expect(onRequestClose.mock.calls.length).toBe(1);
+  });
 
-    expect(wrapper.find(Overlay).props().clickOutsideCloses).toBeFalsy();
+  it('does not trigger onRequestClose callback when clicking inside', () => {
+    const onRequestClose = jest.fn();
+    renderModal({
+      isOpen: true,
+      onRequestClose,
+    });
+    fireEvent.mouseDown(screen.queryByTestId('modal-content'));
+    expect(onRequestClose.mock.calls.length).toBe(0);
   });
 });
