0920a529bbb534e3cfeff9e3b82431fe7b91a137
Aaron Robb <52927224+codecaaron@users.noreply.github.com>
2020-06-09 14:54:22 -0400

    Remove Unused Blocks / Helpers (#831)

--- packages/styleguide/Templating/StoryDescription/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import styles from './styles.module.scss';
-
-export const StoryDescription: React.FC = ({ children }) => {
-  return <div className={styles.description}>{children}</div>;
-};
