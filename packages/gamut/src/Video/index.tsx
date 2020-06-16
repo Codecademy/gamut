feea8bad838e13382e4edb883acef88b96e8baf4
Natalie Wee <natalieweesh@gmail.com>
2020-06-16 18:29:49 -0400

    feat(Video): a new video component

---
+++ packages/gamut/src/Video/index.tsx
@@ -0,0 +1,86 @@
+import React, { useRef } from 'react';
+import ReactPlayer from 'react-player';
+import { PlayIcon } from '@codecademy/gamut/src';
+import cx from 'classnames';
+import styles from './styles/index.module.scss';
+
+/** render the play button with an overlay */
+const OverlayPlayButton = () => {
+  return (
+    <div className={styles.overlay}>
+      <PlayIcon className={styles.hoverButton} />
+    </div>
+  );
+};
+
+type ContentfulAsset = {
+  id: string;
+  content_type?: string;
+  title: string;
+  url: string;
+};
+export type VideoProps = {
+  videoUrl: string;
+  videoTitle: string;
+  placeholderImage?: ContentfulAsset;
+  autoplay?: boolean;
+  controls?: boolean;
+  loop?: boolean;
+  muted?: boolean;
+  componentStyle?: string;
+};
+
+export const Video: React.FC<VideoProps> = ({
+  videoUrl,
+  videoTitle,
+  placeholderImage,
+  autoplay,
+  controls,
+  loop,
+  muted,
+  componentStyle,
+}) => {
+  const videoWrapper = useRef<HTMLDivElement>(null);
+  const checkUrl = (url: string) => {
+    if (url.length === 11) {
+      url = `https://www.youtube.com/watch?v=${url}`;
+    }
+    return url;
+  };
+  /**
+   * handle when the video loads; do it this way so we don't have a white flash
+   * when the video loads and we remove the loading BG
+   **/
+  const clearBackground = () => {
+    const el = videoWrapper.current;
+    if (!el) {
+      return;
+    }
+    el.className = cx([styles.videoWrapper, componentStyle]);
+  };
+  return (
+    <div
+      ref={videoWrapper}
+      className={cx([styles.videoWrapper, styles.loading, componentStyle])}
+    >
+      <ReactPlayer
+        url={checkUrl(videoUrl)}
+        light={placeholderImage?.url}
+        title={videoTitle}
+        playing={autoplay}
+        className={styles.iframe}
+        controls={controls}
+        loop={loop}
+        muted={muted}
+        playIcon={<OverlayPlayButton />}
+        onReady={() => window.setTimeout(() => clearBackground(), 500)}
+      />
+    </div>
+  );
+};
+
+Video.defaultProps = {
+  controls: true,
+};
+
+export default Video;
