4ad51904172033f30210b4c0ee5769d0cdecf9f2
Kip Price <4605379+kipprice@users.noreply.github.com>
2020-06-17 17:54:39 -0400

    update tests to verify coloring

--- packages/gamut/src/HighlightedText/__tests__/HighlightedText-test.tsx
+++ packages/gamut/src/HighlightedText/__tests__/HighlightedText-test.tsx
@@ -11,4 +11,19 @@ describe('HighlightedText', () => {
 
     expect(wrapped.text()).toEqual(text);
   });
+
+  it('defaults text color to yellow', () => {
+    const text = 'highlighted';
+    const wrapped = mount(<HighlightedText>{text}</HighlightedText>);
+    expect(wrapped.find('.yellow')).toHaveLength(1);
+  });
+
+  it('allows selecting a specific other color', () => {
+    const text = 'highlighted';
+    const wrapped = mount(
+      <HighlightedText color="blue">{text}</HighlightedText>
+    );
+    expect(wrapped.find('.blue')).toHaveLength(1);
+    expect(wrapped.find('.yellow')).toHaveLength(0);
+  });
 });
