6a1c87cb651bccbd332d3c60868740c44eb3bd37
Kip Price <4605379+kipprice@users.noreply.github.com>
2020-06-17 17:39:17 -0400

    create highlighted text color options

--- packages/gamut/src/HighlightedText/index.tsx
+++ packages/gamut/src/HighlightedText/index.tsx
@@ -3,19 +3,39 @@ import React, { HTMLAttributes } from 'react';
 
 import styles from './styles.module.scss';
 
+export type HighlightColors =
+  | 'red'
+  | 'orange'
+  | 'yellow'
+  | 'green'
+  | 'blue'
+  | 'purple'
+  | 'pink'
+  | 'beige'
+  | 'gray';
+
 export type HighlightedTextProps = {
+  /** String to transform into the contents of the highlighted text; cannot nest other elements */
   children?: string;
+
+  /** Standard element properties that should be applied to the indivudual words that are being highlighted */
   wordProps?: HTMLAttributes<HTMLSpanElement>;
+
+  /** Standard element properties that should be applied to the entire set of words */
   textProps?: HTMLAttributes<HTMLElement>;
+
+  /** The color that should display behind the text; if not provided, defaults to yellow */
+  color?: HighlightColors;
 };
 
 export const HighlightedText: React.FC<HighlightedTextProps> = ({
   children = '',
   textProps = {},
+  color = 'yellow',
 }) => {
   const combinedProps = {
     ...textProps,
-    className: cx(styles.highlightedText, textProps.className),
+    className: cx(styles.highlightedText, textProps.className, styles[color]),
   };
 
   const words = children
