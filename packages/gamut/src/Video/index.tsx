3465938199b5b1dd93d28f031a571db519a2b6b9
Natalie Wee <natalieweesh@gmail.com>
2020-06-25 09:54:39 -0400

    Update packages/gamut/src/Video/index.tsx
    Co-authored-by: Josh Goldberg <joshuakgoldberg@outlook.com>

--- packages/gamut/src/Video/index.tsx
+++ packages/gamut/src/Video/index.tsx
@@ -53,7 +53,7 @@ export const Video: React.FC<VideoProps> = ({
         muted={muted}
         playIcon={<OverlayPlayButton />}
         onReady={(player: ReactPlayer) => {
-          onReady && onReady(player);
+          onReady?.(player);
           setLoading(false);
         }}
         onPlay={onPlay}
