8e56222de6e31d33fe09c8f50040c023ca276c4a
Aaron Foster Brielyn <afbreilyn@codecademy.com>
2020-07-06 12:19:39 -0400

    Test drive out the passing of props into GridFormTextInput

--- packages/gamut/src/GridForm/GridFormInputGroup/GridFormTextInput/__tests__/GridFormTextInput-test.tsx
+++ packages/gamut/src/GridForm/GridFormInputGroup/GridFormTextInput/__tests__/GridFormTextInput-test.tsx
@@ -29,4 +29,58 @@ describe('GridFormTextInput', () => {
       expect(textInput.find('input#name').length).toBe(1);
     });
   });
+
+  describe('required fields', () => {
+    it('marks a field as required when a required validation boolean is passed', () => {
+      const textInput = mount(
+        <GridFormTextInput
+          field={{ ...stubTextField, validation: { required: true } }}
+          register={jest.fn()}
+        />
+      );
+
+      expect(textInput.find('input').props().required).toBeTruthy();
+    });
+
+    it('marks a field as required when a required message is passed', () => {
+      const textInput = mount(
+        <GridFormTextInput
+          field={{ ...stubTextField, validation: { required: 'Required' } }}
+          register={jest.fn()}
+        />
+      );
+
+      expect(textInput.find('input').props().required).toBeTruthy();
+    });
+
+    it('does __not__ mark a field as required when `required: false` is passed', () => {
+      const textInput = mount(
+        <GridFormTextInput
+          field={{ ...stubTextField, validation: { required: false } }}
+          register={jest.fn()}
+        />
+      );
+
+      expect(textInput.find('input').props().required).toBeFalsy();
+    });
+
+    it('does __not__ mark a field as required when required is not passed', () => {
+      const textInput = mount(
+        <GridFormTextInput
+          field={{
+            ...stubTextField,
+            validation: {
+              pattern: {
+                value: /[^@]+@[^@]+\.[^@]+/,
+                message: 'Email Invalid',
+              },
+            },
+          }}
+          register={jest.fn()}
+        />
+      );
+
+      expect(textInput.find('input').props().required).toBeFalsy();
+    });
+  });
 });
