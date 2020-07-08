77ea548e226fa431623f8d1d6ca68b9bf130571d
Aaron Robb <52927224+codecaaron@users.noreply.github.com>
2020-07-08 11:44:26 -0400

    Rebase (#865)

--- packages/styleguide/stories/Core/Foundations/Layouts/Elements/index.tsx
+++ packages/styleguide/stories/Core/Foundations/Layouts/Elements/index.tsx
@@ -2,14 +2,20 @@ import React from 'react';
 import { Item } from '@codecademy/gamut/src';
 import { colors } from '@codecademy/gamut-styles/utils/variables';
 
-export const Box = () => (
+export const Box: React.FC = ({ children }) => (
   <Item
     style={{
-      height: 30,
+      padding: '5px',
+      minHeight: '30px',
       marginBottom: '0.5rem',
       backgroundColor: colors.blue[500],
+      color: colors.white,
     }}
-  />
+    flex
+    alignSelf="stretch"
+  >
+    {children}
+  </Item>
 );
 
 export const defaultGridProps = {
