2aedae96dd379cca3ffbec605817e947ecd1907a
Aaron Robb <aaron@codecademy.com>
2020-07-08 14:54:27 -0400

    Add grouping

---
+++ packages/styleguide/stories/Core/Atoms/Icons/constants.tsx
@@ -0,0 +1,55 @@
+import { values, pick, pickBy } from 'lodash';
+import * as Icons from '@codecademy/gamut-icons/src';
+
+export const VENDOR = pick(
+  Icons,
+  [
+    'InstagramIcon',
+    'FacebookIcon',
+    'GithubIcon',
+    'GplusIcon',
+    'LinkedinIcon',
+    'TwitterIcon',
+    'YoutubeIcon',
+    'RedditIcon',
+    'MediumIcon',
+    'JavascriptIcon',
+    'PythonIcon',
+    'RailsIcon',
+    'RubyIcon',
+    'FaviconIcon',
+    'FaviconSolidIcon',
+    'JavaIcon',
+  ].sort()
+);
+
+export const LEARNING_ENVIRONMENT = pick(
+  Icons,
+  [
+    'LessonIcon',
+    'ProjectsIcon',
+    'NarrativeIcon',
+    'ObjectiveIcon',
+    'OverviewIcon',
+    'InstructionsIcon',
+    'ResponsiveIcon',
+    'LearnIcon',
+    'ArticleIcon',
+    'ForumsChatIcon',
+    'EditorIcon',
+    'QuizIcon',
+    'StreakIcon',
+    'FitnessDumbbellIcon',
+    'TrophyIcon',
+    'TerminalIcon',
+    'CertificateIcon',
+    'BadgeIcon',
+    'PathIcon',
+    'PathChangeIcon',
+    'PathCompleteIcon',
+  ].sort()
+);
+
+const specialIcons = values({ ...LEARNING_ENVIRONMENT, ...VENDOR });
+
+export const ALL_ICONS = pickBy(Icons, (icon) => !specialIcons.includes(icon));
