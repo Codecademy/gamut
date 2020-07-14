2e45ae7359b439104b1f43eb4e82cd4ed23f9570
Aaron Robb <aaron@codecademy.com>
2020-07-14 13:09:54 -0400

    Rebase from master

--- packages/styleguide/.storybook/utils.js
+++ packages/styleguide/.storybook/utils.js
@@ -1,10 +1,17 @@
-const headers = ['Core', 'Labs', 'Deprecated'];
-const folders = ['Foundations', 'Atoms', 'Molecules', 'Organisms'];
+const folders = ['Foundations', 'Layouts', 'Atoms', 'Molecules', 'Organisms'];
+const headers = ['Meta', ...folders, 'Labs', 'Deprecated'];
+const heirarchy = [headers, folders];
 
 /**
  * Modified from:
  * https://github.com/storybookjs/storybook/issues/6327#issuecomment-613122487
  */
+
+const sortByKind = (kinds, kindA, kindB) => {
+  const aIndex = kinds.findIndex((h) => h === kindA);
+  const bIndex = kinds.findIndex((h) => h === kindB);
+  return aIndex - bIndex;
+};
 
 export const storySort = (a, b) => {
   // a[1].kind is something like: Components|Accordion. Using "Components" for the headers array.
@@ -12,24 +19,11 @@ export const storySort = (a, b) => {
   const aKinds = a[1].kind.split('/');
   const bKinds = b[1].kind.split('/');
 
-  if (aKinds[0] !== bKinds[0]) {
-    // Comparing something like "components-accordion--main" to "getting-started-app--main".
-    const aHeaderIndex = headers.findIndex((h) => h === aKinds[0]);
-    const bHeaderIndex = headers.findIndex((h) => h === bKinds[0]);
-    return aHeaderIndex - bHeaderIndex;
-  }
-
-  if (aKinds[1] !== bKinds[1]) {
-    // Comparing something like "components-accordion--main" to "getting-started-app--main".
-    const aFolderIndex = folders.findIndex((h) => h === aKinds[1]);
-    const bFolderIndex = folders.findIndex((h) => h === bKinds[1]);
-
-    return aFolderIndex - bFolderIndex;
+  for (let i = 0; i < aKinds.length; i += 1) {
+    if (aKinds[i] !== bKinds[i] && heirarchy[i]) {
+      return sortByKind(heirarchy[i], aKinds[i], bKinds[i]);
+    }
   }
 
   return 0;
-  /* Or instead of `return 0` compare something like "components-accordion--small" to "components-accordion--large"
-   * and sort the stories inside each component...
-   */
-  // return a[1].id.localeCompare(b[1].id, undefined, { numeric: true });
 };
