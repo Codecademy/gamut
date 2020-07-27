a6fc1127db7771dd382a2b7b9f6538b39653ee7c
Aaron Robb <aaron@codecademy.com>
2020-07-27 13:57:04 -0400

    Fix linting

--- packages/gamut-styles/utils/variables/index.js
+++ packages/gamut-styles/utils/variables/index.js
@@ -1,4 +1,5 @@
 if (process.env.NODE_ENV === 'development') {
+  // eslint-disable-next-line no-console
   console.warn(
     'Importing from `gamut-styles/utils/variables` is now deprecated, please import directly from the gamut-styles package'
   );
