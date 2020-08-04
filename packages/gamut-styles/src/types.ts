307303780e52603faf9b41af817c85112850a047
Aaron Robb <aaron@codecademy.com>
2020-08-04 16:35:54 -0400

    Remove types that don't matter

--- packages/gamut-styles/src/types.ts
+++ packages/gamut-styles/src/types.ts
@@ -1,22 +1,3 @@
 import * as theme from './variables';
 
 export type Theme = typeof theme;
-
-// Spacing
-export type SpacingSize = keyof Theme['spacing'];
-
-// Typography
-export type TextType = keyof Theme['fontSize'];
-export type HeadingSize = keyof Theme['fontSize']['heading'];
-export type TextSize = keyof Theme['fontSize']['text'];
-export type FontColor = keyof Theme['fontColor'];
-export type FontStack = keyof Theme['fontStack'];
-export type FontDecoration = keyof Theme['fontDecoration'];
-export type LineHeight = keyof Theme['lineHeight'];
-export type FontWeight = keyof Theme['fontWeight'];
-
-// Media Queries
-export type MediaSizes = keyof Theme['breakpoints'];
-
-// Effects
-export type EffectColor = keyof Theme['effectColors'];
