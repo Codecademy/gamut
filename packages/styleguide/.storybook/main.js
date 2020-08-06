4b52ed095bc43fd72ae7f3eeda7f5e566d76993e
Aaron Robb <aaron@codecademy.com>
2020-08-06 10:31:52 -0400

    First class emoji support

--- packages/styleguide/.storybook/main.js
+++ packages/styleguide/.storybook/main.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const createCompiler = require('@storybook/addon-docs/mdx-compiler-plugin');
+const emoji = require('remark-emoji');
 const { configs } = require('@codecademy/webpack-config');
 
 module.exports = {
@@ -7,6 +9,16 @@ module.exports = {
     config.module.rules = config.module.rules.concat(
       configs.css().module.rules
     );
+
+    config.module.rules
+      .find((rule) => new RegExp(rule.test).test('hello.story.mdx'))
+      .use.find((plugin) => plugin.loader.indexOf('@mdx-js') > -1)
+      .options.remarkPlugins.push(emoji);
+
+    config.module.rules
+      .find((rule) => new RegExp(rule.test).test('hello.story.mdx'))
+      .use.find((plugin) => plugin.loader.indexOf('@mdx-js') > -1)
+      .options.remarkPlugins.push(emoji);
 
     config.resolve.alias = {
       ...config.resolve.alias,
