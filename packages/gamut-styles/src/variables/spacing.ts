caa4641677eeadcdfdc7bd96599a580631086629
Aaron Robb <aaron@codecademy.com>
2020-08-14 14:36:16 -0400

    Fix Scale

--- packages/gamut-styles/src/variables/spacing.ts
+++ packages/gamut-styles/src/variables/spacing.ts
@@ -6,11 +6,10 @@ export const baseUnit = pxRem(base);
 export const headerHeight = pxRem(base * 4);
 
 export const spacing = {
-  xxs: pxRem(base * 0.25),
-  xs: pxRem(base * 0.5),
-  sm: pxRem(base * 0.75),
+  xs: pxRem(base * 0.25),
+  sm: pxRem(base * 5),
   md: pxRem(base * 1),
   lg: pxRem(base * 1.5),
-  xl: pxRem(base * 2.5),
-  xxl: pxRem(base * 4),
+  xl: pxRem(base * 2),
+  xxl: pxRem(base * 2.5),
 };
