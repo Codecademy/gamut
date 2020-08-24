2cde4f67b6b3f2a0c9a638c218c2f8cc19580c8b
Jake Hiller <jakemhiller@gmail.com>
2020-08-24 13:50:46 -0400

    cleanup

--- packages/gamut-labs/src/Flex/index.tsx
+++ packages/gamut-labs/src/Flex/index.tsx
@@ -93,10 +93,12 @@ Flex.defaultProps = {
   display: 'flex',
 };
 
+export type FlexItemProps = FlexProps & {};
+
 /**
  * A simple variant of the Flex component that defaults to display block for use as a child component for flex layouts
  */
-export const FlexItem = styled(Flex)<FlexProps>``;
+export const FlexItem = styled(Flex)<FlexItemProps>``;
 
 FlexItem.defaultProps = {
   display: 'block',
