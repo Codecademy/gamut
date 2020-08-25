3e19b90b446303f287f77a6f687d9616f96be831
Aaron Robb <aaron@codecademy.com>
2020-08-25 12:01:54 -0400

    Add Tag

--- packages/gamut-labs/src/labs/Tag/index.tsx
+++ packages/gamut-labs/src/labs/Tag/index.tsx
@@ -1,6 +1,20 @@
 import styled from '@emotion/styled';
-import { spacing } from '@codecademy/gamut-styles';
+import {
+  spacing,
+  colors,
+  fontSize,
+  fontWeight,
+} from '@codecademy/gamut-styles';
+import { startCase, get } from 'lodash';
 
-export const Tag = styled.div`
+export type TagProps = {
+  background: 'blue' | 'pink' | 'green' | 'yellow';
+};
+
+export const Tag = styled.div<TagProps>`
+  font-size: ${fontSize.text.sm};
+  font-weight: ${fontWeight.heading};
   padding: ${spacing[4]} ${spacing[8]};
+  background-color: ${({ background }) =>
+    get(colors, `standard.pale${startCase(background)}`)};
 `;
