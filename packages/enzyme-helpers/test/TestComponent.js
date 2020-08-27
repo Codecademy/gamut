b6773938882c8ac50a7721f7eb640546c0f02e1e
Josh Goldberg <joshuakgoldberg@outlook.com>
2020-08-27 10:50:11 -0400

    feat!: Added import/no-default-export to ESLint config (#929)
    * feat: Added import/no-default-export to ESLint config
    
    * Fixed existing complaints

--- packages/enzyme-helpers/test/TestComponent.js
+++ packages/enzyme-helpers/test/TestComponent.js
@@ -1,11 +1,9 @@
 import React from 'react';
 
-const TestComponent = ({ onClick, ...props }) => (
+export const TestComponent = ({ onClick, ...props }) => (
   <div {...props} data-testid="container">
     <button type="button" onClick={onClick} data-testid="button">
       A test component
     </button>
   </div>
 );
-
-export default TestComponent;
