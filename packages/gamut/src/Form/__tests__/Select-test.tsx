d450e06b5c0d020398f6a5c758b44cb46f066283
Jenesh Napit <47792836+jenesh@users.noreply.github.com>
2020-09-22 11:55:50 -0400

    Updated describe in the select test

--- packages/gamut/src/Form/__tests__/Select-test.tsx
+++ packages/gamut/src/Form/__tests__/Select-test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { Select } from '../Select';
 
-describe('<Select>', () => {
+describe('Select', () => {
   const selectOptions = ['red', 'yellow', 'green'];
   const defaultProps = {
     options: selectOptions,
