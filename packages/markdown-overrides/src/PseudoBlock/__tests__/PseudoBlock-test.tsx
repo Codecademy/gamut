55019da4e51b89fb54a3cd95fa893fdccf42169e
Ian Munro <ian@codecademy.com>
2020-09-24 12:23:47 -0400

    feat(markdown-overrides): Initial markdown-overrides package setup (#967)
    Initial markdown-overrides package setup, including ts and babel configs and a dummy first component, to be filled in later.

---
+++ packages/markdown-overrides/src/PseudoBlock/__tests__/PseudoBlock-test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import { PseudoBlock } from '../';
+
+describe('PsuedoBlock', () => {
+  it('renders', () => {
+    render(<PseudoBlock />);
+  });
+});
