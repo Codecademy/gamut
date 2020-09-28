1ab5b8f3d48ad0ad2ece246fda94f654ad05c883
Ian Munro <ian@codecademy.com>
2020-09-28 11:49:00 -0400

    feat(markdown-overrides): Add PseudoBlock and ErrorBlock (#971)
    * Add PseudoBlock and ErrorBlock with tests

--- packages/markdown-overrides/src/PseudoBlock/__tests__/PseudoBlock-test.tsx
+++ packages/markdown-overrides/src/PseudoBlock/__tests__/PseudoBlock-test.tsx
@@ -4,7 +4,10 @@ import { render } from '@testing-library/react';
 import { PseudoBlock } from '../';
 
 describe('PsuedoBlock', () => {
-  it('renders', () => {
-    render(<PseudoBlock />);
+  it('renders itself and children', () => {
+    const psuedoText = 'pseudocode!';
+    const view = render(<PseudoBlock>{psuedoText}</PseudoBlock>);
+
+    view.getByText(psuedoText);
   });
 });
