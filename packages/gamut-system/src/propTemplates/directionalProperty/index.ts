c8388a1775aba29edfc19850d98dda00e3f158b8
codecaaron <aaron@codecademy.com>
2020-09-29 16:34:56 -0400

    Directional prop comment describing why it does this

--- packages/gamut-system/src/propTemplates/directionalProperty/index.ts
+++ packages/gamut-system/src/propTemplates/directionalProperty/index.ts
@@ -38,12 +38,21 @@ const DIRECTIONAL_PROPS = {
 
 type DirectionalProps = typeof DIRECTIONAL_PROPS;
 
+/**
+ * Directional props requrie destructuring of their values to ensure their order.  Instead
+ * of sorting the styles to make overrides make sense we handle all of these props at once
+ * and use upstream previous values to initialize downstream ones such that upstream specific
+ * values will not be overriden by the CSS cascade erroneously.  We prefer this over manually
+ * sorting properties at runtime and having consistent CSS for these particular props.
+ */
+
 export function directionalProperty<
   Props extends AbstractProps,
   Config extends AbstractPropertyConfig &
     Required<Pick<AbstractPropertyConfig, 'propName' | 'computeValue'>>
 >({ propName, scale, computeValue }: Config): StyleTemplate<Props> {
   return (props: Props): CSSObject => {
+    // Initialize all directional props from base => specific direction
     const {
       [propName as string]: base,
       [`${propName}X`]: x = base,
@@ -54,19 +63,25 @@ export function directionalProperty<
       [`${propName}Bottom`]: b = y,
     } = props as AbstractProps;
     const propKey = propName as keyof DirectionalProps;
+    // Order props in their correct short hand order for consistency.
     const orderedProps = [t, r, b, l];
 
     const styles = {} as CSSObject;
 
+    // Iterate over all possible directions
     DIRECTIONS.forEach((direction, i) => {
+      // Get value
       const value = themeScaleValue(
         props as any,
         scale,
         orderedProps[i] as any
       );
+      // If there's nothing don't add it to the style object
       if (value === undefined) return;
 
+      // Look up valid directional prop name based on direction.
       const prop = DIRECTIONAL_PROPS[propKey][direction];
+      // Do final calculations
       styles[prop] = computeValue(value) as string;
     });
     return styles;
