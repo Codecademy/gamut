9b95edce64288f0b72e6ede1cab7a205c416ec5a
Ian Munro <ian@codecademy.com>
2020-10-01 09:46:24 -0400

    Use shared variable in tests

--- packages/markdown-overrides/src/MathBlock/__tests__/MathBlock-test.tsx
+++ packages/markdown-overrides/src/MathBlock/__tests__/MathBlock-test.tsx
@@ -10,7 +10,7 @@ jest.mock('katex', () => ({
 describe('MathBlock', () => {
   it('renders itself and children', () => {
     const latexText = '1234';
-    const view = render(<MathBlock>1234</MathBlock>);
+    const view = render(<MathBlock>{latexText}</MathBlock>);
 
     view.getByText(latexText);
   });
