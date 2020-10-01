47eb2af5b1cc9ca3096ebf09a6644d1a6a5d1f7c
codecaaron <aaron@codecademy.com>
2020-10-01 12:15:26 -0400

    Add responsive property tests

--- packages/gamut-system/src/propTemplates/responsiveProperty/__tests__/responsiveProperty-test.ts
+++ packages/gamut-system/src/propTemplates/responsiveProperty/__tests__/responsiveProperty-test.ts
@@ -1,4 +1,3 @@
-import { directionalProperty } from '../../directionalProperty';
 import { DEFAULT_MEDIA_QUERIES, responsiveProperty } from '..';
 import { standardProperty } from '../../standardProperty';
 import { ResponsiveProp } from '../../../types/system';
@@ -9,40 +8,97 @@ describe(responsiveProperty, () => {
       propName: 'display',
       computeValue: (val) => val,
     }),
-    margin: directionalProperty({
-      propName: 'margin',
+    height: standardProperty({
+      propName: 'height',
+      computeValue: (val) => val,
+    }),
+    width: standardProperty({
+      propName: 'width',
       computeValue: (val) => val,
     }),
   };
 
   type Theme = {};
   type PropShape = {
-    margin?: string | ResponsiveProp<string>;
-    marginLeft?: string | ResponsiveProp<string>;
-    display?: string | ResponsiveProp<string>;
+    height?: ResponsiveProp<string>;
+    width?: ResponsiveProp<string>;
+    display?: ResponsiveProp<string>;
     theme?: any;
   };
 
-  it('creates a property function', () => {
-    const propFunction = responsiveProperty<Theme, PropShape>({
-      propNames: ['display', 'margin', 'marginLeft'],
-      templateFns,
+  const propFunction = responsiveProperty<Theme, PropShape>({
+    propNames: ['display', 'width', 'height'],
+    templateFns,
+  });
+
+  it('returns empty object if styles are undefined', () => {
+    const outputStyles = propFunction({
+      display: undefined,
     });
 
+    expect(outputStyles).toEqual({});
+  });
+
+  it('returns primitive prop values on the lowest breakpoint', () => {
     const outputStyles = propFunction({
-      margin: '10px',
-      marginLeft: [, '5px'],
       display: 'block',
     });
 
     expect(outputStyles).toEqual({
-      marginLeft: '10px',
-      marginRight: '10px',
-      marginBottom: '10px',
-      marginTop: '10px',
       display: 'block',
+    });
+  });
+
+  it('Creates the correct breapoint with object syntax', () => {
+    const outputStyles = propFunction({
+      display: { sm: 'block' },
+    });
+
+    expect(outputStyles).toEqual({
       [DEFAULT_MEDIA_QUERIES['sm']]: {
-        marginLeft: '5px',
+        display: 'block',
+      },
+    });
+  });
+
+  it('Creates the correct breapoint with array syntax', () => {
+    const outputStyles = propFunction({
+      display: [, 'block'],
+    });
+
+    expect(outputStyles).toEqual({
+      [DEFAULT_MEDIA_QUERIES['sm']]: {
+        display: 'block',
+      },
+    });
+  });
+
+  it('merges smallest breakpoints with the primitive values on the lowest breakpoint', () => {
+    const outputStyles = propFunction({
+      display: 'block',
+      width: ['5px'],
+      height: { xs: '5px' },
+    });
+
+    expect(outputStyles).toEqual({
+      height: '5px',
+      width: '5px',
+      display: 'block',
+    });
+  });
+
+  it('merges higher breakpoints with different props', () => {
+    const outputStyles = propFunction({
+      display: 'block',
+      width: [, , '5px'],
+      height: { md: '5px' },
+    });
+
+    expect(outputStyles).toEqual({
+      display: 'block',
+      [DEFAULT_MEDIA_QUERIES['md']]: {
+        width: '5px',
+        height: '5px',
       },
     });
   });
