bb4b8e6ff059a0e528be096a8bf7065ba4f5f1b1
codecaaron <aaron@codecademy.com>
2020-10-01 18:00:47 -0400

    Tests

--- packages/gamut-system/src/core/system/__tests__/system-test.ts
+++ packages/gamut-system/src/core/system/__tests__/system-test.ts
@@ -1,5 +1,50 @@
+import { uniq } from 'lodash';
 import { system } from '..';
+import * as BaseProps from '../../../props';
+
+const allPossibilities = Object.entries(BaseProps).reduce(
+  (carry, [groupKey, props]) => ({
+    propGroups: carry.propGroups.concat(groupKey),
+    props: carry.props.concat(Object.keys(props)),
+  }),
+  {
+    props: [],
+    propGroups: [],
+  } as { props: string[]; propGroups: string[] }
+);
 
 describe(system, () => {
-  xit('', () => {});
+  it('initializes a system with no arguments by default', () => {
+    const { properties, propertyGroups, variant } = system();
+
+    expect(properties).toBeDefined();
+    expect(propertyGroups).toBeDefined();
+    expect(variant).toBeDefined();
+    variant({
+      propKey: 'hello',
+      variants: { primary: { width: 'in' } },
+    });
+  });
+
+  it('initializes a system with default properties', () => {
+    const { properties } = system();
+
+    expect(Object.keys(properties)).toEqual(uniq(allPossibilities.props));
+  });
+
+  it('initializes a system with default propGroups', () => {
+    const { propertyGroups } = system();
+
+    expect(Object.keys(propertyGroups)).toEqual(
+      uniq(allPossibilities.propGroups)
+    );
+  });
+
+  it('initializes a system with default propGroups', () => {
+    const { propertyGroups } = system();
+
+    expect(Object.keys(propertyGroups)).toEqual(
+      uniq(allPossibilities.propGroups)
+    );
+  });
 });
