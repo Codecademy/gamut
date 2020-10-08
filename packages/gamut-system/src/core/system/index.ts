ca0fe891ec4195977d7d27a156cce3a1f562fd92
codecaaron <aaron@codecademy.com>
2020-10-08 13:06:43 -0400

    Turned interpolated types to generics

--- packages/gamut-system/src/core/system/index.ts
+++ packages/gamut-system/src/core/system/index.ts
@@ -27,74 +27,89 @@ type SystemConfig<Theme extends AbstractTheme> = Record<
   GroupConfig<Theme>
 >;
 
-export const system = <
-  Config extends SystemConfig<Theme>,
-  Theme extends AbstractTheme
+// Initialize all type derivations and declare return signature
+// Intersection of Base and the supplied configuration objects.
+type MergeConfig<
+  Theme extends AbstractTheme,
+  Config extends SystemConfig<Theme>
+> = BaseConfig & Config;
+
+// Intermediate type to derive return types from representing all properties and handler
+type SystemProperties<
+  Theme extends AbstractTheme,
+  Config extends SystemConfig<Theme>
+> = {
+  /** All possible property handlers based off the MergedConfiguration  */
+  [PropGroup in keyof MergeConfig<Theme, Config>]: {
+    handlers: {
+      [Property in keyof MergeConfig<Theme, Config>[PropGroup]]: Handler<
+        ThematicProps<Theme, MergeConfig<Theme, Config>[PropGroup][Property]>
+      >;
+    };
+    /** All possible prop type signatures  */
+    props: {
+      [Property in keyof MergeConfig<Theme, Config>[PropGroup]]: ThematicProps<
+        Theme,
+        MergeConfig<Theme, Config>[PropGroup][Property]
+      >;
+    };
+  };
+};
+
+// Intersection of all possible props
+export type AllSystemProps<
+  Theme extends AbstractTheme,
+  Config extends SystemConfig<Theme>
+> = UnionToIntersection<
+  {
+    [PropGroup in keyof SystemProperties<Theme, Config>]: UnionToIntersection<
+      SystemProperties<
+        Theme,
+        Config
+      >[PropGroup]['props'][keyof SystemProperties<
+        Theme,
+        Config
+      >[PropGroup]['props']]
+    >;
+  }[keyof SystemProperties<Theme, Config>]
+>;
+
+type System<Theme extends AbstractTheme, Config extends SystemConfig<Theme>> = {
+  variant: {
+    /** Customizable PropKey */
+    <Prop extends Readonly<string>, Keys extends string>(config: {
+      prop: Prop;
+      variants: Readonly<Record<Keys, AllSystemProps<Theme, Config>>>;
+    }): (props: WeakRecord<Prop, Keys> & { theme?: Theme }) => CSSObject;
+    /**  */
+    <Keys extends string>(
+      config: Readonly<Record<Keys, AllSystemProps<Theme, Config>>>
+    ): (props: WeakRecord<'variant', Keys> & { theme?: Theme }) => CSSObject;
+  };
+  properties: UnionToIntersection<
+    SystemProperties<Theme, Config>[keyof SystemProperties<
+      Theme,
+      Config
+    >]['handlers']
+  >;
+} & {
+  [PropGroup in keyof SystemProperties<Theme, Config>]: Handler<
+    SystemProperties<Theme, Config>[PropGroup]['props'][keyof SystemProperties<
+      Theme,
+      Config
+    >[PropGroup]['props']]
+  >;
+};
+
+export type ThemedSystem<Theme extends AbstractTheme> = <
+  Config extends SystemConfig<Theme>
 >(
+  config?: Config
+) => System<Theme, Config>;
+
+export const system = <Config extends SystemConfig<{}>>(
   config: Config
-) => {
-  // Initialize all type derivations and declare return signature
-  // Intersection of Base and the supplied configuration objects.
-  type MergedConfiguration = Config extends {}
-    ? BaseConfig & Config
-    : BaseConfig;
-
-  // Intermediate type to derive return types from representing all properties and handler
-  type BaseGroup = {
-    /** All possible property handlers based off the MergedConfiguration  */
-    [PropGroup in keyof MergedConfiguration]: {
-      handlers: {
-        [Property in keyof MergedConfiguration[PropGroup]]: Handler<
-          ThematicProps<Theme, MergedConfiguration[PropGroup][Property]>
-        >;
-      };
-      /** All possible prop type signatures  */
-      props: {
-        [Property in keyof MergedConfiguration[PropGroup]]: ThematicProps<
-          Theme,
-          MergedConfiguration[PropGroup][Property]
-        >;
-      };
-    };
-  };
-
-  // Return type for composed propGroup handlers
-  type PropertyGroups = {
-    [PropGroup in keyof BaseGroup]: Handler<
-      BaseGroup[PropGroup]['props'][keyof BaseGroup[PropGroup]['props']]
-    >;
-  };
-
-  // Intersection of all possible props
-  type GroupProps = {
-    [PropGroup in keyof BaseGroup]: UnionToIntersection<
-      BaseGroup[PropGroup]['props'][keyof BaseGroup[PropGroup]['props']]
-    >;
-  };
-
-  type AllProps = UnionToIntersection<GroupProps[keyof GroupProps]>;
-
-  type BaseProps = {
-    theme?: Theme;
-  };
-
-  type System = {
-    variant: {
-      /** Customizable PropKey */
-      <Prop extends Readonly<string>, Keys extends string>(config: {
-        prop: Prop;
-        variants: Readonly<Record<Keys, AllProps>>;
-      }): (props: WeakRecord<Prop, Keys> & BaseProps) => CSSObject;
-      /**  */
-      <Keys extends string>(config: Readonly<Record<Keys, AllProps>>): (
-        props: WeakRecord<'variant', Keys> & BaseProps
-      ) => CSSObject;
-    };
-    // Map of all propGroup handlers
-    properties: UnionToIntersection<BaseGroup[keyof BaseGroup]['handlers']>;
-    // createVariant with closure types
-  } & PropertyGroups;
-
+): System<{}, Config> => {
   // Initializes the return object
   const systemShape = {
     properties: {},
@@ -146,5 +161,5 @@ export const system = <
   // add the function to the returned object
   systemShape.variant = createVariant;
 
-  return systemShape as System;
+  return systemShape;
 };
