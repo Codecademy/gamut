38bc0749d1a0877d644c90cbc7f12a0fb0506efc
codecaaron <aaron@codecademy.com>
2020-10-19 16:15:06 -0400

    Add variant override tests

--- packages/gamut-system/src/core/system/__tests__/emotion-components-test.tsx
+++ packages/gamut-system/src/core/system/__tests__/emotion-components-test.tsx
@@ -4,10 +4,11 @@ import render from 'react-test-renderer';
 import styled from '@emotion/styled';
 import { matchers } from 'jest-emotion';
 import { DEFAULT_MEDIA_QUERIES } from '../../../styleTemplates/createResponsiveStyleTemplate/constants';
+import { HandlerProps } from '../../../types/config';
 
 expect.extend(matchers);
 
-const { typography } = system({});
+const { variant, typography } = system({});
 
 describe('Styled components integration', () => {
   const MyComponent = styled.div`
@@ -29,5 +30,31 @@ describe('Styled components integration', () => {
     expect(view).toHaveStyleRule('font-size', '2rem', {
       media: DEFAULT_MEDIA_QUERIES.xs.replace('@media ', ''),
     });
+  });
+
+  it('last function used overwrites earlier styles', () => {
+    const textVariants = variant({
+      primary: { fontSize: '1rem', textColor: 'blue' },
+      secondary: { fontSize: '0.85rem' },
+    });
+
+    const MyVariant = styled.div<
+      HandlerProps<typeof typography> & HandlerProps<typeof textVariants>
+    >`
+      ${textVariants}
+      ${typography}
+    `;
+
+    const noOverride = render.create(<MyVariant variant="primary" />).toJSON();
+
+    expect(noOverride).toHaveStyleRule('font-size', '1rem');
+    expect(noOverride).toHaveStyleRule('color', 'blue');
+
+    const withOverride = render
+      .create(<MyVariant variant="primary" fontSize="50rem" />)
+      .toJSON();
+
+    expect(withOverride).toHaveStyleRule('font-size', '50rem');
+    expect(withOverride).toHaveStyleRule('color', 'blue');
   });
 });
