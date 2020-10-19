904c027eb83ebdfda1b32af94f7bd8ef4f0c927c
Andy Cernera <acernera@codecademy.com>
2020-10-19 09:43:17 -0400

    Apply x and y coefficient directly in shadow offset calculations

--- packages/gamut-labs/src/experimental/Box/utils.ts
+++ packages/gamut-labs/src/experimental/Box/utils.ts
@@ -7,15 +7,13 @@ const shadowOffset = 2;
 export const createShadowOffset = (
   offset: ShadowOffset,
 ) => {
-  const [xCoeff, yCoeff] = [1, -1];
-
   return css`
-    transform: translate(${pxRem(xCoeff * offset)}, ${pxRem(yCoeff * offset)});
+    transform: translate(${pxRem(offset)}, ${pxRem(-1 * offset)});
 
     &:after {
       transform: translate(
-        ${pxRem(-1 * xCoeff * offset * shadowOffset)},
-        ${pxRem(-1 * yCoeff * offset * shadowOffset)}
+        ${pxRem(-1 * offset * shadowOffset)},
+        ${pxRem(offset * shadowOffset)}
       );
     }
   `;
