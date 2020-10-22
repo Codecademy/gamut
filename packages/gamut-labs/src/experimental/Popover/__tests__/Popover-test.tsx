c5a805d3c399f49f09a32ca10fa5ac044c5dbebc
Hasan Afzal <hmafzal91@gmail.com>
2020-10-22 10:01:14 -0400

    feat: Add a popover component (#846)
    * Initial popover commit
    
    * Some more updates to popover
    
    * Add a comment
    
    * add tests for getPopoverPosition
    
    * update type; refactor to react-use
    
    * resolved merge conflicts
    
    * added popover to gamut-test.snap
    
    * updated popover mdx and test
    
    * update overlay stories / skip test
    
    * add returnFocusOnDeactivate prop on Overlay
    
    * add lockScroll prop on Overlay
    
    * moving popover to experimental
    
    * update gamut-test.ts.snap
    
    * remove showscreen and add shadow
    
    * remove screen style
    
    * refactor popover to not use Overlay
    
    * add escape hotkey
    
    * refactor to use useClickAway
    
    * add scroll logic and tests
    
    * clean up
    
    * fix import path
    
    * revert import fix
    
    Co-authored-by: Sooin Chung <sooin@codecademy.com>
    Co-authored-by: sooinc <58445391+sooinc@users.noreply.github.com>

---
+++ packages/gamut-labs/src/experimental/Popover/__tests__/Popover-test.tsx
@@ -0,0 +1,252 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { mount } from 'enzyme';
+import { Popover, PopoverProps } from '..';
+
+const targetRefObj = {
+  current: {
+    contains: () => true,
+    getBoundingClientRect: () => {
+      return {
+        bottom: 298,
+        height: 38,
+        left: 57,
+        right: 840,
+        top: 260,
+        width: 783,
+        x: 57,
+        y: 260,
+        toJSON: () => {},
+      };
+    },
+  },
+};
+
+const renderPopover = (props?: Partial<PopoverProps>) => {
+  return render(
+    <Popover isOpen={true} targetRef={targetRefObj} {...props}>
+      <div data-testid={'popover-content'}>
+        Howdy!
+        <button type="button" />
+      </div>
+    </Popover>
+  );
+};
+
+const mountPopover = (props?: Partial<PopoverProps>) => {
+  return mount(
+    <Popover isOpen={true} targetRef={targetRefObj} {...props}>
+      <div data-testid={'popover-content'}>
+        Howdy!
+        <button type="button" />
+      </div>
+    </Popover>
+  );
+};
+
+const popoverIsRendered = () => {
+  return Boolean(screen.queryByTestId('popover-content'));
+};
+
+describe('Popover', () => {
+  it('renders null when isOpen is not true', () => {
+    renderPopover({ isOpen: false });
+
+    expect(popoverIsRendered()).toBeFalsy();
+  });
+
+  it('renders children when isOpen is true', () => {
+    renderPopover({ isOpen: true });
+    expect(popoverIsRendered()).toBeTruthy();
+  });
+
+  it('triggers onRequestClose callback when clicking outside', () => {
+    const onRequestClose = jest.fn();
+    renderPopover({
+      isOpen: true,
+      onRequestClose,
+    });
+
+    fireEvent.mouseDown(
+      screen.getByTestId('popover-content-container').parentElement!
+    );
+    expect(onRequestClose.mock.calls.length).toBe(1);
+  });
+
+  it('does not trigger onRequestClose callback when clicking inside', () => {
+    const onRequestClose = jest.fn();
+    renderPopover({
+      isOpen: true,
+      onRequestClose,
+    });
+    fireEvent.mouseDown(screen.getByTestId('popover-content-container'));
+    expect(onRequestClose.mock.calls.length).toBe(0);
+  });
+
+  it('triggers onRequestClose callback when escape key is triggered', () => {
+    const onRequestClose = jest.fn();
+    const { baseElement } = renderPopover({
+      isOpen: true,
+      onRequestClose,
+    });
+    fireEvent.keyDown(baseElement, { key: 'Escape', keyCode: 27 });
+    expect(onRequestClose.mock.calls.length).toBe(1);
+  });
+
+  it('triggers onRequestClose callback when popover is out of viewport', () => {
+    /* element is inside the viewport if the top and left value is greater than or equal to 0,
+      and right value is less than or equal to window.innerWidth
+      and bottom value is less than or equal to window.innerHeight */
+    const targetRefObj = {
+      current: {
+        contains: () => true,
+        getBoundingClientRect: () => {
+          return {
+            bottom: 35,
+            height: 38,
+            left: 57,
+            right: 840,
+            top: -1,
+            width: 783,
+            x: 41,
+            y: -1,
+            toJSON: () => {},
+          };
+        },
+      },
+    };
+    const onRequestClose = jest.fn();
+    renderPopover({
+      targetRef: targetRefObj,
+      isOpen: true,
+      onRequestClose,
+    });
+    expect(onRequestClose.mock.calls.length).toBe(1);
+  });
+
+  it('does not onRequestClose callback when popover is out of viewport', () => {
+    const targetRefObj = {
+      current: {
+        contains: () => true,
+        getBoundingClientRect: () => {
+          return {
+            bottom: 35,
+            height: 38,
+            left: 57,
+            right: 840,
+            top: 1,
+            width: 783,
+            x: 41,
+            y: 1,
+            toJSON: () => {},
+          };
+        },
+      },
+    };
+    const onRequestClose = jest.fn();
+    renderPopover({
+      targetRef: targetRefObj,
+      isOpen: true,
+      onRequestClose,
+    });
+    expect(onRequestClose.mock.calls.length).toBe(0);
+  });
+
+  it('does not show a beak if the prop is false', () => {
+    renderPopover({
+      isOpen: true,
+      showBeak: false,
+    });
+
+    expect(screen.queryByTestId('popover-beak')).not.toBeInTheDocument();
+  });
+
+  it('shows a beak if the prop is true', () => {
+    renderPopover({
+      isOpen: true,
+      showBeak: true,
+    });
+
+    expect(screen.queryByTestId('popover-beak')).toBeInTheDocument();
+  });
+
+  it("positions with default 'below', 'left', '20' value when position, align, offset props are not provided respectively", () => {
+    Object.defineProperty(window, 'scrollY', { value: 1 });
+    Object.defineProperty(window, 'scrollX', { value: 1 });
+
+    const wrapped = mountPopover({
+      isOpen: true,
+    });
+
+    expect(
+      wrapped.find('[data-testid="popover-content-container"]').props()
+    ).toMatchObject({
+      style: {
+        top: 319,
+        left: 58,
+      },
+    });
+  });
+
+  it('positions with given position and align values when provided', () => {
+    Object.defineProperty(window, 'scrollY', { value: 1 });
+    Object.defineProperty(window, 'scrollX', { value: 1 });
+
+    const wrapped = mountPopover({
+      isOpen: true,
+      position: 'above',
+      align: 'right',
+    });
+
+    expect(
+      wrapped.find('[data-testid="popover-content-container"]').props()
+    ).toMatchObject({
+      style: {
+        top: 241,
+        left: 841,
+      },
+    });
+  });
+
+  it('positions with given offset value when provided', () => {
+    Object.defineProperty(window, 'scrollY', { value: 1 });
+    Object.defineProperty(window, 'scrollX', { value: 1 });
+
+    const wrapped = mountPopover({
+      isOpen: true,
+      position: 'above',
+      align: 'right',
+      offset: 30,
+    });
+
+    expect(
+      wrapped.find('[data-testid="popover-content-container"]').props()
+    ).toMatchObject({
+      style: {
+        top: 231,
+        left: 841,
+      },
+    });
+  });
+
+  it('positions round to whole number when ', () => {
+    Object.defineProperty(window, 'scrollY', { value: 1.5 });
+    Object.defineProperty(window, 'scrollX', { value: 1.5 });
+
+    const wrapped = mountPopover({
+      isOpen: true,
+      position: 'above',
+      align: 'right',
+      offset: 30,
+    });
+
+    expect(
+      wrapped.find('[data-testid="popover-content-container"]').props()
+    ).toMatchObject({
+      style: {
+        top: 232,
+        left: 842,
+      },
+    });
+  });
+});
