491fbadbe8ac0717a1d342381403989d4da62335
Jake Hiller <jakemhiller@gmail.com>
2020-10-28 17:24:29 -0400

    Add options to enable/disable the previously added changes (helpers & runtime path) (#15)
    * add type option to enable/disable some babel options depending on the expected output
    
    * v3.0.0

--- packages/babel-preset-codecademy/README.md
+++ packages/babel-preset-codecademy/README.md
@@ -26,19 +26,35 @@ $ npm install --save-dev babel-preset-codecademy
 $ babel script.js --presets codecademy
 ```
 
-### Via Node API
+### Options
 
-```javascript
-require('babel-core').transform('code', {
-  presets: ['codecademy']
-});
+#### Type
+
+default: 'library'
+
+Certain options can be turned on and off depending on what you're using babel for.
+
+For applications, we enable runtime helpers and `@babel/runtime` becomes a required dependency.
+
+```json
+{
+  "presets": ["codecademy", { "type": "application" }]
+}
+```
+
+For libraries (default), we don't enable runtime helpers because then the resulting package would need `@babel/runtime` as a dependency, which should be handled by the consumer of the package.
+
+```json
+{
+  "presets": ["codecademy", { "type": "library" }]
+}
 ```
 
 ## Publishing this package
 
 This package is automatically published by GitHub Actions when the version number changes
 
-* merge your PR into `main`
-* create a new PR that updates the version of the package in package.json. Base the version bump on all of the changes that will be added in this version.
-* merge the version PR into `main`
-* check the [actions](https://github.com/Codecademy/babel-preset-codecademy/actions) to see when the package is published
+- merge your PR into `main`
+- create a new PR that updates the version of the package in package.json. Base the version bump on all of the changes that will be added in this version.
+- merge the version PR into `main`
+- check the [actions](https://github.com/Codecademy/babel-preset-codecademy/actions) to see when the package is published
--- packages/babel-preset-codecademy/index.js
+++ packages/babel-preset-codecademy/index.js
@@ -5,68 +5,86 @@ const isEnvTest = env === "test";
 const isEnvProduction = env === "production";
 const isEnvDevelopment = !isEnvTest && !isEnvProduction;
 
-const absoluteRuntimePath = path.dirname(
-  require.resolve("@babel/runtime/package.json")
-);
+const PACKAGE_LIBRARY = "library";
+const PACKAGE_APPLICATION = "application";
+const packageTypes = [PACKAGE_LIBRARY, PACKAGE_APPLICATION];
 
-module.exports = () => ({
-  presets: [
-    isEnvTest && [
-      require("@babel/preset-env").default,
-      {
-        targets: {
-          node: "current",
+module.exports = (api, { type = PACKAGE_LIBRARY } = {}) => {
+  if (!packageTypes.includes(type)) {
+    throw new Error(
+      `babel-preset-codecademy: option 'type' should be one of: ${[
+        PACKAGE_LIBRARY,
+        PACKAGE_APPLICATION,
+      ].join(", ")}, received ${type}`
+    );
+  }
+
+  let absoluteRuntimePath;
+  if (type === PACKAGE_APPLICATION) {
+    absoluteRuntimePath = path.dirname(
+      require.resolve("@babel/runtime/package.json")
+    );
+  }
+
+  return {
+    presets: [
+      isEnvTest && [
+        require("@babel/preset-env").default,
+        {
+          targets: {
+            node: "current",
+          },
         },
-      },
-    ],
-    (isEnvProduction || isEnvDevelopment) && [
-      require("@babel/preset-env").default,
-      {
-        useBuiltIns: "entry",
-        modules: false,
-        corejs: 2,
-      },
-    ],
-    require("@babel/preset-react").default,
-  ].filter(Boolean),
-  plugins: [
-    require("@babel/plugin-transform-destructuring").default,
-    [require("@babel/plugin-proposal-decorators"), { legacy: true }],
-    [
-      require("@babel/plugin-proposal-class-properties").default,
-      {
-        loose: true,
-      },
-    ],
-    [
-      require("@babel/plugin-proposal-object-rest-spread").default,
-      {
-        useBuiltIns: true,
-      },
-    ],
-    require("@babel/plugin-proposal-do-expressions"),
-    require("@babel/plugin-proposal-export-default-from"),
-    require("@babel/plugin-proposal-export-namespace-from"),
-    require("@babel/plugin-proposal-nullish-coalescing-operator"),
-    require("@babel/plugin-proposal-optional-chaining"),
-    require("@babel/plugin-syntax-import-meta"),
-    [
-      require("@babel/plugin-transform-runtime").default,
-      {
-        corejs: false,
-        regenerator: true,
-        helpers: true,
-        regenerator: true,
-        useESModules: isEnvDevelopment || isEnvProduction,
-        // Undocumented: ensures that the correct runtime version is used
-        // https://github.com/babel/babel/blob/090c364a90fe73d36a30707fc612ce037bdbbb24/packages/babel-plugin-transform-runtime/src/index.js#L35-L42
-        absoluteRuntime: absoluteRuntimePath,
-      },
-    ],
-    require("babel-plugin-react-anonymous-display-name").default,
-    require("@babel/plugin-syntax-dynamic-import").default,
-    isEnvTest &&
-      // Transform dynamic import to require
-      require("babel-plugin-transform-dynamic-import").default,
-  ].filter(Boolean),
-});
+      ],
+      (isEnvProduction || isEnvDevelopment) && [
+        require("@babel/preset-env").default,
+        {
+          useBuiltIns: "entry",
+          modules: false,
+          corejs: 2,
+        },
+      ],
+      require("@babel/preset-react").default,
+    ].filter(Boolean),
+    plugins: [
+      require("@babel/plugin-transform-destructuring").default,
+      [require("@babel/plugin-proposal-decorators"), { legacy: true }],
+      [
+        require("@babel/plugin-proposal-class-properties").default,
+        {
+          loose: true,
+        },
+      ],
+      [
+        require("@babel/plugin-proposal-object-rest-spread").default,
+        {
+          useBuiltIns: true,
+        },
+      ],
+      require("@babel/plugin-proposal-do-expressions"),
+      require("@babel/plugin-proposal-export-default-from"),
+      require("@babel/plugin-proposal-export-namespace-from"),
+      require("@babel/plugin-proposal-nullish-coalescing-operator"),
+      require("@babel/plugin-proposal-optional-chaining"),
+      require("@babel/plugin-syntax-import-meta"),
+      [
+        require("@babel/plugin-transform-runtime").default,
+        {
+          corejs: false,
+          regenerator: true,
+          helpers: packageType === PACKAGE_APPLICATION,
+          regenerator: true,
+          useESModules: isEnvDevelopment || isEnvProduction,
+          // Undocumented: ensures that the correct runtime version is used
+          // https://github.com/babel/babel/blob/090c364a90fe73d36a30707fc612ce037bdbbb24/packages/babel-plugin-transform-runtime/src/index.js#L35-L42
+          absoluteRuntime: absoluteRuntimePath,
+        },
+      ],
+      require("babel-plugin-react-anonymous-display-name").default,
+      require("@babel/plugin-syntax-dynamic-import").default,
+      isEnvTest &&
+        // Transform dynamic import to require
+        require("babel-plugin-transform-dynamic-import").default,
+    ].filter(Boolean),
+  };
+};
