c1dc760aefdd8b685d80c368adea95bc2a6f3659
codecaaron <aaron@codecademy.com>
2020-10-28 10:03:28 -0400

    Add more text props

--- packages/gamut-labs/src/experimental/Text/index.tsx
+++ packages/gamut-labs/src/experimental/Text/index.tsx
@@ -1,3 +1,10 @@
-import { styled, typography } from '@codecademy/gamut-styles';
+import { compose, HandlerProps } from '@codecademy/gamut-system';
+import { styled, typography, coloration } from '@codecademy/gamut-styles';
 
-export const Text = styled.span(typography);
+export const textStyles = compose(typography, coloration);
+
+export type TextProps = HandlerProps<typeof textStyles> & {
+  as?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p' | 'span' | 'div';
+};
+
+export const Text = styled.span<TextProps>(typography);
