286b8fc468ad23de1a5ca3df8b9ea29942647ee1
Josh Goldberg <josh@codecademy.com>
2020-11-10 11:56:15 -0500

    Export only ButtonProps from shared

--- packages/gamut/src/Button/index.ts
+++ packages/gamut/src/Button/index.ts
@@ -1,6 +1,6 @@
 export * from './CTAButton';
 export * from './FillButton';
 export * from './IconButton';
-export * from './shared';
+export { ButtonProps } from './shared';
 export * from './StrokeButton';
 export * from './TextButton';
