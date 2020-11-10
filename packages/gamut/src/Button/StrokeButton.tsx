1e654ef989e19109ea31854bb3beb5bf5d3facce
Josh Goldberg <joshuakgoldberg@outlook.com>
2020-11-10 15:05:56 -0500

    feat: add new Button (#1119)
    * feat!: deprecate Button and ButtonBase
    
    * feat: add new Button
    
    * Flesh out new buttons
    
    * Fix mode and some props
    
    * Fix VisualTheme in Testimonials
    
    * Added missing root babel.config.js
    
    * Used Button for first Atom example again
    
    * Revert VisualTheme back to string enum
    
    * VisualTheme again in buttons
    
    * Marked story as deprecated
    
    * Colors, and add back missing story file
    
    * Merge branch 'main'
    
    * Added back story (again) and fixed border styles
    
    * cursor: pointer;
    
    * background: none;
    
    * Remove rounding error
    
    * Adjust outer CTA too
    
    * Export only ButtonProps from shared
    
    * Remove modeColorGroups from exports
    
    * Missing ButtonProps
    
    * Ok updated snapshot for real
    
    * Post main merge font size

---
+++ packages/gamut/src/Button/StrokeButton.tsx
@@ -0,0 +1,54 @@
+import { colors } from '@codecademy/gamut-styles';
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+import React from 'react';
+import { VisualTheme } from '../theming/VisualTheme';
+
+import { ButtonInner } from './ButtonInner';
+import { ButtonOutline } from './ButtonOutline';
+import { ButtonProps, modeColorGroups } from './shared';
+
+const StrokeButtonInner = styled(ButtonInner)<ButtonProps>(
+  ({ mode = VisualTheme.LightMode }: ButtonProps) => {
+    const modeColors = modeColorGroups[mode];
+
+    return css`
+      border: 2px solid ${modeColors.background};
+      border-radius: 3px;
+      color: ${modeColors.background};
+      padding: 0.75rem 1rem;
+
+      ${StrokeButtonOuter}:hover & {
+        background-color: ${modeColors.backgroundEmphasized};
+      }
+
+      ${StrokeButtonOuter}:active & {
+        background: ${mode === VisualTheme.LightMode
+          ? colors.hyper
+          : colors.yellow};
+        color: ${modeColors.foreground};
+      }
+
+      ${StrokeButtonOuter}:disabled & {
+        background-color: transparent;
+        border-color: ${modeColors.backgroundMuted};
+        color: ${modeColors.foregroundMuted};
+        cursor: not-allowed;
+      }
+    `;
+  }
+);
+
+const StrokeButtonOuter = styled(ButtonOutline)`
+  padding: 1px;
+`;
+
+export const StrokeButton: React.FC<React.ComponentProps<
+  typeof StrokeButtonOuter
+>> = ({ children, mode, ...props }) => {
+  return (
+    <StrokeButtonOuter mode={mode} {...props}>
+      <StrokeButtonInner mode={mode}>{children}</StrokeButtonInner>
+    </StrokeButtonOuter>
+  );
+};
