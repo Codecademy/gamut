a7ed112f8bea02cb53950bf470170fe857a4cbea
Josh Goldberg <joshuakgoldberg@outlook.com>
2020-11-10 15:05:56 -0500

    feat: add new Button (#1119)
    * feat!: deprecate Button and ButtonBase
    
    * feat: add new Button
    
    * Flesh out new buttons
    
    * Fix mode and some props
    
    * Fix VisualTheme in Testimonials
    
    * Added missing root babel.config.js
    
    * Used Button for first Atom example again
    
    * Revert VisualTheme back to string enum
    
    * VisualTheme again in buttons
    
    * Marked story as deprecated
    
    * Colors, and add back missing story file
    
    * Merge branch 'main'
    
    * Added back story (again) and fixed border styles
    
    * cursor: pointer;
    
    * background: none;
    
    * Remove rounding error
    
    * Adjust outer CTA too
    
    * Export only ButtonProps from shared
    
    * Remove modeColorGroups from exports
    
    * Missing ButtonProps
    
    * Ok updated snapshot for real
    
    * Post main merge font size

---
+++ packages/gamut/src/Button/FillButton.tsx
@@ -0,0 +1,46 @@
+import { swatches } from '@codecademy/gamut-styles';
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+import React from 'react';
+
+import { VisualTheme } from '../theming/VisualTheme';
+import { ButtonInner } from './ButtonInner';
+import { ButtonOutline } from './ButtonOutline';
+import { ButtonProps, modeColorGroups } from './shared';
+
+const FillButtonInner = styled(ButtonInner)<ButtonProps>(
+  ({ mode = VisualTheme.LightMode }: ButtonProps) => {
+    const modeColors = modeColorGroups[mode];
+
+    return css`
+      background-color: ${modeColors.background};
+      border-radius: 3px;
+      color: ${modeColors.foreground};
+      padding: 0.75rem 1rem;
+
+      ${FillButtonOuter}:hover & {
+        background-color: ${swatches.hyper['400']};
+      }
+
+      ${FillButtonOuter}:disabled & {
+        background: ${modeColors.backgroundMuted};
+        color: ${modeColors.foregroundMuted};
+        cursor: not-allowed;
+      }
+    `;
+  }
+);
+
+const FillButtonOuter = styled(ButtonOutline)`
+  padding: 1px;
+`;
+
+export const FillButton: React.FC<React.ComponentProps<
+  typeof FillButtonOuter
+>> = ({ children, mode, ...props }) => {
+  return (
+    <FillButtonOuter mode={mode} {...props}>
+      <FillButtonInner mode={mode}>{children}</FillButtonInner>
+    </FillButtonOuter>
+  );
+};
