8bb0c202fd8c761726516079a0ad7b0cc7e3129b
Josh Goldberg <josh@codecademy.com>
2020-11-10 14:55:34 -0500

    Post main merge font size

--- packages/gamut/src/Button/ButtonInner.tsx
+++ packages/gamut/src/Button/ButtonInner.tsx
@@ -3,7 +3,7 @@ import styled from '@emotion/styled';
 
 export const ButtonInner = styled.span`
   display: flex;
-  font-size: ${fontSize.text.md};
+  font-size: ${fontSize[16]};
   transition: ${timing.fast} background-color, ${timing.fast} box-shadow,
     ${timing.fast} color;
 `;
