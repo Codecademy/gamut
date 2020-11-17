85061c0a9cb203ae0575378a621285a78a9c564d
Josh Goldberg <josh@codecademy.com>
2020-11-17 10:22:22 -0500

    Put the ref on the FillButton

--- packages/styleguide/stories/Labs/Experimental/Atoms/Popover/ReachPopover.examples.tsx
+++ packages/styleguide/stories/Labs/Experimental/Atoms/Popover/ReachPopover.examples.tsx
@@ -4,23 +4,23 @@ import { ReachPopover } from '@codecademy/gamut-labs/src';
 
 export const ReachPopoverExample = () => {
   const [open, setOpen] = useState(false);
-  const activeElRef = useRef<HTMLDivElement>(null);
+  const activeElRef = useRef<HTMLButtonElement>(null);
 
   return (
     <React.Fragment>
-      The plain buttons are just here to show <kbd>Tab</kbd> order being
+      The other plain buttons are just here to show <kbd>Tab</kbd> order being
       preserved. Glorious.
       <br />
       <button type="button">before</button>
-      <div ref={activeElRef}>
-        <FillButton
-          onClick={() => {
-            setOpen(!open);
-          }}
-        >
-          {open ? 'Close' : 'Open'} Popover
-        </FillButton>
-      </div>
+      <FillButton
+        onClick={() => {
+          setOpen(!open);
+        }}
+        ref={activeElRef}
+        type="button"
+      >
+        Toggle Popover
+      </FillButton>
       <Container>
         <ReachPopover isOpen={open} />
       </Container>
