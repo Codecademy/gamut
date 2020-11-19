85fa283d8e8e057f896c489a5bcad27a662cde05
Jake Hiller <jakemhiller@gmail.com>
2020-11-19 12:33:34 -0500

    allow swapping button for a tag when href is present, add more reset styles to button outline

--- packages/gamut/src/Button/ButtonInner.tsx
+++ packages/gamut/src/Button/ButtonInner.tsx
@@ -1,7 +1,10 @@
 import { fontSize, timing } from '@codecademy/gamut-styles';
 import styled from '@emotion/styled';
+import isPropValid from '@emotion/is-prop-valid';
 
-export const ButtonInner = styled.span`
+export const ButtonInner = styled('span', {
+  shouldForwardProp: (prop: any) => isPropValid(prop) && prop !== 'mode',
+})`
   display: flex;
   font-size: ${fontSize[16]};
   transition: ${timing.fast} background-color, ${timing.fast} box-shadow,
--- packages/gamut/src/Button/ButtonOutline.tsx
+++ packages/gamut/src/Button/ButtonOutline.tsx
@@ -1,31 +1,51 @@
+import isPropValid from '@emotion/is-prop-valid';
 import { timing } from '@codecademy/gamut-styles';
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
+import React from 'react';
 
 import { VisualTheme } from '../theming/VisualTheme';
 import { ButtonProps, modeColorGroups } from './shared';
 
-export const ButtonOutline = styled.button<ButtonProps>(
-  ({ mode = VisualTheme.LightMode }: ButtonProps) => {
-    const modeColors = modeColorGroups[mode];
+const StyledButtonOutline = styled('button', {
+  shouldForwardProp: (prop: any) => isPropValid(prop) && prop !== 'mode',
+})<ButtonProps>(({ mode = VisualTheme.LightMode }: ButtonProps) => {
+  const modeColors = modeColorGroups[mode];
 
-    return css`
-      background: none;
-      border: none;
-      border-radius: 4px;
-      box-shadow: 0 0 0 0 transparent;
-      cursor: pointer;
-      transition: ${timing.fast} box-shadow;
+  return css`
+    background: none;
+    border-radius: 4px;
+    border: none;
+    box-shadow: 0 0 0 0 transparent;
+    color: inherit;
+    cursor: pointer;
+    display: inline-block;
+    font-family: inherit;
+    font: inherit;
+    line-height: normal;
+    margin: 0;
+    padding: 0;
+    text-align: center;
+    text-decoration: none;
+    transition: ${timing.fast} box-shadow;
+    vertical-align: middle;
+    white-space: nowrap;
 
-      &:disabled {
-        cursor: not-allowed;
-        user-select: none;
-      }
+    &:disabled {
+      cursor: not-allowed;
+      user-select: none;
+    }
 
-      &:focus {
-        box-shadow: 0 0 0 2px ${modeColors.background};
-        outline: none;
-      }
-    `;
-  }
-);
+    &:focus {
+      box-shadow: 0 0 0 2px ${modeColors.background};
+      outline: none;
+    }
+  `;
+});
+
+export const ButtonOutline: React.FC<React.ComponentProps<
+  typeof StyledButtonOutline
+>> = ({ as, href, ...props }) => {
+  const asComponent = !as && href ? 'a' : as ?? 'button';
+  return <StyledButtonOutline as={asComponent} {...props} />;
+};
