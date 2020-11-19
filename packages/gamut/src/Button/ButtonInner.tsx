93f6572f5f5fa5b44cdbbd1209cd030e0b8a91d9
Jake Hiller <jakemhiller@gmail.com>
2020-11-19 12:37:48 -0500

    fix syntax highlighting

--- packages/gamut/src/Button/ButtonInner.tsx
+++ packages/gamut/src/Button/ButtonInner.tsx
@@ -1,12 +1,15 @@
 import { fontSize, timing } from '@codecademy/gamut-styles';
 import styled from '@emotion/styled';
 import isPropValid from '@emotion/is-prop-valid';
+import { css } from '@emotion/react';
 
 export const ButtonInner = styled('span', {
   shouldForwardProp: (prop: any) => isPropValid(prop) && prop !== 'mode',
-})`
-  display: flex;
-  font-size: ${fontSize[16]};
-  transition: ${timing.fast} background-color, ${timing.fast} box-shadow,
-    ${timing.fast} color;
-`;
+})(() => {
+  return css`
+    display: flex;
+    font-size: ${fontSize[16]};
+    transition: ${timing.fast} background-color, ${timing.fast} box-shadow,
+      ${timing.fast} color;
+  `;
+});
