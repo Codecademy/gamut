f63f3ea3d893a4ce181c8c3917fe5c1901bed440
Josh Goldberg <josh@codecademy.com>
2020-11-23 16:04:15 -0500

    Shared buttonOutlineStyles internally

--- packages/gamut/src/Button/ButtonOutline.tsx
+++ packages/gamut/src/Button/ButtonOutline.tsx
@@ -1,51 +1,15 @@
 import isPropValid from '@emotion/is-prop-valid';
-import { timing } from '@codecademy/gamut-styles';
-import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import React from 'react';
 
 import { VisualTheme } from '../theming/VisualTheme';
-import { ButtonProps, modeColorGroups } from './shared';
+import { ButtonProps } from './shared';
+import { buttonOutlineStyles } from './styles';
 
 const StyledButtonOutline = styled('button', {
   shouldForwardProp: (prop: string) => isPropValid(prop) && prop !== 'mode',
-})<ButtonProps>(({ mode = VisualTheme.LightMode }: ButtonProps) => {
-  const modeColors = modeColorGroups[mode];
-
-  return css`
-    background: none;
-    border-radius: 4px;
-    border: none;
-    box-shadow: 0 0 0 0 transparent;
-    color: inherit;
-    cursor: pointer;
-    display: inline-block;
-    font-family: inherit;
-    font: inherit;
-    line-height: normal;
-    margin: 0;
-    padding: 0;
-    text-align: center;
-    text-decoration: none;
-    transition: ${timing.fast} box-shadow;
-    vertical-align: middle;
-    white-space: nowrap;
-
-    &:disabled,
-    &[aria-disabled='true'] {
-      cursor: not-allowed;
-      user-select: none;
-    }
-
-    &:focus {
-      box-shadow: 0 0 0 2px ${modeColors.background};
-      outline: none;
-    }
-
-    &:hover {
-      text-decoration: none;
-    }
-  `;
+})<ButtonProps>(({ mode = VisualTheme.LightMode }) => {
+  return buttonOutlineStyles({ mode });
 });
 
 const linkTag = 'a';
