6dd62886672d56f91a5acb736a9e3e543c405e8b
Josh Goldberg <josh@codecademy.com>
2020-11-24 05:31:02 -0500

    Revert "Shared buttonOutlineStyles internally"
    This reverts commit f63f3ea3d893a4ce181c8c3917fe5c1901bed440.

--- packages/gamut/src/Menu/MenuButton.tsx
+++ packages/gamut/src/Menu/MenuButton.tsx
@@ -1,17 +1,13 @@
 import { colors, spacing } from '@codecademy/gamut-styles';
 import styled from '@emotion/styled';
 import { MenuButton as ReachMenuButton } from '@reach/menu-button';
-import { buttonOutlineStyles } from '../Button/styles';
-import { VisualTheme } from '../theming/VisualTheme';
 import { MenuButtonProps } from './MenuButtonToggle';
 
 export const MenuButton = styled(ReachMenuButton)<MenuButtonProps>`
-  ${buttonOutlineStyles({ mode: VisualTheme.LightMode })}
-
   background: none;
   border: none;
   cursor: pointer;
-  padding: ${spacing[16]} ${spacing[8]};
+  padding: ${spacing[4]} ${spacing[8]};
 
   ${({ isExpanded }) => isExpanded && `font-weight: bold;`}
 
