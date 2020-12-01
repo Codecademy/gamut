6fe483b57b1120e21248f673419eec6356e09faf
Amy Resnik <aresnik@codecademy.com>
2020-12-01 12:55:17 -0500

    refactor: rename landing page components and remove atoms from storybook
    rename landing page components to not include "section" in name and remove landing page atom storybook pages

--- packages/gamut-labs/src/landingPage/Description.tsx
+++ packages/gamut-labs/src/landingPage/Description.tsx
@@ -3,22 +3,22 @@ import styled from '@emotion/styled';
 
 import { Markdown } from '@codecademy/gamut';
 
-const Description = styled.div`
+const DescriptionContainer = styled.div`
   margin: 2rem 0 0;
 `;
 
-export type PageSectionDescriptionProps = {
+export type DescriptionProps = {
   text: string;
   className?: string;
   testId?: string;
 };
 
-export const PageSectionDescription: React.FC<PageSectionDescriptionProps> = ({
+export const Description: React.FC<DescriptionProps> = ({
   text,
   className,
   testId,
 }) => (
-  <Description className={className} data-testid={testId}>
+  <DescriptionContainer className={className} data-testid={testId}>
     <Markdown text={text} />
-  </Description>
+  </DescriptionContainer>
 );
