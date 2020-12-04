f45ea95fb19fa0207a051b185c2b031a5fce9e1c
codecaaron <aaron@codecademy.com>
2020-12-04 15:39:30 -0500

    Don't extend just invoke

--- packages/gamut/src/Badge/index.tsx
+++ packages/gamut/src/Badge/index.tsx
@@ -1,15 +1,17 @@
-import styled from '@emotion/styled';
+import React from 'react';
 import { Box } from '../Box';
 
-export const Badge = styled(Box)``;
-
-Badge.defaultProps = {
-  backgroundColor: 'blue',
-  borderRadius: '3px',
-  textColor: 'white',
-  display: 'inline-block',
-  fontSize: 14,
-  marginX: 8,
-  paddingY: 4,
-  paddingX: 12,
-};
+export const Badge: React.FC = ({ children }) => (
+  <Box
+    backgroundColor="blue"
+    borderRadius="3px"
+    textColor="white"
+    display="inline-block"
+    fontSize={14}
+    marginX={8}
+    paddingY={4}
+    paddingX={12}
+  >
+    {children}
+  </Box>
+);
