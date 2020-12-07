12ffc1692fc611b8523abb34a2da79250bdd9869
Jake Hiller <jakemhiller@gmail.com>
2020-12-07 18:11:25 -0500

    fix total

--- packages/styleguide/__tests__/splitTestGlobals.ts
+++ packages/styleguide/__tests__/splitTestGlobals.ts
@@ -1,5 +1,5 @@
 // @ts-expect-error we using janky globals
-global.STORYSHOTS_TOTAL = 4;
+global.STORYSHOTS_TOTAL = 5;
 
 export const splitTestGlobals = (index: number) => {
   // @ts-expect-error we using janky globals
