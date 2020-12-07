0b1ee1c3a9403d62376235e44d9afb546db17d0e
Josh Goldberg <josh@codecademy.com>
2020-12-07 10:00:01 -0500

    Unused variable

--- packages/gamut/src/NotificationList/__tests__/NotificationList-test.tsx
+++ packages/gamut/src/NotificationList/__tests__/NotificationList-test.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { mount } from 'enzyme';
 
 import { NotificationList } from '..';
-import { NotificationItem } from '../NotificationItem';
 
 const notifications = [
   {
