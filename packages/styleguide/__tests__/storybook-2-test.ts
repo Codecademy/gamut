b8774461485c75b4c48e19eb7c0bbb44a98be0d3
Jake Hiller <jakemhiller@gmail.com>
2020-12-08 16:11:08 -0500

    missed one

--- packages/styleguide/__tests__/storybook-2-test.ts
+++ packages/styleguide/__tests__/storybook-2-test.ts
@@ -1,15 +1,3 @@
-import { mount } from 'enzyme';
-import path from 'path';
-import initStoryshots, { renderWithOptions } from '@storybook/addon-storyshots';
 import { runSplitStoryshotsTests } from './runSplitStoryshotsTests';
 
 runSplitStoryshotsTests(2);
-
-initStoryshots({
-  framework: 'react',
-  test: renderWithOptions({
-    renderer: mount,
-  }),
-  storyKindRegex: /^((?!.*?Popover).(?!.*?Overlay).)*$/,
-  configPath: path.join(__dirname, '../.storybook'),
-});
