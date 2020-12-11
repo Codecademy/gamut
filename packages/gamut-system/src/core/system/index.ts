04f2829ffca1529beea2cb2c43626f755ded8416
codecaaron <aaron@codecademy.com>
2020-12-11 11:30:47 -0500

    Don't update things

--- packages/gamut-system/src/core/system/index.ts
+++ packages/gamut-system/src/core/system/index.ts
@@ -1,5 +1,5 @@
 import { AbstractTheme } from '../../types/config';
-import * as BaseConfig from '../../props';
+import * as BASE_CONFIG from '../../props';
 import { compose } from '../compose';
 import { createHandler } from '../createHandler';
 import { entries, keys, mapValues, merge, pick, uniq, values } from 'lodash';
@@ -18,7 +18,7 @@ const create = <
   } as any;
 
   // Merge the the default prop configurations and user defined ones together.
-  const propGroups = merge(BaseConfig, config || {});
+  const propGroups = merge(BASE_CONFIG, config || {});
 
   // Iterate over all the property groups
   entries(propGroups).forEach(([groupKey, groupProps]) => {
