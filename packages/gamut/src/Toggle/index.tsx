f0327664a735140e4ebb5619e283011249c543a3
casey-speer <casey@codecademy.com>
2020-12-21 17:16:41 -0500

    feat(Toggle): adds a className prop to the Toggle component
    Adds support for a className prop to the Toggle component.

--- packages/gamut/src/Toggle/index.tsx
+++ packages/gamut/src/Toggle/index.tsx
@@ -5,6 +5,7 @@ import styles from './styles/index.module.scss';
 
 export type ToggleProps = {
   checked?: boolean;
+  className?: string;
   onChange?: (...args: any[]) => any;
   label?: string;
   disabled?: boolean;
@@ -16,6 +17,7 @@ export class Toggle extends Component<ToggleProps, {}> {
   render() {
     const {
       checked,
+      className,
       onChange,
       label,
       disabled,
@@ -24,10 +26,14 @@ export class Toggle extends Component<ToggleProps, {}> {
     } = this.props;
     return (
       <label
-        className={cx(styles.toggleButton, {
-          [styles.toggled]: checked,
-          [styles.disabled]: disabled,
-        })}
+        className={cx(
+          styles.toggleButton,
+          {
+            [styles.toggled]: checked,
+            [styles.disabled]: disabled,
+          },
+          className
+        )}
         arial-label={label}
         htmlFor={label}
       >
