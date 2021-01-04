1e3ce5133524847ec3384c1a8d68c2acb4671fed
Katie Zutter <katie@codecademy.com>
2021-01-04 14:12:12 -0600

    fix errors from merge

--- packages/gamut-labs/src/experimental/AppHeaderElements/AppHeaderDropdown/index.tsx
+++ packages/gamut-labs/src/experimental/AppHeaderElements/AppHeaderDropdown/index.tsx
@@ -54,7 +54,7 @@ export const AppHeaderDropdown: React.FC<AppHeaderDropdownProps> = ({
       <Popover
         className={styles.topDropdown}
         align="left"
-        offset={-2}
+        verticalOffset={-2}
         outline
         isOpen={isOpen}
         onRequestClose={handleClose}
