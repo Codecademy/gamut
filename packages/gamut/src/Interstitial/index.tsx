3369df0745eb3769945d08f18526cf4712f0ed2a
cass spain <cass@codecademy.com>
2021-01-05 17:21:11 -0500

    remove disable-lint line

--- packages/gamut/src/Interstitial/index.tsx
+++ packages/gamut/src/Interstitial/index.tsx
@@ -27,7 +27,6 @@ export const Interstitial: React.FC<InterstitialProps> = ({
   return (
     <div className={cx(styles.Interstitial, className)}>
       <div className={styles.content}>
-        {/* eslint-disable-next-line */}
         <h1 className={styles.title} ref={headerRef} tabIndex={0}>
           {decoration && <div className={styles.decoration}>{decoration}</div>}
           {title}
