b7666191988dd739d98c73a6bb8ebcf01450270b
Sooin Chung <sooin@codecademy.com>
2021-01-05 20:25:39 -0500

    update stories & add default & add color to notInline

--- packages/gamut-labs/src/experimental/Anchor/index.tsx
+++ packages/gamut-labs/src/experimental/Anchor/index.tsx
@@ -1,4 +1,6 @@
 import { variant } from '@codecademy/gamut-styles';
+import { HandlerProps } from '@codecademy/gamut-system';
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 
 const anchorVariants = variant({
@@ -10,6 +12,22 @@ const anchorVariants = variant({
   },
 });
 
+export type AnchorProps = HandlerProps<typeof anchorVariants> & {
+  color?: string;
+};
+
 export const Anchor = styled.a`
   ${anchorVariants}
+  ${({ color, variant }) => {
+    if (variant === 'notInline' && color) {
+      return css`
+        color: ${color};
+      `;
+    }
+  }}
+  text-decoration: 'underline'
 `;
+
+Anchor.defaultProps = {
+  variant: 'inline',
+};
