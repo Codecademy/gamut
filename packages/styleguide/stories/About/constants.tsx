f40fee968842c0c4c31bd48f9402bbe70979c5ee
codecaaron <aaron@codecademy.com>
2021-01-07 10:17:31 -0500

    Fix things

--- packages/styleguide/stories/About/constants.tsx
+++ packages/styleguide/stories/About/constants.tsx
@@ -4,7 +4,7 @@ export const ABOUT_SECTIONS = [
     emoji: '☀️',
     description:
       'Foundations are the abstract concepts our system is built on: colors, spacing, responsive layouts, and the like.',
-    examples: ['Utilities', 'Colors'],
+    examples: ['Legacy/Utilities', 'Legacy/Colors'],
   },
   {
     kind: 'Layouts',
