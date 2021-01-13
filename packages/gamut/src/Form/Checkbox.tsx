112e5f0eb29160a27bf5137648e73ef1b799c810
Matt Schiller <mschiller@codecademy.com>
2021-01-13 18:27:00 -0500

    Is it broken? Let's find out

--- packages/gamut/src/Form/Checkbox.tsx
+++ packages/gamut/src/Form/Checkbox.tsx
@@ -1,46 +1,50 @@
-import { screenReaderOnly } from '@codecademy/gamut-styles';
+import { pxRem, screenReaderOnly } from '@codecademy/gamut-styles';
+import { noSelect } from '@codecademy/gamut-styles';
+import { colors } from '@codecademy/gamut-styles';
 import styled from '@emotion/styled';
 import cx from 'classnames';
 import React, { InputHTMLAttributes, ReactNode } from 'react';
-import { noSelect } from '@codecademy/gamut-styles';
 
 import styles from './styles/Checkbox.module.scss';
+import { variables } from './_variables';
 
-export type CheckboxProps = InputHTMLAttributes<HTMLInputElement> & {
-  checked?: boolean;
-  className?: string;
-  disabled?: boolean;
-  htmlFor: string;
-  label: ReactNode;
-  multiline?: boolean;
-  name?: string;
-  required?: boolean;
-  value?: string;
-  id?: string;
-};
+export type CheckboxProps = InputHTMLAttributes<HTMLInputElement> &
+  Multiline & {
+    checked?: boolean;
+    className?: string;
+    disabled?: boolean;
+    htmlFor: string;
+    label: ReactNode;
+    name?: string;
+    required?: boolean;
+    value?: string;
+    id?: string;
+  };
 
-const Container = styled.div``;
+type Multiline = { multiline?: boolean };
+
+const blue = colors['blue-500'];
 
 const CheckboxLabel = styled.label`
   ${noSelect}
   display: flex;
   align-items: flex-start;
   cursor: pointer;
-  margin: ($form-padding / 2) 0;
+  margin: (${variables.formPadding} / 2) 0;
   width: 100%;
   font-weight: normal;
-  padding: $form-padding 0;
+  padding: ${variables.formPadding} 0;
 `;
 
-const CheckboxElement = styled.div<{ multiline?: boolean }>`
+const CheckboxElement = styled.div<Multiline>`
   position: relative;
   margin-right: 0.5rem;
   min-width: 1.5rem;
   height: 1.5rem;
-  border: 2px solid $color-gray-200;
-  border-radius: $border-radius;
+  border: 2px solid ${variables.borderColor};
+  border-radius: ${variables.borderRadius};
   transition: all 0.1s ease-in-out;
-  margin-top: ${({ multiline }) => '3px'};
+  margin-top: ${({ multiline }) => multiline && '3px'};
 `;
 
 const CheckboxVector = styled.svg`
@@ -50,12 +54,12 @@ const CheckboxVector = styled.svg`
 `;
 
 const Path = styled.path`
-  fill: $color-blue-500;
+  fill: ${blue};
 `;
 
 const Polyline = styled.polyline`
   fill: none;
-  stroke: $color-gray-100;
+  stroke: ${colors['gray-100']};
   stroke-width: 2;
   stroke-linecap: round;
   stroke-linejoin: round;
@@ -67,41 +71,56 @@ const Polyline = styled.polyline`
 const Input = styled.input`
   ${screenReaderOnly}
 
-  &:focus + ${CheckboxLabel}  > ${CheckboxElement} {
-    // styles here
+  &:focus + ${CheckboxLabel} > ${CheckboxElement} {
+    outline: ${pxRem(2)} solid ${blue};
+    outline-offset: ${pxRem(2)};
   }
 
   &:checked + ${CheckboxLabel} ${CheckboxElement} {
+    border-color: ${blue};
+
+    ${CheckboxVector} {
+      ${Path} {
+        fill: ${blue};
+      }
+
+      ${Polyline} {
+        stroke-dashoffset: 0;
+        transition: stroke-dashoffset ${variables.transitionTime};
+      }
+    }
   }
 
   &:checked:disabled + ${CheckboxLabel} ${CheckboxElement} {
+    ${CheckboxVector} {
+      ${Polyline} {
+        fill: ${variables.itemBackgroundHover};
+      }
+    }
   }
+`;
+
+const CheckboxText = styled.span<Multiline>`
+  align-self: center;
+  font-size: ${({ multiline }) => multiline && '0.75rem'};
 `;
 
 export const Checkbox = React.forwardRef<HTMLInputElement, CheckboxProps>(
   ({ className, label, htmlFor, multiline, id, ...rest }, ref) => (
-    <Container className={className}>
+    <div className={className}>
       <Input id={id || htmlFor} type="checkbox" {...rest} ref={ref} />
-      <CheckboxLabel className={styles.checkboxLabel} htmlFor={id || htmlFor}>
+      <CheckboxLabel htmlFor={id || htmlFor}>
         <CheckboxElement multiline={multiline}>
           <CheckboxVector width="24px" height="24px" viewBox="0 0 20 20">
             <Path
               d="M3,1 L17,1 L17,1 C18.1045695,1 19,1.8954305 19,3 L19,17 L19,17 C19,18.1045695 18.1045695,19 17,19 L3,19 L3,19 C1.8954305,19 1,18.1045695 1,17 L1,3 L1,3 C1,1.8954305 1.8954305,1 3,1 Z"
-              className={styles.squareVector}
               fill="none"
             />
             <Polyline points="4 11 8 15 16 6" />
           </CheckboxVector>
         </CheckboxElement>
-        <span
-          className={cx(
-            styles.checkboxText,
-            multiline && styles.checkboxTextMultiline
-          )}
-        >
-          {label}
-        </span>
+        <CheckboxText multiline={multiline}>{label}</CheckboxText>
       </CheckboxLabel>
-    </Container>
+    </div>
   )
 );
