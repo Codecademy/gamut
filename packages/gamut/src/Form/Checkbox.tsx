bca1b68fb1af43ec083f75f911feae9edca43eaf
Matt Schiller <mschiller@codecademy.com>
2021-01-13 18:33:28 -0500

    dope dope dope

--- packages/gamut/src/Form/Checkbox.tsx
+++ packages/gamut/src/Form/Checkbox.tsx
@@ -2,10 +2,8 @@ import { pxRem, screenReaderOnly } from '@codecademy/gamut-styles';
 import { noSelect } from '@codecademy/gamut-styles';
 import { colors } from '@codecademy/gamut-styles';
 import styled from '@emotion/styled';
-import cx from 'classnames';
 import React, { InputHTMLAttributes, ReactNode } from 'react';
 
-import styles from './styles/Checkbox.module.scss';
 import { variables } from './_variables';
 
 export type CheckboxProps = InputHTMLAttributes<HTMLInputElement> &
@@ -53,10 +51,6 @@ const CheckboxVector = styled.svg`
   left: -2px;
 `;
 
-const Path = styled.path`
-  fill: ${blue};
-`;
-
 const Polyline = styled.polyline`
   fill: none;
   stroke: ${colors['gray-100']};
@@ -80,7 +74,7 @@ const Input = styled.input`
     border-color: ${blue};
 
     ${CheckboxVector} {
-      ${Path} {
+      path {
         fill: ${blue};
       }
 
@@ -112,7 +106,7 @@ export const Checkbox = React.forwardRef<HTMLInputElement, CheckboxProps>(
       <CheckboxLabel htmlFor={id || htmlFor}>
         <CheckboxElement multiline={multiline}>
           <CheckboxVector width="24px" height="24px" viewBox="0 0 20 20">
-            <Path
+            <path
               d="M3,1 L17,1 L17,1 C18.1045695,1 19,1.8954305 19,3 L19,17 L19,17 C19,18.1045695 18.1045695,19 17,19 L3,19 L3,19 C1.8954305,19 1,18.1045695 1,17 L1,3 L1,3 C1,1.8954305 1.8954305,1 3,1 Z"
               fill="none"
             />
