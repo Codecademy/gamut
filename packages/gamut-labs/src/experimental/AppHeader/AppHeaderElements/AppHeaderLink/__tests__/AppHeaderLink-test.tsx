f1aa4f380afc0413091a99dc11dab5fe8a666b36
Katie Zutter <katie@codecademy.com>
2021-01-14 14:11:35 -0600

    fix tests

--- packages/gamut-labs/src/experimental/AppHeader/AppHeaderElements/AppHeaderLink/__tests__/AppHeaderLink-test.tsx
+++ packages/gamut-labs/src/experimental/AppHeader/AppHeaderElements/AppHeaderLink/__tests__/AppHeaderLink-test.tsx
@@ -1,3 +1,5 @@
+import { theme } from '@codecademy/gamut-styles';
+import { ThemeProvider } from '@emotion/react';
 import { render, screen } from '@testing-library/react';
 import React from 'react';
 
@@ -19,7 +21,11 @@ const props: AppHeaderLinkProps = {
 };
 
 const renderAppHeaderLink = () => {
-  return render(<AppHeaderLink {...props} />);
+  return render(
+    <ThemeProvider theme={theme}>
+      <AppHeaderLink {...props} />
+    </ThemeProvider>
+  );
 };
 
 describe('AppHeaderLink', () => {
