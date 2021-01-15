a6e2da0e9dae168a572aaf437653dc7d489df46a
codecaaron <aaron@codecademy.com>
2021-01-15 11:56:40 -0500

    Fixes tests

--- packages/macros/__tests__/macros.test.js
+++ packages/macros/__tests__/macros.test.js
@@ -1,14 +1,16 @@
 'use strict';
-
 const babel = require('@babel/core');
+const path = require('path');
 
 const options = {
   plugins: [require.resolve('babel-plugin-macros')],
 };
 
+const resolvePath = (relative) => path.resolve(__dirname, relative);
+
 describe('title.macro', () => {
   test('should transform a js file', () => {
-    const inputFile = './example/stories/World/Hello.js';
+    const inputFile = resolvePath('../example/stories/World/Hello.js');
 
     const result = babel.transformFileSync(inputFile, options);
 
@@ -16,7 +18,9 @@ describe('title.macro', () => {
   });
 
   test('should transform a story file', () => {
-    const inputFile = './example/stories/World/Example.stories.js';
+    const inputFile = resolvePath(
+      '../example/stories/World/Example.stories.js'
+    );
 
     const result = babel.transformFileSync(inputFile, options);
 
