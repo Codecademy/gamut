9f193ecc2ee5fba9e525df0b4a1c750b4b8598b0
codecaaron <aaron@codecademy.com>
2021-01-15 16:19:09 -0500

    Mapping

--- jest.config.js
+++ jest.config.js
@@ -18,7 +18,6 @@ module.exports = {
     'ts',
     'tsx',
     'd.ts',
-    'mdx',
   ],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|md)$':
