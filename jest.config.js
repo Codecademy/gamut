21bd5b30501d9137ff911c027fa3fb09c566176e
codecaaron <aaron@codecademy.com>
2021-01-15 13:52:46 -0500

    Add mdx to module extensions

--- jest.config.js
+++ jest.config.js
@@ -18,6 +18,7 @@ module.exports = {
     'ts',
     'tsx',
     'd.ts',
+    'mdx',
   ],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|md)$':
