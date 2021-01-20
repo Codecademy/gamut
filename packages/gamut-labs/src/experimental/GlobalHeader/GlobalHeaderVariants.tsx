5f5f5e66562f06551d530b158d705db82f5085e7
Katie Zutter <katie@codecademy.com>
2021-01-20 15:55:36 -0600

    add GlobalHeader

---
+++ packages/gamut-labs/src/experimental/GlobalHeader/GlobalHeaderVariants.tsx
@@ -0,0 +1,94 @@
+import { ReactElement } from 'react';
+
+import { AppHeaderItemsProp } from '../AppHeader';
+import { AppHeaderItem } from '../AppHeader/AppHeaderElements/types';
+import {
+  communityDropdown,
+  courseCatalog,
+  forEnterprise,
+  login,
+  logo,
+  myHome,
+  notifications,
+  plansPricingDropdown,
+  profile,
+  proLogo,
+  resourcesDropdown,
+  search,
+  signUp,
+  upgradeToPro,
+} from './GlobalHeaderItems';
+
+export const anonHeaderItems = (
+  renderSearch?: () => ReactElement
+): AppHeaderItemsProp => {
+  const leftItems: AppHeaderItem[] = [
+    logo,
+    courseCatalog,
+    resourcesDropdown,
+    communityDropdown,
+    plansPricingDropdown,
+    forEnterprise,
+  ];
+
+  const rightItems: AppHeaderItem[] = [];
+  renderSearch && rightItems.push(search(renderSearch));
+  rightItems.push(login);
+  rightItems.push(signUp);
+
+  return {
+    left: leftItems,
+    right: rightItems,
+  };
+};
+
+export const freeHeaderItems = (
+  renderSearch?: () => ReactElement,
+  renderNotifications?: () => ReactElement,
+  renderProfile?: () => ReactElement
+): AppHeaderItemsProp => {
+  const leftItems: AppHeaderItem[] = [
+    logo,
+    myHome,
+    courseCatalog,
+    resourcesDropdown,
+    communityDropdown,
+    plansPricingDropdown,
+    forEnterprise,
+  ];
+
+  const rightItems: AppHeaderItem[] = [];
+  renderSearch && rightItems.push(search(renderSearch));
+  renderNotifications && rightItems.push(notifications(renderNotifications));
+  renderProfile && rightItems.push(profile(renderProfile));
+  rightItems.push(upgradeToPro);
+
+  return {
+    left: leftItems,
+    right: rightItems,
+  };
+};
+
+export const proHeaderItems = (
+  renderSearch?: () => ReactElement,
+  renderNotifications?: () => ReactElement,
+  renderProfile?: () => ReactElement
+): AppHeaderItemsProp => {
+  const leftItems: AppHeaderItem[] = [
+    proLogo,
+    myHome,
+    courseCatalog,
+    resourcesDropdown,
+    communityDropdown,
+  ];
+
+  const rightItems: AppHeaderItem[] = [];
+  renderSearch && rightItems.push(search(renderSearch));
+  renderNotifications && rightItems.push(notifications(renderNotifications));
+  renderProfile && rightItems.push(profile(renderProfile));
+
+  return {
+    left: leftItems,
+    right: rightItems,
+  };
+};
---
+++ packages/gamut-labs/src/experimental/GlobalHeader/index.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+
+import { AppHeader } from '../AppHeader';
+import { AppHeaderItem } from '../AppHeader/AppHeaderElements/types';
+import {
+  anonHeaderItems,
+  freeHeaderItems,
+  proHeaderItems,
+} from './GlobalHeaderVariants';
+import styles from './styles.scss';
+import { AnonHeader, FreeHeader, ProHeader } from './types';
+
+export type HeaderClickHandler = (
+  event: React.MouseEvent,
+  item: AppHeaderItem
+) => void;
+
+export type GlobalHeaderProps = {
+  variant: AnonHeader | FreeHeader | ProHeader;
+  onClick: HeaderClickHandler;
+};
+
+const getAppHeaderItems = (variant: AnonHeader | FreeHeader | ProHeader) => {
+  switch (variant.type) {
+    case 'anon':
+      return anonHeaderItems(variant.renderSearch);
+    case 'free':
+      return freeHeaderItems(
+        variant.renderSearch,
+        variant.renderNotifications,
+        variant.renderProfile
+      );
+    case 'pro':
+      return proHeaderItems(
+        variant.renderSearch,
+        variant.renderNotifications,
+        variant.renderProfile
+      );
+  }
+};
+
+export const GlobalHeader: React.FC<GlobalHeaderProps> = ({
+  variant,
+  onClick,
+}) => {
+  return (
+    <AppHeader
+      className={styles.globalHeader}
+      items={getAppHeaderItems(variant)}
+      onClick={onClick}
+    />
+  );
+};
