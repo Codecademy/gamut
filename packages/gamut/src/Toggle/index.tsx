0b3f1e8aeb6d9c386f61f94b0e8c7889cbf7df96
codecaaron <aaron@codecademy.com>
2021-01-22 14:34:17 -0500

    Add focus styles

--- packages/gamut/src/Toggle/index.tsx
+++ packages/gamut/src/Toggle/index.tsx
@@ -1,7 +1,10 @@
-import cx from 'classnames';
+import { screenReaderOnly } from '@codecademy/gamut-styles';
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
 import React, { Component } from 'react';
 
-import styles from './styles/index.module.scss';
+import { Box } from '../Box';
+import { HiddenText } from '../HiddenText';
 
 export type ToggleProps = {
   checked?: boolean;
@@ -9,9 +12,74 @@ export type ToggleProps = {
   onChange?: (...args: any[]) => any;
   label?: string;
   disabled?: boolean;
-  theme?: 'gray-blue' | 'purple';
-  size?: 'small' | 'medium';
+  variant?: keyof typeof colors;
+  size?: keyof typeof sizes;
 };
+
+const sizes = {
+  medium: {
+    height: '30px',
+    width: '60px',
+  },
+  small: {
+    height: '16px',
+    width: '32px',
+  },
+};
+
+const colors = {
+  'gray-blue': 'blue-500',
+  purple: 'hyper',
+} as const;
+
+const Track = styled(Box)`
+  ${Box} {
+    transition: all 0.5s cubic-bezier(0.23, 1, 0.32, 1);
+  }
+`;
+
+export type LabelProps = {
+  disabled?: boolean;
+  variant?: keyof typeof colors;
+};
+
+const ToggleInput = styled.input(screenReaderOnly);
+
+const ToggleLabel = styled.label<LabelProps>(
+  ({ disabled, variant = 'gray-blue', theme }) => {
+    const currentColor = theme.colors[colors[variant]];
+    return css`
+      display: inline-block;
+      position: relative;
+      cursor: pointer;
+      border: 0;
+      padding: 0;
+      ${disabled &&
+      css`
+        cursor: auto;
+        opacity: 0.75;
+      `}
+
+      ${ToggleInput}:focus + ${Track} {
+        &:after {
+          content: '';
+          border-radius: 99rem;
+          position: absolute;
+          width: calc(100% + 8px);
+          height: calc(100% + 8px);
+          top: -4px;
+          left: -4px;
+          border: 2px solid ${currentColor};
+        }
+      }
+
+      ${Track} {
+        background-color: ${currentColor};
+        transition: all 0.2s ease;
+      }
+    `;
+  }
+);
 
 export class Toggle extends Component<ToggleProps, {}> {
   render() {
@@ -21,36 +89,41 @@ export class Toggle extends Component<ToggleProps, {}> {
       onChange,
       label,
       disabled,
-      theme = 'gray-blue',
+      variant = 'gray-blue',
       size = 'medium',
     } = this.props;
+
+    const sizeStyles = sizes[size];
+    const thumbStyles = checked ? { right: '5%' } : { left: '5%' };
+
     return (
-      <label
-        className={cx(
-          styles.toggleButton,
-          {
-            [styles.toggled]: checked,
-            [styles.disabled]: disabled,
-          },
-          className
-        )}
+      <ToggleLabel
+        className={className}
         aria-label={label}
         htmlFor={label}
+        variant={variant}
+        disabled={disabled}
       >
-        <input
+        <HiddenText>{label}</HiddenText>
+        <ToggleInput
           type="checkbox"
           checked={checked}
-          className={styles.invisible}
           id={label}
           disabled={disabled}
           onChange={onChange}
         />
-        <span className={styles.invisible}>{label}</span>
-        <div
-          className={cx(styles.track, styles[theme], styles[`track-${size}`])}
-        />
-        <div className={cx(styles.thumb, styles[`thumb-${size}`])} />
-      </label>
+        <Track {...sizeStyles} borderRadius="99rem" position="relative">
+          <Box
+            borderRadius="50%"
+            backgroundColor="white"
+            position="absolute"
+            top="10%"
+            bottom="10%"
+            width="40%"
+            {...thumbStyles}
+          />
+        </Track>
+      </ToggleLabel>
     );
   }
 }
