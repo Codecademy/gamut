2c9848e929eca003b014fd76bca89c542bd788a8
Alexander Onate <aionate0812@gmail.com>
2021-01-22 17:23:26 -0500

    inital changes to page hero to take in videos

--- packages/gamut-labs/src/landingPage/PageHero.tsx
+++ packages/gamut-labs/src/landingPage/PageHero.tsx
@@ -1,3 +1,4 @@
+import { VideoProps } from '@codecademy/gamut';
 import { Column, LayoutGrid } from '@codecademy/gamut';
 import { breakpoints } from '@codecademy/gamut-styles';
 import styled from '@emotion/styled';
@@ -18,45 +19,64 @@ const Image = styled.img`
   width: 100%;
 `;
 
+export type ImageProps = {
+  src: string;
+  alt: string;
+};
+
+export type MediaProps =
+  | ({
+      type: 'image';
+    } & ImageProps)
+  | ({
+      type: 'video';
+    } & VideoProps);
+
 export type PageHeroProps = BaseProps & {
-  /**
-   * Hero image URL
-   */
-  imgSrc?: string;
-  /**
-   * Hero image alt text (for screen readers)
-   */
-  imgAlt?: string;
+  media?: MediaProps;
 };
 
 export const PageHero: React.FC<PageHeroProps> = ({
   title,
   desc,
   cta,
-  imgSrc,
-  imgAlt = '',
+  media,
   testId,
   onAnchorClick,
-}) => (
-  <LayoutGrid testId={testId}>
-    <Column
-      size={{
-        xs: 12,
-        sm: imgSrc ? 9 : 12,
-      }}
-    >
-      {title && <Title isPageHeading>{title}</Title>}
-      {desc && <Description text={desc} onAnchorClick={onAnchorClick} />}
-      {cta && (
-        <CTA href={cta.href} onCtaButtonClick={cta.onClick}>
-          {cta.text}
-        </CTA>
-      )}
-    </Column>
-    {imgSrc && (
-      <RightColumn size={3}>
-        <Image src={imgSrc} alt={imgAlt} />
-      </RightColumn>
-    )}
-  </LayoutGrid>
-);
+}) => {
+  const renderMedia = () => {
+    if (media && media.type === 'image') {
+      return (
+        <RightColumn size={3}>
+          <Image src={media.src} alt={media.alt} />
+        </RightColumn>
+      );
+    } else {
+      // return (
+      //   <RightColumn size={3}>
+      //     <Image src={imgSrc} alt={imgAlt} />
+      //   </RightColumn>
+      // );
+    }
+  };
+
+  return (
+    <LayoutGrid testId={testId}>
+      <Column
+        size={{
+          xs: 12,
+          sm: media ? 9 : 12,
+        }}
+      >
+        {title && <Title isPageHeading>{title}</Title>}
+        {desc && <Description text={desc} onAnchorClick={onAnchorClick} />}
+        {cta && (
+          <CTA href={cta.href} onCtaButtonClick={cta.onClick}>
+            {cta.text}
+          </CTA>
+        )}
+      </Column>
+      {media && renderMedia()}
+    </LayoutGrid>
+  );
+};
