5dd2f3d20c93b686dc53b4f099f2c1c8e8e73b58
codecaaron <aaron@codecademy.com>
2021-01-23 20:10:50 -0500

    Remove theme garbage

--- packages/styleguide/.storybook/theme.ts
+++ packages/styleguide/.storybook/theme.ts
@@ -1,25 +1,9 @@
-import { theme as gamutTheme } from '@codecademy/gamut-styles';
 import { create } from '@storybook/theming';
 import logo from './assets/gamut-logo.svg';
 
-export const theme = create(
-  {
-    base: 'light',
-    brandTitle: 'Gamut',
-    brandImage: logo,
-    brandUrl: '/',
-
-    // Colors
-    appBg: gamutTheme.colors.white,
-
-    //Text
-    textColor: gamutTheme.colors.navy,
-
-    fontBase: gamutTheme.fontFamily.base,
-  },
-  {
-    random: { 1: 2 },
-  }
-);
-
-console.log(theme);
+export const theme = create({
+  base: 'light',
+  brandTitle: 'Gamut',
+  brandImage: logo,
+  brandUrl: '/',
+});
