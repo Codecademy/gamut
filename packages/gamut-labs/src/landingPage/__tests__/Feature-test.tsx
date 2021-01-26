c2b2284b3a1103174122bb13bb30b66799d9e10a
Josh Goldberg <josh@codecademy.com>
2021-01-26 18:06:37 -0500

    Corrected builds and filled out README.md

--- packages/gamut-labs/src/landingPage/__tests__/Feature-test.tsx
+++ packages/gamut-labs/src/landingPage/__tests__/Feature-test.tsx
@@ -1,63 +1,57 @@
-import { mount } from 'enzyme';
-import React from 'react';
+import { setupEnzyme } from '@codecademy/gamut-tests';
 
-import { Feature, FeatureProps } from '..';
+import { Feature } from '..';
 
-const renderComponent = (overrides: Partial<FeatureProps> = {}) => {
-  const props: FeatureProps = {
-    featuresMedia: 'image',
-    imgSrc: 'https://content.codecademy.com/courses/free/boba.svg',
-    imgAlt: 'Codey Boba Tea',
-    ...overrides,
-  };
-
-  return mount(<Feature {...props} />);
-};
+const renderWrapper = setupEnzyme(Feature, {
+  featuresMedia: 'image',
+  imgSrc: 'https://content.codecademy.com/courses/free/boba.svg',
+  imgAlt: 'Codey Boba Tea',
+});
 
 describe('Feature', () => {
   it('renders a title when title prop is provided', () => {
-    const wrapper = renderComponent({ title: 'Test Title' });
+    const { wrapper } = renderWrapper({ title: 'Test Title' });
     expect(wrapper.find('h3').text()).toEqual('Test Title');
   });
 
   it('does not render a title when title prop is not provided', () => {
-    const wrapper = renderComponent();
+    const { wrapper } = renderWrapper();
     expect(wrapper.find('h3')).toHaveLength(0);
   });
 
   it('renders a description when desc prop is provided', () => {
-    const wrapper = renderComponent({ desc: 'Test Description' });
+    const { wrapper } = renderWrapper({ desc: 'Test Description' });
     expect(wrapper.find('p').text()).toEqual('Test Description');
   });
 
   it('does not render a description when desc prop is not provided', () => {
-    const wrapper = renderComponent();
+    const { wrapper } = renderWrapper();
     expect(wrapper.find('p')).toHaveLength(0);
   });
 
   it('renders a no-media feature', () => {
-    const wrapper = renderComponent({ featuresMedia: 'none' });
+    const { wrapper } = renderWrapper({ featuresMedia: 'none' });
     expect(wrapper.find('img[data-testid="feature-image"]')).toHaveLength(0);
     expect(wrapper.find('img[data-testid="feature-icon"]')).toHaveLength(0);
     expect(wrapper.find('div[data-testid="feature-stat"]')).toHaveLength(0);
   });
 
   it('renders an image', () => {
-    const wrapper = renderComponent();
+    const { wrapper } = renderWrapper();
     expect(wrapper.find('img[data-testid="feature-image"]')).toHaveLength(1);
     expect(wrapper.find('img[data-testid="feature-icon"]')).toHaveLength(0);
     expect(wrapper.find('div[data-testid="feature-stat"]')).toHaveLength(0);
   });
 
   it('renders an icon', () => {
-    const wrapper = renderComponent({ featuresMedia: 'icon' });
+    const { wrapper } = renderWrapper({ featuresMedia: 'icon' });
     expect(wrapper.find('img[data-testid="feature-image"]')).toHaveLength(0);
     expect(wrapper.find('img[data-testid="feature-icon"]')).toHaveLength(1);
     expect(wrapper.find('div[data-testid="feature-stat"]')).toHaveLength(0);
   });
 
   it('renders a stat', () => {
-    const wrapper = renderComponent({
+    const { wrapper } = renderWrapper({
       featuresMedia: 'stat',
       statText: '99.9999%',
     });
--- packages/gamut-labs/src/landingPage/__tests__/FeaturesSection-test.tsx
+++ packages/gamut-labs/src/landingPage/__tests__/FeaturesSection-test.tsx
@@ -1,54 +1,41 @@
-import { mount } from 'enzyme';
-import React from 'react';
+import { setupEnzyme } from '@codecademy/gamut-tests';
 
-import {
-  CTA,
-  Description,
-  Feature,
-  PageFeatures,
-  PageFeaturesProps,
-  Title,
-} from '..';
+import { CTA, Description, Feature, PageFeatures, Title } from '..';
 
-const renderComponent = (overrides: Partial<PageFeaturesProps> = {}) => {
-  const props = {
-    features: [
-      {
-        imgSrc: 'https://content.codecademy.com/courses/free/boba.svg',
-        imgAlt: 'Codey boba tea',
-        title: 'Software Engineer',
-        desc: '**Software Engineer**. Example link [here](#).',
-      },
-    ],
-    ...overrides,
-  };
-
-  return mount(<PageFeatures {...props} />);
-};
+const renderWrapper = setupEnzyme(PageFeatures, {
+  features: [
+    {
+      imgSrc: 'https://content.codecademy.com/courses/free/boba.svg',
+      imgAlt: 'Codey boba tea',
+      title: 'Software Engineer',
+      desc: '**Software Engineer**. Example link [here](#).',
+    },
+  ],
+});
 
 describe('PageFeatures', () => {
   it('renders a title when title prop is provided', () => {
-    const wrapper = renderComponent({ title: 'Test Title' });
+    const { wrapper } = renderWrapper({ title: 'Test Title' });
     expect(wrapper.find(Title).text()).toEqual('Test Title');
   });
 
   it('does not render a title when title prop is not provided', () => {
-    const wrapper = renderComponent();
+    const { wrapper } = renderWrapper();
     expect(wrapper.find(Title)).toHaveLength(0);
   });
 
   it('renders a description when desc prop is provided', () => {
-    const wrapper = renderComponent({ desc: 'Test Description' });
+    const { wrapper } = renderWrapper({ desc: 'Test Description' });
     expect(wrapper.find(Description).prop('text')).toEqual('Test Description');
   });
 
   it('does not render a description when desc prop is not provided', () => {
-    const wrapper = renderComponent();
+    const { wrapper } = renderWrapper();
     expect(wrapper.find(Description)).toHaveLength(0);
   });
 
   it('renders a cta button when cta prop is provided', () => {
-    const wrapper = renderComponent({
+    const { wrapper } = renderWrapper({
       cta: {
         text: 'Click Me',
         href: '#',
@@ -58,12 +45,12 @@ describe('PageFeatures', () => {
   });
 
   it('does not render a cta button when cta prop is not provided', () => {
-    const wrapper = renderComponent();
+    const { wrapper } = renderWrapper();
     expect(wrapper.find(CTA)).toHaveLength(0);
   });
 
   it('renders a PageFeature component for each element in the features array prop', () => {
-    const wrapper = renderComponent({
+    const { wrapper } = renderWrapper({
       features: [
         {
           imgSrc: 'https://content.codecademy.com/courses/free/boba.svg',
