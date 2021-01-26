d4f2c744500c25e6642b03d33828b2594d895ace
christian-dinh <christian.t.dinh@gmail.com>
2021-01-26 17:58:46 -0500

    PR feedback

--- packages/gamut-labs/src/landingPage/PageVideoGallery.tsx
+++ packages/gamut-labs/src/landingPage/PageVideoGallery.tsx
@@ -1,31 +1,31 @@
 import { Video } from '@codecademy/gamut';
-import { breakpoints } from '@codecademy/gamut-styles';
+import { mediaQueries } from '@codecademy/gamut-styles';
 import styled from '@emotion/styled';
 import React from 'react';
 
 const Grid = styled.div`
   display: grid;
-  gap: 32px;
+  grid-template: 1fr / 1fr;
+  gap: 8px;
 
-  // there must be a mathematical explanation for why ~12:5 is the correct ratio for
-  // the column widths but I do not know what it is. Regardless, the effect is that
-  // all three videos will maintain a 16:9 aspect ratio with an even gap between them.
-  grid-template: 1fr 1fr / 12fr 5fr;
+  ${mediaQueries.sm} {
+    // there must be a mathematical explanation for why ~12:5 is the correct ratio for
+    // the column widths but I do not know what it is. Regardless, the effect is that
+    // all three videos will maintain a 16:9 aspect ratio with an even gap between them.
+    grid-template: 1fr 1fr / 12fr 5fr;
 
-  @media only screen and (max-width: ${breakpoints.lg}) {
-    gap: 24px;
-  }
-  @media only screen and (max-width: ${breakpoints.md}) {
     gap: 16px;
   }
-  @media only screen and (max-width: ${breakpoints.sm}) {
-    grid-template: 1fr / 1fr;
-    gap: 8px;
+  ${mediaQueries.md} {
+    gap: 24px;
+  }
+  ${mediaQueries.lg} {
+    gap: 32px;
   }
 `;
 
 const GridVideo = styled(Video)`
-  @media only screen and (min-width: ${breakpoints.sm}) {
+  ${mediaQueries.sm} {
     &:first-of-type {
       grid-row: 1 / span 2;
     }
@@ -39,15 +39,12 @@ export type PageVideo = {
 };
 
 export type PageVideoGalleryProps = {
-  /**
-   * Array of {url, title, placeholderImage}
-   */
-  videos?: PageVideo[];
+  videos: PageVideo[];
   testId?: string;
 };
 
 export const PageVideoGallery: React.FC<PageVideoGalleryProps> = ({
-  videos = [],
+  videos,
   testId,
 }) => (
   <Grid data-testid={testId}>
