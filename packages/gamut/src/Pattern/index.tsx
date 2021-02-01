31736c35b0636e2e17684592c14decce2bb7d622
Natalie Wee <natalieweesh@gmail.com>
2021-02-01 16:23:23 -0500

    separate out defs

--- packages/gamut/src/Pattern/index.tsx
+++ packages/gamut/src/Pattern/index.tsx
@@ -2,6 +2,7 @@ import styled from '@emotion/styled';
 import React from 'react';
 
 import { Box, BoxProps } from '../Box';
+import { defs } from './defs';
 
 export type PatternName =
   | 'diagonalStripesLoose'
@@ -22,124 +23,7 @@ const Svg = styled.svg`
 export const Pattern: React.FC<PatternProps> = ({ name, ...props }) => (
   <Box {...props}>
     <Svg>
-      <defs>
-        {name === 'diagonalStripesLoose' && (
-          <pattern
-            id="diagonalStripesLoose"
-            x="0"
-            y="0"
-            width="16"
-            height="16"
-            patternUnits="userSpaceOnUse"
-          >
-            <rect y="15" width="1" height="1" fill="currentColor" />
-            <rect x="1" y="14" width="1" height="1" fill="currentColor" />
-            <rect x="2" y="13" width="1" height="1" fill="currentColor" />
-            <rect x="3" y="12" width="1" height="1" fill="currentColor" />
-            <rect x="4" y="11" width="1" height="1" fill="currentColor" />
-            <rect x="5" y="10" width="1" height="1" fill="currentColor" />
-            <rect x="6" y="9" width="1" height="1" fill="currentColor" />
-            <rect x="7" y="8" width="1" height="1" fill="currentColor" />
-            <rect x="8" y="7" width="1" height="1" fill="currentColor" />
-            <rect x="9" y="6" width="1" height="1" fill="currentColor" />
-            <rect x="10" y="5" width="1" height="1" fill="currentColor" />
-            <rect x="11" y="4" width="1" height="1" fill="currentColor" />
-            <rect x="12" y="3" width="1" height="1" fill="currentColor" />
-            <rect x="13" y="2" width="1" height="1" fill="currentColor" />
-            <rect x="14" y="1" width="1" height="1" fill="currentColor" />
-            <rect x="15" width="1" height="1" fill="currentColor" />
-          </pattern>
-        )}
-        {name === 'diagonalStripesRegular' && (
-          <pattern
-            id="diagonalStripesRegular"
-            x="0"
-            y="0"
-            width="8"
-            height="8"
-            patternUnits="userSpaceOnUse"
-          >
-            <g clipPath="url(#clipDiagonalStripesRegular)">
-              <rect width="8" height="8" fill="white" />
-              <rect y="7" width="1" height="1" fill="currentColor" />
-              <rect x="1" y="6" width="1" height="1" fill="currentColor" />
-              <rect x="2" y="5" width="1" height="1" fill="currentColor" />
-              <rect x="3" y="4" width="1" height="1" fill="currentColor" />
-              <rect x="4" y="3" width="1" height="1" fill="currentColor" />
-              <rect x="5" y="2" width="1" height="1" fill="currentColor" />
-              <rect x="6" y="1" width="1" height="1" fill="currentColor" />
-              <rect x="7" width="1" height="1" fill="currentColor" />
-            </g>
-            <defs>
-              <clipPath id="clipDiagonalStripesRegular">
-                <rect width="8" height="8" fill="white" />
-              </clipPath>
-            </defs>
-          </pattern>
-        )}
-        {name === 'diagonalStripesDense' && (
-          <pattern
-            id="diagonalStripesDense"
-            x="0"
-            y="0"
-            width="4"
-            height="4"
-            patternUnits="userSpaceOnUse"
-          >
-            <g clipPath="url(#clipDiagonalStripesDense)">
-              <rect width="4" height="4" fill="white" />
-              <rect y="3" width="1" height="1" fill="currentColor" />
-              <rect x="1" y="2" width="1" height="1" fill="currentColor" />
-              <rect x="2" y="1" width="1" height="1" fill="currentColor" />
-              <rect x="3" width="1" height="1" fill="currentColor" />
-            </g>
-            <defs>
-              <clipPath id="clipDiagonalStripesDense">
-                <rect width="4" height="4" fill="white" />
-              </clipPath>
-            </defs>
-          </pattern>
-        )}
-        {name === 'dotsLoose' && (
-          <pattern
-            id="dotsLoose"
-            x="0"
-            y="0"
-            width="16"
-            height="16"
-            patternUnits="userSpaceOnUse"
-          >
-            <rect width="1" height="1" fill="currentColor" />
-            <rect x="8" y="8" width="1" height="1" fill="currentColor" />
-          </pattern>
-        )}
-        {name === 'dotsRegular' && (
-          <pattern
-            id="dotsRegular"
-            x="0"
-            y="0"
-            width="8"
-            height="8"
-            patternUnits="userSpaceOnUse"
-          >
-            <rect width="1" height="1" fill="currentColor" />
-            <rect x="4" y="4" width="1" height="1" fill="currentColor" />
-          </pattern>
-        )}
-        {name === 'dotsDense' && (
-          <pattern
-            id="dotsDense"
-            x="0"
-            y="0"
-            width="4"
-            height="4"
-            patternUnits="userSpaceOnUse"
-          >
-            <rect width="1" height="1" fill="currentColor" />
-            <rect x="2" y="2" width="1" height="1" fill="currentColor" />
-          </pattern>
-        )}
-      </defs>
+      {defs(name)}
       <rect x="0" y="0" width="100%" height="100%" fill={`url(#${name})`} />
     </Svg>
   </Box>
