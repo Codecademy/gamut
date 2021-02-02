9816f5d7c81731816898f37dafff89aef64b283d
Sanam Aghdaey <sanam@codecademy.com>
2021-02-02 10:46:13 -0500

    removing AppHeaderMobile from this PR

--- packages/gamut-labs/src/experimental/index.ts
+++ packages/gamut-labs/src/experimental/index.ts
@@ -5,7 +5,6 @@ export * from './AppHeader';
 export * from './AppHeader/AppHeaderElements/AppHeaderDropdown';
 export * from './AppHeader/AppHeaderElements/AppHeaderLink';
 export * from './AppHeader/AppHeaderElements/AppHeaderLogo';
-export * from './AppHeaderMobile';
 export * from './AppHeaderMobile/AppHeaderLinkMobile';
 export * from './AppHeaderMobile/AppHeaderMainMenuMobile';
 export * from './AppHeaderMobile/AppHeaderSubMenuMobile';
