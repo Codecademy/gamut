6e139359c8a2a1ff3b193225f2220201c0c90f96
Josh Goldberg <josh@codecademy.com>
2021-02-02 14:28:28 -0500

    WIP: Emotion ToolTip

--- packages/gamut/src/ToolTip/index.tsx
+++ packages/gamut/src/ToolTip/index.tsx
@@ -1,8 +1,8 @@
-import cx from 'classnames';
+import { fontSmoothing, pxRem } from '@codecademy/gamut-styles';
+import styled from '@emotion/styled';
 import React, { ReactNode } from 'react';
 
 import { VisualTheme } from '../theming/VisualTheme';
-import styles from './styles.module.scss';
 
 export enum ToolTipPosition {
   BottomLeft = 'bottom-left',
@@ -10,6 +10,157 @@ export enum ToolTipPosition {
   TopLeft = 'top-left',
   TopRight = 'top-right',
 }
+
+const arrowWidth = `1rem`;
+const arrowHeight = `0.5rem`;
+const arrowPaddingHorizontal = `1.5rem`;
+const arrowPaddingVertical = `0.25rem`;
+
+const containerOffsetHorizontal = `calc(100% - (${arrowWidth} / 2) + ${arrowPaddingHorizontal})`;
+const containerOffsetVertical = `calc(100% + ${arrowHeight} + ${arrowPaddingVertical})`;
+
+const arrowOffsetHorizontal = `calc(${arrowPaddingHorizontal} - (${arrowWidth} / 2))`;
+const arrowOffsetVertical = `calc(-${arrowHeight} / 2)`;
+
+const slightShadow = `rgba(0, 0, 0, 0.15)`;
+
+const TooltipWrapper = styled.div`
+  position: relative;
+  display: inline-flex;
+`;
+
+const TargetContainer = styled.div`
+  cursor: pointer;
+  border: 0;
+  background: none;
+  padding: 0;
+`;
+
+type ToolTipContainerProps = {
+  position: ToolTipPosition;
+  variant: VisualTheme;
+};
+
+const ToolTipContainer = styled.div<ToolTipContainerProps>`
+  ${fontSmoothing()}
+
+  display: flex;
+  opacity: 0;
+  transition: opacity 0ms;
+  transition-delay: 750ms;
+  position: absolute;
+  width: 16rem;
+  visibility: hidden;
+
+  ${({ theme, variant }) =>
+    variant === 'dark' && `background: ${theme.colors.black};`}
+
+  // Both before and after psuedo-elements are used because ::after's background should go over the container's
+  // and ::before's box-shadow should be behind the container itself
+  &::after,
+  &::before {
+    content: '';
+    display: block;
+    height: ${arrowHeight};
+    position: absolute;
+    transform: rotate(45deg);
+    width: ${arrowHeight};
+  }
+
+  &::after {
+    background: ${({ theme }) => theme.colors.white};
+  }
+
+  ${TargetContainer}:hover + &,
+  ${TargetContainer}:focus + &,
+  &:hover {
+    opacity: 1;
+    visibility: visible;
+  }
+
+  ${({ position }) =>
+    [ToolTipPosition.TopLeft, ToolTipPosition.TopRight].includes(position) &&
+    `
+      bottom: ${containerOffsetVertical};
+
+      &::after,
+      &::before {
+        bottom: ${arrowOffsetVertical};
+      }
+
+      &::before {
+        box-shadow: 2px 2px 4px 0 ${slightShadow};
+      }
+
+      ${ToolTipBody} {
+        box-shadow: 0 2px 4px 0 ${slightShadow};
+      }
+    `}
+
+  ${({ position }) =>
+    [ToolTipPosition.BottomLeft, ToolTipPosition.BottomRight].includes(
+      position
+    ) &&
+    `
+      top: ${containerOffsetVertical};
+
+      &::after,
+      &::before {
+        top: ${arrowOffsetVertical};
+      }
+
+      &::before {
+        box-shadow: -2px -2px 4px 0 ${slightShadow};
+      }
+
+      ${ToolTipBody} {
+        box-shadow: 0 0 4px 0 ${slightShadow};
+      }
+    `}
+
+${({ position }) =>
+    [ToolTipPosition.BottomLeft, ToolTipPosition.TopLeft].includes(position) &&
+    `
+      justify-content: flex-end;
+      right: $container-offset-horizontal;
+
+      &::after,
+      &::before {
+        right: ${arrowOffsetHorizontal};
+      }
+    `}
+
+${({ position }) =>
+    [ToolTipPosition.BottomRight, ToolTipPosition.TopRight].includes(
+      position
+    ) &&
+    `
+  left: ${containerOffsetHorizontal};
+
+      &::after,
+      &::before {
+        left: ${arrowOffsetHorizontal};
+      }
+    `}
+`;
+
+const ToolTipBody = styled.div<{ variant: VisualTheme }>`
+  display: inline-block;
+  font-size: ${pxRem(14)};
+  line-height: ${pxRem(18)};
+  padding: 0.6rem 0.75rem;
+
+  ${({ theme, variant }) =>
+    variant === 'light'
+      ? `
+      background: ${theme.colors.white};
+      color: ${theme.colors.black};
+      `
+      : `
+      background: ${theme.colors.black};
+      color: ${theme.colors.white};
+      `}
+`;
 
 export type ToolTipProps = {
   children?: ReactNode;
@@ -21,9 +172,8 @@ export type ToolTipProps = {
   id: string;
   position?: ToolTipPosition;
   target?: ReactNode;
-  theme?: VisualTheme;
   tipClassName?: string;
-  wrapperClassName?: string;
+  variant?: VisualTheme;
 };
 
 export const ToolTip: React.FC<ToolTipProps> = ({
@@ -32,36 +182,30 @@ export const ToolTip: React.FC<ToolTipProps> = ({
   id,
   position = ToolTipPosition.TopRight,
   target,
-  theme = 'light',
   tipClassName,
-  wrapperClassName,
+  variant = 'light',
 }) => {
   return (
-    <div className={cx(styles.toolTipWrapper, wrapperClassName)}>
-      <div
+    <TooltipWrapper>
+      <TargetContainer
         aria-labelledby={id}
-        className={styles.targetContainer}
         // ToolTips sometimes contain actual <button>s, which cannot be a child of a button.
         // This element still needs tab focus so we must use the `tabIndex=0` hack. Sigh.
         // eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex
         tabIndex={focusable ? 0 : undefined}
       >
         {target}
-      </div>
-      <div
-        className={cx(
-          styles.toolTipContainer,
-          styles[position],
-          theme === 'dark'
-            ? styles.toolTipContainerDark
-            : styles.toolTipContainerLight,
-          tipClassName
-        )}
-        role="tooltip"
+      </TargetContainer>
+      <ToolTipContainer
+        data-test="tooltip-container"
+        className={tipClassName}
         id={id}
+        position={position}
+        role="tooltip"
+        variant={variant}
       >
-        <div className={styles.toolTipBody}>{children}</div>
-      </div>
-    </div>
+        <ToolTipBody variant={variant}>{children}</ToolTipBody>
+      </ToolTipContainer>
+    </TooltipWrapper>
   );
 };
