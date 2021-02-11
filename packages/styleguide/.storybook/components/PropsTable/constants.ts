bf6cf596de3f8da51a1c167a05c0cc23d1e4c58f
codecaaron <aaron@codecademy.com>
2021-02-11 12:46:55 -0500

    Remove CSS destructuring

--- packages/styleguide/.storybook/components/PropsTable/constants.ts
+++ packages/styleguide/.storybook/components/PropsTable/constants.ts
@@ -1,6 +1,6 @@
 import * as system from '@codecademy/gamut-styles/src/system';
 
-const { css, properties: props, variant, ...groups } = system;
+const { properties: props, variant, ...groups } = system;
 
 export const properties = Object.entries(props).reduce<string[]>(
   (carry, [key, handler]) => {
