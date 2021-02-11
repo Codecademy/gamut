c89d3e02a1b5afd27ba06c031cf08b67a7f694a9
Josh Goldberg <josh@codecademy.com>
2021-02-10 20:34:25 -0500

    Popover-related docs

--- packages/gamut/src/ToolTip/index.tsx
+++ packages/gamut/src/ToolTip/index.tsx
@@ -185,6 +185,7 @@ export const ToolTip: React.FC<ToolTipProps> = ({
         aria-labelledby={id}
         // ToolTips sometimes contain actual <button>s, which cannot be a child of a button.
         // This element still needs tab focus so we must use the `tabIndex=0` hack. Sigh.
+        // This behavior is considered deprecated; we recommend using Popover instead.
         // eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex
         tabIndex={focusable ? 0 : undefined}
       >
