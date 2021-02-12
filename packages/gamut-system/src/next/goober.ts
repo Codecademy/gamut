fda2aae4ce6af0a63aa2c09dac0baf71774b8053
codecaaron <aaron@codecademy.com>
2021-02-11 20:05:46 -0500

    GOTCHYA

--- packages/gamut-system/src/next/goober.ts
+++ packages/gamut-system/src/next/goober.ts
@@ -1,9 +1,8 @@
-import { get, merge } from 'lodash';
+import { get } from 'lodash';
 
-import { AbstractTheme } from '../types/config';
+import { AbstractProps, AbstractTheme } from '../types/config';
 import { CSSObject } from '../types/css';
 import { Properties, PropName } from '../types/properties';
-import { UnionToIntersection } from '../types/utils';
 
 export interface Theme {
   boxShadows: {
@@ -126,28 +125,57 @@ interface Prop<T extends AbstractTheme> {
   transform?: (val: unknown) => string | number;
 }
 
-type Scale<
-  T extends AbstractTheme,
-  Config extends Prop<T>
-> = Config['scale'] extends keyof T
-  ? keyof T[Config['scale']]
-  : Properties[Config['property']]['defaultScale'];
-
-interface Transform<T extends AbstractTheme, Config extends Prop<T>> {
-  (props: Record<PropName, Scale<T, Config>>): CSSObject;
+interface PropFns<T extends AbstractTheme> {
+  prop: string;
+  property: PropName;
+  scale?: keyof T | unknown;
+  transform?: ((val: unknown) => string | number) | unknown;
+  styleFn: (props: AbstractProps) => CSSObject;
 }
 
+type Scale<T extends AbstractTheme, Config extends Prop<T>> = ResponsiveProp<
+  Config['scale'] extends keyof T
+    ? keyof T[Config['scale']]
+    : Properties[Config['property']]['defaultScale']
+>;
+
+interface Transform<
+  T extends AbstractTheme,
+  P extends string,
+  Config extends Prop<T>
+> {
+  (props: { [K in P]?: Scale<T, Config> }): CSSObject;
+}
+
+interface PropTransformer<
+  T extends AbstractTheme,
+  P extends string,
+  C extends Prop<T>
+> extends PropFns<T> {
+  prop: P;
+  property: C['property'];
+  scale?: C['scale'];
+  transform?: C['transform'];
+  styleFn: Transform<T, P, C>;
+}
+
+interface AbstractParser<T> {
+  (props: AbstractProps): CSSObject;
+  propNames: string[];
+  config: Record<string, PropFns<T>>;
+}
 interface Parser<
   T extends AbstractTheme,
-  Config extends Record<string, Prop<T>>,
-  Transforms extends {
-    [P in keyof Config as Key<P>]: Transform<T, Config[P]>;
-  }
+  Config extends Record<string, PropFns<T>>
 > {
   (
-    props: { [Prop in keyof Config]?: ResponsiveProp<Scale<T, Config[Prop]>> }
+    props: {
+      [P in keyof Config]?: Parameters<
+        Config[P]['styleFn']
+      >[0][Config[P]['prop']];
+    }
   ): CSSObject;
-  propNames: (keyof Transforms)[];
+  propNames: (keyof Config)[];
   config: Config;
 }
 
@@ -156,60 +184,70 @@ type Key<T> = T extends string ? T : never;
 export const creator = {
   withTheme<T extends AbstractTheme>() {
     return {
-      createParser<
-        Config extends Record<string, Prop<T>>,
-        Transforms extends {
-          [P in keyof Config as Key<P>]: Transform<T, Config[P]>;
-        }
-      >(config: Config, transforms: Transforms) {
-        return {} as Parser<T, Config, Transforms>;
+      createParser<Config extends Record<string, PropFns<T>>>(config: Config) {
+        return {} as Parser<T, Config>;
       },
-      createTransform<PropName extends string, Config extends Prop<T>>(
-        prop: PropName,
+      createTransform<P extends string, Config extends Prop<T>>(
+        prop: P,
         config: Config
-      ): Transform<T, Config> {
-        return (props) => {
-          const value = get(props, prop);
-          const scaleValue = get(
-            props,
-            `theme.${config.scale}.${value}`,
-            value
-          );
+      ): PropTransformer<T, P, Config> {
+        return {
+          ...config,
+          prop,
+          styleFn: (props) => {
+            const value = get(props, prop);
+            const scaleValue = get(
+              props,
+              `theme.${config.scale}.${value}`,
+              value
+            );
 
-          return {
-            [config.property]: scaleValue,
-          };
+            return {
+              [config.property]: scaleValue,
+            };
+          },
         };
       },
     };
   },
 };
 
+type AllUnionKeys<T> = T extends any ? keyof T : never;
+type KeyFromUnion<T, K> = T extends any
+  ? K extends keyof T
+    ? T[K]
+    : never
+  : never;
+
 export const variance = {
   withTheme<T extends AbstractTheme>() {
     return {
-      compose<
-        Parsers extends Parser<
-          T,
-          Record<string, Prop<T>>,
-          Record<string, Transform<T, Prop<T>>>
-        >[],
-        Configs extends UnionToIntersection<Parsers[number]['config']>,
-        PropNames extends Parsers[number]['propNames'][number][]
-      >(...parsers: Parsers) {
-        return {};
+      compose<Args extends AbstractParser<T>[]>(...parsers: Args) {
+        const { createParser } = creator.withTheme<T>();
+
+        const config = parsers.reduce(
+          (carry, parser) => ({ ...carry, ...parser.config }),
+          {}
+        ) as {
+          [K in AllUnionKeys<Args[number]['config']>]: KeyFromUnion<
+            Args[number]['config'],
+            K
+          >;
+        };
+
+        return createParser(config);
       },
       create<Config extends Record<string, Prop<T>>>(config: Config) {
         const { createTransform, createParser } = creator.withTheme<T>();
         const transforms = {} as {
-          [P in keyof Config as Key<P>]: Transform<T, Config[P]>;
+          [P in keyof Config as Key<P>]: PropTransformer<T, Key<P>, Config[P]>;
         };
 
         for (const prop in config) {
           transforms[prop] = createTransform(prop, config[prop]);
         }
 
-        const parser = createParser(config, transforms);
+        const parser = createParser(transforms);
         return parser;
       },
     };
@@ -217,7 +255,7 @@ export const variance = {
 };
 
 const { create, compose } = variance.withTheme<Theme>();
-export const margin = create({
+export const space = create({
   margin: { property: 'margin', scale: 'spacing' },
   padding: { property: 'padding', scale: 'spacing' },
 });
@@ -227,4 +265,11 @@ export const layout = create({
   height: { property: 'height' },
 });
 
-export const hello = compose(margin, layout);
+space.config.margin.styleFn({ margin: 16 });
+space.config.margin.styleFn({});
+space.propNames;
+space({ padding: 4, margin: 16 });
+
+export const myComposed = compose(space, layout);
+myComposed({ width: '1', margin: 0 });
+myComposed.propNames;
