7f073cb74c4a4bed2b4137327c1dc4bd63efad4a
codecaaron <aaron@codecademy.com>
2021-02-14 17:02:50 -0500

    add shorthand

--- packages/variance/src/variance/props/spacing.ts
+++ packages/variance/src/variance/props/spacing.ts
@@ -14,7 +14,30 @@ export const margin = {
   marginBottom: { property: 'marginBottom' },
 } as const;
 
+const marginShorthand = {
+  m: margin.margin,
+  mX: margin.marginX,
+  mY: margin.marginY,
+  mL: margin.marginLeft,
+  mR: margin.marginRight,
+  mT: margin.marginTop,
+  mB: margin.marginBottom,
+};
+
 export const padding = {
+  p: { property: 'padding' },
+  pX: {
+    property: 'padding',
+    properties: ['paddingLeft', 'paddingRight'],
+  },
+  pY: {
+    property: 'padding',
+    properties: ['paddingTop', 'paddingBottom'],
+  },
+  pL: { property: 'paddingLeft' },
+  pR: { property: 'paddingRight' },
+  pT: { property: 'paddingTop' },
+  pB: { property: 'paddingBottom' },
   padding: { property: 'padding' },
   paddingX: {
     property: 'padding',
@@ -30,4 +53,19 @@ export const padding = {
   paddingBottom: { property: 'paddingBottom' },
 } as const;
 
-export const spacing = { ...padding, ...margin } as const;
+const paddingShorthand = {
+  p: padding.padding,
+  pX: padding.paddingX,
+  pY: padding.paddingY,
+  pL: padding.paddingLeft,
+  pR: padding.paddingRight,
+  pT: padding.paddingTop,
+  pB: padding.paddingBottom,
+};
+
+export const spacing = {
+  ...padding,
+  ...paddingShorthand,
+  ...margin,
+  ...marginShorthand,
+} as const;
