3f8a045f29b15f02da66fb10069064866c44a9e7
Josh Goldberg <joshuakgoldberg@outlook.com>
2021-02-16 14:32:09 -0500

    chore!: converted ToolTip to Emotion (#1357)
    Converts `ToolTip` to Emotion and switches its position to a string literal under a `"variant"` prop instead of an enum under `"theme"`.

--- packages/gamut/src/ToolTip/index.tsx
+++ packages/gamut/src/ToolTip/index.tsx
@@ -1,15 +1,154 @@
-import cx from 'classnames';
+import { fontSmoothing, pxRem, variant } from '@codecademy/gamut-styles';
+import styled from '@emotion/styled';
 import React, { ReactNode } from 'react';
 
 import { VisualTheme } from '../theming/VisualTheme';
-import styles from './styles.module.scss';
 
-export enum ToolTipPosition {
-  BottomLeft = 'bottom-left',
-  BottomRight = 'bottom-right',
-  TopLeft = 'top-left',
-  TopRight = 'top-right',
-}
+export type ToolTipPosition =
+  | 'bottom-left'
+  | 'bottom-right'
+  | 'top-left'
+  | 'top-right';
+
+const arrowWidth = `1rem`;
+const arrowHeight = `0.5rem`;
+const arrowPaddingHorizontal = `1.5rem`;
+const arrowPaddingVertical = `0.25rem`;
+
+const containerOffsetHorizontal = `calc(100% - (${arrowWidth} / 2) + ${arrowPaddingHorizontal})`;
+const containerOffsetVertical = `calc(100% + ${arrowHeight} + ${arrowPaddingVertical})`;
+
+const arrowOffsetHorizontal = `calc(${arrowPaddingHorizontal} - (${arrowWidth} / 2))`;
+const arrowOffsetVertical = `calc(-${arrowHeight} / 2)`;
+
+const slightShadow = `rgba(0, 0, 0, 0.15)`;
+
+const TooltipWrapper = styled.div`
+  position: relative;
+  display: inline-flex;
+`;
+
+const TargetContainer = styled.div`
+  cursor: pointer;
+  border: 0;
+  background: none;
+  padding: 0;
+`;
+
+type ToolTipContainerProps = {
+  position: ToolTipPosition;
+  variant: VisualTheme;
+};
+
+const ToolTipContainer = styled.div<ToolTipContainerProps>`
+  ${fontSmoothing()}
+
+  display: flex;
+  opacity: 0;
+  transition: opacity 0ms;
+  transition-delay: 750ms;
+  position: absolute;
+  width: 16rem;
+  visibility: hidden;
+
+  ${variant({
+    dark: { backgroundColor: 'black', textColor: 'white' },
+    light: { backgroundColor: 'white', textColor: 'black' },
+  })}
+
+  // Both before and after psuedo-elements are used because ::after's background should go over the container's
+  // and ::before's box-shadow should be behind the container itself
+  &::after,
+  &::before {
+    content: '';
+    display: block;
+    height: ${arrowHeight};
+    position: absolute;
+    transform: rotate(45deg);
+    width: ${arrowHeight};
+  }
+
+  &::after {
+    background: inherit;
+  }
+
+  ${TargetContainer}:hover + &,
+  ${TargetContainer}:focus + &,
+  &:hover {
+    opacity: 1;
+    visibility: visible;
+  }
+
+  ${({ position }) =>
+    ['top-left', 'top-right'].includes(position) &&
+    `
+      bottom: ${containerOffsetVertical};
+
+      &::after,
+      &::before {
+        bottom: ${arrowOffsetVertical};
+      }
+
+      &::before {
+        box-shadow: 2px 2px 4px 0 ${slightShadow};
+      }
+
+      ${ToolTipBody} {
+        box-shadow: 0 2px 4px 0 ${slightShadow};
+      }
+    `}
+
+  ${({ position }) =>
+    ['bottom-left', 'bottom-right'].includes(position) &&
+    `
+      top: ${containerOffsetVertical};
+
+      &::after,
+      &::before {
+        top: ${arrowOffsetVertical};
+      }
+
+      &::before {
+        box-shadow: -2px -2px 4px 0 ${slightShadow};
+      }
+
+      ${ToolTipBody} {
+        box-shadow: 0 0 4px 0 ${slightShadow};
+      }
+    `}
+
+  ${({ position }) =>
+    ['bottom-left', 'top-left'].includes(position) &&
+    `
+      justify-content: flex-end;
+      right: $container-offset-horizontal;
+
+      &::after,
+      &::before {
+        right: ${arrowOffsetHorizontal};
+      }
+    `}
+
+  ${({ position }) =>
+    ['bottom-right', 'top-right'].includes(position) &&
+    `
+      left: ${containerOffsetHorizontal};
+
+      &::after,
+      &::before {
+        left: ${arrowOffsetHorizontal};
+      }
+    `}
+`;
+
+const ToolTipBody = styled.div<{ variant: VisualTheme }>`
+  background: inherit;
+  color: inherit;
+  display: inline-block;
+  font-size: ${pxRem(14)};
+  line-height: ${pxRem(18)};
+  padding: 0.6rem 0.75rem;
+`;
 
 export type ToolTipProps = {
   children?: ReactNode;
@@ -21,47 +160,40 @@ export type ToolTipProps = {
   id: string;
   position?: ToolTipPosition;
   target?: ReactNode;
-  theme?: VisualTheme;
   tipClassName?: string;
-  wrapperClassName?: string;
+  variant?: VisualTheme;
 };
 
 export const ToolTip: React.FC<ToolTipProps> = ({
   children,
   focusable,
   id,
-  position = ToolTipPosition.TopRight,
+  position = 'top-right',
   target,
-  theme = 'light',
   tipClassName,
-  wrapperClassName,
+  variant = 'light',
 }) => {
   return (
-    <div className={cx(styles.toolTipWrapper, wrapperClassName)}>
-      <div
+    <TooltipWrapper>
+      <TargetContainer
         aria-labelledby={id}
-        className={styles.targetContainer}
         // ToolTips sometimes contain actual <button>s, which cannot be a child of a button.
         // This element still needs tab focus so we must use the `tabIndex=0` hack. Sigh.
+        // This behavior is considered deprecated; we recommend using Popover instead.
         // eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex
         tabIndex={focusable ? 0 : undefined}
       >
         {target}
-      </div>
-      <div
-        className={cx(
-          styles.toolTipContainer,
-          styles[position],
-          theme === 'dark'
-            ? styles.toolTipContainerDark
-            : styles.toolTipContainerLight,
-          tipClassName
-        )}
-        role="tooltip"
+      </TargetContainer>
+      <ToolTipContainer
+        className={tipClassName}
         id={id}
+        position={position}
+        role="tooltip"
+        variant={variant}
       >
-        <div className={styles.toolTipBody}>{children}</div>
-      </div>
-    </div>
+        <ToolTipBody variant={variant}>{children}</ToolTipBody>
+      </ToolTipContainer>
+    </TooltipWrapper>
   );
 };
--- packages/styleguide/stories/Atoms/TooltipArea.tsx
+++ packages/styleguide/stories/Atoms/TooltipArea.tsx
@@ -1,14 +1,45 @@
-import { Box } from '@codecademy/gamut/src';
+import { Box, ToolTipPosition } from '@codecademy/gamut/src';
 import styled from '@emotion/styled';
 import React from 'react';
 
-const AreaBox = styled(Box)`
-  margin: 2rem 10rem;
+const tipPositionStyles: Record<ToolTipPosition, string> = {
+  'bottom-left': `
+    right: 0;
+    margin-right: -0.6rem;
+  `,
+  'bottom-right': `margin-left: -3rem;`,
+  'top-left': `
+    margin-right: -0.6rem;
+    right: 0;
+  `,
+  'top-right': `margin-left: -3rem;`,
+};
+
+const AreaBox = styled(Box)<{ tipPosition: ToolTipPosition }>`
+  margin: 4rem 10rem;
+
+  ${({ tipPosition }) => `
+  & [role=tooltip] {
+    ${tipPositionStyles[tipPosition]}
+  }
+  `}
 `;
 
-export const TooltipArea: React.FC = ({ children }) => {
+export type ToolTipAreaProps = {
+  tipPosition: ToolTipPosition;
+};
+
+export const TooltipArea: React.FC<ToolTipAreaProps> = ({
+  children,
+  tipPosition,
+}) => {
   return (
-    <AreaBox as="span" display="inline-block" minHeight="5rem">
+    <AreaBox
+      as="span"
+      display="inline-block"
+      minHeight="5rem"
+      tipPosition={tipPosition}
+    >
       {children}
     </AreaBox>
   );
