fbd27e86694c528046247c897f450e60f3e285ce
codecaaron <aaron@codecademy.com>
2021-02-17 08:26:10 -0500

    Unneeded

--- packages/gamut/src/Button/CTAButton.tsx
+++ packages/gamut/src/Button/CTAButton.tsx
@@ -8,31 +8,32 @@ import { ButtonOutline } from './ButtonOutline';
 import { ButtonProps, modeColorGroups } from './shared';
 
 const CTAButtonInner = styled(ButtonInner)<ButtonProps>(
-  ({ mode = 'light' }) => {
+  ({ mode = 'light' }: ButtonProps) => {
     const modeColors = modeColorGroups[mode];
 
     return css`
-      border-radius: 2px;
-      padding: 0.75rem 1.25rem;
-      font-family: ${fontFamily.accent};
-      color: ${modeColors.foreground};
       background-color: ${modeColors.background};
+      border-radius: 2px;
       box-shadow: -4px 4px 0 0 ${modeColors.shadow};
+      color: ${modeColors.foreground};
+      font-family: ${fontFamily.accent};
+      font-weight: bold;
+      padding: 0.75rem 1.25rem;
 
       ${CTAButtonOuter}:hover & {
         box-shadow: -8px 8px 0 0 ${modeColors.shadow};
       }
 
       ${CTAButtonOuter}:active & {
-        background-color: ${modeColors.shadow};
+        background: ${modeColors.shadow};
         box-shadow: none;
       }
 
       ${CTAButtonOuter}:disabled &,
       ${CTAButtonOuter}[aria-disabled='true'] & {
-        color: ${modeColors.foregroundMuted};
-        background-color: ${modeColors.backgroundMuted};
+        background: ${modeColors.backgroundMuted};
         box-shadow: -4px 4px 0 1px ${modeColors.foregroundMuted};
+        color: ${modeColors.foregroundMuted};
       }
     `;
   }
