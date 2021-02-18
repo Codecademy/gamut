fda8b077a4c1b86a981258416ef3e362749cd8c7
Jasmine English <jasmine@codecademy.com>
2021-02-17 20:59:10 -0500

    scrolling works

--- packages/gamut-labs/src/experimental/GlobalHeader/index.tsx
+++ packages/gamut-labs/src/experimental/GlobalHeader/index.tsx
@@ -1,5 +1,7 @@
 import { Box } from '@codecademy/gamut';
-import React from 'react';
+import cx from 'classnames';
+import { throttle } from 'lodash';
+import React, { useCallback, useEffect, useState } from 'react';
 
 import { AppHeader } from '../AppHeader';
 import {
@@ -23,6 +25,7 @@ import {
   proHeaderItems,
   proMobileHeaderItems,
 } from './GlobalHeaderVariants';
+import styles from './styles.module.scss';
 import { AnonHeader, FreeHeader, LoadingHeader, ProHeader } from './types';
 
 export type GlobalHeaderProps =
@@ -93,22 +96,112 @@ const getMobileAppHeaderItems = (
   }
 };
 
-export const GlobalHeader: React.FC<GlobalHeaderProps> = (props) => (
-  <>
-    <Box display={{ base: 'none', md: 'block' }} height="80">
-      <AppHeader action={props.action} items={getAppHeaderItems(props)} />
-    </Box>
-    <Box
-      display={{ base: 'block', md: 'none' }}
-      height="64"
-      position="relative"
-      zIndex={0}
-    >
-      <AppHeaderMobile
-        action={props.action}
-        items={getMobileAppHeaderItems(props)}
-        renderSearch={props.renderSearch?.mobile}
-      />
-    </Box>
-  </>
-);
+const defaultScrollingState = {
+  isInHeaderRegion: true,
+  isScrollingDown: true,
+  isScrollingDownFromHeaderRegion: true,
+  prevScrollPosition: 0,
+};
+
+export const GlobalHeader: React.FC<GlobalHeaderProps> = (props) => {
+  const [scrollingState, setScrollingState] = useState(defaultScrollingState);
+
+  const {
+    isScrollingDownFromHeaderRegion,
+    isInHeaderRegion,
+    isScrollingDown,
+    prevScrollPosition,
+  } = scrollingState;
+
+  const handleScrolling = () => {
+    const currentScrollPosition = window.pageYOffset;
+
+    if (currentScrollPosition <= 80) {
+      setScrollingState((prevState) => ({
+        ...prevState,
+        isInHeaderRegion: true,
+        isScrollingDownFromHeaderRegion: true,
+      }));
+    } else {
+      setScrollingState((prevState) => ({
+        ...prevState,
+        isInHeaderRegion: false,
+        isScrollingDownFromHeaderRegion:
+          prevState.isScrollingDown &&
+          prevState.isScrollingDownFromHeaderRegion,
+      }));
+    }
+
+    if (
+      currentScrollPosition > prevScrollPosition ||
+      currentScrollPosition === 0
+    ) {
+      setScrollingState((prevState) => {
+        return {
+          ...prevState,
+          isScrollingDown: true,
+          prevScrollPosition: currentScrollPosition,
+          isScrollingDownFromHeaderRegion:
+            prevState.isScrollingDown &&
+            prevState.isScrollingDownFromHeaderRegion,
+        };
+      });
+    } else {
+      setScrollingState((prevState) => ({
+        ...prevState,
+        isScrollingDown: false,
+        prevScrollPosition: currentScrollPosition,
+        isScrollingDownFromHeaderRegion: false,
+      }));
+    }
+  };
+
+  const throttledHandleScroll = throttle(handleScrolling, 200);
+
+  useEffect(() => {
+    window.addEventListener('scroll', throttledHandleScroll);
+
+    // returned function will be called on component unmount
+    return () => {
+      window.removeEventListener('scroll', throttledHandleScroll);
+    };
+  }, [throttledHandleScroll, isScrollingDown]);
+
+  return (
+    <>
+      <Box
+        display={{ base: 'none', md: 'block' }}
+        height="80"
+        className={cx(
+          isInHeaderRegion && [styles.staticHeader, styles.fadeTransition],
+          {
+            [styles.stickyHeader]:
+              !isInHeaderRegion || (isInHeaderRegion && !isScrollingDown),
+            [styles.fadeTransition]: isScrollingDownFromHeaderRegion,
+            [styles.slideTransition]:
+              !isInHeaderRegion && !isScrollingDownFromHeaderRegion,
+            [styles.showHeader]: !isScrollingDown,
+            [styles.hideHeader]:
+              isScrollingDown &&
+              !isInHeaderRegion &&
+              !isScrollingDownFromHeaderRegion,
+          }
+        )}
+      >
+        <AppHeader action={props.action} items={getAppHeaderItems(props)} />
+      </Box>
+      <Box
+        display={{ base: 'block', md: 'none' }}
+        height="64"
+        position="relative"
+        zIndex={0}
+      >
+        <AppHeaderMobile
+          action={props.action}
+          items={getMobileAppHeaderItems(props)}
+          renderSearch={props.renderSearch?.mobile}
+        />
+      </Box>
+    </>
+  );
+};
