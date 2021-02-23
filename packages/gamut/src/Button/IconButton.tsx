609f2b2676827d37cbc60830a5f7929e7045fc96
Aaron <aaron@codecademy.com>
2021-02-23 13:04:01 -0500

    Fix mess

--- packages/gamut/src/Button/IconButton.tsx
+++ packages/gamut/src/Button/IconButton.tsx
@@ -1,4 +1,5 @@
 import { GamutIconProps } from '@codecademy/gamut-icons';
+import styled from '@emotion/styled';
 import React from 'react';
 
 import { SizedButtonProps } from './shared';
@@ -9,12 +10,19 @@ export type IconButtonProps = SizedButtonProps & {
   icon: React.ComponentType<GamutIconProps>;
 };
 
+const IconWrapper = styled.div`
+  display: inline-block;
+  margin: 0 -1px;
+`;
+
 export const IconButton: React.FC<
   IconButtonProps & React.ComponentProps<typeof TextButton>
 > = ({ icon: Icon, size, ...props }) => {
   return (
     <TextButton size={size} {...props}>
-      <Icon size={size === 'small' ? 16 : 24} aria-hidden />
+      <IconWrapper>
+        <Icon size={size === 'small' ? 16 : 24} aria-hidden />
+      </IconWrapper>
     </TextButton>
   );
 };
