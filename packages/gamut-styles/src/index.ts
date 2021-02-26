db86d30b33c9236b36b61332ab153e492a9744ea
Aaron <aaron@codecademy.com>
2021-02-26 16:22:03 -0500

    Whoops

--- packages/gamut-styles/src/index.ts
+++ packages/gamut-styles/src/index.ts
@@ -4,3 +4,11 @@ export * from './cache';
 export * from './variables';
 export * from './utilities';
 export * from './theme';
+
+import { theme } from './theme';
+
+export type ThemeShape = typeof theme;
+
+declare module '@emotion/react' {
+  export interface Theme extends ThemeShape {}
+}
