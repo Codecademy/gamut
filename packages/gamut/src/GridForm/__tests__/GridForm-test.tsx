13a806660a674d5dfcfa2ce51024d3025517e991
cass spain <cass@codecademy.com>
2021-03-02 22:32:29 -0500

    theme provider added to tests

--- packages/gamut/src/GridForm/__tests__/GridForm-test.tsx
+++ packages/gamut/src/GridForm/__tests__/GridForm-test.tsx
@@ -1,4 +1,6 @@
-import { setupEnzyme } from '@codecademy/gamut-tests';
+import { theme } from '@codecademy/gamut-styles';
+import { ThemeProvider } from '@emotion/react';
+import { mount } from 'enzyme';
 import React from 'react';
 import { act } from 'react-dom/test-utils';
 
@@ -14,19 +16,24 @@ import {
   stubTextField,
 } from './stubs';
 
-const selectValue = stubSelectOptions[1];
-const textValue = 'Hooray!';
-
-const renderWrapper = setupEnzyme(GridForm, {
-  submit: { contents: <>Submit</>, size: 6 },
-});
-
 describe('GridForm', () => {
   it('submits the form when all inputs are filled out', async () => {
+    const fields = [stubCheckboxField, stubSelectField, stubTextField];
     const api = createPromise<{}>();
     const onSubmit = async (values: {}) => api.resolve(values);
-    const fields = [stubCheckboxField, stubSelectField, stubTextField];
-    const { wrapper } = renderWrapper({ fields: fields, onSubmit: onSubmit });
+    const selectValue = stubSelectOptions[1];
+    const textValue = 'Hooray!';
+
+    const wrapped = mount(
+      <ThemeProvider theme={theme}>
+        <GridForm
+          fields={fields}
+          onSubmit={onSubmit}
+          submit={{ contents: <>Submit</>, size: 6 }}
+        />
+      </ThemeProvider>
+    );
+
     const newValues = [
       ['input[type="checkbox"]', 'checked', true],
       ['select', 'value', selectValue],
@@ -36,14 +43,16 @@ describe('GridForm', () => {
     await act(async () => {
       for (const [selector, key, value] of newValues) {
         // https://github.com/react-hook-form/react-hook-form/issues/1382
-        const node = wrapper.find(selector).getDOMNode();
+        const node = wrapped.find(selector).getDOMNode();
         (node as any)[key] = value;
         node.dispatchEvent(new Event('input'));
       }
     });
 
+    wrapped.setProps(wrapped.props());
+
     await act(async () => {
-      wrapper.find('form').simulate('submit');
+      wrapped.find('form').simulate('submit');
       await api.innerPromise;
     });
 
@@ -57,8 +66,6 @@ describe('GridForm', () => {
   });
 
   it('only sets aria-live prop on the first validation error in a form', async () => {
-    const api = createPromise<{}>();
-    const onSubmit = async (values: {}) => api.resolve(values);
     const fields = [
       { ...stubTextField, validation: { required: 'Please enter text' } },
       {
@@ -67,157 +74,179 @@ describe('GridForm', () => {
         name: 'second-stub',
       },
     ];
+    const api = createPromise<{}>();
+    const onSubmit = async (values: {}) => api.resolve(values);
+    const wrapped = mount(
+      <ThemeProvider theme={theme}>
+        <GridForm
+          fields={fields}
+          onSubmit={onSubmit}
+          submit={{ contents: <>Submit</>, size: 6 }}
+        />
+      </ThemeProvider>
+    );
 
-    const { wrapper } = renderWrapper({ fields: fields, onSubmit: onSubmit });
+    wrapped.setProps(wrapped.props());
 
     await act(async () => {
-      wrapper.find('form').simulate('submit');
+      wrapped.find('form').simulate('submit');
     });
-    wrapper.update();
+    wrapped.update();
 
     // there should only be a single "assertive" error from the form submission
-    expect(wrapper.find('span[aria-live="assertive"]').length).toBe(1);
+    expect(wrapped.find('span[aria-live="assertive"]').length).toBe(1);
   });
 
   describe('when "onSubmit" validation is selected', () => {
     it('never disables the submit button', () => {
-      const api = createPromise<{}>();
-      const onSubmit = async (values: {}) => api.resolve(values);
       const fields = [
         { ...stubTextField, validation: { required: 'Please enter text' } },
       ];
+      const api = createPromise<{}>();
+      const onSubmit = async (values: {}) => api.resolve(values);
 
-      const { wrapper } = renderWrapper({
-        fields: fields,
-        onSubmit: onSubmit,
-      });
+      const wrapped = mount(
+        <ThemeProvider theme={theme}>
+          <GridForm
+            fields={fields}
+            onSubmit={onSubmit}
+            submit={{ contents: <>Submit</>, size: 6 }}
+            validation="onSubmit"
+          />
+        </ThemeProvider>
+      );
 
-      wrapper.setProps({ validation: 'onSubmit' });
+      wrapped.setProps(wrapped.props());
 
       expect(
-        wrapper.find('button[type="submit"]').prop('disabled')
+        wrapped.find('button[type="submit"]').prop('disabled')
       ).not.toBeTruthy();
     });
   });
 
   describe('when "onChange" validation is selected', () => {
     it('disables the submit button when required fields are incomplete', async () => {
-      const api = createPromise<{}>();
-      const onSubmit = async (values: {}) => api.resolve(values);
       const fields = [
         { ...stubTextField, validation: { required: 'Please enter text' } },
       ];
+      const api = createPromise<{}>();
+      const onSubmit = async (values: {}) => api.resolve(values);
 
-      const { wrapper } = renderWrapper({
-        fields: fields,
-        onSubmit: onSubmit,
-      });
+      const wrapped = mount(
+        <ThemeProvider theme={theme}>
+          <GridForm
+            fields={fields}
+            onSubmit={onSubmit}
+            submit={{ contents: <>Submit</>, size: 6 }}
+            validation="onChange"
+          />
+        </ThemeProvider>
+      );
 
-      wrapper.setProps({ validation: 'onChange' });
+      wrapped.setProps(wrapped.props());
 
       expect(
-        wrapper.find('button[type="submit"]').prop('disabled')
+        wrapped.find('button[type="submit"]').prop('disabled')
       ).toBeTruthy();
+    });
+
+    it('enables the submit button after the required fields are completed', async () => {
+      const fields = [
+        { ...stubTextField, validation: { required: 'Please enter text' } },
+      ];
+      const api = createPromise<{}>();
+      const onSubmit = async (values: {}) => api.resolve(values);
+
+      const wrapped = mount(
+        <ThemeProvider theme={theme}>
+          <GridForm
+            fields={fields}
+            onSubmit={onSubmit}
+            submit={{ contents: <>Submit</>, size: 6 }}
+            validation="onChange"
+          />
+        </ThemeProvider>
+      );
+
+      await act(async () => {
+        // https://github.com/react-hook-form/react-hook-form/issues/1382
+        const node = wrapped.find('input[type="text"]').getDOMNode();
+        (node as HTMLInputElement).value = 'Hooray!';
+        node.dispatchEvent(new Event('input'));
+      });
+
+      wrapped.setProps(wrapped.props());
+
+      expect(
+        wrapped.find('button[type="submit"]').prop('disabled')
+      ).not.toBeTruthy();
+    });
+
+    it('keeps the submit button disabled when overridden and there are no incomplete fields', async () => {
+      const api = createPromise<{}>();
+      const onSubmit = async (values: {}) => api.resolve(values);
+
+      const wrapped = mount(
+        <ThemeProvider theme={theme}>
+          <GridForm
+            fields={[]}
+            onSubmit={onSubmit}
+            submit={{ contents: <>Submit</>, disabled: true, size: 6 }}
+            validation="onChange"
+          />
+        </ThemeProvider>
+      );
+
+      wrapped.setProps(wrapped.props());
+
+      expect(wrapped.find('button[type="submit"]').prop('disabled')).toBe(true);
     });
   });
 
-  // it('enables the submit button after the required fields are completed', async () => {
-  //   const fields = [
-  //     { ...stubTextField, validation: { required: 'Please enter text' } },
-  //   ];
-  //   const api = createPromise<{}>();
-  //   const onSubmit = async (values: {}) => api.resolve(values);
+  it('passes custom ids to the fields', () => {
+    const form = mount(
+      <ThemeProvider theme={theme}>
+        <GridForm
+          fields={[
+            {
+              ...stubTextField,
+              id: 'mycoolid',
+            },
+            {
+              ...stubSelectField,
+              id: 'swaggy-id',
+            },
+            {
+              ...stubCheckboxField,
+              id: 'another-dank-id',
+            },
+            {
+              ...stubRadioGroupField,
+              id: 'and-another-one',
+              name: 'name',
+            },
+            {
+              ...stubTextareaField,
+              id: 'id-2-the-ego',
+            },
+            {
+              ...stubFileField,
+              id: 'fire-file',
+            },
+          ]}
+          onSubmit={jest.fn()}
+          submit={{ contents: <>Submit</>, size: 6 }}
+        />
+      </ThemeProvider>
+    );
 
-  //   const { wrapper } = renderWrapper({
-  //     fields: fields,
-  //     onSubmit: onSubmit,
-  //   });
-
-  //   wrapper.setProps({ validation: 'onChange' });
-
-  //   await act(async () => {
-  //     // https://github.com/react-hook-form/react-hook-form/issues/1382
-  //     const node = wrapper.find('input[type="text"]').getDOMNode();
-  //     (node as HTMLInputElement).value = 'Hooray!';
-  //     node.dispatchEvent(new Event('input'));
-  //     wrapper.update();
-  //   });
-
-  //   await act(async () => {
-  //     wrapper.find('input').simulate('change', { target: { value: 'foo' } });
-  //     console.log(wrapper.find('input'));
-  //   });
-
-  //   const newWrapperProps = wrapper.props();
-  //   wrapper.setProps({ newWrapperProps });
-  //   // console.log(wrapper.debug());
-
-  //   expect(
-  //     wrapper.find('button[type="submit"]').prop('disabled')
-  //   ).not.toBeTruthy();
-  // });
-
-  //   it('keeps the submit button disabled when overridden and there are no incomplete fields', async () => {
-  //     const api = createPromise<{}>();
-  //     const onSubmit = async (values: {}) => api.resolve(values);
-
-  //     const wrapped = mount(
-  //       <GridForm
-  //         fields={[]}
-  //         onSubmit={onSubmit}
-  //         submit={{ contents: <>Submit</>, disabled: true, size: 6 }}
-  //         validation="onChange"
-  //       />
-  //     );
-
-  //     wrapped.setProps(wrapped.props());
-
-  //     expect(wrapped.find('button[type="submit"]').prop('disabled')).toBe(true);
-  //   });
-  // });
-
-  // it('passes custom ids to the fields', () => {
-  //   const form = mount(
-  //     <GridForm
-  //       fields={[
-  //         {
-  //           ...stubTextField,
-  //           id: 'mycoolid',
-  //         },
-  //         {
-  //           ...stubSelectField,
-  //           id: 'swaggy-id',
-  //         },
-  //         {
-  //           ...stubCheckboxField,
-  //           id: 'another-dank-id',
-  //         },
-  //         {
-  //           ...stubRadioGroupField,
-  //           id: 'and-another-one',
-  //           name: 'name',
-  //         },
-  //         {
-  //           ...stubTextareaField,
-  //           id: 'id-2-the-ego',
-  //         },
-  //         {
-  //           ...stubFileField,
-  //           id: 'fire-file',
-  //         },
-  //       ]}
-  //       onSubmit={jest.fn()}
-  //       submit={{ contents: <>Submit</>, size: 6 }}
-  //     />
-  //   );
-
-  //   expect(form.find('input#mycoolid').length).toBe(1);
-  //   expect(form.find('select#swaggy-id').length).toBe(1);
-  //   expect(form.find('input#another-dank-id').length).toBe(1);
-  //   expect(form.find('input#name-0-and-another-one').length).toBe(1);
-  //   expect(form.find('input#name-1-and-another-one').length).toBe(1);
-  //   expect(form.find('input#another-dank-id').length).toBe(1);
-  //   expect(form.find('textarea#id-2-the-ego').length).toBe(1);
-  //   expect(form.find('input#fire-file').length).toBe(1);
-  // });
+    expect(form.find('input#mycoolid').length).toBe(1);
+    expect(form.find('select#swaggy-id').length).toBe(1);
+    expect(form.find('input#another-dank-id').length).toBe(1);
+    expect(form.find('input#name-0-and-another-one').length).toBe(1);
+    expect(form.find('input#name-1-and-another-one').length).toBe(1);
+    expect(form.find('input#another-dank-id').length).toBe(1);
+    expect(form.find('textarea#id-2-the-ego').length).toBe(1);
+    expect(form.find('input#fire-file').length).toBe(1);
+  });
 });
