3769035a7d74a3460b6b3752a2ff3290106ccb9c
Aaron <aaron@codecademy.com>
2021-03-04 12:41:49 -0500

    remove box

--- packages/gamut/src/AppWrapper/index.tsx
+++ packages/gamut/src/AppWrapper/index.tsx
@@ -1,10 +1,15 @@
 import styled from '@emotion/styled';
 
-import { Box } from '../Box';
+/**
+ *
+ * This element safely resets the stacking context with limited side effects.
+ * https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Positioning/Understanding_z_index/The_stacking_context
+ *
+ * **WARNING**: Please do not change the values of position or z-index here or extend this component with overrides
+ * to these propertiesas it will no longer guarantee a predictable behavior
+ */
 
-// Ensure that positioning and index stay constant even if configured
-
-export const AppWrapper = styled(Box)`
+export const AppWrapper = styled.div`
   position: relative;
   z-index: 1;
 `;
