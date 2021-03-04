ae1c1c51996ce39726004aa69355ab7b80fa0f7e
Aaron <aaron@codecademy.com>
2021-03-04 12:36:24 -0500

    Add delayed render for portal

--- packages/gamut/src/BodyPortal/index.tsx
+++ packages/gamut/src/BodyPortal/index.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+import { useIsomorphicLayoutEffect } from 'react-use';
 
 import { AppWrapper } from '../AppWrapper';
 
 export const BodyPortal: React.FC = ({ children }) => {
+  const [ready, setReady] = useState(false);
+
+  /** Delay initial render once to ensure that rehydration does not conflict with portal mounting */
+  useIsomorphicLayoutEffect(() => {
+    setReady(typeof document !== 'undefined');
+  }, []);
+
+  if (!ready) return null;
+
   return ReactDOM.createPortal(
     <AppWrapper>{children}</AppWrapper>,
     document.body
