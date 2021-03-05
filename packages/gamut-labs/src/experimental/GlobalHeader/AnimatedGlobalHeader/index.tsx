0abe828d6fa1bcea5efb0837528c0cdcd81e36bb
Katie Zutter <katie@codecademy.com>
2021-03-05 11:03:30 -0600

    remove slide up/down transitions

--- packages/gamut-labs/src/experimental/GlobalHeader/AnimatedGlobalHeader/index.tsx
+++ packages/gamut-labs/src/experimental/GlobalHeader/AnimatedGlobalHeader/index.tsx
@@ -7,74 +7,17 @@ import { GlobalHeaderProps } from '..';
 import { BasicGlobalHeader } from '../BasicGlobalHeader';
 import styles from './styles.module.scss';
 
-const defaultScrollingState = {
-  isAtTopOfPage: true,
-  isInsideHeaderRegion: true,
-  isScrollingDown: true,
-  isScrollingDownFromHeaderRegion: true,
-  prevScrollPosition: typeof window === 'undefined' ? 0 : window?.pageYOffset,
-};
-
 export const AnimatedGlobalHeader: React.FC<GlobalHeaderProps> = (props) => {
-  const [scrollingState, setScrollingState] = useState(defaultScrollingState);
-
-  const {
-    isScrollingDownFromHeaderRegion,
-    isAtTopOfPage,
-    isInsideHeaderRegion,
-    isScrollingDown,
-    prevScrollPosition,
-  } = scrollingState;
+  const [isInHeaderRegion, setIsInHeaderRegion] = useState(true);
 
   const handleScrolling = useCallback(() => {
     const currentScrollPosition =
       typeof window === 'undefined' ? 0 : window?.pageYOffset;
 
-    // handle down/up scrolling
-    if (currentScrollPosition > prevScrollPosition) {
-      setScrollingState((prevState) => {
-        return {
-          ...prevState,
-          isScrollingDown: true,
-          prevScrollPosition: currentScrollPosition,
-        };
-      });
-    } else {
-      setScrollingState((prevState) => {
-        return {
-          ...prevState,
-          isScrollingDown: false,
-          prevScrollPosition: currentScrollPosition,
-        };
-      });
-    }
-
-    // handle static header region
-    if (currentScrollPosition === 0) {
-      setScrollingState((prevState) => ({
-        ...prevState,
-        isAtTopOfPage: true,
-        isInsideHeaderRegion: true,
-        isScrollingDownFromHeaderRegion: true,
-      }));
-    } else if (
-      currentScrollPosition < parseInt(theme.elements.headerHeight, 10)
-    ) {
-      setScrollingState((prevState) => ({
-        ...prevState,
-        isInsideHeaderRegion: true,
-      }));
-    } else {
-      setScrollingState((prevState) => ({
-        ...prevState,
-        isAtTopOfPage: false,
-        isInsideHeaderRegion: false,
-        isScrollingDownFromHeaderRegion:
-          prevState.isScrollingDown &&
-          prevState.isScrollingDownFromHeaderRegion,
-      }));
-    }
-  }, [prevScrollPosition]);
+    currentScrollPosition === 0
+      ? setIsInHeaderRegion(true)
+      : setIsInHeaderRegion(false);
+  }, []);
 
   useEffect(() => {
     window?.addEventListener('scroll', handleScrolling, {
@@ -91,17 +34,8 @@ export const AnimatedGlobalHeader: React.FC<GlobalHeaderProps> = (props) => {
     <Box height={theme.elements.headerHeight}>
       <BasicGlobalHeader
         className={cx(
-          isInsideHeaderRegion && isScrollingDownFromHeaderRegion
-            ? styles.staticHeader
-            : styles.stickyHeader,
-          !isScrollingDown && styles.transitionSlideDown,
-          !isInsideHeaderRegion &&
-            isScrollingDownFromHeaderRegion &&
-            styles.slideUp,
-          isScrollingDown &&
-            !isScrollingDownFromHeaderRegion &&
-            styles.transitionSlideUp,
-          isAtTopOfPage && styles.transitionFadeOut
+          styles.stickyHeader,
+          isInHeaderRegion && styles.transitionFadeOut
         )}
         {...props}
       />
