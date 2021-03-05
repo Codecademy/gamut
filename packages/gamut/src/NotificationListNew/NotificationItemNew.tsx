7ba432b03e32779ecfe850ca6c9fe7a715004051
Sanam Aghdaey <sanam@codecademy.com>
2021-03-05 10:29:15 -0500

    use notification type to determine icon if no image url provided

--- packages/gamut/src/NotificationListNew/NotificationItemNew.tsx
+++ packages/gamut/src/NotificationListNew/NotificationItemNew.tsx
@@ -1,5 +1,5 @@
 import { MiniDeleteIcon } from '@codecademy/gamut-icons';
-import { Bell } from '@codecademy/gamut-illustrations';
+import { Bell, Megaphone, New } from '@codecademy/gamut-illustrations';
 import { colors } from '@codecademy/gamut-styles';
 import styled from '@emotion/styled';
 import React, { ReactElement } from 'react';
@@ -15,6 +15,13 @@ const StyledLink = styled.a`
     text-decoration: none;
     background-color: ${({ theme }) => theme.colors['gray-100']};
   }
+`;
+
+const StyledImg = styled.img`
+  border-radius: 50%;
+  border: 1px solid ${({ theme }) => theme.colors.navy};
+  height: 3rem;
+  width: 3rem;
 `;
 
 const DateText = styled(Text)`
@@ -33,17 +40,30 @@ export const NotificationItemNew: React.FC<NotificationItemNewProps> = ({
   notification,
   handleClick,
 }) => {
-  const { date, link, text } = notification;
+  const { date, imageUrl, link, text, type } = notification;
 
   const dismissNotification = (event: React.MouseEvent) => {
     event.preventDefault();
     handleDismiss && handleDismiss();
   };
 
+  const getIcon = () => {
+    if (imageUrl) {
+      return <StyledImg src={imageUrl} alt="" />;
+    }
+    if (type === 'marketing_blast') {
+      return <Megaphone height={48} width={48} aria-label="marketing update" />;
+    }
+    if (type === 'curriculum_blast') {
+      return <New height={48} width={48} aria-label="curriculum update" />;
+    }
+    return <Bell height={48} width={48} />;
+  };
+
   const renderNotificationContent = (): ReactElement => {
     return (
       <FlexBox paddingY={24} justifyContent="space-between" paddingX={32}>
-        <Bell height={48} width={48} />
+        {getIcon()}
         <Box flexBasis={0} flexGrow={1} paddingLeft={12} textColor="navy">
           <Text as="span" fontSize="sm">
             {text}
