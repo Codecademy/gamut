fd2d4b99ffc4910dea5cce7f50be9f9b1b975192
Aaron <aaron@codecademy.com>
2021-03-06 20:16:50 -0500

    Convert skip to content to emotion

--- packages/gamut/src/SkipToContent/index.tsx
+++ packages/gamut/src/SkipToContent/index.tsx
@@ -1,30 +1,52 @@
 import { ArrowDownIcon } from '@codecademy/gamut-icons';
-import cx from 'classnames';
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
 import React from 'react';
 
-import styles from './styles.module.scss';
+import { Box } from '../Box';
 
 export type SkipToContentProps = {
   className?: string;
   contentId: string;
 };
 
+const SkipToContentLink = styled.a(
+  ({ theme }) => css`
+    color: ${theme.colors.navy};
+    background-color: ${theme.colors.white};
+    cursor: pointer;
+    display: flex;
+    font-weight: ${theme.fontWeight.title};
+    justify-content: center;
+    opacity: 0;
+    padding: 1.5rem 0;
+    position: absolute;
+    text-decoration: none;
+    left: -1000px;
+    z-index: 15;
+
+    &:focus {
+      opacity: 1;
+      left: 50%;
+      transform: translate(-50%, 0);
+      top: calc(${theme.elements.headerHeight} + 1rem);
+    }
+  `
+);
+
 export const SkipToContent: React.FC<SkipToContentProps> = ({
-  className,
   contentId,
+  ...rest
 }) => {
   const href = `#${contentId}`;
   const onClick = () => document.querySelector<HTMLElement>(href)!.focus();
 
   return (
-    <a
-      className={cx(styles.skipToContent, className)}
-      href={href}
-      onClick={onClick}
-      type="button"
-    >
-      Skip to Content
-      <ArrowDownIcon aria-hidden className={styles.downArrowIcon} />
-    </a>
+    <SkipToContentLink href={href} onClick={onClick} type="button" {...rest}>
+      <Box as="span" marginRight={8}>
+        Skip to Content
+      </Box>
+      <ArrowDownIcon aria-hidden />
+    </SkipToContentLink>
   );
 };
