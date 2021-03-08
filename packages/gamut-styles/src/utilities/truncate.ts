7dad4216405c139b4b894bee7fd3e4caf3aad6cc
Aaron <aaron@codecademy.com>
2021-03-08 03:10:35 -0500

    remove fade truncate

--- packages/gamut-styles/src/utilities/truncate.ts
+++ packages/gamut-styles/src/utilities/truncate.ts
@@ -1,38 +1,5 @@
 import { css } from '@emotion/react';
 
-export interface FadeTruncateProps {
-  lineHeight: string | number;
-  lines: number;
-  backgroundColor?: string;
-}
-
-export const fadeTruncate = ({
-  backgroundColor,
-  lineHeight,
-  lines,
-}: FadeTruncateProps) => css`
-  position: relative;
-  width: 100%;
-  max-height: ${lines * parseInt(`${lineHeight}`, 10) * 16}rem;
-  display: inline-block;
-  overflow: hidden;
-
-  &:after {
-    content: '';
-    text-align: right;
-    position: absolute;
-    top: ${lines * (parseInt(`${lineHeight}`, 10) - 1) * 16}rem;
-    right: 0;
-    width: 35%;
-    height: ${lineHeight}rem;
-    background: linear-gradient(
-      to right,
-      rgba(${backgroundColor}, 0),
-      rgba(${backgroundColor}, 1) 75%
-    );
-  }
-`;
-
 export const truncate = css`
   overflow: hidden;
   white-space: nowrap;
