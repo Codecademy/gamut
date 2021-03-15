eb8e0f203db530c00e6c3d25737ef31a26e94da3
Aaron <aaron@codecademy.com>
2021-03-15 14:54:23 -0400

    Split into prefetch and preload

--- packages/gamut-styles/src/fonts/index.tsx
+++ packages/gamut-styles/src/fonts/index.tsx
@@ -8,18 +8,21 @@ export const FONTS = [
     filePath: '/apercu-regular-pro',
     extensions: ['woff', 'woff2'],
     name: 'Apercu',
+    rel: 'preload',
   },
   {
     filePath: '/apercu-italic-pro',
     extensions: ['woff', 'woff2'],
     name: 'Apercu',
     style: 'italic',
+    rel: 'prefetch',
   },
   {
     filePath: '/apercu-bold-pro',
     extensions: ['woff', 'woff2'],
     name: 'Apercu',
     weight: 'bold',
+    rel: 'preload',
   },
   {
     filePath: '/apercu-bold-italic-pro',
@@ -27,17 +30,20 @@ export const FONTS = [
     name: 'Apercu',
     weight: 'bold',
     style: 'italic',
+    rel: 'prefetch',
   },
   {
     filePath: '/SuisseIntlMono-Bold-WebS',
     extensions: ['woff', 'woff2'],
     name: 'Suisse',
     weight: 'bold',
+    rel: 'preload',
   },
   {
     filePath: '/SuisseIntlMono-Regular-WebS',
     extensions: ['woff', 'woff2'],
     name: 'Suisse',
+    rel: 'prefetch',
   },
 ];
 
@@ -59,22 +65,18 @@ const fontFaces = css(
 
 export const FontGlobals = () => <Global styles={fontFaces} />;
 
-const createLinkProps = (url: string) => ({
-  rel: 'preload',
-  as: 'font',
-  crossOrigin: 'anonymous',
-  href: url,
-  type: `font/${url.split('.')[1]}`,
-});
-
 export const createFontLinks = () => {
   const links: React.ReactNode[] = [];
-  FONTS.forEach(({ filePath, extensions }) =>
+  FONTS.forEach(({ filePath, extensions, rel }) =>
     extensions.forEach((ext) =>
       links.push(
         <link
           key={`${filePath}-${ext}`}
-          {...createLinkProps(`${FONT_ASSET_PATH}/${filePath}.${ext}`)}
+          rel={ext === 'woff2' ? rel : 'prefetch'}
+          href={`${FONT_ASSET_PATH}${filePath}.${ext}`}
+          crossOrigin="anonymous"
+          as="font"
+          type={`font/${ext}`}
         />
       )
     )
