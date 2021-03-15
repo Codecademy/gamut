8fce004954ec4f6f955a0a7f3163be6db973a1f7
christian-dinh <christian.t.dinh@gmail.com>
2021-03-15 15:44:41 -0400

    feat(PageHero): don't hide video on xs screens

--- packages/gamut-labs/src/landingPage/PageHeroMedia.tsx
+++ packages/gamut-labs/src/landingPage/PageHeroMedia.tsx
@@ -1,4 +1,4 @@
-import { Column, Video } from '@codecademy/gamut';
+import { Column, ColumnSizes, Video } from '@codecademy/gamut';
 import { breakpoints } from '@codecademy/gamut-styles';
 import styled from '@emotion/styled';
 import { omit } from 'lodash';
@@ -6,40 +6,41 @@ import React from 'react';
 
 import { MediaProps } from './PageHero';
 
-const RightColumn = styled(Column)`
-  margin-left: 1rem;
-
-  @media only screen and (max-width: ${breakpoints.sm}) {
-    display: none;
-  }
-`;
-
 const Image = styled.img`
-  align-self: center;
   width: 100%;
 `;
 
-const StyledVideo = styled(Video)`
-  align-self: center;
+const RightColumn = styled(Column)<{ hideOnMobile?: boolean }>`
+  align-items: center;
+  margin-left: 1rem;
+
+  @media only screen and (max-width: ${breakpoints.sm}) {
+    ${({ hideOnMobile }) =>
+      hideOnMobile ? 'display: none;' : 'margin-left: 0;'}
+  }
 `;
 
 export type PageHeroMediaProps = {
   media: MediaProps;
+  size: ColumnSizes;
 };
 
-export const PageHeroMedia: React.FC<PageHeroMediaProps> = ({ media }) => {
+export const PageHeroMedia: React.FC<PageHeroMediaProps> = ({
+  media,
+  size,
+}) => {
   switch (media.type) {
     case 'image':
       return (
-        <RightColumn size={3}>
+        <RightColumn size={size} hideOnMobile>
           <Image src={media.src} alt={media.alt} />
         </RightColumn>
       );
     case 'video':
       const videoArgs = omit(media, 'type');
       return (
-        <RightColumn size={5}>
-          <StyledVideo {...videoArgs} />
+        <RightColumn size={{ xs: 12, sm: size }}>
+          <Video {...videoArgs} />
         </RightColumn>
       );
   }
