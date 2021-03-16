098938a04f11352937ef9aad2fd76ec79d13ce08
Aaron <aaron@codecademy.com>
2021-03-16 15:10:52 -0400

    Fix tests

--- packages/styleguide/.storybook/decorators/theme.tsx
+++ packages/styleguide/.storybook/decorators/theme.tsx
@@ -8,13 +8,18 @@ import { GamutProvider } from '@codecademy/gamut-styles';
  */
 
 export const withEmotion = (Story: any, parameters) => {
+  // Always give iframes the full provider
   if (parameters.viewMode === 'canvas') {
-    const isBuild = process.env.NODE_ENV !== 'test';
-    return (
-      <GamutProvider useCache={isBuild} preload={isBuild}>
-        {Story()}
-      </GamutProvider>
-    );
+    return <GamutProvider>{Story()}</GamutProvider>;
   }
+
+  // Wrap tested stories always
+  if (process.env.NODE_ENV === 'test') {
+    <GamutProvider useCache={false} preload={false}>
+      {Story()}
+    </GamutProvider>;
+  }
+
+  // Docs mode stories do not need this
   return Story();
 };
