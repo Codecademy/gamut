70f92ad0252aa1c4706161ec39267b8b87af50ed
Natalie Wee <natalieweesh@gmail.com>
2021-03-16 11:58:35 -0400

    fix svgs

--- packages/gamut-illustrations/src/Envelope.tsx
+++ packages/gamut-illustrations/src/Envelope.tsx
@@ -20,16 +20,16 @@ export const Envelope: React.FC<IllustrationProps> = ({
     <path
       d="M15.25 17.9585H32.75V30.4585H15.25V17.9585Z"
       stroke="black"
-      stroke-width="1.25"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="1.25"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
     <path
       d="M32.4682 18.417L25.6815 23.637C25.1996 24.0077 24.6087 24.2087 24.0007 24.2087C23.3927 24.2087 22.8018 24.0077 22.3199 23.637L15.5332 18.417"
       stroke="black"
-      stroke-width="1.25"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="1.25"
+      strokeLinecap="round"
+      strokeLinejoin="round"
     />
   </svg>
 );
