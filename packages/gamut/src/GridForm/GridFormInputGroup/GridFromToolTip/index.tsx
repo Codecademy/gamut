683ed34daf2c9dad65d2bafa5b335383e003f271
vonbarown <voniel@codecademy.com>
2021-03-18 09:22:41 -0400

    fix visual bug for tooltip body overlap with icon

--- packages/gamut/src/GridForm/GridFormInputGroup/GridFromToolTip/index.tsx
+++ packages/gamut/src/GridForm/GridFormInputGroup/GridFromToolTip/index.tsx
@@ -2,11 +2,18 @@ import { MiniInfoOutlineIcon } from '@codecademy/gamut-icons';
 import styled from '@emotion/styled';
 import React from 'react';
 
-import { ToolTip, ToolTipProps } from '../../../ToolTip';
+import { ToolTip, ToolTipPosition, ToolTipProps } from '../../../ToolTip';
 
 const StyledToolTip = styled.span`
   position: absolute;
   left: calc(100% - 1.1rem);
+
+  div {
+    > *:last-child {
+      z-index: 1;
+      left: calc(100% - 1.7rem);
+    }
+  }
 `;
 
 export const GridFormToolTip: React.FC<ToolTipProps> = (props) => {
@@ -14,6 +21,7 @@ export const GridFormToolTip: React.FC<ToolTipProps> = (props) => {
     <StyledToolTip>
       <ToolTip
         {...props}
+        position={ToolTipPosition.BottomRight}
         target={<MiniInfoOutlineIcon height="0.8rem" width="0.8rem" />}
       />
     </StyledToolTip>
