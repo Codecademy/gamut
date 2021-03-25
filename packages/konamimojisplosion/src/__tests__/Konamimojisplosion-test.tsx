31371ac4690dc6119a4dc9407ea33477a671f9f1
Josh Goldberg <josh@codecademy.com>
2021-03-25 10:12:50 -0400

    Lol whatever KonamiCode

--- packages/konamimojisplosion/src/__tests__/Konamimojisplosion-test.tsx
+++ packages/konamimojisplosion/src/__tests__/Konamimojisplosion-test.tsx
@@ -2,6 +2,10 @@ import { setupRtl } from '@codecademy/gamut-tests';
 
 import { Konamimojisplosion } from '..';
 
+jest.mock('../useKonamimojisplosion', () => ({
+  useKonamimojisplosion: () => {},
+}));
+
 const renderView = setupRtl(Konamimojisplosion);
 
 describe('Konamimojisplosion', () => {
