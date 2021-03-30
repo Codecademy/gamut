946fa166ce7e1415dedc3a52e42bb462e2cbf2cd
Aaron Robb <52927224+codecaaron@users.noreply.github.com>
2021-03-29 21:03:00 -0400

    chore(Dependencies):  Upgrade TS / RHF to enable `themed` function [WEB-1370] [WEB-1371] [GM-191]
    * Adds lodash like higher order function for looking up theme values in `styled` tagged templates.
    * Gives us a simple way to ensure that code will always have values from the current context.
    * Updates typescript to `4.2.3` to ensure number interpolation in paths.  We will need these for spacing and fontsizes to work.
    * Updates `react-hook-form` to `6.8.5` for typescript compatibility.
    * Updates some types and tests in GridForm to correctly update after the package bump.
    * Updates some unit tests that might have more asynchronous behavior to RTL to ensure that internal RHF hooks fire correctly.

--- packages/gamut/src/Form/FormGroup.tsx
+++ packages/gamut/src/Form/FormGroup.tsx
@@ -1,5 +1,5 @@
 import styled from '@emotion/styled';
-import React, { HTMLAttributes } from 'react';
+import React, { ComponentProps } from 'react';
 
 import { Box } from '../Box';
 import { ToolTipProps } from '../ToolTip';
@@ -7,7 +7,8 @@ import { FormError } from './FormError';
 import { FormGroupDescription } from './FormGroupDescription';
 import { FormGroupLabel } from './FormGroupLabel';
 
-export type FormGroupProps = HTMLAttributes<HTMLDivElement> & {
+export interface FormGroupProps
+  extends ComponentProps<typeof FormGroupContainer> {
   label?: string;
   /**
    * [The for/id string of a label or labelable form-related element](https://developer.mozilla.org/en-US/docs/Web/API/HTMLLabelElement/htmlFor). The outer FormGroup or FormLabel should have an identical string as the inner FormElement for accessibility purposes.
@@ -20,14 +21,17 @@ export type FormGroupProps = HTMLAttributes<HTMLDivElement> & {
   disabled?: boolean;
   labelSize?: 'small' | 'large';
   tooltip?: ToolTipProps;
-};
+}
 
-const FormGroupContainer = styled(Box)<FormGroupProps>`
+const FormGroupContainer = styled(Box)`
   position: relative;
-  padding-bottom: 0.5rem;
-  margin-bottom: 1.5rem;
   width: 100%;
 `;
+
+FormGroupContainer.defaultProps = {
+  paddingBottom: 8,
+  marginBottom: 24,
+};
 
 export const FormGroup: React.FC<FormGroupProps> = ({
   tooltip,
