7dbb56dbc5e4feaf572ded6a27c3f63403df3e65
Aaron Robb <52927224+codecaaron@users.noreply.github.com>
2021-03-31 12:50:38 -0400

    feat(Variance): `css` - Standard CSS Property Passthrough + Selector System Props [WEB-1117]
    #### Static CSS Utility Types
    ```tsx
    export type SelectorMap<Props, System> = {
      [K in keyof Props]?: SelectorProps<Props[K], System>;
    };
    
    export type SelectorProps<Props, System> = {
      [K in keyof Props]?: K extends keyof System
        ? System[K] // System Property Types
        : K extends keyof PropertyTypes
        ? PropertyTypes[K] // Any Valid CSS Prop
        : Omit<PropertyTypes, keyof System> & System // No Match Opens a single level of nesting for System + General CSS;
    };
    ```
    
    #### Accept and Pass Through all valid CSS properties on Static Utilities (as they are not props).
    This change lets us configure anything that is likely to be non dynamic but required for a complete and discrete style.   The typings for both `variant` and `css` accept all properties that are not present on the passed configuration.
    
    **These are still guaranteed to be typesafe by CSSType.**
    
    These props can only be the value specified and will not accept values with the responsive syntax.  
    ```tsx
    const styled.ul(variant({
       key: 'listVariant',
       variants: {
          vertical: {
             listStyle: 'none',  // not a system prop but valid 
             p: 0,
             m: 0,
          },
          emoji: {
             listStyleType: "\1F44D",  // not 
             pr: [12, 24],
             m: 0
          }
    }))
    ```
    When generating the styles the function will ignore the all props not specified by our configuration and simply pass them through to the result CSSObject. 
    
    #### Selectors for Static Utilities
    This change lets us specify scoped selector syntax for anything not matching a valid CSS prop or system property.  The typings here are very loose as the evaluation of possible selectors is very taxing even with stronger aliasing.   This with the addition of the CSS Passthrough will allows us to create complete variants for much more complex css as scale.  And create robust sharable utilities. 
    
    ```tsx
    const styled.a(variant({
       variants: {
          interface: {
             color: 'navy',
             '&:hover': { 
                 textDecoration: 'none',
                 border: '2px solid currentColor',
                 borderWidth: {  md: '4px' } // Responsive syntax party town
             }
          },
          inline: {
             color: 'hyper',
             transition: 'scale 150ms ease',
             '&:hover': {
                textDecoration: 'underline',
                transform: 'scale(1.2)', // static properties work here too!
             }
          }
    }))
    ```

--- packages/variance/src/__tests__/variance-test.ts
+++ packages/variance/src/__tests__/variance-test.ts
@@ -212,7 +212,10 @@ describe('css', () => {
     });
   });
   it('works with media queries', () => {
-    const returnedFn = css({ width: ['100%', '200%'], height: '50' });
+    const returnedFn = css({
+      width: ['100%', '200%'],
+      height: '50',
+    });
 
     expect(returnedFn({ theme })).toEqual({
       width: '100%',
@@ -223,11 +226,61 @@ describe('css', () => {
   it('allows selectors', () => {
     const returnedFn = css({
       width: ['100%', '200%'],
+      '&:hover': {
+        width: '50%',
+      },
     });
 
     expect(returnedFn({ theme })).toEqual({
       width: '100%',
       XS: { width: '200%' },
+      '&:hover': {
+        width: '50%',
+      },
+    });
+  });
+  it('allows selectors with media queries', () => {
+    const returnedFn = css({
+      width: ['100%', '200%'],
+      '&:hover': {
+        width: ['50%', '25%'],
+      },
+    });
+
+    expect(returnedFn({ theme })).toEqual({
+      width: '100%',
+      XS: { width: '200%' },
+      '&:hover': {
+        width: '50%',
+        XS: { width: '25%' },
+      },
+    });
+  });
+
+  it('allows static valid CSS to pass through', () => {
+    const returnedFn = css({
+      display: 'grid',
+      width: ['100%', '200%'],
+    });
+
+    expect(returnedFn({ theme })).toEqual({
+      display: 'grid',
+      width: '100%',
+      XS: { width: '200%' },
+    });
+  });
+
+  it('allows static valid CSS to pass through within selectors', () => {
+    const returnedFn = css({
+      display: 'grid',
+      '&:hover': {
+        display: 'none',
+      },
+    });
+
+    expect(returnedFn({ theme })).toEqual({
+      display: 'grid',
+      '&:hover': { display: 'none' },
     });
   });
 
@@ -310,6 +363,53 @@ describe('variants', () => {
 
     expect(myVariant({ theme, sweet: 'cool' })).toEqual({
       width: '100%',
+    });
+  });
+  it('allows variant props with selectors', () => {
+    const myVariant = variant({
+      prop: 'sweet',
+      variants: {
+        cool: {
+          width: '100%',
+          '&:hover': {
+            width: '50%',
+          },
+        },
+      },
+    });
+
+    expect(myVariant({ theme, sweet: 'cool' })).toEqual({
+      width: '100%',
+      '&:hover': {
+        width: '50%',
+      },
+    });
+  });
+
+  it('allows variant props with selectors with media queries', () => {
+    const myVariant = variant({
+      prop: 'sweet',
+      variants: {
+        cool: {
+          width: '100%',
+          '&:hover': {
+            width: ['50%', '25%'],
+          },
+        },
+        story: {
+          margin: 0,
+        },
+      },
+    });
+
+    expect(myVariant({ theme, sweet: 'cool' })).toEqual({
+      width: '100%',
+      '&:hover': {
+        width: '50%',
+        XS: {
+          width: '25%',
+        },
+      },
     });
   });
   it('caches the variant once called', () => {
