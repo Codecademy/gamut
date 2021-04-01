2d0dcd3d0b7e7d687e414592150482e49425c7d0
Roger Vera <roger@codecademy.com>
2021-04-01 12:02:33 -0400

    Rebase for image

--- packages/gamut-labs/src/experimental/CurriculumCard/Image/index.tsx
+++ packages/gamut-labs/src/experimental/CurriculumCard/Image/index.tsx
@@ -1,25 +1,46 @@
-import React from 'react';
+import { pxRem } from '@codecademy/gamut-styles';
+import styled from '@emotion/styled';
+import React, { useState } from 'react';
 
-// import { DeferredRender } from '~/components/DeferredRender';
-// import { CardPlaceholderAssetPath } from '~/libs/contentAssetPaths';
+import { DeferredRender } from '../../DeferredRender';
+
+const StyledImg = styled.img`
+  display: block;
+  margin: 0 auto;
+  width: ${pxRem(160)};
+`;
+
+type ProgressState = 'inProgress' | 'completed';
+
+const BASE_STATIC_ASSET_PATH =
+  'https://static-assets.codecademy.com/components';
+
+const getPlaceholderAssetPath = (pathProgressState?: ProgressState) => {
+  const progressState = pathProgressState ? `-${pathProgressState}` : '';
+  return `${BASE_STATIC_ASSET_PATH}/curriculum/path/placeholder${progressState}.svg`;
+};
 
 export type ImageProps = {
   image: string;
-  progressState?: 'inProgress' | 'completed';
+  progressState?: ProgressState;
 };
 
-export const Image: React.FC<ImageProps> = ({ image }) => {
-  // const [error, setError] = useState(false);
+export const Image: React.FC<ImageProps> = ({ image, progressState }) => {
+  const [error, setError] = useState(false);
 
-  // const addDefaultImageSource = (event: React.SyntheticEvent) => {
-  //   if (!error) {
-  //     (event.target as HTMLImageElement).onerror = null;
-  //     (event.target as HTMLImageElement).src = CardPlaceholderAssetPath(
-  //       progressState
-  //     );
-  //     setError(true);
-  //   }
-  // };
+  const addDefaultImageSource = (event: React.SyntheticEvent) => {
+    if (!error) {
+      (event.target as HTMLImageElement).onerror = null;
+      (event.target as HTMLImageElement).src = getPlaceholderAssetPath(
+        progressState
+      );
+      setError(true);
+    }
+  };
 
-  return <img src={image} alt="" />;
+  return (
+    <DeferredRender>
+      <StyledImg src={image} alt="" onError={addDefaultImageSource} />
+    </DeferredRender>
+  );
 };
