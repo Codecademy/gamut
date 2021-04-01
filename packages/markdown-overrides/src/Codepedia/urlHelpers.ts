dda8327ea0d21e6eb3cff6ed2cdd492d750a7094
Kip Price <4605379+kipprice@users.noreply.github.com>
2021-04-01 11:43:04 -0400

    fix codepedia references

--- packages/markdown-overrides/src/Codepedia/urlHelpers.ts
+++ packages/markdown-overrides/src/Codepedia/urlHelpers.ts
@@ -1,11 +1,9 @@
 export const VIEW_ROOT = 'https://codecademy.github.io/codepedia/entries';
 export const RAW_ROOT =
-  'https://raw.githubusercontent.com/codecademy/codepedia/main/codepedia';
+  'https://raw.githubusercontent.com/codecademy/codepedia/main/codepedia/entries';
 
 export const getLoadUrl = (concept: string, language?: string) => {
-  return `${RAW_ROOT}/${concept}/${concept}${
-    language ? `.${language}` : ''
-  }.md`;
+  return `${RAW_ROOT}/${language || 'concept'}/${concept}.md`;
 };
 
 export const getViewUrl = (concept: string, language?: string) => {
