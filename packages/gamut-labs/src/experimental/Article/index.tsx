051264fd3c9af3e55c41dfa130c0de71c61fe848
Aaron Robb <aaron@codecademy.com>
2021-04-01 12:31:43 -0400

    Article :/

--- packages/gamut-labs/src/experimental/Article/index.tsx
+++ packages/gamut-labs/src/experimental/Article/index.tsx
@@ -1,4 +1,6 @@
 import { Markdown, Text } from '@codecademy/gamut';
+import { ThemeShape } from '@codecademy/gamut-styles';
+import { useTheme } from '@emotion/react';
 import React from 'react';
 
 export type ArticleProps = {
@@ -13,15 +15,20 @@ export const Article: React.FC<ArticleProps> = ({
   description,
   content,
 }) => {
+  const {
+    colorModes: { active },
+  } = useTheme();
+  const titleColors = active === 'dark' ? 'lightBlue' : 'navy';
+
   return (
     <>
-      <Text as="strong" fontFamily="accent">
+      <Text as="strong" fontFamily="accent" textColor={titleColors}>
         Article
       </Text>
-      <Text as="h1" lineHeight="base">
+      <Text as="h1" lineHeight="base" textColor={titleColors}>
         {title}
       </Text>
-      <Text as="h2" fontSize={22} fontWeight="base">
+      <Text as="h2" fontSize={22} fontWeight="base" textColor={titleColors}>
         {description}
       </Text>
       <Markdown text={content} />
