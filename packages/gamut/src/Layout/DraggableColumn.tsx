704f614db4cabdc79d30846ab0cf1931152e672a
cass spain <cass@codecademy.com>
2021-04-01 17:04:25 -0400

    lets try a ref

---
+++ packages/gamut/src/Layout/DraggableColumn.tsx
@@ -0,0 +1,35 @@
+import cx from 'classnames';
+import React, { forwardRef } from 'react';
+
+import {
+  OptionalResponsiveProperty,
+  ResponsiveProperty,
+} from '../typings/responsive-properties';
+import { generateResponsiveClassnames } from '../utils/generateResponsiveClassnames';
+import styles from './styles/Column.module.scss';
+import { ColumnSizes, ContainerElementProps, OffsetColumnSizes } from './types';
+
+export type DraggablColumnProps = {
+  /** The number of columns this element should span out of 12 */
+  size: ResponsiveProperty<ColumnSizes>;
+  /** The column that this element should start at */
+  offset?: OptionalResponsiveProperty<OffsetColumnSizes>;
+  /** The number of rows this element should span */
+  rowspan?: OptionalResponsiveProperty<1 | 2 | 3>;
+} & HTMLDivElement &
+  ContainerElementProps;
+
+export const DraggableColumn = forwardRef<HTMLDivElement, DraggablColumnProps>(
+  ({ children, className, size, offset, rowspan, testId, ...rest }, ref) => {
+    const classNames = cx(
+      styles.container,
+      className,
+      generateResponsiveClassnames({ size, offset, rowspan }, styles)
+    );
+    return (
+      <div className={classNames} data-testid={testId} ref={ref}>
+        {children}
+      </div>
+    );
+  }
+);
