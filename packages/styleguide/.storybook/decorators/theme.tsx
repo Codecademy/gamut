1ab1299d61363f4cb7bd6f84825a1e641badd591
Aaron Robb <aaron@codecademy.com>
2021-04-02 18:00:27 -0400

    Add Params

--- packages/styleguide/.storybook/decorators/theme.tsx
+++ packages/styleguide/.storybook/decorators/theme.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { Box, ContentContainer } from '@codecademy/gamut/src';
-import { GlobalHeader } from '@codecademy/gamut-labs';
-
 import { ColorMode, GamutProvider } from '@codecademy/gamut-styles/src';
 
 /**
@@ -34,39 +32,29 @@ export const withContext = (Story: any, options) => {
     return Story();
   }
   const {
-    parameters: {
-      layoutOptions = {
-        context: false,
-        backgroundColor: 'background',
-      },
-    },
+    parameters: { layoutType = 'padded' },
   } = options;
-  const { context, backgroundColor } = layoutOptions;
-
-  if (!context) {
-    return (
-      <Box backgroundColor={backgroundColor} padding={32}>
-        {Story()}
-      </Box>
-    );
+  switch (layoutType) {
+    case 'isolated':
+      return <Box backgroundColor="background">{Story()}</Box>;
+    case 'content':
+      return (
+        <Box backgroundColor="background">
+          <ContentContainer>{Story()}</ContentContainer>
+        </Box>
+      );
+    case 'wide':
+      return (
+        <Box backgroundColor="background">
+          <ContentContainer size="wide">{Story()}</ContentContainer>
+        </Box>
+      );
+    case 'padded':
+    default:
+      return (
+        <Box backgroundColor="background" padding={32}>
+          {Story()}
+        </Box>
+      );
   }
-
-  const { size = 'medium', showHeader = false } = context;
-
-  return (
-    <Box backgroundColor={backgroundColor}>
-      {showHeader && (
-        <GlobalHeader
-          action={() => {}}
-          type="free"
-          user={{
-            avatar:
-              'https://www.gravatar.com/avatar/1c959a9a1e2f9f9f1ac06b05cccc1d60?s=150&d=retro',
-            displayName: 'Codey',
-          }}
-        />
-      )}
-      <ContentContainer size={size}>{Story()}</ContentContainer>
-    </Box>
-  );
 };
