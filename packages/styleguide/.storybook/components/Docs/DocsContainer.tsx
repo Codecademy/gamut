321dd8f084f1613182a357218b8ce64246972e10
Aaron Robb <52927224+codecaaron@users.noreply.github.com>
2021-04-05 14:27:14 -0400

    chore(Styleguide):  Spring Cleaning
    This has been sitting in the backlog a while and since storybook is linking was completely broken I decided to have a hackathon to clean up some of the cruft and knock out the planned design updates.
    
    ### Visual Identity => Docs
    Brings out Docs Pages into the fold with our visual identity (making it even easier to migrate if we care to, and making our docs a living showcase of gamut).
    - Replaces the current DocsWrapper with our own root wrapper.
    - Theme overrides and custom mapped components
    - Background and ContentContainer standardization.
    
    #### Before
    ![gamut codecademy com_storybook__path=_story_gamut--page](https://user-images.githubusercontent.com/52927224/113532769-2731a800-959a-11eb-9fe8-c5038fab2ebe.png)
    ![gamut codecademy com_storybook__path=_story_gamut--page (1)](https://user-images.githubusercontent.com/52927224/113532765-26007b00-959a-11eb-834d-8e503bd76baf.png)
    
    #### After 
    ![localhost_6006__path=_story_gamut--page](https://user-images.githubusercontent.com/52927224/113532768-2731a800-959a-11eb-9c54-05e45571046d.png)
    ![localhost_6006__path=_story_gamut--page (1)](https://user-images.githubusercontent.com/52927224/113532767-26991180-959a-11eb-82e0-02ad52097b8b.png)
    
    
    ### Navigation
    Completely refactors the navigation derivation into a single provider and set of methods to look up specific paths along a generated hierarchy with an index / root specification that is configurable at the global level.
    - Refactors the base hooks and access pattern for generated links.
    - Refactors and uses our own custom `Link` component (LinkTo was consistently broken and unusable).
    - Updates components like BreadCrumbs and TableOfContents to uses these new patterns and gamut components exclusively.
    
    ### System Prop Addon
    A lot of our docgen got completely messed up with Typescript 4.2, since type aliases have an extra layer of caching we cannot get the raw scales from these anymore. 
    - Removes a lot of the unnecessary formatting and derivation.
    - Refactors to use gamut components.
    - Updates all `<Props />` instances to `PropsTable`.
    
    ### Other Changes
    - Complete reorg and pruning of the `.storybook/components` directory.
    - Removes now fully removable stories from the codebase `SplitInterstitial` is dead.

---
+++ packages/styleguide/.storybook/components/Docs/DocsContainer.tsx
@@ -0,0 +1,64 @@
+import { MDXProvider } from '@mdx-js/react';
+import { ThemeProvider, ensure as ensureTheme } from '@storybook/theming';
+import { components as htmlComponents } from '@storybook/components';
+import {
+  DocsContext,
+  SourceContainer,
+  DocsContextProps,
+  CodeOrSourceMdx,
+  HeadersMdx,
+} from '@storybook/addon-docs/blocks';
+import { AssetProvider } from '@codecademy/gamut-styles/src/AssetProvider';
+import {
+  createEmotionCache,
+  GamutProvider,
+} from '@codecademy/gamut-styles/src';
+import { NavigationProvider } from '../Navigation/NavigationProvider';
+import React from 'react';
+import { merge } from 'lodash';
+import { Link } from '../Markdown/Elements';
+
+const emotionCache = createEmotionCache({ speedy: false });
+
+const defaultComponents = {
+  ...htmlComponents,
+  code: CodeOrSourceMdx,
+  a: Link,
+  ...HeadersMdx,
+};
+
+export const DocsContainer: React.FC<{ context: DocsContextProps }> = ({
+  context,
+  children,
+}) => {
+  const { parameters = {} } = context || {};
+  const { docs = {} } = parameters;
+
+  let themeVars = docs.theme;
+
+  const theme = ensureTheme(themeVars);
+  const overrides = merge({}, theme, {
+    appBorderRadius: 2,
+    typography: {
+      size: {
+        s2: 16,
+      },
+    },
+  });
+  const allComponents = { ...defaultComponents, ...docs.components };
+
+  return (
+    <DocsContext.Provider value={context}>
+      <GamutProvider cache={emotionCache}>
+        <AssetProvider />
+        <NavigationProvider>
+          <SourceContainer>
+            <ThemeProvider theme={overrides}>
+              <MDXProvider components={allComponents}>{children}</MDXProvider>
+            </ThemeProvider>
+          </SourceContainer>
+        </NavigationProvider>
+      </GamutProvider>
+    </DocsContext.Provider>
+  );
+};
