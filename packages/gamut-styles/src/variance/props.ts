0f9b0ad67c7cbaebddf8a380667b6e42669ca060
Aaron Robb <aaron@codecademy.com>
2021-04-06 11:06:57 -0400

    Rename

--- packages/gamut-styles/src/variance/props.ts
+++ packages/gamut-styles/src/variance/props.ts
@@ -7,16 +7,16 @@ import * as PROPERTIES from './config';
  * These are not exported yet, but will be used when gamut-system is deprecated
  */
 
-export const typographyProps = variance.create(PROPERTIES.typography);
-export const gridProps = variance.create(PROPERTIES.grid);
-export const flexProps = variance.create(PROPERTIES.flex);
-export const layoutProps = variance.create(PROPERTIES.layout);
-export const positioningProps = variance.create(PROPERTIES.positioning);
-export const backgroundProps = variance.create(PROPERTIES.background);
-export const colorProps = variance.create(PROPERTIES.color);
-export const shadowProps = variance.create(PROPERTIES.shadows);
-export const spaceProps = variance.create(PROPERTIES.space);
-export const borderProps = variance.create(PROPERTIES.border);
+export const typography = variance.create(PROPERTIES.typography);
+export const grid = variance.create(PROPERTIES.grid);
+export const flex = variance.create(PROPERTIES.flex);
+export const layout = variance.create(PROPERTIES.layout);
+export const positioning = variance.create(PROPERTIES.positioning);
+export const background = variance.create(PROPERTIES.background);
+export const color = variance.create(PROPERTIES.color);
+export const shadow = variance.create(PROPERTIES.shadows);
+export const space = variance.create(PROPERTIES.space);
+export const border = variance.create(PROPERTIES.border);
 
 export const css = variance.createCss(PROPERTIES.all);
 
