e7cfefab52371e0676de11e871c9b470d1260e61
Aaron Robb <aaron@codecademy.com>
2021-04-10 18:46:00 -0400

    Add Color Tests

--- packages/variance/src/createTheme/createTheme.test.ts
+++ packages/variance/src/createTheme/createTheme.test.ts
@@ -49,7 +49,7 @@ describe('createTheme', () => {
     expect(variables.root).toEqual({ '--test-test': 1, '--test-test2': 2 });
   });
 
-  it('creates a colorMode', () => {
+  describe('colors', () => {
     const staticColors = {
       white: 'white',
       black: 'black',
@@ -57,26 +57,56 @@ describe('createTheme', () => {
       green: 'green',
       red: 'red',
     };
+    const cssVariableReferences = mapValues(
+      staticColors,
+      (val) => `var(--color-${val})`
+    );
+    const builder = createTheme(base);
+    it('creates color variables', () => {
+      const { theme } = builder.addColors(staticColors).build();
 
-    const { theme, variables } = createTheme(base)
-      .addColors(staticColors)
-      .createColorMode('light', {
-        light: {
-          primary: 'red',
-        },
-        dark: {
-          primary: 'blue',
-        },
-      })
-      .build();
+      expect(theme.colors).toEqual(cssVariableReferences);
+    });
+    it('adds colorModes', () => {
+      const { theme, variables } = builder
+        .addColors(staticColors)
+        .createColorMode('light', {
+          light: {
+            primary: 'red',
+          },
+          dark: {
+            primary: 'blue',
+          },
+        })
+        .build();
 
-    expect(theme.colors).toEqual({
-      ...mapValues(staticColors, (val) => `var(--color-${val})`),
-      primary: 'var(--color-primary)',
+      expect(theme.colors).toEqual({
+        ...mapValues(staticColors, (val) => `var(--color-${val})`),
+        primary: 'var(--color-primary)',
+      });
+
+      expect(variables.colorMode).toEqual({
+        '--color-primary': 'var(--color-red)',
+      });
     });
 
-    expect(variables.colorMode).toEqual({
-      '--color-primary': 'var(--color-red)',
+    it('returns value checker for colors', () => {
+      const { theme, getColorValue } = builder
+        .addColors({ black: '#000000', white: '#FFFFFF' })
+        .createColorMode('light', {
+          light: {
+            primary: 'black',
+          },
+          dark: {
+            primary: 'white',
+          },
+        })
+        .build();
+
+      expect(getColorValue('white')).toEqual('#FFFFFF');
+      expect(getColorValue(theme.colorModes.modes.light.primary)).toEqual(
+        '#000000'
+      );
     });
   });
 });
