3efa337edcd1ddcb717bb67e1f5faa9b0535439b
Zack M Fleischman <zFleischman@codecademy.com>
2021-04-13 17:29:59 -0700

    Adding SectionButton renderers

--- packages/gamut-labs/src/experimental/PageSection/index.tsx
+++ packages/gamut-labs/src/experimental/PageSection/index.tsx
@@ -1,23 +1,62 @@
-import { Box, FlexBox, TextButtonProps } from '@codecademy/gamut';
+import {
+  Box,
+  FlexBox,
+  Text,
+  TextButton,
+  TextButtonProps,
+} from '@codecademy/gamut';
 import React from 'react';
+
+type SectionButton = TextButtonProps & {
+  text: string;
+};
 
 export type PageSectionProps = {
   title: string;
-  headerButton?: TextButtonProps;
-  headerSecondaryButton?: TextButtonProps;
-  footerButton?: TextButtonProps;
+  headerButton?: SectionButton;
+  headerSecondaryButton?: SectionButton;
+  footerButton?: SectionButton;
 };
 
 export const PageSection: React.FC<PageSectionProps> = ({
   title,
+  headerButton,
+  headerSecondaryButton,
+  footerButton,
   children,
 }) => {
+  const renderSectionButton = (button: SectionButton) => {
+    const { text, ...textButtonProps } = button;
+    return <TextButton {...textButtonProps}>{text}</TextButton>;
+  };
+
+  const maybeRenderHeaderButton = () => {
+    if (!headerButton) return null;
+    renderSectionButton(headerButton);
+  };
+
+  const maybeRenderHeaderSecondaryButton = () => {
+    if (!headerSecondaryButton) return null;
+    renderSectionButton(headerSecondaryButton);
+  };
+
+  const maybeRenderFooter = () => {
+    if (!footerButton) return null;
+    return <Box>{renderSectionButton(footerButton)}</Box>;
+  };
+
   return (
     <FlexBox flexDirection="column">
-      <Box as="h5" color="navy" paddingBottom={4}>
-        {title}
+      <Box paddingBottom={4}>
+        <Text as="h5" paddingBottom={4}>
+          {' '}
+          {title}{' '}
+        </Text>
+        {maybeRenderHeaderButton()}
+        {maybeRenderHeaderSecondaryButton()}
       </Box>
       {children}
+      {maybeRenderFooter()}
     </FlexBox>
   );
 };
