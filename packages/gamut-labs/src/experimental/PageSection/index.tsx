815939dc0533f8d22affef8406aa25c2313fdfc7
Zack M Fleischman <zFleischman@codecademy.com>
2021-04-13 17:14:54 -0700

    Adding TextButtonProps as an export

--- packages/gamut-labs/src/experimental/PageSection/index.tsx
+++ packages/gamut-labs/src/experimental/PageSection/index.tsx
@@ -1,8 +1,11 @@
-import { Box, FlexBox } from '@codecademy/gamut';
+import { Box, FlexBox, TextButtonProps } from '@codecademy/gamut';
 import React from 'react';
 
 export type PageSectionProps = {
   title: string;
+  headerButton?: TextButtonProps;
+  headerSecondaryButton?: TextButtonProps;
+  footerButton?: TextButtonProps;
 };
 
 export const PageSection: React.FC<PageSectionProps> = ({
