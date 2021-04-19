048d4f9f5807bf4564d44f7b78d7089767764cff
Roger Vera <roger@codecademy.com>
2021-04-19 12:32:57 +0100

    Port Curriculum Card

---
+++ packages/gamut-labs/src/experimental/CurriculumCard/Difficulty/index.tsx
@@ -0,0 +1,33 @@
+import { Box } from '@codecademy/gamut';
+import styled from '@emotion/styled';
+import React from 'react';
+
+import { Text } from '../../Text';
+
+export type DifficultyProps = {
+  difficulty: 0 | 1;
+};
+
+const DifficultySpan = styled(Box)`
+  width: 8px;
+  height: 8px;
+  border-radius: 8px;
+  display: inline-block;
+  margin-right: 4px;
+  vertical-align: middle;
+  border: 1px solid ${({ theme }) => theme.colors.navy};
+`;
+
+export const Difficulty: React.FC<DifficultyProps> = ({ difficulty }) => (
+  <Text fontSize={14}>
+    <DifficultySpan
+      as="span"
+      backgroundColor={difficulty >= 0 ? 'navy' : 'white'}
+    />
+    <DifficultySpan
+      as="span"
+      backgroundColor={difficulty >= 1 ? 'navy' : 'white'}
+    />
+    {difficulty === 0 ? 'Beginner friendly' : 'Intermediate'}
+  </Text>
+);
---
+++ packages/gamut-labs/src/experimental/CurriculumCard/Image/index.tsx
@@ -0,0 +1,49 @@
+import { pxRem } from '@codecademy/gamut-styles';
+import styled from '@emotion/styled';
+import React, { useState } from 'react';
+import { useIsomorphicLayoutEffect } from 'react-use';
+
+const StyledImg = styled.img`
+  display: block;
+  margin: 0 auto;
+  width: ${pxRem(160)};
+`;
+
+type ProgressState = 'inProgress' | 'completed';
+
+const BASE_STATIC_ASSET_PATH =
+  'https://static-assets.codecademy.com/components';
+
+const getPlaceholderAssetPath = (pathProgressState?: ProgressState) => {
+  const progressState = pathProgressState ? `-${pathProgressState}` : '';
+  return `${BASE_STATIC_ASSET_PATH}/curriculum/path/placeholder${progressState}.svg`;
+};
+
+export type ImageProps = {
+  image: string;
+  progressState?: ProgressState;
+};
+
+export const Image: React.FC<ImageProps> = ({ image, progressState }) => {
+  const [ready, setReady] = useState(false);
+  const [error, setError] = useState(false);
+
+  /** Delay initial render once to ensure that rehydration does not conflict with portal mounting */
+  useIsomorphicLayoutEffect(() => {
+    setReady(typeof document !== 'undefined');
+  }, []);
+
+  if (!ready) return null;
+
+  const addDefaultImageSource = (event: React.SyntheticEvent) => {
+    if (!error) {
+      (event.target as HTMLImageElement).onerror = null;
+      (event.target as HTMLImageElement).src = getPlaceholderAssetPath(
+        progressState
+      );
+      setError(true);
+    }
+  };
+
+  return <StyledImg src={image} alt="" onError={addDefaultImageSource} />;
+};
