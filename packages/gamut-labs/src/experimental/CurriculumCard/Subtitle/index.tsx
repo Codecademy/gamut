05eeae60dc8e1bd67c1420390d9850f4546da0e5
Roger Vera <roger@codecademy.com>
2021-04-19 12:49:55 +0100

    Remove redundant navy style rule for span

--- packages/gamut-labs/src/experimental/CurriculumCard/Subtitle/index.tsx
+++ packages/gamut-labs/src/experimental/CurriculumCard/Subtitle/index.tsx
@@ -1,15 +1,9 @@
 import { FlexBox } from '@codecademy/gamut';
-import { colors } from '@codecademy/gamut-styles';
-import styled from '@emotion/styled';
 import { capitalize } from 'lodash';
 import pluralize from 'pluralize';
 import React from 'react';
 
 import { Difficulty, DifficultyProps } from '../Difficulty';
-
-const StyledText = styled.span`
-  color: ${colors.navy};
-`;
 
 export type SubtitleProps = DifficultyProps & {
   scope: string;
@@ -27,9 +21,9 @@ export const Subtitle: React.FC<SubtitleProps> = ({
       {scopeCount && (
         <>
           ,&nbsp;
-          <StyledText data-testid="scope-count">
+          <span data-testid="scope-count">
             <b>{scopeCount}</b> {capitalize(pluralize(scope, scopeCount))}
-          </StyledText>
+          </span>
         </>
       )}
     </FlexBox>
