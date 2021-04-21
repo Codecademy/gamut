e217ff1b5dd425894b6bd3bce985e2038517ce4e
Natalie Wee <natalie@codecademy.com>
2021-04-21 09:43:08 -0400

    rm js files

--- packages/gamut-patterns/__tests__/gamut-patterns.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const gamutPatterns = require('..');
-
-describe('@codecademy/gamut-patterns', () => {
-  it('needs tests');
-});
--- packages/gamut-patterns/lib/gamut-patterns.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = gamutPatterns;
-
-function gamutPatterns() {
-  // TODO
-}
