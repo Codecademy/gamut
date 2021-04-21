ecc10bfdad75e23d25e8eb449aab244a3fd94ccf
Roger Vera <roger@codecademy.com>
2021-04-21 12:37:06 +0100

    feat(Curriculum Card): Port from monolith
    ### Overview
    Ports the CurriculumCard component from the Monolith!
    See: https://www.notion.so/codecademy/Gamut-Status-Timeline-dd3c135d3848464ea6eb1b48e68fbb1d
    
    ### PR Checklist
    
    - [ ] Related to designs:
    - [x] Related to JIRA ticket: https://codecademy.atlassian.net/browse/REACH-835
    - [x] I have run this code to verify it works
    - [x] This PR includes unit tests for the code change
    
    
    ### Testing
    
    https://607d7cf60074371eb86d24f6--gamut-preview.netlify.app/storybook/?path=/docs/molecules-curriculumcard--curriculum-card

---
+++ packages/gamut-labs/src/CurriculumCard/Footer/index.tsx
@@ -0,0 +1,45 @@
+import { Box, FlexBox, Text } from '@codecademy/gamut';
+import React from 'react';
+
+import { BottomTag, TagColor } from '../BottomTag/index';
+
+export type FooterProps = {
+  progressState?: 'completed' | 'inProgress';
+  tag?: string;
+  tagColor?: TagColor;
+};
+
+export const Footer: React.FC<FooterProps> = ({
+  progressState,
+  tag,
+  tagColor,
+}) => {
+  if (!progressState && tag && tagColor) {
+    return <BottomTag text={tag} color={tagColor} />;
+  }
+  return (
+    <div>
+      {progressState === 'completed' && (
+        <Text fontSize={16} fontFamily="accent" textColor="yellow">
+          Completed
+        </Text>
+      )}
+      {progressState === 'inProgress' && (
+        <FlexBox
+          as="p"
+          textColor="hyper"
+          flexWrap="wrap"
+          justifyContent="space-between"
+          alignItems="flex-end"
+          marginBottom={0}
+          width="100%"
+        >
+          <Box as="span" textColor="navy" fontFamily="accent" fontSize={14}>
+            Enrolled...
+          </Box>
+          <Box as="span">Keep Going</Box>
+        </FlexBox>
+      )}
+    </div>
+  );
+};
