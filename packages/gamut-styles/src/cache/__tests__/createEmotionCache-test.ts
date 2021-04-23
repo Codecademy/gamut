87bbb737a0cd2a9f5430c65b44cd82071b186668
Aaron Robb <52927224+codecaaron@users.noreply.github.com>
2021-04-23 11:56:53 -0400

    fix: Add GamutProvider Tests and Remove .storybook from coverage
    * Adds Variable Global component
    * Adds GamutProvider tests
    * Add createEmotionCache tests
    * Remove `.storybook` from coverage (components in there should be moved to separate package).

---
+++ packages/gamut-styles/src/cache/__tests__/createEmotionCache-test.ts
@@ -0,0 +1,35 @@
+import { createEmotionCache } from '../createEmotionCache';
+
+describe(createEmotionCache, () => {
+  beforeEach(() => {
+    document.body.innerHTML = '';
+  });
+
+  it('creates a container if none exists', () => {
+    const prebuild = document.getElementById('emotion-styles');
+    expect(prebuild).toBe(null);
+
+    createEmotionCache();
+    const container = document.getElementById('emotion-styles');
+
+    expect(container).toBeDefined();
+  });
+  it('creates a cache with our correct cache key', () => {
+    const cache = createEmotionCache();
+    expect(cache.key).toBe('gamut');
+  });
+  it('accepts arguments to override the default settings', () => {
+    const customContainer = document.createElement('div');
+    customContainer.setAttribute('id', 'custom');
+    document.body.appendChild(customContainer);
+
+    const cache = createEmotionCache({
+      key: 'custom',
+      container: customContainer,
+    });
+
+    expect(cache.key).toBe('custom');
+    const container = document.getElementById('emotion-styles');
+    expect(container).toBe(null);
+  });
+});
