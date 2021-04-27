40728c5ca881ab542736a4165f6acd60b7458735
Natalie Wee <natalie@codecademy.com>
2021-04-27 17:43:16 -0400

    fix styling

--- packages/gamut-patterns/src/patterns/diagonalB/DiagonalBDense.tsx
+++ packages/gamut-patterns/src/patterns/diagonalB/DiagonalBDense.tsx
@@ -1,9 +1,20 @@
+import { styledConfig, system } from '@codecademy/gamut-styles';
+import { variance } from '@codecademy/variance';
+import styled from '@emotion/styled';
 import React from 'react';
 
 import { PatternProps } from '../../types';
 
+const logoStyles = variance.compose(
+  system.layout,
+  system.positioning,
+  system.space
+);
+
+const Svg = styled('svg', styledConfig)<PatternProps>(logoStyles);
+
 export const DiagonalBDense: React.FC<PatternProps> = ({ ...props }) => (
-  <svg width="100%" height="100%" {...props} aria-hidden>
+  <Svg width="100%" height="100%" {...props} aria-hidden>
     <pattern
       id="diagonalBDense"
       x="0"
@@ -26,5 +37,5 @@ export const DiagonalBDense: React.FC<PatternProps> = ({ ...props }) => (
       </defs>
     </pattern>
     <rect x="0" y="0" width="100%" height="100%" fill="url(#diagonalBDense)" />
-  </svg>
+  </Svg>
 );
