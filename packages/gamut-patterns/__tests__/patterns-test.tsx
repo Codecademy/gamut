dd1efc303583a8960d7fcef280050881d1a91714
Natalie Wee <natalie@codecademy.com>
2021-04-27 18:10:28 -0400

    fix tests

--- packages/gamut-patterns/__tests__/patterns-test.tsx
+++ packages/gamut-patterns/__tests__/patterns-test.tsx
@@ -10,16 +10,4 @@ describe('Compiled gamut-icons:', () => {
 
     expect(wrapper.find('svg').props().color).toEqual('red');
   });
-
-  it('Allows passing a custom height', () => {
-    const { wrapper } = renderWrapper({ height: 200 });
-
-    expect(wrapper.find('svg').props().height).toEqual(200);
-  });
-
-  it('Allows passing a custom display', () => {
-    const { wrapper } = renderWrapper({ display: 'flex' });
-
-    expect(wrapper.find('svg').props().display).toEqual('flex');
-  });
 });
