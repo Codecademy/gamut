b00dcac4517962f7d671151a944d42f49cddc3e6
Natalie Wee <natalie@codecademy.com>
2021-04-27 10:14:43 -0400

    add some tests

---
+++ packages/gamut-patterns/__tests__/patterns-test.tsx
@@ -0,0 +1,27 @@
+import { mount } from 'enzyme';
+import React from 'react';
+
+import { DotLoose } from '../dist/index';
+
+describe('Compiled gamut-icons:', () => {
+  it('Allows passing a custom color', () => {
+    const wrapper = mount(<DotLoose color="red" />);
+
+    const svgEl = wrapper.find('svg');
+    expect(svgEl.props().color).toEqual('red');
+  });
+
+  it('Allows passing a custom height', () => {
+    const wrapper = mount(<DotLoose height={200} />);
+
+    const svgEl = wrapper.find('svg');
+    expect(svgEl.props().height).toEqual(200);
+  });
+
+  it('Allows passing a custom display', () => {
+    const wrapper = mount(<DotLoose display="flex" />);
+
+    const svgEl = wrapper.find('svg');
+    expect(svgEl.props().display).toEqual('flex');
+  });
+});
