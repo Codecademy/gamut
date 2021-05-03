5db6a4f55295b77342a61cf016a1410eadefc1fb
Rebecca Borison <borisonr@gmail.com>
2021-05-03 16:44:25 -0400

    make it accessible

--- packages/gamut-labs/src/GlobalFooter/FooterLegal/MadeIn.tsx
+++ packages/gamut-labs/src/GlobalFooter/FooterLegal/MadeIn.tsx
@@ -2,17 +2,27 @@ import { Box } from '@codecademy/gamut';
 import styled from '@emotion/styled';
 import React from 'react';
 
-const HeartContainer = styled.span`
+const ClickableSpan = styled.span`
+  cursor: pointer;
+`;
+
+const AccessibleSpan = ({ children, onClick }: any) => (
+  <ClickableSpan
+    onClick={onClick}
+    onKeyPress={onClick}
+    role="button"
+    tabIndex={0}
+  >
+    {children}
+  </ClickableSpan>
+);
+
+const HeartContainer = styled(AccessibleSpan)`
   display: inline-block;
   filter: saturate(0.7);
   margin-right: 0.25rem;
   min-width: 1.75em;
   text-align: center;
-  cursor: pointer;
-`;
-
-const ClickableSpan = styled.span`
-  cursor: pointer;
 `;
 
 export type MadeInProps = {
@@ -20,28 +30,18 @@ export type MadeInProps = {
 };
 
 export const MadeIn: React.FC<MadeInProps> = ({ onClick }) => {
-  const AccessibleSpan = ({ children }: any) => (
-    <ClickableSpan
-      onClick={onClick}
-      onKeyPress={onClick}
-      role="button"
-      tabIndex={0}
-    >
-      {children}
-    </ClickableSpan>
-  );
-
   return (
     <Box
       display={{ md: 'inline-block' }}
       textAlign={{ base: 'center', md: 'right' }}
     >
-      <AccessibleSpan>Made</AccessibleSpan> with
+      <AccessibleSpan onClick={onClick}>Made</AccessibleSpan> with
       <HeartContainer aria-label="love" onClick={onClick}>
         ️❤️
       </HeartContainer>
-      in <AccessibleSpan>NYC</AccessibleSpan> ©{` ${new Date().getFullYear()} `}{' '}
-      <AccessibleSpan>Codecademy</AccessibleSpan>
+      in <AccessibleSpan onClick={onClick}>NYC</AccessibleSpan> ©
+      {` ${new Date().getFullYear()} `}{' '}
+      <AccessibleSpan onClick={onClick}>Codecademy</AccessibleSpan>
     </Box>
   );
 };
