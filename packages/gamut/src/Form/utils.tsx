222335e994ea9c0fe4863d7e08a6002195b0be40
cass spain <cass@codecademy.com>
2021-05-03 17:00:19 -0400

    linty

--- packages/gamut/src/Form/utils.tsx
+++ packages/gamut/src/Form/utils.tsx
@@ -10,7 +10,7 @@ export type ParseOptionProps = {
 const formatAsOptions = ({ label, value, rawText }: OptionTypeBase) => {
   return (
     <option key={label} value={value} data-testid={label}>
-      {rawText ? rawText : value}
+      {rawText ?? value}
     </option>
   );
 };
