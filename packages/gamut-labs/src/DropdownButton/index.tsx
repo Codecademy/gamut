183e9c402d9d0c45a16cf753f3cc55b525fa0963
Sylvana Santos <sylvana@codecademy.com>
2021-05-05 07:19:56 -0700

    add click target

--- packages/gamut-labs/src/DropdownButton/index.tsx
+++ packages/gamut-labs/src/DropdownButton/index.tsx
@@ -1,11 +1,27 @@
+import { FillButton, StrokeButton } from '@codecademy/gamut';
 import React from 'react';
 
 export type DropdownButtonProps = {
   buttonText: string;
+  buttonType: 'fill' | 'stroke';
 };
 
 export const DropdownButton: React.FC<DropdownButtonProps> = ({
   buttonText,
+  buttonType,
 }) => {
-  return <div>{buttonText}</div>;
+  let clickTarget: React.ReactNode;
+
+  switch (buttonType) {
+    case 'fill':
+      clickTarget = <FillButton>{buttonText}</FillButton>;
+      break;
+    case 'stroke':
+      clickTarget = <StrokeButton>{buttonText}</StrokeButton>;
+      break;
+    default:
+      clickTarget = null;
+  }
+
+  return <div>{clickTarget}</div>;
 };
