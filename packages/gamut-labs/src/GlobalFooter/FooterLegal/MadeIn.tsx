ca4f9183bb2b4018a689888cccc3d89be01672f1
Rebecca Borison <borisonr@gmail.com>
2021-05-05 13:06:03 -0400

    use anchor

--- packages/gamut-labs/src/GlobalFooter/FooterLegal/MadeIn.tsx
+++ packages/gamut-labs/src/GlobalFooter/FooterLegal/MadeIn.tsx
@@ -1,8 +1,8 @@
-import { Box } from '@codecademy/gamut';
+import { Box, Anchor } from '@codecademy/gamut';
 import styled from '@emotion/styled';
 import React from 'react';
 
-const HeartContainer = styled.span`
+const HeartContainer = styled(Anchor)`
   display: inline-block;
   filter: saturate(0.7);
   margin-right: 0.25rem;
@@ -11,43 +11,29 @@ const HeartContainer = styled.span`
   cursor: pointer;
 `;
 
-const ClickableSpan = styled.span`
-  cursor: pointer;
-`;
-
 export type MadeInProps = {
   onClick?: (e: React.MouseEvent | React.KeyboardEvent) => void;
 };
 
-export const MadeIn: React.FC<MadeInProps> = ({ onClick }) => {
-  const AccessibleSpan = ({ children }: any) => (
-    <ClickableSpan
-      onClick={onClick}
-      onKeyPress={onClick}
-      role="button"
-      tabIndex={0}
-    >
-      {children}
-    </ClickableSpan>
-  );
-
-  return (
-    <Box
-      display={{ md: 'inline-block' }}
-      textAlign={{ _: 'center', md: 'right' }}
-    >
-      <AccessibleSpan>Made</AccessibleSpan> with
-      <HeartContainer
-        aria-label="love"
-        onClick={onClick}
-        onKeyPress={onClick}
-        role="button"
-        tabIndex={0}
-      >
-        ️❤️
-      </HeartContainer>
-      in <AccessibleSpan>NYC</AccessibleSpan> ©{` ${new Date().getFullYear()} `}{' '}
-      <AccessibleSpan>Codecademy</AccessibleSpan>
-    </Box>
-  );
-};
+export const MadeIn: React.FC<MadeInProps> = ({ onClick }) => (
+  <Box
+    display={{ md: 'inline-block' }}
+    textAlign={{ _: 'center', md: 'right' }}
+  >
+    <Anchor variant="interface" onClick={onClick}>
+      Made
+    </Anchor>{' '}
+    with
+    <HeartContainer variant="interface" onClick={onClick}>
+      ️❤️
+    </HeartContainer>
+    in{' '}
+    <Anchor variant="interface" onClick={onClick}>
+      NYC
+    </Anchor>{' '}
+    ©{` ${new Date().getFullYear()} `}{' '}
+    <Anchor variant="interface" onClick={onClick}>
+      Codecademy
+    </Anchor>
+  </Box>
+);
