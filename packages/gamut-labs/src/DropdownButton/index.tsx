17afb63483965c796f3a6e26c8015fe6223361e8
Sylvana Santos <sylvana@codecademy.com>
2021-05-05 07:38:35 -0700

    add popover

--- packages/gamut-labs/src/DropdownButton/index.tsx
+++ packages/gamut-labs/src/DropdownButton/index.tsx
@@ -1,5 +1,7 @@
-import { FillButton, StrokeButton } from '@codecademy/gamut';
-import React from 'react';
+import { Box, FillButton, StrokeButton } from '@codecademy/gamut';
+import React, { useRef, useState } from 'react';
+
+import { Popover } from '../Popover';
 
 export type DropdownButtonProps = {
   buttonText: string;
@@ -10,18 +12,32 @@ export const DropdownButton: React.FC<DropdownButtonProps> = ({
   buttonText,
   buttonType,
 }) => {
-  let clickTarget: React.ReactNode;
+  const targetRef = useRef<HTMLDivElement>(null);
+  const [isOpen, setIsOpen] = useState(false);
+  const handleClick = (event: React.MouseEvent) => {
+    setIsOpen(!isOpen);
+  };
 
+  let clickTarget: React.ReactNode;
   switch (buttonType) {
     case 'fill':
-      clickTarget = <FillButton>{buttonText}</FillButton>;
+      clickTarget = <FillButton onClick={handleClick}>{buttonText}</FillButton>;
       break;
     case 'stroke':
-      clickTarget = <StrokeButton>{buttonText}</StrokeButton>;
+      clickTarget = (
+        <StrokeButton onClick={handleClick}>{buttonText}</StrokeButton>
+      );
       break;
     default:
       clickTarget = null;
   }
 
-  return <div>{clickTarget}</div>;
+  return (
+    <>
+      <div ref={targetRef}>{clickTarget}</div>
+      <Popover targetRef={targetRef} isOpen={isOpen}>
+        <Box>I am a popover</Box>
+      </Popover>
+    </>
+  );
 };
