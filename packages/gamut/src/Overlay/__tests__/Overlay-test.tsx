6f9e8e9278e75895ea1d361ec71ebbb0d0cc0810
Aaron Robb <aaron@codecademy.com>
2021-05-07 00:35:08 -0400

    Remove hook annoyingness

--- packages/gamut/src/Overlay/__tests__/Overlay-test.tsx
+++ packages/gamut/src/Overlay/__tests__/Overlay-test.tsx
@@ -1,6 +1,6 @@
 import { theme } from '@codecademy/gamut-styles';
 import { ThemeProvider } from '@emotion/react';
-import { fireEvent, render, screen } from '@testing-library/react';
+import { cleanup, fireEvent, render, screen } from '@testing-library/react';
 import { mount } from 'enzyme';
 import React from 'react';
 
@@ -9,12 +9,14 @@ import { Overlay, OverlayProps } from '..';
 const renderOverlay = (props?: Partial<OverlayProps>) => {
   return render(
     <ThemeProvider theme={theme}>
-      <Overlay onRequestClose={() => {}} {...props}>
-        <div data-testid="overlay-content">
-          Howdy!
-          <button aria-label="Button" type="button" />
-        </div>
-      </Overlay>
+      <main>
+        <Overlay onRequestClose={() => {}} {...props}>
+          <div data-testid="overlay-content">
+            Howdy!
+            <button aria-label="Button" type="button" />
+          </div>
+        </Overlay>
+      </main>
     </ThemeProvider>
   );
 };
@@ -37,6 +39,9 @@ const overlayIsRendered = () => {
 };
 
 describe('Overlay', () => {
+  afterEach(() => {
+    /** Clean up enzyme  */
+  });
   it('renders null when isOpen is not true', () => {
     renderOverlay();
 
@@ -90,6 +95,36 @@ describe('Overlay', () => {
       onRequestClose,
     });
     fireEvent.mouseDown(screen.getByTestId('overlay-content'));
+    expect(onRequestClose.mock.calls.length).toBe(0);
+  });
+
+  it('renders outside its mounting container when inline is false', () => {
+    renderOverlay({ isOpen: true });
+
+    /** Overlay prevents main from being accessible  */
+    expect(screen.queryByRole('main')).toBe(null);
+  });
+
+  it('renders inside its mounting container when inline is true', () => {
+    renderOverlay({ inline: true, isOpen: true });
+
+    /** main is accessible as overlay is rendering within its bounds */
+    screen.getByRole('main');
+  });
+
+  it('it disables focus trap when rendered inline', () => {
+    const onRequestClose = jest.fn();
+    const { baseElement } = renderOverlay({
+      inline: true,
+      isOpen: true,
+      clickOutsideCloses: true,
+      onRequestClose,
+    });
+
+    // focus-trap listens to mouseDown, not click
+    fireEvent.mouseDown(screen.getByTestId('overlay-content-container'));
+    expect(onRequestClose.mock.calls.length).toBe(0);
+    fireEvent.keyDown(baseElement, { key: 'Escape', code: 'Escape' });
     expect(onRequestClose.mock.calls.length).toBe(0);
   });
 
