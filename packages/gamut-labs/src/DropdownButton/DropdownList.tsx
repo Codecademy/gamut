03c3b9973b4505d0d749f59b864651bea407f89c
Sylvana Santos <sylvana@codecademy.com>
2021-05-06 23:03:08 -0700

    add item click handler

--- packages/gamut-labs/src/DropdownButton/DropdownList.tsx
+++ packages/gamut-labs/src/DropdownButton/DropdownList.tsx
@@ -1,11 +1,11 @@
-import { Box, FlexBox } from '@codecademy/gamut';
+import { Anchor, Box, FlexBox } from '@codecademy/gamut';
 import { GamutIconProps } from '@codecademy/gamut-icons';
 import React from 'react';
 
 export type DropdownItem = {
   icon?: React.ComponentType<GamutIconProps>;
   text: string;
-  clickHandler?: React.MouseEvent;
+  clickHandler?: (event: React.MouseEvent) => void;
 };
 
 export type DropdownListProps = {
@@ -16,21 +16,28 @@ export const DropdownList: React.FC<DropdownListProps> = ({
   dropdownItems,
 }) => {
   return (
-    <Box borderRadius="2px" bg="white" py={12}>
+    <FlexBox flexDirection="column" borderRadius="2px" bg="white" py={12}>
       {dropdownItems.map((item) => {
         const Icon = item.icon;
 
         return (
-          <FlexBox px={24} py={12}>
-            {Icon && (
-              <Box mr={12}>
-                <Icon size={20} aria-hidden />
-              </Box>
-            )}
-            {item.text}
-          </FlexBox>
+          <Anchor
+            variant="interface"
+            onClick={item.clickHandler}
+            px={24}
+            py={12}
+          >
+            <FlexBox>
+              {Icon && (
+                <Box mr={12}>
+                  <Icon size={20} aria-hidden />
+                </Box>
+              )}
+              {item.text}
+            </FlexBox>
+          </Anchor>
         );
       })}
-    </Box>
+    </FlexBox>
   );
 };
