97f3d1813e2cde747e2d016abffcddb5d9caf18c
Aaron Robb <aaron@codecademy.com>
2021-05-11 18:18:24 -0400

    Works one way

---
+++ packages/gamut-icons/src/theme.ts
@@ -0,0 +1,7 @@
+import '@emotion/react';
+
+import { CoreTheme } from '@codecademy/gamut-styles';
+
+declare module '@emotion/react' {
+  export interface Theme extends CoreTheme {}
+}
--- packages/styleguide/typings.d.ts
+++ packages/styleguide/typings.d.ts
@@ -1,3 +1,5 @@
+import { CoreTheme } from '@codecademy/gamut-styles';
+
 declare module 'global';
 
 declare namespace NodeJS {
@@ -9,4 +11,8 @@ declare namespace NodeJS {
 
 declare module '@storybook/addon-links/react' {
   export declare const LinkTo: React.FC<{ kind: string; id: string }>;
+}
+
+declare module '@emotion/react' {
+  export interface Theme extends CoreTheme {}
 }
