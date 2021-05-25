3f1636bef298fb6f03c252862e65bcf804bd5b8f
Aaron Robb <aaron@codecademy.com>
2021-05-25 17:03:45 -0400

    :(

--- packages/gamut-icons/icon-template.js
+++ packages/gamut-icons/icon-template.js
@@ -1,16 +1,17 @@
 const path = require('path');
-const { startCase } = require('lodash');
+const { startCase } = require('lodash/fp');
 
 function iconTemplate(api, opts, { jsx /* imports, props, exports */ }) {
-  const template = api.template.smart({ plugins: ['typescript'] });
+  const template = api.template.smart({ plugins: ['jsx', 'typescript'] });
   const { componentName, filePath } = opts.state;
   const exportName = componentName.replace('Svg', '');
   const title = startCase(path.basename(filePath, '.svg'));
 
   return template.ast`
     import * as React from 'react';
+    import { ClassNames, useTheme } from '@emotion/react';
     import { GamutIconProps } from '../../types';
-    import { getAttrValue, IconStyleProps, IconSvg } from '../../props';
+    import { getAttrValue, IconStyleProps, iconStyles } from '../../props';
     const LocalIcon = React.forwardRef<SVGSVGElement, GamutIconProps>(({
       title = "${title}",
       titleId,
@@ -18,14 +19,19 @@ function iconTemplate(api, opts, { jsx /* imports, props, exports */ }) {
       color,
       width,
       height,
+      className: cn,
       ...rest
     },
       svgRef
     ) => {
+      const theme = useTheme();
       const props = rest as Omit<GamutIconProps, keyof IconStyleProps>;
-      return ${jsx};
+      return <ClassNames>{({ css, cx }) => {
+        const classNames = cx(cn, css(iconStyles({ ...props, width, height, theme })));
+        return ${jsx}
+      }}</ClassNames>;
     });
-    export const ${exportName} = IconSvg.withComponent(LocalIcon);
+    export const ${exportName} = LocalIcon;
   `;
 }
 module.exports = iconTemplate;
