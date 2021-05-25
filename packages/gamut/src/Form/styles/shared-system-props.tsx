dc43b17b8f920ac179dc295aa3f718ef57cc4da4
cass spain <cass@codecademy.com>
2021-05-25 17:37:43 -0400

    start radio button ii

--- packages/gamut/src/Form/styles/shared-system-props.tsx
+++ packages/gamut/src/Form/styles/shared-system-props.tsx
@@ -1,8 +1,16 @@
 import { system, theme } from '@codecademy/gamut-styles';
 import { StandardPropertiesHyphen } from 'csstype';
 
-const radioWrapper = system.css({
+export const radioWrapper = system.css({
   margin: '0.25rem 0',
   width: '100%',
   fontWeight: 'normal',
+});
+
+export const radioLabel = system.css({
+  display: 'flex',
+  padding: '$form-padding 0',
+  alignItems: 'center',
+  cursor: 'pointer',
+  position: 'relative',
 });
