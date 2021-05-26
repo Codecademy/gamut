4de39188d152f2dfe7c290dc7155b0ea0194aa02
Aaron Robb <aaron@codecademy.com>
2021-05-25 21:30:23 -0400

    Fix

--- packages/styleguide/.storybook/addons/system/components/Elements.tsx
+++ packages/styleguide/.storybook/addons/system/components/Elements.tsx
@@ -1,5 +1,5 @@
 import styled from '@emotion/styled';
-import { themed, system } from '@codecademy/gamut-styles/src';
+import { themed, variant } from '@codecademy/gamut-styles/src';
 import { StyleProps } from '@codecademy/variance';
 
 export const PropGroupTooltip = styled.ul`
