6ceb0ea770134bff2a6813f1f39b92af8c7a6e56
Amy Resnik <aresnik@codecademy.com>
2021-05-27 11:29:31 -0400

    add focus trap

--- packages/gamut-labs/src/Flyout/Flyout.tsx
+++ packages/gamut-labs/src/Flyout/Flyout.tsx
@@ -1,7 +1,7 @@
-import { IconButton } from '@codecademy/gamut';
+import { FocusTrap, IconButton } from '@codecademy/gamut';
 import { MiniDeleteIcon } from '@codecademy/gamut-icons';
 import { AnimatePresence } from 'framer-motion';
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 
 import { DrawerBase, FlyoutBaseProps, transitionDuration } from './shared';
 import { SidebarCloneButton } from './SidebarCloneButton';
@@ -11,6 +11,14 @@ export type FlyoutProps = FlyoutBaseProps & {
    * chooses color of drop shadow
    */
   button: React.ReactNode;
+  /**
+   * Whether clicking on the screen outside of the container should close the Flyout
+   */
+  clickOutsideCloses?: boolean;
+  /**
+   * Whether clicking the escape key should close the Flyout
+   */
+  escapeCloses?: boolean;
 };
 
 export const Flyout: React.FC<FlyoutProps> = ({
@@ -20,39 +28,54 @@ export const Flyout: React.FC<FlyoutProps> = ({
   openFrom = 'left',
   openWidth = 30,
   testId,
+  clickOutsideCloses = true,
+  escapeCloses = true,
   ...styleProps
 }) => {
   const [isSidebarOpen, setSidebarOpen] = useState(expanded);
   const toggleDrawer = () => setSidebarOpen(!isSidebarOpen);
   const initialX = openFrom === 'left' ? -1000 : 1000;
 
+  const handleOutsideClick = useCallback(() => {
+    clickOutsideCloses && toggleDrawer();
+  }, [clickOutsideCloses, toggleDrawer]);
+
+  const handleEscapeKey = useCallback(() => {
+    escapeCloses && toggleDrawer();
+  }, [escapeCloses, toggleDrawer]);
+
   return (
     <>
       <AnimatePresence>
         {isSidebarOpen ? (
-          <DrawerBase
-            aria-expanded={isSidebarOpen}
-            initial={{ x: initialX }}
-            animate={{ x: 0 }}
-            exit={{ x: initialX }}
-            transition={{ duration: transitionDuration }}
-            data-testid={testId}
-            width={`${openWidth}rem`}
-            openFrom={openFrom}
-            position="fixed"
-            height="100vh"
-            top="0"
-            zIndex={15}
-            {...styleProps}
+          <FocusTrap
+            onClickOutside={handleOutsideClick}
+            onEscapeKey={handleEscapeKey}
           >
-            <IconButton
-              icon={MiniDeleteIcon}
-              onClick={() => toggleDrawer()}
-              position="absolute"
-              right="0"
-            />
-            {children}
-          </DrawerBase>
+            <DrawerBase
+              aria-expanded={isSidebarOpen}
+              initial={{ x: initialX }}
+              animate={{ x: 0 }}
+              exit={{ x: initialX }}
+              transition={{ duration: transitionDuration }}
+              data-testid={testId}
+              width={`${openWidth}rem`}
+              openFrom={openFrom}
+              position="fixed"
+              height="100vh"
+              top="0"
+              zIndex={15}
+              {...styleProps}
+            >
+              <IconButton
+                icon={MiniDeleteIcon}
+                onClick={() => toggleDrawer()}
+                position="absolute"
+                right="0"
+              />
+              {children}
+            </DrawerBase>
+          </FocusTrap>
         ) : null}
       </AnimatePresence>
       <SidebarCloneButton
