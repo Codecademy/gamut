51a8dea252e573b208a5a7aee40ceb5a367ebd77
cass spain <cass@codecademy.com>
2021-06-01 10:36:34 -0400

    prettier now

--- packages/gamut/src/Form/Input.tsx
+++ packages/gamut/src/Form/Input.tsx
@@ -62,7 +62,9 @@ export const reactRecurlyFormFieldFocusStyles = system.css(
   formFieldFocusStyles
 );
 
-export const reactRecurlyFormFieldPaddingStyles = system.css(formFieldPaddingStyles);
+export const reactRecurlyFormFieldPaddingStyles = system.css(
+  formFieldPaddingStyles
+);
 
 export const iFrameWrapper = styled.div<conditionalStyleProps>`
   ${formBaseFieldStyles}
