a4906c7d1d2f99d3e2d49b0f7bb51e5a053dfe3d
Aaron Robb <aaron@codecademy.com>
2021-06-01 22:17:38 -0400

    One would think I had learned by now

--- packages/variance/src/core.ts
+++ packages/variance/src/core.ts
@@ -166,13 +166,11 @@ export const variance = {
         if (cache) return cache;
         const css = parser({ ...cssProps, theme } as any);
         selectors.forEach((selector) => {
-          const selectorConfig = cssProps[selector];
-          if (isObject(selectorConfig)) {
-            css[selector] = {
-              ...getStaticCss(selectorConfig, filteredProps),
-              ...parser(Object.assign(selectorConfig, { theme }) as any),
-            };
-          }
+          const selectorConfig = cssProps[selector] ?? {};
+          css[selector] = {
+            ...getStaticCss(selectorConfig, filteredProps),
+            ...parser({ ...selectorConfig, theme } as any),
+          };
         });
 
         /** Merge the static and generated css and save it to the cache */
