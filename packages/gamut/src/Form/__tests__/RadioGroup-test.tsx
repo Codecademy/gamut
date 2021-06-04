16375474182f5fe2b412a8877b2cb19e52a70048
cass spain <cass@codecademy.com>
2021-06-04 12:36:07 -0400

    RadioGroup test goin

--- packages/gamut/src/Form/__tests__/RadioGroup-test.tsx
+++ packages/gamut/src/Form/__tests__/RadioGroup-test.tsx
@@ -4,12 +4,9 @@ import React from 'react';
 import { Radio } from '../Radio';
 import { RadioGroup } from '../RadioGroup';
 
-const onChangeCallback = jest.fn();
-
 const renderWrapper = setupEnzyme(RadioGroup, {
   htmlForPrefix: 'what-salad-maker-do-you-prefer',
   name: 'what-salad-maker-do-you-prefer',
-  onChange: onChangeCallback,
   'data-testid': 'my-test-id',
   children: [
     <Radio label="Sweet Green" value="sweet-green" data-testid="firstInput" />,
@@ -20,7 +17,7 @@ const renderWrapper = setupEnzyme(RadioGroup, {
 describe('<RadioGroup>', () => {
   const createComponent = () => {
     const onChange = jest.fn();
-    const { wrapper } = renderWrapper();
+    const { wrapper } = renderWrapper({ onChange: onChange });
 
     const firstInput = wrapper.find('input[type="radio"]').first();
 
@@ -36,23 +33,17 @@ describe('<RadioGroup>', () => {
     });
   });
 
-  xit('sets the onChange prop on the child', () => {
-    const { wrapper } = renderWrapper();
+  it('sets the onChange prop on the child', () => {
+    const { firstInput, onChange } = createComponent();
     const event = {} as React.FormEvent;
 
-    const getByTestId = wrapper.find(`input[data-testid="firstInput"]`);
+    firstInput.props().onChange!(event);
 
-    getByTestId.simulate('change', {
-      target: {
-        value: 'sweet-green',
-      },
-    });
-
-    expect(onChangeCallback).toHaveBeenCalledWith(event);
+    expect(onChange).toHaveBeenCalledWith(event);
   });
 
   it('sets any additional props on the outer div', () => {
-    const { wrapper } = renderWrapper();
+    const { wrapper } = createComponent();
     const getByTestId = wrapper.find('div[data-testid="my-test-id"]');
 
     expect(getByTestId.exists()).toBe(true);
