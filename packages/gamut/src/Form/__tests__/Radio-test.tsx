03de48fd8af132319359e8e98cd1b89247b24483
cass spain <cass@codecademy.com>
2021-06-04 11:38:34 -0400

    update Radio tests

--- packages/gamut/src/Form/__tests__/Radio-test.tsx
+++ packages/gamut/src/Form/__tests__/Radio-test.tsx
@@ -1,41 +1,50 @@
+import { setupEnzyme } from '@codecademy/gamut-tests';
 import { shallow } from 'enzyme';
 import React from 'react';
 
 import { Radio } from '../Radio';
 
+const onChangeCallback = jest.fn();
+const testid = 'my-test-id';
+
+const renderWrapper = setupEnzyme(Radio, {
+  htmlFor: 'some-label',
+  checked: true,
+  onChange: onChangeCallback,
+  value: 'a',
+  'data-testid': 'my-test-id',
+});
+
 describe('<Radio>', () => {
   it('sets the input checked state when the prop is passed', () => {
-    const wrapper = shallow(<Radio htmlFor="some-label" checked />);
+    const { wrapper } = renderWrapper();
     expect(wrapper.find('input[type="radio"]').prop('checked')).toEqual(true);
   });
 
   it('calls the onChange callback when the input changes', () => {
-    const onChangeCallback = jest.fn();
+    const { wrapper } = renderWrapper();
 
-    const wrapper = shallow(
-      <Radio htmlFor="some-label" onChange={onChangeCallback} value="a" />
-    );
     wrapper.find('input[type="radio"]').simulate('change', {
       target: {
         value: 'a',
       },
     });
+
     const firstArgument = onChangeCallback.mock.calls[0][0];
     expect(firstArgument.target.value).toBe('a');
   });
 
   it('accepts JSX in the label', () => {
-    const wrapper = shallow(
-      <Radio htmlFor="some-label" label={<img alt="my cat" src="cat.jpg" />} />
-    );
+    const { wrapper } = renderWrapper({
+      label: <img alt="my cat" src="cat.jpg" />,
+    });
+
     expect(wrapper.find('img').length).toBe(1);
   });
 
   it('accepts additional props not specified by the component', () => {
-    const testid = 'my-test-id';
-    const wrapper = shallow(
-      <Radio htmlFor="some-label" data-testid={testid} />
-    );
+    const { wrapper } = renderWrapper();
+
     const getByTestId = wrapper.find(`input[data-testid="${testid}"]`);
     expect(getByTestId.exists()).toBe(true);
     expect(getByTestId.isEmptyRender()).toBe(false);
