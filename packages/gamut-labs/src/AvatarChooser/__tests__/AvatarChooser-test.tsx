1e5897f42d7c4ce1ac3181e7c7b82c9019adde02
Zack M Fleischman <zFleischman@codecademy.com>
2021-06-04 12:34:08 -0700

    Updating tests

--- packages/gamut-labs/src/AvatarChooser/__tests__/AvatarChooser-test.tsx
+++ packages/gamut-labs/src/AvatarChooser/__tests__/AvatarChooser-test.tsx
@@ -1,6 +1,6 @@
 import { setupRtl } from '@codecademy/gamut-tests';
+import userEvent from '@testing-library/user-event';
 
-// import userEvent from '@testing-library/user-event';
 import { AvatarChooser } from '..';
 
 const testImageSource = 'testImageSource';
@@ -8,9 +8,11 @@ const renderView = setupRtl(AvatarChooser, {
   src: testImageSource,
 });
 
-// const mockAvatarImageFile = new File(['fileBlob'], 'profileImage.png', {
-//   type: 'image/png',
-// });
+const mockAvatarImageFile = new File(['fileBlob'], 'profileImage.png', {
+  type: 'image/png',
+});
+
+global.URL.createObjectURL = jest.fn();
 
 describe('AvatarChooser', () => {
   it('Renders "Choose Photo"', () => {
@@ -30,28 +32,33 @@ describe('AvatarChooser', () => {
     view.getByText(error);
   });
 
-  // it('Calls onImageChanged with the uploaded image', () => {
-  //   const onImageChanged = jest.fn();
-  //   const { view } = renderView({ onImageChanged });
+  it('Calls onImageChanged with the uploaded image', () => {
+    const onImageChanged = jest.fn();
+    const { view } = renderView({ onImageChanged });
 
-  //   userEvent.upload(
-  //     view.getByLabelText('Choose Photo') as HTMLInputElement,
-  //     mockAvatarImageFile
-  //   );
+    userEvent.upload(
+      view.getByLabelText('Choose Photo') as HTMLInputElement,
+      mockAvatarImageFile
+    );
 
-  //   const fileList: FileList = onImageChanged.mock.calls[0][0];
-  //   expect(fileList[0]).toBe(mockAvatarImageFile);
-  // });
+    const fileList: FileList = onImageChanged.mock.calls[0][0];
+    expect(fileList[0]).toBe(mockAvatarImageFile);
+  });
 
-  // it('Updates the img src when an image is uploaded', () => {
-  //   const { view } = renderView();
+  it('Updates the img src when an image is uploaded', () => {
+    const uploadedImageUrl = 'uploadedImageUrl';
+    global.URL.createObjectURL = jest
+      .fn()
+      .mockImplementation(() => uploadedImageUrl);
+    const { view } = renderView();
 
-  //   userEvent.upload(
-  //     view.getByLabelText('Choose Photo') as HTMLInputElement,
-  //     mockAvatarImageFile
-  //   );
+    userEvent.upload(
+      view.getByLabelText('Choose Photo') as HTMLInputElement,
+      mockAvatarImageFile
+    );
 
-  //   const img = view.getByRole('img') as HTMLImageElement;
-  //   expect(img.src).not.toMatch(testImageSource);
-  // });
+    const img = view.getByRole('img') as HTMLImageElement;
+    expect(global.URL.createObjectURL).toBeCalledWith(mockAvatarImageFile);
+    expect(img.src).toMatch(uploadedImageUrl);
+  });
 });
