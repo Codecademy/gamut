c749b812319983923d7b7145049d8ad9064ba44a
Zack M Fleischman <zFleischman@codecademy.com>
2021-06-07 10:30:14 -0700

    Adding tests that fail

--- packages/gamut/src/GridForm/GridFormInputGroup/GridFormRadioGroupInput/__tests__/GridFormRadioGroupInput-test.tsx
+++ packages/gamut/src/GridForm/GridFormInputGroup/GridFormRadioGroupInput/__tests__/GridFormRadioGroupInput-test.tsx
@@ -22,6 +22,31 @@ describe('GridFormRadioGroupInput', () => {
     });
   });
 
+  it('accepts JSX for the group label', () => {
+    const radioButtons = renderGridFormRadioGroupInput({
+      label: (
+        <div>
+          <span>Test Label</span>
+        </div>
+      ),
+    });
+
+    console.log(radioButtons.debug());
+
+    expect(radioButtons.text()).toContain('Test Label');
+  });
+
+  describe('when label is passed as a ReactNode', () => {
+    it('renders an input with the same id', () => {
+      const radioButtons = renderGridFormRadioGroupInput({
+        id: 'mycoolid',
+        name: 'name',
+      });
+
+      expect(radioButtons.find('input#name-0-mycoolid').length).toBe(1);
+    });
+  });
+
   describe('Radio', () => {
     it('accepts JSX for the label', () => {
       const radioButtons = renderGridFormRadioGroupInput({
