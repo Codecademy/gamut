aeb29d0cbc00febebc894b5ff8480dea3be09109
Amy Resnik <aresnik@codecademy.com>
2021-06-07 17:43:46 -0400

    PR feedback

--- packages/gamut-labs/src/LayoutMenu/SelectedSectionItem.tsx
+++ packages/gamut-labs/src/LayoutMenu/SelectedSectionItem.tsx
@@ -3,12 +3,7 @@ import React from 'react';
 
 export const SelectedSectionItem: React.FC = ({ children }) => {
   return (
-    <Box
-      borderColor="navy"
-      borderStyleLeft="solid"
-      borderWidthLeft="6px"
-      pl={12}
-    >
+    <Box borderLeft={1} borderWidthLeft="6px" pl={12}>
       <Text fontWeight="title">{children}</Text>
     </Box>
   );
