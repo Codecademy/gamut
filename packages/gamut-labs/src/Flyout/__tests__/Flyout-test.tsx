a0457a55d520c813bd8e3abb593be9a8bfedda82
Roger Vera <roger@codecademy.com>
2021-06-11 12:41:30 -0400

    update tests

--- packages/gamut-labs/src/Flyout/__tests__/Flyout-test.tsx
+++ packages/gamut-labs/src/Flyout/__tests__/Flyout-test.tsx
@@ -8,11 +8,7 @@ import { Flyout, FlyoutProps } from '..';
 const FlyoutContainer: React.FC<Partial<FlyoutProps>> = (props) => {
   return (
     <div data-testid="flyout-outside">
-      <Flyout
-        onToggle={() => null}
-        {...props}
-        button={<button type="button">Test</button>}
-      >
+      <Flyout {...props} button={<button type="button">Test</button>}>
         <div data-testid="flyout-content">Howdy!</div>
       </Flyout>
     </div>
@@ -22,74 +18,69 @@ const FlyoutContainer: React.FC<Partial<FlyoutProps>> = (props) => {
 const renderFlyout = setupRtl(FlyoutContainer);
 
 describe('Flyout', () => {
-  it('renders flyout content when expanded is true', () => {
-    renderFlyout({ expanded: true });
+  it('renders flyout content when "initialExpanded" is true', () => {
+    renderFlyout({ initialExpanded: true });
     screen.getByTestId('flyout-content');
   });
 
-  it('does not render flyout content when expanded is false', () => {
+  it('does not render flyout content when "initialExpanded" is false', () => {
     renderFlyout();
     expect(screen.queryByTestId('flyout-content')).toBe(null);
   });
 
   it('renders flyout content when button is clicked', async () => {
-    const onToggle = jest.fn();
-
-    renderFlyout({ onToggle });
+    renderFlyout();
     fireEvent.click(screen.getByText('Test'));
-    expect(onToggle.mock.calls.length).toBe(1);
-  });
-
-  describe('clicking outside the flyout', () => {
-    it('closes flyout when clickOutsideCloses is true', () => {
-      const onToggle = jest.fn();
-
-      renderFlyout({ expanded: true, onToggle });
-
-      fireEvent.mouseDown(screen.getByTestId('flyout-outside'));
-
-      expect(onToggle.mock.calls.length).toBe(1);
-    });
-
-    it('does not close flyout when clickOutsideCloses is false', async () => {
-      renderFlyout({ expanded: true, clickOutsideDoesNotClose: true });
-
-      fireEvent.mouseDown(screen.getByTestId('flyout-outside'));
-
-      await waitFor(() => screen.getByTestId('flyout-content'));
-    });
-  });
-
-  it('does not close flyout when clicking inside flyout', () => {
-    renderFlyout({ expanded: true });
-    fireEvent.mouseDown(screen.getByTestId('flyout-content'));
     screen.getByTestId('flyout-content');
   });
 
-  describe('pressing the escape key', () => {
-    it('closes flyout when escapeCloses is true', () => {
-      const onToggle = jest.fn();
-
-      const { view } = renderFlyout({ expanded: true, onToggle });
-
-      fireEvent.keyDown(view.baseElement, {
-        key: 'Escape',
-        code: 'Escape',
-      });
-
-      expect(onToggle.mock.calls.length).toBe(1);
+  describe('clicking outside the flyout', () => {
+    it('closes flyout when "clickOutsideCloses" is true', async () => {
+      renderFlyout();
+      fireEvent.mouseDown(screen.getByTestId('flyout-outside'));
+      await waitFor(() =>
+        expect(screen.queryByTestId('flyout-content')).toBe(null)
+      );
     });
 
-    it('does not close flyout when escapeCloses is false', () => {
-      const { view } = renderFlyout({
-        expanded: true,
-        escapeDoesNotClose: true,
-      });
+    it('does not close flyout when "clickOutsideCloses" is false', async () => {
+      renderFlyout({ initialExpanded: true, clickOutsideCloses: false });
+      fireEvent.mouseDown(screen.getByTestId('flyout-outside'));
+      await waitFor(() => expect(screen.queryByTestId('flyout-content')));
+    });
+  });
+
+  it('does not close flyout when clicking inside flyout', async () => {
+    renderFlyout({ initialExpanded: true });
+    fireEvent.mouseDown(screen.getByTestId('flyout-content'));
+    await waitFor(() => expect(screen.queryByTestId('flyout-content')));
+  });
+
+  describe('pressing the escape key', () => {
+    it('closes flyout when "escapeCloses" is true', async () => {
+      const { view } = renderFlyout();
       fireEvent.keyDown(view.baseElement, {
         key: 'Escape',
         code: 'Escape',
       });
-      screen.getByTestId('flyout-content');
+
+      await waitFor(() =>
+        expect(screen.queryByTestId('flyout-content')).toBe(null)
+      );
+    });
+
+    it('does not close flyout when "escapeCloses" is false', async () => {
+      const { view } = renderFlyout({
+        initialExpanded: true,
+        escapeCloses: false,
+      });
+
+      fireEvent.keyDown(view.baseElement, {
+        key: 'Escape',
+        code: 'Escape',
+      });
+
+      await waitFor(() => expect(screen.queryByTestId('flyout-content')));
     });
   });
 });
