befd35a3c0bf70c5ab8938442b4fa6ae91769e57
Josh Goldberg <josh@codecademy.com>
2021-06-15 10:48:48 -0400

    1px and 100vw left

--- packages/gamut/src/GridForm/GridFormInputGroup/GridFormSweetContainerInput/index.tsx
+++ packages/gamut/src/GridForm/GridFormInputGroup/GridFormSweetContainerInput/index.tsx
@@ -18,12 +18,12 @@ export const GridFormSweetContainerInput: React.FC<GridFormSweetContainerInputPr
 }) => {
   return (
     <Box
-      height="0"
-      left="0"
+      height="1px"
+      left="100vw"
       overflow="hidden"
       position="fixed"
       top="0"
-      width="0"
+      width="1px"
     >
       <label htmlFor={name}>{label}</label>
       <Input name={name} ref={register()} type="checkbox" id={name} />
