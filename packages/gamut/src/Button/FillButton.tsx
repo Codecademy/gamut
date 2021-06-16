743c2488ad5b62d41bfe3aec30504d9191a8c83c
Aaron Robb <52927224+codecaaron@users.noreply.github.com>
2021-06-16 17:24:50 -0400

    feat(Buttons): Color + Structure Redux [GM-237] (#1809)

--- packages/gamut/src/Button/FillButton.tsx
+++ packages/gamut/src/Button/FillButton.tsx
@@ -1,50 +1,7 @@
-import { useCurrentMode } from '@codecademy/gamut-styles';
-import styled from '@emotion/styled';
-import React, { forwardRef } from 'react';
+import { createButtonComponent } from './shared';
+import { fillButtonVariants, sizeVariants } from './variants';
 
-import { ButtonBaseElements } from '../ButtonBase/ButtonBase';
-import { ButtonInner } from './ButtonInner';
-import {
-  buttonColors,
-  ButtonOutline,
-  ButtonOutlineProps,
-  createStates,
-} from './ButtonOutline';
-import { buttonSizing, config } from './shared';
-import { SizedButtonProps } from './types';
-
-const {
-  background,
-  foreground,
-  backgroundMuted,
-  backgroundDull,
-  foregroundMuted,
-} = buttonColors;
-
-const FillButtonInner = styled(ButtonInner, config)<SizedButtonProps>(
-  createStates({
-    base: { color: foreground, backgroundColor: background },
-    hover: { backgroundColor: backgroundDull },
-    active: { borderColor: background },
-    disabled: {
-      color: foregroundMuted,
-      backgroundColor: backgroundMuted,
-    },
-  }),
-  buttonSizing
-);
-
-export type FillButtonProps = SizedButtonProps & ButtonOutlineProps;
-
-export const FillButton = forwardRef<ButtonBaseElements, FillButtonProps>(
-  ({ children, mode, size, variant, ...props }, ref) => {
-    const currentMode = useCurrentMode(mode);
-    return (
-      <ButtonOutline mode={currentMode} variant={variant} {...props} ref={ref}>
-        <FillButtonInner mode={currentMode} variant={variant} size={size}>
-          {children}
-        </FillButtonInner>
-      </ButtonOutline>
-    );
-  }
+export const FillButton = createButtonComponent(
+  sizeVariants,
+  fillButtonVariants
 );
