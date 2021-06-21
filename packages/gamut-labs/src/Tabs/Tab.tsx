ca88b9ce126c31725d4d3a9292b2a200f1ddb3dc
Troy Chryssos <troy@codecademy.com>
2021-06-21 11:48:03 -0400

    styling tab component

--- packages/gamut-labs/src/Tabs/Tab.tsx
+++ packages/gamut-labs/src/Tabs/Tab.tsx
@@ -1,7 +1,19 @@
 import { Box, TextButton } from '@codecademy/gamut';
+import styled from '@emotion/styled';
 import React from 'react';
 
 import { TabProps } from './types';
+
+const TabButton = styled(TextButton)<Pick<TabProps, 'isActiveTab'>>`
+  border: 0;
+  border-bottom: ${({ theme, isActiveTab }) =>
+    `0.1875rem ${isActiveTab ? theme.colors.primary : 'transparent'} solid`};
+  border-bottom-left-radius: 0px;
+  border-bottom-right-radius: 0px;
+  font-weight: ${({ theme, isActiveTab }) =>
+    isActiveTab ? theme.fontWeight[700] : theme.fontWeight[400]};
+  font-size: ${({ theme }) => theme.fontSize[18]};
+`;
 
 export const Tab: React.FC<TabProps> = ({
   title,
@@ -9,15 +21,18 @@ export const Tab: React.FC<TabProps> = ({
   disabled,
   onClick,
 }) => (
-  <Box>
-    <TextButton
+  <Box borderBottom={1} borderColor={isActiveTab ? 'primary' : 'text-disabled'}>
+    <TabButton
       variant={isActiveTab ? 'primary' : 'secondary'}
       onClick={onClick}
       disabled={disabled}
       role="tab"
       aria-selected={isActiveTab}
+      width={160}
+      isActiveTab={isActiveTab}
+      pt={8}
     >
       {title}
-    </TextButton>
+    </TabButton>
   </Box>
 );
