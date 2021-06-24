5f507a309303f98221a072146d5afcd410a11230
cass spain <cass@codecademy.com>
2021-06-24 11:25:38 -0400

    revert

--- packages/gamut/src/Form/SelectDropdown.tsx
+++ packages/gamut/src/Form/SelectDropdown.tsx
@@ -4,7 +4,9 @@ import { css } from '@emotion/react';
 import React, { SelectHTMLAttributes, useMemo, useState } from 'react';
 import ReactSelect, {
   components as SelectDropdownElements,
+  ContainerProps,
   IndicatorProps,
+  InputProps,
   NamedProps,
   OptionTypeBase,
   StylesConfig,
@@ -41,12 +43,28 @@ type OptionStrict = {
   value: string;
 };
 
+type CustomInputProps = InputProps &
+  ContainerProps<OptionStrict, false> & {
+    selectProps?: SelectDropdownProps;
+  };
+
 const ChevronDropdown = (props: IndicatorProps<OptionTypeBase, false>) => {
   return (
     <DropdownIndicator {...props}>
       <ArrowChevronDownIcon size={16} />
     </DropdownIndicator>
   );
+};
+
+const CustomInput = (props: CustomInputProps) => {
+  const value = props.hasValue ? props.getValue()[0].value : '';
+
+  if (props.selectProps?.inputProps) {
+    const { inputProps } = props.selectProps;
+    const newProps = { ...inputProps, ...props, value };
+    return <Input {...newProps} />;
+  }
+  return <Input {...props} />;
 };
 
 const selectBaseStyles = ({
@@ -87,6 +105,20 @@ const customStyles: StylesConfig<OptionTypeBase, false> = {
     ...formDropdownStyles(state.selectProps.error),
   }),
 
+  input: (provided, state) => ({
+    background: 0,
+    border: 0,
+    fontSize: 'inherit',
+    outline: 0,
+    padding: 0,
+    width: '1px',
+    color: 'transparent',
+    left: '-100px',
+    opacity: '0',
+    position: 'relative',
+    transform: 'scale(0)',
+  }),
+
   option: (provided, state) => ({
     padding: '14px 11px 14px 11px',
     cursor: 'pointer',
@@ -125,6 +157,7 @@ const defaultProps = {
   isMulti: false,
   styles: customStyles,
   components: {
+    Input: CustomInput,
     DropdownIndicator: ChevronDropdown,
     IndicatorSeparator: () => null,
   },
