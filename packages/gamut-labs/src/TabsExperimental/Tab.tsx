afba73f45c43e63679e7ff1d9a9ee68d5bad993d
Troy Chryssos <troy@codecademy.com>
2021-07-01 16:03:29 -0400

    using system state for styling:

--- packages/gamut-labs/src/TabsExperimental/Tab.tsx
+++ packages/gamut-labs/src/TabsExperimental/Tab.tsx
@@ -1,48 +1,54 @@
 import { Box, TextButton } from '@codecademy/gamut';
+import { css, states, useCurrentMode } from '@codecademy/gamut-styles';
 import styled from '@emotion/styled';
 import React from 'react';
 
 import { TabProps } from './types';
 
-type TabButtonProps = Pick<TabProps, 'isActiveTab'>;
+type TabButtonProps = Pick<TabProps, 'isActiveTab'> & {
+  isLightActiveTab: boolean;
+};
 
-const TabButton = styled(TextButton)<TabButtonProps>`
-  border: 0;
-  border-bottom: ${({ theme, isActiveTab }) => {
-    let borderColor = 'transparent';
-    if (isActiveTab) {
-      if (theme.mode === 'light') {
-        borderColor = theme.colors.blue;
-      } else {
-        borderColor = theme.colors.primary;
-      }
-    }
-    return `0.1875rem ${borderColor} solid`;
-  }};
-  border-bottom-left-radius: 0px;
-  border-bottom-right-radius: 0px;
-  font-weight: ${({ theme, isActiveTab }) =>
-    isActiveTab ? theme.fontWeight[700] : theme.fontWeight[400]};
-  font-size: ${({ theme }) => theme.fontSize[18]};
-  text-overflow: ellipsis;
-  color: ${({ theme, isActiveTab }) => {
-    let textColor: string = theme.colors.text;
-    if (isActiveTab) {
-      if (theme.mode === 'light') {
-        textColor = theme.colors.blue;
-      } else {
-        textColor = theme.colors.primary;
-      }
-    }
-    return textColor;
-  }};
-`;
+const TabButton = styled(TextButton)<TabButtonProps>(
+  css({
+    borderLeft: 'none',
+    borderRight: 'none',
+    borderTop: 'none',
+    borderColor: 'transparent',
+    borderBottomStyle: 'solid',
+    borderBottomWidth: '0.1875rem',
+    borderBottomLeftRadius: 0,
+    borderBottomRightRadius: 0,
+    fontWeight: 400,
+    fontSize: 18,
+    textOverflow: 'ellipsis',
+    color: 'text',
+  }),
+  states({
+    isActiveTab: {
+      borderColor: 'primary',
+      fontWeight: 700,
+      color: 'primary',
+    },
+    isLightActiveTab: {
+      borderColor: 'navy',
+      color: 'navy',
+    },
+  })
+);
 
 export const Tab: React.FC<TabProps> = ({ title, isActiveTab, onClick }) => {
+  const colorMode = useCurrentMode();
   return (
     <Box
       borderBottom={1}
-      borderColor={isActiveTab ? 'primary' : 'text-disabled'}
+      borderColor={
+        isActiveTab && colorMode === 'light'
+          ? 'navy-800'
+          : isActiveTab
+          ? 'primary'
+          : 'text-disabled'
+      }
     >
       <TabButton
         variant={isActiveTab ? 'primary' : 'secondary'}
@@ -54,6 +60,7 @@ export const Tab: React.FC<TabProps> = ({ title, isActiveTab, onClick }) => {
         pt={8}
         overflow="hidden"
         display="block"
+        isLightActiveTab={colorMode === 'light' && isActiveTab}
       >
         {title}
       </TabButton>
