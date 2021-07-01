be29c68ad255093cad6ea54aaf0e8ec9570308d2
cass spain <cass@codecademy.com>
2021-07-01 12:31:35 -0400

    FormGroupLabel +desc

--- packages/gamut/src/Form/FormGroupDescription.tsx
+++ packages/gamut/src/Form/FormGroupDescription.tsx
@@ -1,15 +1,19 @@
-import cx from 'classnames';
+import { system } from '@codecademy/gamut-styles';
+import styled from '@emotion/styled';
 import React from 'react';
 
-import styles from './styles/FormGroupDescription.module.scss';
+import { formBaseStyles } from './styles/shared-system-props';
 
 export type FormGroupDescriptionProps = React.HTMLAttributes<HTMLDivElement> & {
   className?: string;
 };
 
+const StyledDiv = styled.div<FormGroupDescriptionProps>`
+  ${system.css(formBaseStyles)}
+`;
+
 export const FormGroupDescription: React.FC<FormGroupDescriptionProps> = (
   props
 ) => {
-  const className = cx(styles.FormGroupDescription, props.className);
-  return <div {...props} className={className} />;
+  return <StyledDiv {...props} />;
 };
