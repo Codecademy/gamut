7c7fefc6460ae48bb5f91cea54d53bae80b2c4c6
cass spain <cass@codecademy.com>
2021-07-14 13:16:16 -0400

    polyline => checked

--- packages/gamut/src/Form/Checkbox.tsx
+++ packages/gamut/src/Form/Checkbox.tsx
@@ -1,7 +1,12 @@
 import { noSelect, screenReaderOnly, states } from '@codecademy/gamut-styles';
 import { StyleProps } from '@codecademy/variance';
 import styled from '@emotion/styled';
-import React, { forwardRef, InputHTMLAttributes, ReactNode } from 'react';
+import React, {
+  forwardRef,
+  InputHTMLAttributes,
+  ReactNode,
+  useState,
+} from 'react';
 
 import { variables } from './_variables';
 import {
@@ -13,6 +18,7 @@ import {
 } from './styles/shared-system-props';
 
 type Multiline = { multiline?: boolean };
+type Checked = { checked?: boolean };
 
 export type CheckboxProps = InputHTMLAttributes<HTMLInputElement> &
   Multiline & {
@@ -48,7 +54,7 @@ const CheckboxVector = styled.svg`
   left: -2px;
 `;
 
-const Polyline = styled.polyline`
+const Polyline = styled.polyline<Checked>`
   ${polyline}
   fill: none;
   stroke: currentColor;
@@ -57,21 +63,13 @@ const Polyline = styled.polyline`
   stroke-linejoin: round;
   stroke-dasharray: 18px;
   stroke-dashoffset: 18px;
+  stroke-dashoffset: ${({ checked }) => (checked ? 0 : `18px`)};
+  transition: stroke-dashoffset ${variables.transitionTime};
 `;
 
 const Input = styled.input`
   ${screenReaderOnly}
   ${checkboxInput}
-
-
-  &:checked + ${CheckboxLabel} ${CheckboxElement} {
-    ${CheckboxVector} {
-      ${Polyline} {
-        stroke-dashoffset: 0;
-        transition: stroke-dashoffset ${variables.transitionTime};
-      }
-    }
-  }
 `;
 
 const CheckboxText = styled.span<CheckboxTextProps>(checkboxTextStates);
@@ -81,13 +79,17 @@ export const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(
     { className, label, htmlFor, multiline, id, checked, disabled, ...rest },
     ref
   ) => {
+    const [currentlyChecked, setCurrentlyChecked] = useState(Boolean(checked));
+
     return (
       <div className={className}>
+        {console.log(currentlyChecked)}
         <Input
           id={id || htmlFor}
           type="checkbox"
-          checked={checked}
+          checked={currentlyChecked}
           disabled={disabled}
+          onChange={() => setCurrentlyChecked(!currentlyChecked)}
           {...rest}
           ref={ref}
         />
@@ -98,7 +100,7 @@ export const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(
                 d="M3,1 L17,1 L17,1 C18.1045695,1 19,1.8954305 19,3 L19,17 L19,17 C19,18.1045695 18.1045695,19 17,19 L3,19 L3,19 C1.8954305,19 1,18.1045695 1,17 L1,3 L1,3 C1,1.8954305 1.8954305,1 3,1 Z"
                 fill="currentColor"
               />
-              <Polyline points="4 11 8 15 16 6" />
+              <Polyline checked={currentlyChecked} points="4 11 8 15 16 6" />
             </CheckboxVector>
           </CheckboxElement>
           <CheckboxText multiline={multiline} disabled={disabled}>
