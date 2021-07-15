d5628ee80f4431037b54b414a4d9270e951f0a0a
Aaron Robb <aaron@codecademy.com>
2021-07-15 17:21:50 -0400

    React 17

--- script/jest/base-setup.js
+++ script/jest/base-setup.js
@@ -3,7 +3,7 @@
  */
 
 const Enzyme = require('enzyme');
-const Adapter = require('enzyme-adapter-react-16');
+const Adapter = require('@wojtekmaj/enzyme-adapter-react-17');
 
 Enzyme.configure({ adapter: new Adapter() });
 
