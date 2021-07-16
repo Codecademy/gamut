3a14a2d6592c6bf98e356483ea9bd87878f6a377
Aaron Robb <52927224+codecaaron@users.noreply.github.com>
2021-07-16 17:57:40 -0400

    feat(Toast):  Allow JSX icons GM-262
    * Allow toasts to render an inline icon within the container.
    * Any string passed is treated as a url and used as a background image.
    * Adds example using an illustration.

---
+++ packages/gamut/src/Toast/__tests__/Toast-test.tsx
@@ -0,0 +1,44 @@
+import { setupRtl } from '@codecademy/gamut-tests';
+import { fireEvent, screen } from '@testing-library/react';
+import React from 'react';
+
+import { Toast } from '../Toast';
+
+const onClose = jest.fn();
+
+const renderView = setupRtl(Toast, {
+  children: 'I am a toast',
+  onClose,
+});
+
+describe('Toast', () => {
+  it('renders a close button by default', () => {
+    renderView();
+
+    fireEvent.click(screen.getByRole('button'));
+    expect(onClose).toHaveBeenCalled();
+  });
+
+  it('does not provision an icon area by default', () => {
+    renderView();
+
+    const toast = screen.getByRole('status');
+    expect(toast.childNodes.length).toBe(2);
+  });
+
+  it('renders an image as a background when specified', () => {
+    renderView({ icon: './myimg.svg' });
+
+    const toast = screen.getByRole('status');
+    expect(toast.childNodes.length).toBe(3);
+    expect(toast.childNodes[0].childNodes.length).toEqual(0);
+  });
+
+  it('wraps any icon element passed when specified', () => {
+    renderView({ icon: <div /> });
+
+    const toast = screen.getByRole('status');
+    expect(toast.childNodes.length).toBe(3);
+    expect(toast.childNodes[0].childNodes.length).toEqual(1);
+  });
+});
