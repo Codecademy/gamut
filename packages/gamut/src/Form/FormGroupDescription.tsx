781983041078a74dff009c488dd639d312a65ee7
cass spain <cass@codecademy.com>
2021-07-16 18:01:15 -0400

    fixy desc

--- packages/gamut/src/Form/FormGroupDescription.tsx
+++ packages/gamut/src/Form/FormGroupDescription.tsx
@@ -1,17 +1,6 @@
 import { system } from '@codecademy/gamut-styles';
 import styled from '@emotion/styled';
-import React from 'react';
 
 import { formBaseStyles } from './styles/shared-system-props';
 
-export type FormGroupDescriptionProps = React.HTMLAttributes<HTMLDivElement> & {
-  className?: string;
-};
-
 export const FormGroupDescription = styled.div(system.css(formBaseStyles));
-
-export const FormGroupDescription: React.FC<FormGroupDescriptionProps> = (
-  props
-) => {
-  return <StyledDiv {...props} />;
-};
