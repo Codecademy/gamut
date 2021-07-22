7131ced127ebcefe016c7dbe66dafc807d6227e2
cass spain <cass@codecademy.com>
2021-07-22 14:19:20 -0400

    remove g lol

--- packages/gamut/src/Form/Checkbox.tsx
+++ packages/gamut/src/Form/Checkbox.tsx
@@ -97,7 +97,7 @@ export const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(
                 d="M3,1 L17,1 L17,1 C18.1045695,1 19,1.8954305 19,3 L19,17 L19,17 C19,18.1045695 18.1045695,19 17,19 L3,19 L3,19 C1.8954305,19 1,18.1045695 1,17 L1,3 L1,3 C1,1.8954305 1.8954305,1 3,1 Z"
                 fill="currentColor"
               />
-              <Polyline checked={checked} points="4 11 8 15 16 6" />g{' '}
+              <Polyline checked={checked} points="4 11 8 15 16 6" />
             </CheckboxVector>
           </CheckboxElement>
           <CheckboxText multiline={multiline} disabled={disabled}>
