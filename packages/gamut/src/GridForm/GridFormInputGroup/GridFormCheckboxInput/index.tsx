be3d238ac5f813ce265eaedd05f4af76b7417b3e
cassie spain <58054751+dreamwasp@users.noreply.github.com>
2021-07-27 09:51:04 -0400

    refactor: GridForm ColorModes + system props

--- packages/gamut/src/GridForm/GridFormInputGroup/GridFormCheckboxInput/index.tsx
+++ packages/gamut/src/GridForm/GridFormInputGroup/GridFormCheckboxInput/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { UseFormMethods } from 'react-hook-form';
+import { Controller, useFormContext, UseFormMethods } from 'react-hook-form';
 
 import { Checkbox } from '../../../Form';
 import { GridFormCheckboxField } from '../../types';
@@ -17,19 +17,33 @@ export const GridFormCheckboxInput: React.FC<GridFormCheckboxInputProps> = ({
   register,
   showRequired,
 }) => {
+  const { control } = useFormContext();
+
   return (
-    <Checkbox
-      className={className}
-      defaultChecked={field.defaultValue}
-      disabled={field.disabled}
-      htmlFor={field.name}
+    <Controller
+      control={control}
+      defaultValue={Boolean(field.defaultValue)}
       name={field.name}
-      onChange={(event) => field.onUpdate?.(event.target.checked)}
-      label={field.description}
-      multiline={field.multiline}
-      ref={register(field.validation)}
-      id={field.id}
-      aria-required={showRequired}
+      render={({ onChange, value }) => (
+        <>
+          <Checkbox
+            checked={value}
+            className={className}
+            disabled={field.disabled}
+            htmlFor={field.name}
+            name={field.name}
+            onChange={(event) => {
+              field.onUpdate?.(event.target.checked);
+              onChange?.(event.target.checked);
+            }}
+            label={field.description}
+            multiline={field.multiline}
+            ref={register(field.validation)}
+            id={field.id}
+            aria-required={showRequired}
+          />
+        </>
+      )}
     />
   );
 };
