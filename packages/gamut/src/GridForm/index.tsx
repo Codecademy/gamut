b9bfcf2c871b9118980c5805889f8e41d001f6ca
cassie spain <58054751+dreamwasp@users.noreply.github.com>
2021-08-03 09:31:55 -0400

    Update packages/gamut/src/GridForm/index.tsx
    Co-authored-by: Josh Goldberg <joshuakgoldberg@outlook.com>

--- packages/gamut/src/GridForm/index.tsx
+++ packages/gamut/src/GridForm/index.tsx
@@ -1,3 +1,5 @@
 export * from './GridForm';
-// this is being exported as a piece of an in progress API. if you think you have a use-case for it, please reach out to the web-plat team.
+/**
+ * This is being exported as a piece of an in progress API. if you think you have a use-case for it, please reach out to the web-plat team.
+ */
 export { GridFormContent } from './GridFormSections/GridFormContent';
