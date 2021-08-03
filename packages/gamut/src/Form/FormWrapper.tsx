c10825f33c517343dac6237ef6a3eeafc0607777
cassie spain <58054751+dreamwasp@users.noreply.github.com>
2021-08-03 09:31:46 -0400

    Update packages/gamut/src/Form/FormWrapper.tsx
    Co-authored-by: Josh Goldberg <joshuakgoldberg@outlook.com>

--- packages/gamut/src/Form/FormWrapper.tsx
+++ packages/gamut/src/Form/FormWrapper.tsx
@@ -22,8 +22,9 @@ export type FormWrapperProps<Values extends {}> = {
   validation?: Mode;
 };
 
-// this is an in progress API! please reach out to the web-plat team if you're interested in using it.
-
+/**
+ * This is an in progress API! please reach out to the web-plat team if you're interested in using it.
+ */
 export function FormWrapper<Values extends FormValues>({
   children,
   onSubmit,
