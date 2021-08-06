9aa04f7b2e7ddcf31d31b621e1f66d345832d2ee
cstatro <45748761+cstatro@users.noreply.github.com>
2021-08-06 10:35:12 -0400

    better centering strategy

--- packages/gamut-labs/src/Testimonial/index.tsx
+++ packages/gamut-labs/src/Testimonial/index.tsx
@@ -110,7 +110,7 @@ export const Testimonial: React.FC<TestimonialProps> = ({
             <TestimonialPicture src={imageUrl} alt="testimonial" />
           )}
           <Box
-            pt={{ _: 32, sm: isVerticleLayout && !hideAvatar ? 24 : 0 }}
+            my={{ _: 32, sm: isVerticleLayout && !hideAvatar ? 'auto' : 0 }}
             mr={32}
             gridArea={!hideAvatar ? 'byline' : 'avatar'}
           >
