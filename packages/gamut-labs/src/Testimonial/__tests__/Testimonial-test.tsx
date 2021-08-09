a53f23284034c9eb0f14b42f5cb8d7d3087ffcc3
cstatro <45748761+cstatro@users.noreply.github.com>
2021-08-09 18:14:30 -0400

    we can rebuild it

---
+++ packages/gamut-labs/src/Testimonial/__tests__/Testimonial-test.tsx
@@ -0,0 +1,58 @@
+import { setupRtl } from '../../../../gamut-tests/dist';
+import { Testimonial, TestimonialProps } from '..';
+
+describe('Testimonial', () => {
+  const defaultProps: TestimonialProps = {
+    firstName: 'Christopher',
+    lastName: 'Moltisanti',
+    imageUrl: 'okthen',
+    href: 'bloip',
+    quote: "He's the hair apparent",
+    location: 'New Jersey',
+    occupation: 'Waste Management',
+    longQuote:
+      "Like everybody starts out somewheres. and they do something, something gets done to them and it changes their life. That's called an arc. Where's my arc?",
+  };
+  const renderView = setupRtl(Testimonial, defaultProps);
+
+  it('formats and renders the first and last name', () => {
+    const { view } = renderView();
+    view.getByText('Christopher M.');
+  });
+
+  describe('when the image url is defined', () => {
+    it('displays testimonial photo', () => {
+      const { view } = renderView();
+      view.getByTestId('testimonial-photo');
+    });
+    describe('when hidePhoto is true', () => {
+      it('does not display testimonial photo', () => {
+        const { view } = renderView({ hidePhoto: true });
+        expect(view.queryAllByTestId('testimonial-photo').length).toBe(0);
+      });
+    });
+  });
+
+  describe('when image url is undefined', () => {
+    it('does not display testimonial photo', () => {
+      const { view } = renderView({ imageUrl: undefined });
+      expect(view.queryAllByTestId('testimonial-photo').length).toBe(0);
+    });
+  });
+
+  describe('when href is undefined', () => {
+    it('does not render an anchor tag', () => {
+      const { view } = renderView({ href: undefined });
+      expect(view.queryAllByTestId('testimonial-link').length).toBeFalsy();
+    });
+  });
+  describe('when href is defined', () => {
+    it('renders the card inside of an anchor tag', () => {
+      const { view } = renderView();
+      expect(view.getByTestId('testimonial-link')).toHaveAttribute(
+        'href',
+        defaultProps.href
+      );
+    });
+  });
+});
---
+++ packages/gamut-labs/src/Testimonial/index.tsx
@@ -0,0 +1,165 @@
+import { Anchor, Box, FloatingCard, Text } from '@codecademy/gamut';
+import { modeColorProps, system } from '@codecademy/gamut-styles';
+import styled from '@emotion/styled';
+import React, { ComponentProps } from 'react';
+
+import darkQuotes from '../assets/navyQuotes.svg';
+
+const QuoteArt = styled.img`
+  height: 25px;
+  grid-area: art;
+`;
+
+const TestimonialPicture = styled.img`
+  height: 98px;
+  width: 98px;
+  border-radius: 70px;
+  grid-area: avatar;
+`;
+const TestimonialCard = styled(FloatingCard)(modeColorProps);
+
+const gridLayouts = {
+  vertical: `'art art art'
+             'text text text'
+             'avatar byline byline'
+             'avatar byline byline'
+             `,
+  horizontal: `'avatar art text'
+               'byline art text'
+               'byline art text'
+               'byline art text'
+               `,
+};
+
+const TestimonialContent = styled(Box)(
+  system.variant({
+    defaultVariant: 'horizontal',
+    base: {
+      display: 'grid',
+      color: 'text-accent',
+      gridTemplateColumns: 'max-content max-content 1fr',
+      gridTemplateRows: 'repeat(max-content, 4)',
+      gap: 16,
+    },
+    variants: {
+      horizontal: {
+        gridTemplateAreas: {
+          _: gridLayouts.vertical,
+          md: gridLayouts.horizontal,
+        },
+      },
+      vertical: {
+        gridTemplateAreas: gridLayouts.vertical,
+      },
+    },
+  })
+);
+
+export type TestimonialProps = ComponentProps<typeof TestimonialCard> &
+  ComponentProps<typeof TestimonialContent> & {
+    firstName: string;
+    lastName: string;
+    quote: string;
+    longQuote: string;
+    /**
+     * City location
+     */
+    location: string;
+    /**
+     * associated occupation of the person.
+     */
+    occupation?: string;
+    /**
+     * Associated workplace or institution
+     */
+    company?: string;
+    /**
+     * Portrait image src
+     */
+    imageUrl?: string;
+    /**
+     * setting this href will wrap the testimonial card with an anchor tag.
+     */
+    href?: string;
+    /**
+     * used to conditonally hide the portrait photo
+     */
+    hidePhoto?: boolean;
+    onClick?: () => void;
+  };
+
+export const Testimonial: React.FC<TestimonialProps> = ({
+  firstName,
+  lastName,
+  company,
+  occupation,
+  location,
+  href,
+  quote,
+  onClick,
+  hidePhoto,
+  imageUrl,
+  variant,
+  mode,
+  ...rest
+}) => {
+  const isVerticleLayout = variant === 'vertical';
+
+  const renderTestimonial = () => (
+    <TestimonialCard
+      {...rest}
+      p={32}
+      minWidth="22rem"
+      width="100%"
+      height="100%"
+      mode={mode}
+    >
+      <TestimonialContent variant={variant}>
+        {!hidePhoto && imageUrl && (
+          <TestimonialPicture
+            data-testid="testimonial-photo"
+            src={imageUrl}
+            alt="testimonial"
+          />
+        )}
+        <Box
+          my={{ _: 'auto', md: isVerticleLayout && !hidePhoto ? 'auto' : 0 }}
+          mr={32}
+          gridArea={!hidePhoto ? 'byline' : 'avatar'}
+        >
+          <Text variant="p-small" as="p" fontFamily="accent">
+            {`${firstName} ${lastName[0]}.`}
+          </Text>
+          <Text variant="p-small" as="p" fontFamily="accent">
+            {occupation}
+          </Text>
+          <Text variant="p-small" as="p" fontFamily="accent">
+            {`@ ${company}${location ? `, ${location}` : ''}`}
+          </Text>
+        </Box>
+        <QuoteArt src={darkQuotes} />
+        <Text
+          pt={{ _: 0, md: isVerticleLayout ? 0 : 4 }}
+          gridArea="text"
+          variant="title-md"
+          as="p"
+        >
+          {quote}
+        </Text>
+      </TestimonialContent>
+    </TestimonialCard>
+  );
+
+  const renderTestimonialWithAnchor = () => (
+    <Anchor
+      data-testid="testimonial-link"
+      href={href}
+      variant="interface"
+      onClick={onClick}
+    >
+      {renderTestimonial()}
+    </Anchor>
+  );
+
+  return href ? renderTestimonialWithAnchor() : renderTestimonial();
+};
