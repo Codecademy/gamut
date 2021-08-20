4fee2393a9218f8bcc372848f2b3898431240d90
Aaron Robb <aaron@codecademy.com>
2021-08-19 22:35:39 -0400

    Remove

--- packages/gamut/src/Form/__tests__/SubmitButton-test.tsx
+++ packages/gamut/src/Form/__tests__/SubmitButton-test.tsx
@@ -1,5 +1,5 @@
 import { setupEnzyme } from '@codecademy/gamut-tests';
-import hookform from 'react-hook-form';
+import hookform, { FormState } from 'react-hook-form';
 
 import { CTAButton, FillButton } from '../../Button';
 import { Spinner } from '../../Spinner';
@@ -11,28 +11,18 @@ jest.mock('react-hook-form', () => ({
 
 const mockedHook = hookform.useFormContext as jest.Mock;
 
-interface FormState {
-  isValidating?: boolean;
-  isValid?: boolean;
-  isSubmitting?: boolean;
-  isOnChange?: boolean;
-}
-
 const mockFormState = ({
   isValidating = false,
   isValid = true,
   isSubmitting = false,
-  isOnChange = true,
-}: FormState) => {
+}: FormState<{}>) => {
   mockedHook.mockImplementation(() => ({
     formState: { isSubmitting, isValidating, isValid },
-    control: { mode: { isOnChange } },
   }));
 };
 
 mockedHook.mockImplementation(() => ({
   formState: { isSubmitting: false, isValidating: false, isValid: false },
-  control: { mode: { isOnChange: true } },
 }));
 
 const renderWrapper = setupEnzyme(SubmitButton, {});
@@ -49,25 +39,12 @@ describe('SubmitButton', () => {
 
     expect(wrapper.find(CTAButton).length).toBe(1);
   });
-  it('disables a button if mode is onChange and form is invalid by default', () => {
-    mockFormState({ isValid: false });
-
-    const { wrapper } = renderWrapper();
-    expect(wrapper.find('button').prop('disabled')).toBe(true);
-  });
-
-  it('does not disable a button if mode is not onChange while form is invalid', () => {
-    mockFormState({ isValid: false, isOnChange: false });
-
-    const { wrapper } = renderWrapper();
-    expect(wrapper.find('button').prop('disabled')).toBe(false);
-  });
 
   it.each([
     [{ isValid: false }, true],
     [{ isValidating: true }, true],
     [{ isSubmitting: true }, true],
-  ])('disabled states (%o)', (formState: FormState, expected: boolean) => {
+  ])('disabled states (%o)', (formState: FormState<{}>, expected: boolean) => {
     mockFormState(formState);
     const { wrapper } = renderWrapper({
       disabled: ({ isValidating, isSubmitting, isValid }) =>
@@ -79,7 +56,7 @@ describe('SubmitButton', () => {
     [{}, 0],
     [{ isValidating: true }, 1],
     [{ isSubmitting: true }, 1],
-  ])('loading states (%o)', (formState: FormState, expected: number) => {
+  ])('loading states (%o)', (formState: FormState<{}>, expected: number) => {
     mockFormState(formState);
     const { wrapper } = renderWrapper({
       loading: ({ isValidating, isSubmitting }) => isValidating || isSubmitting,
