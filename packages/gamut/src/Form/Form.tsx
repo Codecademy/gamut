0f379af72fde705bec715cfe0a7061d227ed2d1d
cassie spain <58054751+dreamwasp@users.noreply.github.com>
2021-08-31 09:09:29 -0400

    refactor: FormWrapper => GridForm
    Refactor to have GridForm => FormWrapper, monolith PR [here](https://github.com/codecademy-engineering/Codecademy/pull/26626)

--- packages/gamut/src/Form/Form.tsx
+++ packages/gamut/src/Form/Form.tsx
@@ -1,10 +1,7 @@
 import { styledOptions, system } from '@codecademy/gamut-styles';
 import { variance } from '@codecademy/variance';
 import styled from '@emotion/styled';
-import cx from 'classnames';
 import React, { ComponentProps } from 'react';
-
-import styles from './styles/Form.module.scss';
 
 const formSystemProps = variance.compose(
   system.space,
@@ -19,8 +16,10 @@ const StyledForm = styled('form', styledOptions<'form'>())(formSystemProps);
 
 export type FormProps = ComponentProps<typeof StyledForm>;
 
-export const Form: React.FC<FormProps> = ({ method = 'post', ...props }) => {
-  const className = cx(styles.Form, props.className);
+export const Form: React.FC<FormProps> = ({ method, ...props }) => {
+  return <StyledForm {...props} noValidate method={method} />;
+};
 
-  return <StyledForm {...props} method={method} className={className} />;
+Form.defaultProps = {
+  method: 'post',
 };
