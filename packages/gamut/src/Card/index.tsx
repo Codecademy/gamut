a988524c04d645e41ce1855fbad4de1442f2b4ba
sooinc <sooin@codecademy.com>
2021-09-02 19:12:22 -0400

    add dynamic wrapper to card

--- packages/gamut/src/Card/index.tsx
+++ packages/gamut/src/Card/index.tsx
@@ -3,6 +3,33 @@ import { StyleProps } from '@codecademy/variance';
 import styled from '@emotion/styled';
 import React, { ComponentProps } from 'react';
 
+import { Box } from '..';
+
+const DynamicCardWrapper = styled(Box)<CardWrapperProps>(
+  variant({
+    prop: 'shadow',
+    base: {
+      position: 'relative',
+      boxShadow: `0px 0px 0 ${theme.colors.secondary}`,
+      transition: 'box-shadow 200ms ease, transform 200ms ease',
+    },
+    variants: {
+      small: {
+        '&:hover': {
+          transform: 'translate(2px, -2px)',
+          boxShadow: `-4px 4px 0 ${theme.colors.secondary}`,
+        },
+      },
+      medium: {
+        '&:hover': {
+          transform: 'translate(4px, -4px)',
+          boxShadow: `-8px 8px 0 ${theme.colors.secondary}`,
+        },
+      },
+    },
+  })
+);
+
 const shadowVariants = variant({
   prop: 'shadow',
   base: {
@@ -28,7 +55,7 @@ const shadowVariants = variant({
 
 export interface CardProps
   extends Omit<ComponentProps<typeof CardWrapper>, 'outline' | 'bg'> {
-  variant?: 'navy' | 'white' | 'hyper' | 'yellow';
+  variant?: 'navy' | 'white' | 'hyper' | 'yellow' | 'mode';
 }
 
 interface CardWrapperProps extends StyleProps<typeof shadowVariants> {
@@ -46,13 +73,27 @@ const CardWrapper = styled(Background)<CardWrapperProps>(
   })
 );
 
-export const Card: React.FC<CardProps> = ({ variant = 'white', ...rest }) => (
-  <CardWrapper
-    bg={variant}
-    border={1}
-    borderColor="navy"
-    p={16}
-    outline={variant === 'navy'}
-    {...rest}
-  />
-);
+export const Card: React.FC<CardProps> = ({ variant = 'white', ...rest }) => {
+  if (variant === 'mode') {
+    return (
+      <DynamicCardWrapper
+        border={1}
+        borderColor="secondary-hover"
+        bg="background"
+        p={16}
+        {...rest}
+      />
+    );
+  }
+
+  return (
+    <CardWrapper
+      bg={variant}
+      border={1}
+      borderColor="navy"
+      p={16}
+      outline={variant === 'navy'}
+      {...rest}
+    />
+  );
+};
