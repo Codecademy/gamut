d95aa955380935a3579f298d45d94ff25012b461
Aaron Robb <52927224+codecaaron@users.noreply.github.com>
2021-09-14 10:54:00 -0400

    feat(List): Adds List presentation components GM-185
    Adds list primitives elements for arbitrary construction.
    
    ### Example Usage
    ```tsx
    import { List, ListRow, ListCol } from "@codecademy/gamut";
    
    const View = () => (
      <List variant="slat">
        <ListRow>
          <ListCol type="header" size="md">Row Header</ListCol>
          <ListCol size="lg">Some Row Detail</ListCol> // Implicitly content
         <ListCol type="action"><IconButton /></ListCol>
        </ListRow>
      </List>
    );
    ```
    
    ### 
    
    ### Principles
    - In order to ensure consistent column widths between rows sizes are predefined.
    - Primarily functions to list polymorphic entities like users / curriculum / exercises that may have information / interactions that are associated with them.  While statistics / metrics can be displayed in this component and it does its best to behave as a table should it is not the primary function of it.
    - Purely presentational.   While we would like to provide a component to manage controlling the data inside the List we do not have enough information to create an API that will serve those needs.
    
    ### Components
    - `List`:  A list wrapper that creates a new context to share downstream styles with nested components (allowing prop level overrides on nested components).  Comes with 3 stylistic variants: `table`, `slat` and `bar`. 
    - `ListRow`: A row component that handles the layout / responsiveness of each constituent column.  At the `xs` breakpoint this will switch from `flex` to `grid` and provide a restructured layout that is agnostic to column configuration.
    - `ListCol`: A flex column that comes with predefined column sizes and boolean states for responsiveness.   In addition it includes a variant for 3 possible types `header` `content` and `control` which correspond to the mobile layout.
    
    #### Column Configuration
    The 3 type variants have specific effects depending on the top level variant and all effect layout:
    - `header`:  Is placed on the top left of the mobile view no matter where it is specified in the markup.  When the `List` is scrollable the element will remain stuck to the left hand border of the list.
    - `control`: Is placed at the top right (1/3rd of the row) of the mobile layout.
    - `content`: All become full width and are rendered in order below the top row in the mobile layout.
    
    ### Behaviors
    - Responsiveness:  by default rows will render their columns vertically before 480px.  The top row is split with 2:1 ratio with special column types `header` and `control`.  All other columns render beneath this as 100%.
    - `scrollable`:  You can optionally configure lists to keep columns at their defined widths and scroll LTR if there is not enough room, rather than collapsing.  When this is specified the `header` column will lock to the left side and all other content will scroll underneath it.
    
    ### Rollout Plan
    1. Release to LX to be used in code challenges for their 10/18 layout.  This will act as a base test for the API / Options.
    2. Test with teams that have table / list based visuals (Business / Enterprise) to see how well this suits their immediate needs on the presentational level.  At the same time discuss with them access patterns / needs for how they manipulate / control their data.
    3. Adjust API / styles to final version.
    4. Create proposal for a larger component that provides more logical structure based on the findings of product team user testings.

---
+++ packages/gamut/src/List/__tests__/List-test.tsx
@@ -0,0 +1,50 @@
+import { setupEnzyme } from '@codecademy/gamut-tests';
+import React from 'react';
+
+import { List } from '../List';
+import { ListCol } from '../ListCol';
+import { ListRow } from '../ListRow';
+
+const renderWrapper = setupEnzyme(List, {
+  children: (
+    <ListRow>
+      <ListCol>Hello</ListCol>
+    </ListRow>
+  ),
+});
+
+describe('Menu', () => {
+  it('renders a slat list by default', () => {
+    const { wrapper } = renderWrapper();
+
+    expect(wrapper.find('ListEl').prop('variant')).toBe('slat');
+    expect(wrapper.find('ListEl').prop('scrollable')).toBe(false);
+  });
+  it('configures rows with the correct variants', () => {
+    const { wrapper } = renderWrapper();
+
+    expect(wrapper.find('RowEl').prop('variant')).toBe('slat');
+    expect(wrapper.find('RowEl').prop('spacing')).toBe('normal');
+  });
+  it('configures columns with the correct variants', () => {
+    const { wrapper } = renderWrapper();
+
+    expect(wrapper.find('ColEl').prop('variant')).toBe('slat');
+    expect(wrapper.find('ColEl').prop('spacing')).toBe('normal');
+    expect(wrapper.find('ColEl').prop('sticky')).toBe(false);
+  });
+  it('fixes the row header column when scrollable - but not other columns', () => {
+    const { wrapper } = renderWrapper({
+      scrollable: true,
+      children: (
+        <ListRow>
+          <ListCol type="header">Hello</ListCol>
+          <ListCol>Content</ListCol>
+        </ListRow>
+      ),
+    });
+
+    expect(wrapper.find({ type: 'header', sticky: true }).length).toBe(1);
+    expect(wrapper.find({ type: 'content', sticky: true }).length).toBe(0);
+  });
+});
