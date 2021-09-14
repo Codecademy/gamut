7c2124de746f45b3014ddbd46bd8682140c4efbd
Aaron Robb <52927224+codecaaron@users.noreply.github.com>
2021-09-14 10:54:00 -0400

    feat(List): Adds List presentation components GM-185
    Adds list primitives elements for arbitrary construction.
    
    ### Example Usage
    ```tsx
    import { List, ListRow, ListCol } from "@codecademy/gamut";
    
    const View = () => (
      <List variant="slat">
        <ListRow>
          <ListCol type="header" size="md">Row Header</ListCol>
          <ListCol size="lg">Some Row Detail</ListCol> // Implicitly content
         <ListCol type="action"><IconButton /></ListCol>
        </ListRow>
      </List>
    );
    ```
    
    ### 
    
    ### Principles
    - In order to ensure consistent column widths between rows sizes are predefined.
    - Primarily functions to list polymorphic entities like users / curriculum / exercises that may have information / interactions that are associated with them.  While statistics / metrics can be displayed in this component and it does its best to behave as a table should it is not the primary function of it.
    - Purely presentational.   While we would like to provide a component to manage controlling the data inside the List we do not have enough information to create an API that will serve those needs.
    
    ### Components
    - `List`:  A list wrapper that creates a new context to share downstream styles with nested components (allowing prop level overrides on nested components).  Comes with 3 stylistic variants: `table`, `slat` and `bar`. 
    - `ListRow`: A row component that handles the layout / responsiveness of each constituent column.  At the `xs` breakpoint this will switch from `flex` to `grid` and provide a restructured layout that is agnostic to column configuration.
    - `ListCol`: A flex column that comes with predefined column sizes and boolean states for responsiveness.   In addition it includes a variant for 3 possible types `header` `content` and `control` which correspond to the mobile layout.
    
    #### Column Configuration
    The 3 type variants have specific effects depending on the top level variant and all effect layout:
    - `header`:  Is placed on the top left of the mobile view no matter where it is specified in the markup.  When the `List` is scrollable the element will remain stuck to the left hand border of the list.
    - `control`: Is placed at the top right (1/3rd of the row) of the mobile layout.
    - `content`: All become full width and are rendered in order below the top row in the mobile layout.
    
    ### Behaviors
    - Responsiveness:  by default rows will render their columns vertically before 480px.  The top row is split with 2:1 ratio with special column types `header` and `control`.  All other columns render beneath this as 100%.
    - `scrollable`:  You can optionally configure lists to keep columns at their defined widths and scroll LTR if there is not enough room, rather than collapsing.  When this is specified the `header` column will lock to the left side and all other content will scroll underneath it.
    
    ### Rollout Plan
    1. Release to LX to be used in code challenges for their 10/18 layout.  This will act as a base test for the API / Options.
    2. Test with teams that have table / list based visuals (Business / Enterprise) to see how well this suits their immediate needs on the presentational level.  At the same time discuss with them access patterns / needs for how they manipulate / control their data.
    3. Adjust API / styles to final version.
    4. Create proposal for a larger component that provides more logical structure based on the findings of product team user testings.

---
+++ packages/gamut/src/List/ListRow.tsx
@@ -0,0 +1,19 @@
+import React, { ComponentProps, forwardRef } from 'react';
+
+import { RowEl } from './elements';
+import { useListContext } from './ListProvider';
+import { PublicListProps } from './types';
+
+export interface ListRowProps
+  extends PublicListProps<ComponentProps<typeof RowEl>> {}
+
+export const ListRow = forwardRef<HTMLLIElement, ListRowProps>(
+  ({ children, ...rest }, ref) => {
+    const activeVariants = useListContext();
+    return (
+      <RowEl {...activeVariants} {...rest} ref={ref}>
+        {children}
+      </RowEl>
+    );
+  }
+);
---
+++ packages/gamut/src/List/elements.tsx
@@ -0,0 +1,214 @@
+import { css, states, variant } from '@codecademy/gamut-styles';
+import { StyleProps } from '@codecademy/variance';
+import styled from '@emotion/styled';
+
+const listVariants = variant({
+  prop: 'variant',
+  defaultVariant: 'slat',
+  base: {
+    listStyleType: 'none',
+    p: 0,
+    display: 'grid',
+    gridAutoRows: '1fr',
+    gridTemplateColumns: 'minmax(0, 1fr)',
+  },
+  variants: {
+    slat: {
+      border: 1,
+      borderRadius: '2px',
+      overflow: 'hidden',
+    },
+    table: {},
+    bar: {
+      gap: 16,
+    },
+  },
+});
+
+const listStates = states({
+  scrollable: {
+    overflowX: 'auto',
+    maxWidth: 1,
+  },
+});
+
+export interface ListProps
+  extends StyleProps<typeof listStates>,
+    StyleProps<typeof listVariants> {}
+
+export const ListEl = styled.ul<ListProps>(listVariants, listStates);
+
+const rowStates = states({
+  scrollable: {
+    minWidth: 'min-content',
+    width: '100%',
+  },
+});
+
+const spacingVariants = variant({
+  prop: 'spacing',
+  variants: {
+    normal: {
+      gap: { _: 8, xs: 16 },
+    },
+    condensed: {
+      fontSize: 14,
+      gap: 8,
+    },
+  },
+});
+
+const rowVariants = variant({
+  prop: 'variant',
+  base: {
+    py: { _: 8, xs: 0 },
+    display: { _: 'grid', xs: 'flex' },
+    gridAutoRows: 'minmax(1.5rem, max-content)',
+    gridTemplateColumns: 'minmax(0, 1fr) max-content',
+    flexDirection: { _: 'column', xs: 'row' },
+  },
+  variants: {
+    slat: {
+      bg: 'background',
+      borderBottom: 1,
+      '&:last-child': {
+        borderBottom: 'none',
+      },
+    },
+    table: {
+      bg: 'background',
+      '&:nth-child(2n)': {
+        bg: 'background-selected',
+      },
+    },
+    bar: {
+      border: 1,
+      borderRadius: '2px',
+      bg: 'background',
+    },
+  },
+});
+
+export interface RowProps
+  extends StyleProps<typeof rowVariants>,
+    StyleProps<typeof spacingVariants> {}
+
+export const RowEl = styled.li<RowProps>(
+  rowVariants,
+  spacingVariants,
+  rowStates
+);
+
+const columnType = variant({
+  prop: 'type',
+  defaultVariant: 'content',
+  variants: {
+    header: {
+      gridColumn: 1,
+    },
+    content: {
+      gridColumnEnd: 'span 2',
+    },
+    control: {
+      minWidth: 'min-content',
+      alignItems: {
+        _: 'flex-start',
+        xs: 'center',
+      },
+      gridColumn: 2,
+      gridRow: 1,
+    },
+  },
+});
+
+const columnJustify = variant({
+  prop: 'justify',
+  defaultVariant: 'left',
+  variants: {
+    left: {
+      justifyContent: { xs: 'flex-start' },
+    },
+    right: {
+      justifyContent: { xs: 'flex-end' },
+    },
+  },
+});
+
+const columnSizes = variant({
+  prop: 'size',
+  defaultVariant: 'content',
+  base: { minWidth: 0, maxWidth: 1, flexShrink: 1 },
+  variants: {
+    sm: {
+      flexBasis: { xs: '6rem' },
+      width: { xs: '6rem' },
+    },
+    md: {
+      flexBasis: { xs: '10rem' },
+      width: { xs: '10rem' },
+    },
+    lg: {
+      flexBasis: { xs: '12rem' },
+      width: { xs: '12rem' },
+    },
+    xl: {
+      flexBasis: { xs: '20rem' },
+      width: { xs: '20rem' },
+    },
+    content: {
+      flexShrink: 0,
+    },
+  },
+});
+
+const columnStates = states({
+  fill: { flexGrow: { xs: 1 } },
+  sticky: {
+    position: 'sticky',
+    left: 0,
+    zIndex: 1,
+    bg: 'inherit',
+  },
+});
+
+const columnSpacing = variant({
+  prop: 'spacing',
+  base: {
+    px: { _: 16, xs: 0 },
+    '&:first-of-type': {
+      pl: 16,
+    },
+    '&:last-of-type': {
+      pr: 16,
+    },
+  },
+  variants: {
+    normal: {
+      py: { _: 0, xs: 16 },
+    },
+    condensed: {
+      py: { _: 0, xs: 8 },
+    },
+  },
+});
+
+export interface ColProps
+  extends StyleProps<typeof columnSizes>,
+    StyleProps<typeof columnType>,
+    StyleProps<typeof columnStates>,
+    StyleProps<typeof columnJustify> {}
+
+export const ColEl = styled.div<ColProps>(
+  css({
+    display: 'inline-flex',
+    alignItems: 'center',
+    whiteSpace: 'nowrap',
+    overflow: 'hidden',
+    position: 'relative',
+  }),
+  columnSpacing,
+  columnSizes,
+  columnType,
+  columnStates,
+  columnJustify
+);
