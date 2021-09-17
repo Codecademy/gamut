c856d4e4eeea61d3bd8468e341ee7eda1f5bb811
Amanda <abarrafato@gmail.com>
2021-09-17 15:25:05 -0400

    [CONPLAT-1121] ext org curriculum card

--- packages/gamut-labs/src/CurriculumCard/Subtitle/index.tsx
+++ packages/gamut-labs/src/CurriculumCard/Subtitle/index.tsx
@@ -3,35 +3,38 @@ import { capitalize } from 'lodash';
 import React from 'react';
 
 import { Difficulty, DifficultyProps } from '../Difficulty';
+import { pluralizeWithS } from './helpers';
 
 export type SubtitleProps = DifficultyProps & {
-  scope: string;
-  scopeCount?: number;
+  scope: Record<string, number>;
+  showAltSubtitle: boolean;
 };
-
-/* Quick and easy credit: https://stackoverflow.com/a/39835908 */
-const pluralizeWithS = (scope: string, scopeCount: number) =>
-  `${scope}${scopeCount !== 1 ? 's' : ''}`;
 
 export const Subtitle: React.FC<SubtitleProps> = ({
   difficulty,
   scope,
-  scopeCount,
+  showAltSubtitle,
 }) => {
+  const scopeToMap = Object.keys(scope);
+  const separatingChar = showAltSubtitle ? '|' : ',';
+
   return (
     <>
-      <Difficulty difficulty={difficulty} />
-      {scopeCount ? (
+      {!showAltSubtitle && (
         <>
-          ,
-          <Text ml={4} variant="p-small">
-            <b>{scopeCount}</b>{' '}
-            <Text textColor="gray-900">
-              {capitalize(pluralizeWithS(scope, scopeCount))}
-            </Text>
-          </Text>
+          <Difficulty difficulty={difficulty} />{' '}
         </>
-      ) : null}
+      )}
+      {scopeToMap.map((scopeType, index) =>
+        scope[scopeType] ? (
+          <Text ml={4} variant="p-small" key={`${scopeType}-count`}>
+            {index !== 0 && separatingChar} <b>{scope[scopeType]}</b>{' '}
+            <Text textColor="gray-900">
+              {capitalize(pluralizeWithS(scopeType, scope[scopeType]))}
+            </Text>{' '}
+          </Text>
+        ) : null
+      )}
     </>
   );
 };
