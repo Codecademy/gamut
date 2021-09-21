daf7833b7e261ab2a231dff9cdf63138e08d5132
cass spain <cass@codecademy.com>
2021-09-21 12:33:26 -0400

    isTextArea

--- packages/gamut/src/ConnectedForm/Inputs/types.tsx
+++ packages/gamut/src/ConnectedForm/Inputs/types.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { RegisterOptions } from 'react-hook-form';
 
-import { InputProps, SelectProps } from '../../Form';
-import { ConnectedInput, ConnectedSelect } from '.';
+import { InputProps, SelectProps, TextAreaProps } from '../../Form';
+import { ConnectedInput, ConnectedSelect, ConnectedTextArea } from '.';
 
 export interface BaseConnectedFieldProps {
   onUpdate?: (value: boolean) => void;
@@ -12,39 +12,61 @@ export interface BaseConnectedFieldProps {
 export interface ConnectedFieldProps extends BaseConnectedFieldProps {
   name: string;
 }
+export type ConnectedBaseInputProps = Omit<InputProps, 'defaultValue' | 'name'>;
 
 export type ConnectedBaseSelectProps = Omit<
   SelectProps,
   'defaultValue' | 'name'
 >;
 
-export type ConnectedBaseInputProps = Omit<InputProps, 'defaultValue' | 'name'>;
+export type ConnectedBaseTextAreaProps = Omit<
+  TextAreaProps,
+  'defaultValue' | 'name'
+>;
 
 export type ConnectedInputProps = ConnectedBaseInputProps & ConnectedFieldProps;
 
 export type ConnectedSelectProps = ConnectedBaseSelectProps &
   ConnectedFieldProps;
 
-export type SelectField = BaseConnectedFieldProps &
-  ConnectedBaseSelectProps & {
-    component: typeof ConnectedSelect;
-  };
+export type ConnectedTextAreaProps = ConnectedBaseTextAreaProps &
+  ConnectedFieldProps;
 
 export type InputField = BaseConnectedFieldProps &
   ConnectedBaseInputProps & {
     component: typeof ConnectedInput;
   };
 
-export type ConnectedField = SelectField | InputField;
+export type SelectField = BaseConnectedFieldProps &
+  ConnectedBaseSelectProps & {
+    component: typeof ConnectedSelect;
+  };
+
+export type TextAreaField = BaseConnectedFieldProps &
+  ConnectedBaseTextAreaProps & {
+    component: typeof ConnectedTextArea;
+  };
+
+export type ConnectedField = SelectField | InputField | TextAreaField;
+
 const isSelect = (field: ConnectedField): field is SelectField => {
   return true;
 };
 
-export const renderComponent = (field: ConnectedField, name: string) => {
+const isTextArea = (field: ConnectedField): field is TextAreaField => {
+  return true;
+};
+
+export const renderField = (field: ConnectedField, name: string) => {
   if (isSelect(field)) {
     const { component: Component, ...rest } = field;
     return <Component name={name} {...rest} />;
   }
+
+  if (isTextArea(field)) {
+    const { component: Component, ...rest } = field;
+    return <Component name={name} {...rest} />;
+  }
   const { component: Component, ...rest } = field;
-  return <Component name="not hey" {...rest} />;
+  return <Component name={name} {...rest} />;
 };
