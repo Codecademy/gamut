fde4dd0b7a028666a9f42ac3744d228f3ac74868
Amy Resnik <aresnik@codecademy.com>
2021-09-24 15:01:15 -0400

    feat: update landing page `CTA` to show fill button or cta button
    Updates `CTA` to take a `buttonType` prop to determine whether to show a `CTAButton` (default) or a regular `FillButton`. Also refactors `PagePrefooter` to use newer gamut components.

--- packages/gamut-labs/src/LandingPage/CTA.tsx
+++ packages/gamut-labs/src/LandingPage/CTA.tsx
@@ -1,24 +1,20 @@
-import { Box, CTAButton } from '@codecademy/gamut';
+import { CTAButton, FillButton } from '@codecademy/gamut';
 import React from 'react';
 
 import { DarkModeProps } from './types';
 
 export type CTAProps = DarkModeProps & {
   href: string;
-  className?: string;
   onCtaButtonClick?: React.MouseEventHandler;
+  buttonType?: 'cta' | 'fill';
 };
 
 export const CTA: React.FC<CTAProps> = ({
-  href,
-  mode,
-  className,
-  onCtaButtonClick,
-  children,
-}) => (
-  <Box mt={32} className={className}>
-    <CTAButton href={href} onClick={onCtaButtonClick} mode={mode}>
-      {children}
-    </CTAButton>
-  </Box>
-);
+  buttonType = 'cta',
+  ...buttonProps
+}) =>
+  buttonType === 'fill' ? (
+    <FillButton {...buttonProps} />
+  ) : (
+    <CTAButton {...buttonProps} />
+  );
--- packages/gamut-labs/src/LandingPage/PagePrefooter.tsx
+++ packages/gamut-labs/src/LandingPage/PagePrefooter.tsx
@@ -1,29 +1,10 @@
-import { Container } from '@codecademy/gamut';
-import { mediaQueries } from '@codecademy/gamut-styles';
-import styled from '@emotion/styled';
+import { Box, FlexBox } from '@codecademy/gamut';
 import React from 'react';
 
 import { CTA } from './CTA';
 import { Description } from './Description';
 import { Title } from './Title';
 import { BaseProps, DarkModeProps } from './types';
-
-const FlexContainer = styled(Container)`
-  ${mediaQueries.sm} {
-    flex-direction: row;
-    align-items: center;
-  }
-`;
-
-const FlexContent = styled.div`
-  flex: 1;
-`;
-
-const StyledCTA = styled(CTA)`
-  ${mediaQueries.sm} {
-    margin: 0 0 0 2rem;
-  }
-`;
 
 export const PagePrefooter: React.FC<BaseProps & DarkModeProps> = ({
   title,
@@ -39,15 +20,27 @@ export const PagePrefooter: React.FC<BaseProps & DarkModeProps> = ({
   );
 
   return cta ? (
-    <FlexContainer data-testid={testId} nowrap column justify="spaceBetween">
-      <FlexContent>
+    <FlexBox
+      data-testid={testId}
+      flexDirection={{ _: 'column', sm: 'row' }}
+      justifyContent="space-between"
+      alignItems={{ sm: 'center' }}
+    >
+      <Box flex={1}>
         {SectionTitle}
         {Desc}
-      </FlexContent>
-      <StyledCTA href={cta.href} onCtaButtonClick={cta.onClick} mode={mode}>
-        {cta.text}
-      </StyledCTA>
-    </FlexContainer>
+      </Box>
+      <Box mt={{ _: 32, sm: 0 }} ml={{ sm: 32 }}>
+        <CTA
+          href={cta.href}
+          onCtaButtonClick={cta.onClick}
+          mode={mode}
+          buttonType={cta.buttonType}
+        >
+          {cta.text}
+        </CTA>
+      </Box>
+    </FlexBox>
   ) : (
     <div data-testid={testId}>
       {SectionTitle}
