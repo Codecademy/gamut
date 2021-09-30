bc71dbd76330f31557ac1bca083d1c9ae561866d
Josh Goldberg <josh@codecademy.com>
2021-09-30 17:53:20 -0400

    timingValues

--- packages/gamut-styles/src/variables/timing.ts
+++ packages/gamut-styles/src/variables/timing.ts
@@ -1,11 +1,11 @@
-export const timing = {
-  fast: '150ms',
-  medium: `${timingValues.medium}ms`,
-  slow: `${timingValues.slow}ms`,
-};
-
 export const timingValues = {
   fast: 150,
   medium: 200,
   slow: 350,
+};
+
+export const timing = {
+  fast: `${timingValues.fast}ms`,
+  medium: `${timingValues.medium}ms`,
+  slow: `${timingValues.slow}ms`,
 };
