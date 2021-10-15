f8d2e9f4fcbf52437680715501b90d0b19bfeba2
cass spain <cass@codecademy.com>
2021-10-15 12:17:15 -0400

    now thats what we call generic

--- packages/gamut/src/Form/types.ts
+++ packages/gamut/src/Form/types.ts
@@ -1,5 +1,5 @@
 export type FormValues<T> = {
-  [key in keyof T]?: boolean | string | Pick<FileList, 'item'>;
+  [key in keyof T]?: boolean | string | Pick<FileList, 'item'> | T[key];
 };
 
 export { CheckboxPaddingProps } from './Inputs';
