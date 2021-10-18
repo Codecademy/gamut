85f2b640d68f3ea9cecd7d57e944b9f63cbc6306
cass spain <cass@codecademy.com>
2021-10-18 16:46:46 -0400

    gamut-tests :)

--- packages/gamut/src/Form/__tests__/Checkbox-test.tsx
+++ packages/gamut/src/Form/__tests__/Checkbox-test.tsx
@@ -1,4 +1,5 @@
 import { setupEnzyme } from '@codecademy/gamut-tests';
+import React from 'react';
 
 import { Checkbox } from '../Checkbox';
 
@@ -17,5 +18,32 @@ describe('<Checkbox>', () => {
     expect(wrapper.find('input[type="checkbox"]').prop('checked')).toEqual(
       true
     );
+  });
+
+  it('calls the onChange callback when the input changes', () => {
+    const onChange = jest.fn();
+
+    const { wrapper } = renderWrapper({ onChange });
+
+    wrapper.find('input[type="checkbox"]').simulate('change');
+
+    expect(onChange).toHaveBeenCalled();
+  });
+
+  it('accepts JSX in the label', () => {
+    const { wrapper } = renderWrapper({
+      label: <img alt="my cat" src="cat.jpg" />,
+    });
+
+    expect(wrapper.find('img').length).toBe(1);
+  });
+
+  it.only('accepts an aria-label', () => {
+    const { wrapper } = renderWrapper({
+      'aria-label': 'i am a labeled checkbox',
+    });
+    expect(
+      wrapper.find('input[aria-label="i am a labeled checkbox"]').length
+    ).toBe(1);
   });
 });
