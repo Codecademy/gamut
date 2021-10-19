ef32fbc31036efe92bc41db6a0e18ccf07f3a4d2
cassie spain <58054751+dreamwasp@users.noreply.github.com>
2021-10-19 15:12:20 -0400

    fix(Checkbox): adds aria-label prop
    Adds an explicit `aria-label` prop.  Also updates tests.
    
    Would like to type guard this in the future, but will hold off until converting GridForm inputs to ConnectedForms
     
    monolith alpha branch [here](https://github.com/codecademy-engineering/Codecademy/pull/27756)

--- packages/gamut/src/Form/__tests__/Checkbox-test.tsx
+++ packages/gamut/src/Form/__tests__/Checkbox-test.tsx
@@ -1,53 +1,49 @@
-import { mount } from 'enzyme';
+import { setupEnzyme } from '@codecademy/gamut-tests';
 import React from 'react';
 
 import { Checkbox } from '../Checkbox';
 
+const onChange = jest.fn();
+
+const renderWrapper = setupEnzyme(Checkbox, {
+  htmlFor: 'some-label',
+  label: 'Some label',
+  onChange,
+});
+
 describe('<Checkbox>', () => {
-  const defaultProps = {
-    htmlFor: 'some-label',
-    label: 'Some label',
-  };
-
   it('sets the input checked state when the prop is passed', () => {
-    const onChange = jest.fn();
+    const { wrapper } = renderWrapper({ checked: true });
 
-    const wrapper = mount(
-      <Checkbox {...defaultProps} checked onChange={onChange} />
-    );
     expect(wrapper.find('input[type="checkbox"]').prop('checked')).toEqual(
       true
     );
   });
 
   it('calls the onChange callback when the input changes', () => {
-    const onChangeCallback = jest.fn();
+    const onChange = jest.fn();
 
-    const wrapper = mount(
-      <Checkbox
-        {...defaultProps}
-        onChange={onChangeCallback}
-        value="a"
-        checked={false}
-      />
-    );
-    wrapper.find('input[type="checkbox"]').simulate('change', {
-      target: {
-        value: 'a',
-      },
-    });
-    const firstArgument = onChangeCallback.mock.calls[0][0];
-    expect(firstArgument.target.value).toBe('a');
+    const { wrapper } = renderWrapper({ onChange });
+
+    wrapper.find('input[type="checkbox"]').simulate('change');
+
+    expect(onChange).toHaveBeenCalled();
   });
 
   it('accepts JSX in the label', () => {
-    const wrapper = mount(
-      <Checkbox
-        {...defaultProps}
-        label={<img alt="my cat" src="cat.jpg" />}
-        checked
-      />
-    );
+    const { wrapper } = renderWrapper({
+      label: <img alt="my cat" src="cat.jpg" />,
+    });
+
     expect(wrapper.find('img').length).toBe(1);
+  });
+
+  it('accepts an aria-label', () => {
+    const { wrapper } = renderWrapper({
+      'aria-label': 'i am a labeled checkbox',
+    });
+    expect(
+      wrapper.find('input[aria-label="i am a labeled checkbox"]').length
+    ).toBe(1);
   });
 });
