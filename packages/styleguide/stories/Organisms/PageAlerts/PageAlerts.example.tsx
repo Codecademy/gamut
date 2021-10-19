352c4e85c21a726da9d5a56b241393fa222379cc
Jasmine English <jasmine@codecademy.com>
2021-10-19 12:47:31 -0400

    cleaned up the story a bit

--- packages/styleguide/stories/Organisms/PageAlerts/PageAlerts.example.tsx
+++ packages/styleguide/stories/Organisms/PageAlerts/PageAlerts.example.tsx
@@ -19,46 +19,36 @@ export const PageAlertsExample: React.FC = () => (
 const Component: React.FC = () => {
   const { addAlert } = useContext(PageAlertsContext);
 
-  const alerts: Record<string, PageAlert> = {
-    success: {
+  const alerts: PageAlert[] = [
+    {
       type: 'success',
-      message: `Random message: ${Math.random()}`,
+      message: `This alert can appear multiple times in a row because the message is different each time: ${Math.random()}`,
     },
-    error: {
+    {
       type: 'error',
-      message: 'Error! The thing did not happen!',
+      message: 'Error!',
     },
-    general: {
+    {
       type: 'general',
       message: 'General alert!',
     },
-    notice: {
+    {
       type: 'notice',
       message: 'Notice!',
     },
-    feature: {
+    {
       type: 'feature',
       message: 'Feature alert!',
     },
-  };
+  ];
 
   return (
     <GridBox width={300} rowGap={16}>
-      <FillButton onClick={() => addAlert(alerts.success)}>
-        Trigger random success alert
-      </FillButton>
-      <FillButton onClick={() => addAlert(alerts.error)}>
-        Trigger error alert
-      </FillButton>
-      <FillButton onClick={() => addAlert(alerts.general)}>
-        Trigger general alert
-      </FillButton>
-      <FillButton onClick={() => addAlert(alerts.notice)}>
-        Trigger notice alert
-      </FillButton>
-      <FillButton onClick={() => addAlert(alerts.feature)}>
-        Trigger feature alert
-      </FillButton>
+      {alerts.map((alert) => (
+        <FillButton key={alert.type} onClick={() => addAlert(alert)}>
+          {`Trigger ${alert.type} alert`}
+        </FillButton>
+      ))}
     </GridBox>
   );
 };
