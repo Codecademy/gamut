cbaf9aa9a1a02ed046151f47f20bd0c937929907
Josh Goldberg <josh@codecademy.com>
2021-11-01 15:23:50 -0400

    fix(Drawer): add missing max-width: 100%

--- packages/gamut-labs/src/Drawer/index.tsx
+++ packages/gamut-labs/src/Drawer/index.tsx
@@ -36,7 +36,13 @@ export const Drawer: React.FC<DrawerProps> = ({
           transition={{ duration: timingValues.slow / 1000 }}
           {...props}
         >
-          <Box height="100%" left="0" position="absolute" minWidth={fullWidth}>
+          <Box
+            height="100%"
+            left="0"
+            position="absolute"
+            maxWidth="100%"
+            minWidth={fullWidth}
+          >
             {children}
           </Box>
         </DrawerBase>
