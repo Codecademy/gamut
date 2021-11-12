a49b5edff19dadf6e700b925bb9c6c450ccc3608
cass spain <cass@codecademy.com>
2021-11-12 17:10:54 -0500

    dark mode + menu examples

--- packages/styleguide/stories/Molecules/Menu/Menu.examples.tsx
+++ packages/styleguide/stories/Molecules/Menu/Menu.examples.tsx
@@ -6,31 +6,93 @@ import {
   PopoverContainer,
   PopoverContainerProps,
 } from '@codecademy/gamut';
-import React, { useRef } from 'react';
+import { MultipleUsersIcon } from '@codecademy/gamut-icons';
+import { Background } from '@codecademy/gamut-styles';
+import React, { useRef, useState } from 'react';
 
-export const PopoverMenuExample: React.FC<PopoverContainerProps> = (args) => {
+export const PopoverMenuExample: React.FC<PopoverContainerProps> = () => {
   const target = useRef<HTMLDivElement>(null);
+  const [isOpen, setIsOpen] = useState(false);
+  const [currentTarget, setCurrentTarget] = useState(0);
+  const [activeIndex, setActiveIndex] = useState(4);
+
+  const clickHandler = (targetNumber: number, currentIndex: number) => {
+    setCurrentTarget(targetNumber);
+    // eslint-disable-next-line no-unused-expressions
+    isOpen && currentIndex === activeIndex ? setIsOpen(false) : setIsOpen(true);
+    if (currentIndex !== activeIndex) setActiveIndex(currentIndex);
+  };
 
   return (
-    <FlexBox minHeight="480px" width={1} position="relative">
-      <FlexBox center flex={1}>
-        <PopoverContainer isOpen inline targetRef={target} {...args}>
-          <Menu variant="action">
-            <MenuItem>Item 1</MenuItem>
-            <MenuSeparator />
-            <MenuItem href="cool">Link 1</MenuItem>
-            <MenuSeparator />
-          </Menu>
-        </PopoverContainer>
-        <FlexBox
-          justifyContent="center"
-          alignItems="center"
-          dimensions="200px"
-          border={1}
-          ref={target}
-        >
-          This balanced breakfast
-        </FlexBox>
+    <FlexBox minHeight="30px" width={1} position="relative" m={48}>
+      <FlexBox flex={1}>
+        <Background bg="black" height="100%" pl={48} borderRadius="10px">
+          <FlexBox
+            bg="navy"
+            justifyContent="right"
+            alignItems="center"
+            dimensions="200px"
+            height="100%"
+            ref={target}
+          >
+            <Menu border="none" variant="navigation">
+              <MenuItem
+                active={activeIndex === 0}
+                onClick={() => clickHandler(-70, 0)}
+              >
+                i have a side menu
+              </MenuItem>
+              <MenuItem
+                active={activeIndex === 1}
+                onClick={() => clickHandler(-118, 1)}
+              >
+                Active Item
+              </MenuItem>
+              <MenuSeparator />
+              <MenuItem
+                active={activeIndex === 2}
+                onClick={() => clickHandler(-166, 2)}
+                icon={MultipleUsersIcon}
+              >
+                Icon Item
+              </MenuItem>
+              <MenuItem
+                active={activeIndex === 3}
+                onClick={() => clickHandler(-212, 3)}
+              >
+                Menu Item
+              </MenuItem>
+            </Menu>
+          </FlexBox>
+          <PopoverContainer
+            isOpen={isOpen}
+            inline
+            alignment="top-right"
+            x={-21}
+            y={currentTarget}
+            targetRef={target}
+          >
+            <Background bg="white" borderRadius="10px">
+              <Menu variant="select">
+                {activeIndex % 2 === 0 ? (
+                  <>
+                    <MenuItem>i am a side menu!</MenuItem>
+                    <MenuSeparator />
+                    <MenuItem href="cool">cool link</MenuItem>
+                  </>
+                ) : (
+                  <>
+                    <MenuItem>i am a DIFFERENT menu!</MenuItem>
+                    <MenuSeparator />
+                    <MenuItem href="cool-too"> another cool link</MenuItem>
+                    <MenuSeparator />
+                    <MenuItem>another item</MenuItem>
+                  </>
+                )}
+              </Menu>
+            </Background>
+          </PopoverContainer>
+        </Background>
       </FlexBox>
     </FlexBox>
   );
