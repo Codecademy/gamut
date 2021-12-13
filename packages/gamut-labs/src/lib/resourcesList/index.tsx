e1983c65f156ff9ebd9ddb4429297ebde9d91972
Alexander Onate <aionate0812@gmail.com>
2021-12-13 11:59:05 -0500

    updated styled badge with default

--- packages/gamut-labs/src/lib/resourcesList/index.tsx
+++ packages/gamut-labs/src/lib/resourcesList/index.tsx
@@ -1,5 +1,4 @@
 import { Badge } from '@codecademy/gamut';
-import styled from '@emotion/styled';
 import React from 'react';
 
 import { AppHeaderLinkItem } from '../../AppHeader/AppHeaderElements/types';
@@ -18,28 +17,10 @@ type FooterResourceList = {
   newTab?: boolean;
 };
 
-export const StyledBadge = styled(Badge)`
-  font-size: 10px;
-  padding-top: 1px;
-  padding-bottom: 10px;
-  padding-left: 8px;
-  padding-right: 8px;
-  border-radius: 16px;
-`;
-
 export const renderBadge = (text: string) => (
-  <StyledBadge
-    round
-    variant="blue"
-    fontFamily="accent"
-    ml={8}
-    pl={8}
-    pr={8}
-    height={16}
-    alignSelf="center"
-  >
+  <Badge size="sm" ml={8} alignSelf="center">
     {text}
-  </StyledBadge>
+  </Badge>
 );
 
 export const resourcesList: ResourcesList[] = [
