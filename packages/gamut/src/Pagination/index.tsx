d3a84a9141763f83b9c6fba32f623f83253f39ee
cass spain <cass@codecademy.com>
2021-12-16 15:57:54 -0500

    add total buttons

--- packages/gamut/src/Pagination/index.tsx
+++ packages/gamut/src/Pagination/index.tsx
@@ -2,7 +2,7 @@ import {
   MiniChevronLeftIcon,
   MiniChevronRightIcon,
 } from '@codecademy/gamut-icons';
-import React from 'react';
+import React, { useMemo, useState } from 'react';
 
 import { FlexBox } from '../Box';
 import { PaginationButton } from './PaginationButton';
@@ -30,26 +30,59 @@ interface PaginationProps {
   totalPages: number;
 }
 
+// @to-do :
 /**
  * @todo - port ••• + « to change on hover (via contents)
  */
+/**
+ * @todo - chapterSize
+ */
 export const Pagination: React.FC<PaginationProps> = ({
+  defaultCurrent = 1,
   totalPages,
   variant = 'stroke',
   ...rest
 }) => {
+  const [currentPage, setCurrentPage] = useState(defaultCurrent);
+
+  const pageCount = useMemo(
+    () => Array.from(Array(totalPages), (_, index) => index + 1),
+    [totalPages]
+  );
+
   return (
-    <FlexBox alignContent="center">
-      <PaginationButton icon={MiniChevronLeftIcon} />
-      <PaginationButton selected>•••</PaginationButton>
-      <PaginationButton selected> « </PaginationButton>
-      <PaginationButton selected>1</PaginationButton>
-      <PaginationButton>2</PaginationButton> Pagination
-      <PaginationButton variant="text">1</PaginationButton>
-      <PaginationButton variant="text">2</PaginationButton>
-      <PaginationButton variant="text">»</PaginationButton>
-      <PaginationButton variant="text">•••</PaginationButton>
-      <PaginationButton variant="text" icon={MiniChevronRightIcon} />
-    </FlexBox>
+    <>
+      <FlexBox alignContent="center">
+        <PaginationButton icon={MiniChevronLeftIcon} />
+        <PaginationButton showMore="forward" />
+        <PaginationButton selected>•••</PaginationButton>
+        <PaginationButton selected> « </PaginationButton>
+        <PaginationButton selected>1</PaginationButton>
+        <PaginationButton>2</PaginationButton> Pagination
+        <PaginationButton variant="text">1</PaginationButton>
+        <PaginationButton variant="text">2</PaginationButton>
+        <PaginationButton variant="text">»</PaginationButton>
+        <PaginationButton variant="text">•••</PaginationButton>
+        <PaginationButton variant="text" icon={MiniChevronRightIcon} />
+      </FlexBox>
+
+      <FlexBox alignContent="center">
+        {currentPage !== 1 && (
+          <PaginationButton variant={variant} icon={MiniChevronLeftIcon} />
+        )}
+        {pageCount.map((elem) => (
+          <PaginationButton
+            variant={variant}
+            selected={elem === currentPage}
+            onClick={() => setCurrentPage(elem)}
+          >
+            {elem}
+          </PaginationButton>
+        ))}
+        {currentPage !== totalPages && (
+          <PaginationButton variant={variant} icon={MiniChevronRightIcon} />
+        )}
+      </FlexBox>
+    </>
   );
 };
