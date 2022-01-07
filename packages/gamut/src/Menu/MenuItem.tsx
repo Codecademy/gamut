ff5fcc80e18d6d0dc0ab0acf6bada8a25c241a8e
Matt Schiller <mschiller@codecademy.com>
2022-01-07 17:16:29 -0500

    now, with new window

--- packages/gamut/src/Menu/MenuItem.tsx
+++ packages/gamut/src/Menu/MenuItem.tsx
@@ -17,49 +17,60 @@ export const MenuItem = React.forwardRef<
     'variant' | 'selected' | 'active-navlink'
   > & {
     href?: string;
+    openLinkInNewWindow?: boolean;
     icon?: React.ComponentType<GamutIconProps>;
   }
->(({ href, children, active, icon: Icon, ...props }, ref) => {
-  const { variant, ...rest } = useMenuContext();
-  const activeProp = activePropnames[variant];
-  const computed = {
-    ...props,
-    ...rest,
-    variant: variant === 'select' ? 'select' : 'link',
-    role: 'menuitem',
-    [activeProp]: active,
-  } as ListItemProps;
+>(
+  (
+    { href, openLinkInNewWindow, children, active, icon: Icon, ...props },
+    ref
+  ) => {
+    const { variant, ...rest } = useMenuContext();
+    const activeProp = activePropnames[variant];
+    const computed = {
+      ...props,
+      ...rest,
+      variant: variant === 'select' ? 'select' : 'link',
+      role: 'menuitem',
+      [activeProp]: active,
+    } as ListItemProps;
 
-  const content = (
-    <>
-      {Icon && (
-        <Icon
-          size={rest.spacing === 'condensed' ? 16 : 24}
-          mr={12}
-          data-testid="menuitem-icon"
-        />
-      )}
-      {children}
-    </>
-  );
+    const content = (
+      <>
+        {Icon && (
+          <Icon
+            size={rest.spacing === 'condensed' ? 16 : 24}
+            mr={12}
+            data-testid="menuitem-icon"
+          />
+        )}
+        {children}
+      </>
+    );
 
-  if (href) {
-    const linkRef = ref as MutableRefObject<HTMLAnchorElement>;
+    if (href) {
+      const linkRef = ref as MutableRefObject<HTMLAnchorElement>;
+
+      return (
+        <ListItem role="none">
+          <ListLink
+            {...(computed as ListLinkProps)}
+            href={href}
+            ref={linkRef}
+            target={openLinkInNewWindow ? '_blank' : undefined}
+          >
+            {content}
+          </ListLink>
+        </ListItem>
+      );
+    }
+
+    const liRef = ref as MutableRefObject<HTMLLIElement>;
 
     return (
-      <ListItem role="none">
-        <ListLink {...(computed as ListLinkProps)} href={href} ref={linkRef}>
-          {content}
-        </ListLink>
+      <ListItem {...computed} ref={liRef}>
+        {content}
       </ListItem>
     );
   }
-
-  const liRef = ref as MutableRefObject<HTMLLIElement>;
-
-  return (
-    <ListItem {...computed} ref={liRef}>
-      {content}
-    </ListItem>
-  );
-});
+);
