5ddb1b76242aab1e635e4ebd1713a856d662098d
cass spain <cass@codecademy.com>
2022-02-02 12:56:19 -0500

    fix pagination-test

--- packages/gamut/src/Pagination/__tests__/Pagination-test.tsx
+++ packages/gamut/src/Pagination/__tests__/Pagination-test.tsx
@@ -64,7 +64,7 @@ describe('Pagination', () => {
   });
 
   it('correctly renders number of pages according to chapterSize', () => {
-    const { view } = renderView({ type: 'ellipsis', chapterSize: 3 });
+    const { view } = renderView({ chapterSize: 3 });
 
     expect(getPage({ view, pageNumber: 1 }));
     expect(getPage({ view, pageNumber: 3 }));
@@ -164,19 +164,19 @@ describe('Pagination', () => {
     });
 
     it('renders a jump forward and back button when on the middle pages', () => {
-      const { view } = renderView({ type: 'ellipsis', defaultCurrent: 3 });
+      const { view } = renderView({ defaultCurrent: 3 });
 
       expect(getJumpButtonCount({ view })).toBe(2);
     });
 
     it('does not render a jump forward button when on the last page', () => {
-      const { view } = renderView({ type: 'ellipsis', defaultCurrent: 15 });
+      const { view } = renderView({ defaultCurrent: 15 });
 
       expect(view.queryByRole('button', { name: 'Page 1' })).toBe(null);
     });
 
     it('advances chapterSize forward on jump forward button click', () => {
-      const { view } = renderView({ type: 'ellipsis' });
+      const { view } = renderView({});
 
       const page1 = getPage({ view, pageNumber: 1 });
       expect(page1).toHaveAttribute('aria-current', 'page');
@@ -191,7 +191,7 @@ describe('Pagination', () => {
     });
 
     it('advances chapterSize back on jump back ellipsis button click', () => {
-      const { view } = renderView({ type: 'ellipsis', defaultCurrent: 15 });
+      const { view } = renderView({ defaultCurrent: 15 });
 
       const page15 = view.getByRole('button', { name: `Last Page, Page 15` });
 
@@ -207,7 +207,7 @@ describe('Pagination', () => {
     });
 
     it('calls onChange with page number whenever jump buttons are clicked', () => {
-      const { view } = renderView({ type: 'ellipsis', defaultCurrent: 10 });
+      const { view } = renderView({ defaultCurrent: 10 });
 
       const backButton = getJumpButton({ view, pageNumber: 5 });
 
