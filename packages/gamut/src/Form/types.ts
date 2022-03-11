3a5b8a2b2c92ac103d8efb16014f3ecb2ceb17fb
Amanda Barrafato <abarrafato@codecademy.com>
2022-03-11 16:01:14 -0500

    Additional form value type

--- packages/gamut/src/Form/types.ts
+++ packages/gamut/src/Form/types.ts
@@ -1,4 +1,4 @@
 export type FormValues<T> = {
-  [key in keyof T]?: boolean | string | Pick<FileList, 'item'>;
+  [key in keyof T]?: boolean | string | Pick<FileList, 'item'> | T[key];
 };
 export { CheckboxPaddingProps } from './Checkbox';
