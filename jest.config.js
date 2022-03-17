9fe90f8399a3ab55e2c2217291b17886b4d96635
cass spain <cass@codecademy.com>
2022-03-17 11:22:11 -0400

    revert jest config

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,3 @@
 module.exports = {
-  projects: [
-    '<rootDir>/packages/eslint-plugin-gamut',
-    '<rootDir>/packages/gamut',
-    '<rootDir>/packages/gamut-icons',
-    '<rootDir>/packages/gamut-illustrations',
-    '<rootDir>/packages/gamut-kit',
-    '<rootDir>/packages/gamut-labs',
-    '<rootDir>/packages/gamut-patterns',
-    '<rootDir>/packages/gamut-styles',
-    '<rootDir>/packages/gamut-tests',
-    '<rootDir>/packages/macros',
-    '<rootDir>/packages/markdown-overrides',
-    '<rootDir>/packages/monaco',
-    '<rootDir>/packages/styleguide',
-    '<rootDir>/packages/variance',
-  ],
+  projects: ['<rootDir>/packages/*'],
 };
