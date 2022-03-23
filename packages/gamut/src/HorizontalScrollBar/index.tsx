bc1d3d5762640e7b8234e4f8fadc76221309e3fc
Colin Tatro <ctatro@codecademy.com>
2022-03-23 17:07:12 -0400

    dynamically setting refs via element cloning

--- packages/gamut/src/HorizontalScrollBar/index.tsx
+++ packages/gamut/src/HorizontalScrollBar/index.tsx
@@ -1,20 +1,39 @@
-import React, { Children, useEffect, useRef } from 'react';
+import React, { Children, cloneElement, ReactNode, useRef } from 'react';
 
 import { FlexBox } from '../Box';
 
 export const HorizontalScrollBar: React.FC = ({ children }) => {
   // HOW to get all elements into a single observer
 
-  const elementsRef = useRef<HTMLBaseElement[]>([]);
+  const elementsRef = useRef<ReactNode[]>([]);
 
-  useEffect(() => {
-    if (!elementsRef.current.length) {
-      Children.map(
-        children,
-        (child, index) => (elementsRef.current[index] = child)
-      );
-    }
-  }, [elementsRef]);
+  // useEffect(() => {
+  //   if (!elementsRef.current.length) {
+  //     Children.map<ReactNode, ReactNode>(
+  //       children,
+  //       (child, index) =>{
+  //         elementsRef.current[index] = child
 
-  return <FlexBox>{children}</FlexBox>;
+  //         console.log(elementsRef.current[index])
+  //         return null
+  //       }
+  //     );
+  //   }
+  // }, [elementsRef, children]);
+
+  console.log(elementsRef);
+
+  return (
+    <FlexBox>
+      {/* this needs some work  */}
+      {Children.map<any, any>(children, (child, index) => {
+        // elementsRef.current[index] = child
+
+        // console.log(elementsRef.current[index])
+        return cloneElement(child, {
+          ref: (element: ReactNode) => (elementsRef.current[index] = element),
+        });
+      })}
+    </FlexBox>
+  );
 };
