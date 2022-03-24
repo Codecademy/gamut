84f4e601df1f843a11c3380060234c57aa90052e
Hailey <hailey@codecademy.com>
2022-03-24 14:46:36 -0400

    new higher ord component and useMemo

--- packages/gamut/src/HorizontalScrollBar/index.tsx
+++ packages/gamut/src/HorizontalScrollBar/index.tsx
@@ -4,20 +4,44 @@ import React, {
   ReactElement,
   ReactNode,
   useEffect,
+  useMemo,
   useRef,
   useState,
 } from 'react';
 
 import { FlexBox } from '../Box';
 
+type VisibilityControllerProps = {
+  visible: boolean;
+};
+
+export const VisibilityController: React.FC<VisibilityControllerProps> = ({
+  visible,
+  children,
+}) => {
+  console.log(visible);
+  return <>{children}</>;
+};
+
 export const HorizontalScrollBar: React.FC = ({ children }) => {
   // HOW to get all elements into a single observer
 
   const elementsRef = useRef<ReactNode[]>([]);
-  const intersectionObserver = new IntersectionObserver(
-    (entries: IntersectionObserverEntry[]) => {
-      entries.forEach((entry) => console.log(entry));
-    }
+  const [visibilityStates, setVisibilityStates] = useState<boolean[]>([]);
+
+  const intersectionObserver = useMemo(
+    () =>
+      new IntersectionObserver((entries: IntersectionObserverEntry[]) => {
+        entries.forEach((entry, index) => {
+          const newVisibilityStates = [...visibilityStates];
+
+          if (visibilityStates[index] !== entry.isIntersecting) {
+            newVisibilityStates[index] = entry.isIntersecting;
+            setVisibilityStates(newVisibilityStates);
+          }
+        });
+      }),
+    [visibilityStates]
   );
 
   useEffect(() => {
@@ -28,20 +52,23 @@ export const HorizontalScrollBar: React.FC = ({ children }) => {
     }
   }, [elementsRef, intersectionObserver]);
 
-  const [visibilityStates, setVisibilityStates] = useState([]);
+  const stuff = Children.map<ReactNode, ReactElement>(
+    children,
+    (child, index) => {
+      // elementsRef.current[index] = child
 
-  return (
-    <FlexBox>
-      {/* this needs some work  */}
-      {Children.map<ReactNode, ReactElement>(children, (child, index) => {
-        // elementsRef.current[index] = child
-
-        // console.log(elementsRef.current[index])
-        return cloneElement(child, {
-          'aria-hidden': false,
-          ref: (element: ReactNode) => (elementsRef.current[index] = element),
-        });
-      })}
-    </FlexBox>
+      // console.log(elementsRef.current[index])
+      return (
+        <VisibilityController visible={visibilityStates[index]}>
+          {cloneElement(child, {
+            ref: (element: ReactNode) => (elementsRef.current[index] = element),
+          })}
+        </VisibilityController>
+      );
+    }
   );
+
+  console.log(stuff);
+
+  return <FlexBox>{stuff}</FlexBox>;
 };
