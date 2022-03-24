60324f24d46958ae7d0c8877e219403403b317b2
cass spain <cass@codecademy.com>
2022-03-24 15:02:08 -0400

    refactor

---
+++ packages/gamut/src/Toggle/elements.tsx
@@ -0,0 +1,82 @@
+import {
+  screenReaderOnly,
+  states,
+  system,
+  theme,
+  variant,
+} from '@codecademy/gamut-styles';
+import { StyleProps } from '@codecademy/variance';
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+
+import { Box } from '../Box';
+
+export const sizes = {
+  medium: {
+    height: '30px',
+    width: '60px',
+  },
+  small: {
+    height: '18px',
+    width: '36px',
+  },
+} as const;
+
+export const ToggleTrack = styled(Box)(
+  variant({
+    base: {
+      transition: 'background-color 0.2s ease',
+
+      '&:after': {
+        content: '""',
+        transition: 'opacity 0.2s ease',
+        opacity: 0,
+        borderRadius: 'inherit',
+        position: 'absolute',
+        width: 'calc(100% + 8px)',
+        height: 'calc(100% + 8px)',
+        top: '-4px',
+        left: '-4px',
+        borderColor: 'inherit',
+        borderStyle: 'solid',
+        borderWidth: 2,
+      },
+      '&:focus-within': {
+        outline: `3px solid ${theme.colors.primary}`,
+        outlineOffset: '2px',
+      },
+    },
+    prop: 'size',
+    defaultVariant: 'medium',
+    variants: sizes,
+  })
+);
+
+export const Circle = styled(Box)(
+  css({
+    transition: 'all 0.5s cubic-bezier(0.23, 1, 0.32, 1)',
+  })
+);
+
+export const ToggleInput = styled.input(screenReaderOnly);
+
+const ToggleLabelStates = states({
+  disabled: { cursor: 'not-allowed', opacity: 0.5 },
+  labelRight: { flexDirection: 'row-reverse' },
+});
+
+export type ToggleStyleProps = StyleProps<typeof system.space>;
+
+export const ToggleLabel = styled.label<
+  StyleProps<typeof ToggleLabelStates> & ToggleStyleProps
+>(
+  css({
+    alignItems: 'center',
+    border: 'none',
+    cursor: 'pointer',
+    display: 'flex',
+    width: 'fit-content',
+  }),
+  ToggleLabelStates,
+  system.space
+);
--- packages/gamut/src/Toggle/index.tsx
+++ packages/gamut/src/Toggle/index.tsx
@@ -1,20 +1,22 @@
-import { css, screenReaderOnly, states, theme } from '@codecademy/gamut-styles';
-import { StyleProps } from '@codecademy/variance';
-import styled from '@emotion/styled';
 import React from 'react';
 
-import { Box } from '../Box';
 import { Text } from '../Typography/Text';
+import {
+  Circle,
+  sizes,
+  ToggleInput,
+  ToggleLabel,
+  ToggleStyleProps,
+  ToggleTrack,
+} from './elements';
 
 export type ToggleSizes = keyof typeof sizes;
 
-export type LabelProps = {
-  disabled?: boolean;
-};
-
-export type ToggleProps = {
+export interface ToggleProps extends ToggleStyleProps {
   /** The state of the checkbox input (this can be out of sync with the input's value if not passed) */
   checked: boolean;
+  /** If the Toggle is disabled */
+  disabled?: boolean;
   /** Called when the input value has changed */
   onChange?: (event?: React.FormEvent<HTMLInputElement>) => void;
   /** A label used for accessibility and control, unique to the page */
@@ -23,89 +25,25 @@ export type ToggleProps = {
   labelSide?: 'left' | 'right';
   /** Changes the dimensions of the element for using the component outside of a form context */
   size?: ToggleSizes;
-  disabled?: boolean;
-  className?: string;
-};
-
-const sizes = {
-  medium: {
-    height: '30px',
-    width: '60px',
-  },
-  small: {
-    height: '18px',
-    width: '36px',
-  },
-};
-
-const ToggleTrack = styled(Box)(
-  css({
-    transition: 'background-color 0.2s ease',
-
-    '&:after': {
-      content: '""',
-      transition: 'opacity 0.2s ease',
-      opacity: 0,
-      borderRadius: 'inherit',
-      position: 'absolute',
-      width: 'calc(100% + 8px)',
-      height: 'calc(100% + 8px)',
-      top: '-4px',
-      left: '-4px',
-      borderColor: 'inherit',
-      borderStyle: 'solid',
-      borderWidth: 2,
-    },
-    '&:focus-within': {
-      outline: `3px solid ${theme.colors.primary}`,
-      outlineOffset: '2px',
-    },
-  })
-);
-
-const Circle = styled(Box)(
-  css({
-    transition: 'all 0.5s cubic-bezier(0.23, 1, 0.32, 1)',
-  })
-);
-
-const ToggleInput = styled.input(screenReaderOnly);
-
-const ToggleDisabledState = states({
-  disabled: { cursor: 'not-allowed', opacity: 0.5 },
-  labelRight: { flexDirection: 'row-reverse' },
-});
-
-const ToggleLabel = styled.label<StyleProps<typeof ToggleDisabledState>>(
-  css({
-    alignItems: 'center',
-    border: 'none',
-    cursor: 'pointer',
-    display: 'flex',
-    padding: 0,
-    width: 'fit-content',
-  }),
-  ToggleDisabledState
-);
+}
 
 export const Toggle: React.FC<ToggleProps> = ({
   checked,
-  className,
   onChange,
   label,
   labelSide = 'right',
   disabled,
   size = 'medium',
+  ...rest
 }) => {
   const checkedColor = checked ? 'primary' : 'navy-600';
-  const sizeStyles = sizes[size];
 
   return (
     <ToggleLabel
-      className={className}
       htmlFor={label}
       disabled={disabled}
       labelRight={labelSide === 'right'}
+      {...rest}
     >
       {label && (
         <Text
@@ -116,11 +54,11 @@ export const Toggle: React.FC<ToggleProps> = ({
         </Text>
       )}
       <ToggleTrack
-        {...sizeStyles}
-        borderColor="primary"
         bg={checkedColor}
+        borderColor="primary"
         borderRadius="99rem"
         position="relative"
+        size={size}
       >
         <ToggleInput
           type="checkbox"
