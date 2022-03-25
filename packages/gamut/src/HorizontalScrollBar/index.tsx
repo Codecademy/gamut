39aaf594bda0fb6c67aa9bf46a7bde390ee2d7cd
Colin Tatro <ctatro@codecademy.com>
2022-03-25 16:40:45 -0400

    button hiding logic implemented into entry call back

--- packages/gamut/src/HorizontalScrollBar/index.tsx
+++ packages/gamut/src/HorizontalScrollBar/index.tsx
@@ -1,7 +1,8 @@
+import styled from '@emotion/styled';
 import React, {
   Children,
   cloneElement,
-  forwardRef,
+  JSXElementConstructor,
   ReactElement,
   ReactNode,
   useEffect,
@@ -10,69 +11,86 @@ import React, {
   useState,
 } from 'react';
 
-import { FlexBox } from '../Box';
+import { Text } from '../Typography';
 
-type VisibilityControllerProps = {
-  visible?: boolean;
-};
-
-// export const VisibilityController: React.FC<VisibilityControllerProps> = ({
-//   visible,
-//   children,
-// }) => {
-//   const child = Children.toArray(children)[0];
-//   return <>{cloneElement(child, { 'aria-hidden': !visible })}</>;
-// };
-
-const VisibilityController = forwardRef<HTMLElement, VisibilityControllerProps>(
-  ({ visible, children }, ref) => {
-    const child = Children.toArray(children)[0];
-    return <>{cloneElement(child, { 'aria-hidden': visible })}</>;
-  }
-);
+const Container = styled.div`
+  height: 500px;
+  width: 700px;
+  overflow-x: scroll;
+  display: flex;
+  padding-top: 16px;
+`;
 
 export const HorizontalScrollBar: React.FC = ({ children }) => {
-  // HOW to get all elements into a single observer
-
   const elementsRef = useRef<ReactNode[]>([]);
-  const [visibilityStates, setVisibilityStates] = useState<boolean[]>([]);
+
+  const [showLeftButton, setShowLeftButton] = useState<boolean>(false);
+  const [showRightButton, setShowRightButton] = useState<boolean>(true);
 
   const intersectionObserver = useMemo(
     () =>
       new IntersectionObserver((entries: IntersectionObserverEntry[]) => {
         entries.forEach((entry, index) => {
-          const newVisibilityStates = [...visibilityStates];
+          const shouldToggleOnShowLeftButton =
+            index === 0 && !entry.isIntersecting && !showLeftButton;
+          const shouldToggleOffShowLeftButton =
+            index === 0 && entry.isIntersecting && showLeftButton;
+          const shouldToggleOnShowRightButton =
+            index === entries.length - 1 &&
+            !entry.isIntersecting &&
+            !showRightButton;
+          const shouldToggleOffShowRightButton =
+            index === entries.length - 1 &&
+            entry.isIntersecting &&
+            showRightButton;
 
-          if (visibilityStates[index] !== entry.isIntersecting) {
-            newVisibilityStates[index] = entry.isIntersecting;
-            setVisibilityStates(newVisibilityStates);
+          if (shouldToggleOnShowLeftButton) {
+            setShowLeftButton(true);
           }
+          if (shouldToggleOffShowLeftButton) {
+            setShowLeftButton(false);
+          }
+          if (shouldToggleOnShowRightButton) {
+            setShowRightButton(true);
+          }
+          if (shouldToggleOffShowRightButton) {
+            setShowRightButton(false);
+          }
+
+          entry.target.ariaHidden = entry.isIntersecting ? 'false' : 'true';
         });
       }),
-    [visibilityStates]
+    [showLeftButton, showRightButton]
   );
 
   useEffect(() => {
     if (elementsRef.current.length) {
-      elementsRef.current.forEach((entry) =>
+      elementsRef.current.forEach((entry: HTMLElement) =>
         intersectionObserver.observe(entry)
       );
     }
   }, [elementsRef, intersectionObserver]);
 
-  const scrollBarElements = Children.map<ReactNode, ReactElement>(
-    children,
-    (child, index) => {
-      return (
-        <VisibilityController
-          ref={(element: HTMLElement) => (elementsRef.current[index] = element)}
-          visible={!!visibilityStates[index]}
-        >
-          {child}
-        </VisibilityController>
-      );
-    }
+  return (
+    <>
+      <Text display={showLeftButton ? 'block ' : 'none'}>Left Button</Text>
+      <Text display={showRightButton ? 'block' : 'none'}>Right Button</Text>
+      <Container>
+        {Children.map(
+          children,
+          (
+            child: ReactElement<
+              { ref: (element: HTMLElement) => HTMLElement },
+              string | JSXElementConstructor<any>
+            >,
+            index
+          ) =>
+            cloneElement(child, {
+              ref: (element: HTMLElement) =>
+                (elementsRef.current[index] = element),
+            })
+        )}
+      </Container>
+    </>
   );
-
-  return <FlexBox>{scrollBarElements}</FlexBox>;
 };
