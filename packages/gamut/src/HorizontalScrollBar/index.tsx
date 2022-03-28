2a580919a870e355bde05a47823d79d36b6d64b4
Colin Tatro <ctatro@codecademy.com>
2022-03-28 16:50:33 -0400

    high level observer logic is working on button toggle states and aria label states

--- packages/gamut/src/HorizontalScrollBar/index.tsx
+++ packages/gamut/src/HorizontalScrollBar/index.tsx
@@ -6,12 +6,14 @@ import React, {
   ReactElement,
   ReactNode,
   RefObject,
+  useCallback,
   useEffect,
   useMemo,
   useRef,
   useState,
 } from 'react';
 
+import { Box, FillButton } from '..';
 import { Text } from '../Typography';
 
 const Container = styled.div`
@@ -37,84 +39,124 @@ export const HorizontalScrollBar: React.FC = ({ children }) => {
   const [showLeftButton, setShowLeftButton] = useState<boolean>(false);
   const [showRightButton, setShowRightButton] = useState<boolean>(true);
 
-  const intersectionObserver = useMemo(
-    () =>
-      new IntersectionObserver((entries: IntersectionObserverEntry[]) => {
-        entries.forEach((entry, index) => {
-          const containerWidth = parentContainerRef.current
-            ? parentContainerRef.current.clientWidth
-            : 0;
+  const toggleButtonStates = useCallback(
+    (
+      index: number,
+      elementIsHorizontallyIntersecting: boolean,
+      numberOfChildElements: number
+    ) => {
+      const shouldToggleOnShowLeftButton =
+        index === 0 && !elementIsHorizontallyIntersecting && !showLeftButton;
+      const shouldToggleOffShowLeftButton =
+        index === 0 && elementIsHorizontallyIntersecting && showLeftButton;
+      const shouldToggleOnShowRightButton =
+        index === numberOfChildElements - 1 &&
+        !elementIsHorizontallyIntersecting &&
+        !showRightButton;
+      const shouldToggleOffShowRightButton =
+        index === numberOfChildElements - 1 &&
+        elementIsHorizontallyIntersecting &&
+        showRightButton;
 
-          const { x, width } = entry.boundingClientRect;
-          const elementIsHorizontallyIntersecting = isHorizontallyIntersecting(
-            x,
-            width,
-            containerWidth
-          );
-
-          const shouldToggleOnShowLeftButton =
-            index === 0 &&
-            !elementIsHorizontallyIntersecting &&
-            !showLeftButton;
-          const shouldToggleOffShowLeftButton =
-            index === 0 && elementIsHorizontallyIntersecting && showLeftButton;
-          const shouldToggleOnShowRightButton =
-            index === entries.length - 1 &&
-            !elementIsHorizontallyIntersecting &&
-            !showRightButton;
-          const shouldToggleOffShowRightButton =
-            index === entries.length - 1 &&
-            elementIsHorizontallyIntersecting &&
-            showRightButton;
-
-          if (shouldToggleOnShowLeftButton) {
-            setShowLeftButton(true);
-          }
-          if (shouldToggleOffShowLeftButton) {
-            setShowLeftButton(false);
-          }
-          if (shouldToggleOnShowRightButton) {
-            setShowRightButton(true);
-          }
-          if (shouldToggleOffShowRightButton) {
-            setShowRightButton(false);
-          }
-
-          entry.target.ariaHidden = entry.isIntersecting ? 'false' : 'true';
-        });
-      }),
+      if (shouldToggleOnShowLeftButton) {
+        setShowLeftButton(true);
+      }
+      if (shouldToggleOffShowLeftButton) {
+        setShowLeftButton(false);
+      }
+      if (shouldToggleOnShowRightButton) {
+        setShowRightButton(true);
+      }
+      if (shouldToggleOffShowRightButton) {
+        setShowRightButton(false);
+      }
+    },
     [showLeftButton, showRightButton]
   );
 
+  const intersectionObserver = useMemo(() => {
+    return new IntersectionObserver((entries: IntersectionObserverEntry[]) => {
+      entries.forEach((entry) => {
+        const containerWidth = parentContainerRef.current
+          ? parentContainerRef.current.clientWidth
+          : 711;
+
+        const { x, width } = entry.boundingClientRect;
+
+        // console.log(
+        //   `index: ${index} xposition:${x},  width:${width} entriesLength: ${entries.length}`
+        // );
+        const elementIsHorizontallyIntersecting = isHorizontallyIntersecting(
+          x,
+          width,
+          containerWidth
+        );
+
+        const elementIndex = parseInt(
+          entry.target.getAttribute('data-observerindex') || '',
+          10
+        );
+
+        const elementIsFirstIndex = elementIndex === 0;
+        const elementIsLastIndex =
+          elementIndex === elementsRef.current.length - 1;
+        if (elementIsFirstIndex || elementIsLastIndex) {
+          toggleButtonStates(
+            elementIndex,
+            elementIsHorizontallyIntersecting,
+            elementsRef.current.length
+          );
+        }
+        entry.target.ariaHidden = elementIsHorizontallyIntersecting
+          ? 'false'
+          : 'true';
+      });
+    });
+  }, [parentContainerRef, toggleButtonStates]);
+
   useEffect(() => {
-    if (elementsRef.current.length) {
+    const numberOfChildElements = Children.toArray(children).length;
+    if (elementsRef.current.length === numberOfChildElements) {
       elementsRef.current.forEach((entry: HTMLElement) =>
         intersectionObserver.observe(entry)
       );
     }
-  }, [elementsRef, intersectionObserver]);
+  }, [elementsRef, intersectionObserver, children]);
 
   return (
     <>
-      <Text display={showLeftButton ? 'block ' : 'none'}>Left Button</Text>
-      <Text display={showRightButton ? 'block' : 'none'}>Right Button</Text>
-      {/* TODO figure out this shit  */}
-      <Container ref={parentContainerRef as RefObject<HTMLDivElement>}>
-        {Children.map(
-          children,
-          (
-            child: ReactElement<
-              { ref: (element: HTMLElement) => HTMLElement },
-              string | JSXElementConstructor<any>
-            >,
-            index
-          ) =>
-            cloneElement(child, {
-              ref: (element: HTMLElement) =>
-                (elementsRef.current[index] = element),
-            })
-        )}
-      </Container>
+      <Box position="relative">
+        <Container ref={parentContainerRef as RefObject<HTMLDivElement>}>
+          <FillButton
+            position="absolute"
+            display={showLeftButton ? 'block ' : 'none'}
+          >
+            Left Button
+          </FillButton>
+          <FillButton
+            right={0}
+            position="absolute"
+            display={showRightButton ? 'block' : 'none'}
+          >
+            Right Button
+          </FillButton>
+          {Children.map(
+            children,
+            (
+              child: ReactElement<
+                { ref: (element: HTMLElement) => HTMLElement },
+                string | JSXElementConstructor<any>
+              >,
+              index
+            ) =>
+              cloneElement(child, {
+                ref: (element: HTMLElement) =>
+                  (elementsRef.current[index] = element),
+                'data-observerIndex': index,
+              })
+          )}
+        </Container>
+      </Box>
     </>
   );
 };
