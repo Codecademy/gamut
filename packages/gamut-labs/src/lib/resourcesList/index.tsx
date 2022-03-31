b1427ec199e86cf751a8743bfc3441e0a1b670b7
borisonr <borisonr@gmail.com>
2022-03-31 10:53:03 -0400

    feat(AppHeader): hide projects and challenges in nav variant
    If a user is in the new Catalog dropdown feature flag test (useNewCatalogDropdown is true), they should not see “Challenges” and “Projects” in the Resources dropdown because we will have these items in the new Catalog dropdown.

--- packages/gamut-labs/src/lib/resourcesList/index.tsx
+++ packages/gamut-labs/src/lib/resourcesList/index.tsx
@@ -32,6 +32,7 @@ export const resourcesList: ResourcesList[] = [
     text: 'Projects',
     type: 'link',
     badgeText: 'New',
+    hideWithNewCatalogDropdown: true,
   },
   {
     id: 'challenges',
@@ -40,6 +41,7 @@ export const resourcesList: ResourcesList[] = [
     headerTrackingTarget: 'topnav_resources_challenges',
     text: 'Challenges',
     type: 'link',
+    hideWithNewCatalogDropdown: true,
   },
   {
     id: 'docs',
@@ -92,19 +94,26 @@ export const resourcesList: ResourcesList[] = [
   },
 ];
 
-export const headerResourcesList: AppHeaderLinkItem[] = resourcesList.map(
-  ({ id, href, headerTrackingTarget, text, type, newTab, badgeText }) => {
-    return {
-      id,
-      href,
-      trackingTarget: headerTrackingTarget,
-      text,
-      type,
-      newTab,
-      badge: badgeText ? renderBadge(badgeText) : undefined,
-    };
-  }
-);
+export const headerResourcesList = (
+  useNewCatalogDropdown?: boolean
+): AppHeaderLinkItem[] => {
+  const filteredResources = useNewCatalogDropdown
+    ? resourcesList.filter((resource) => !resource.hideWithNewCatalogDropdown)
+    : resourcesList;
+  return filteredResources.map(
+    ({ id, href, headerTrackingTarget, text, type, newTab, badgeText }) => {
+      return {
+        id,
+        href,
+        trackingTarget: headerTrackingTarget,
+        text,
+        type,
+        newTab,
+        badge: badgeText ? renderBadge(badgeText) : undefined,
+      };
+    }
+  );
+};
 
 export const footerResourcesList: FooterResourceList[] = resourcesList.map(
   ({ id, href, footerTrackingTarget, text, newTab }) => ({
