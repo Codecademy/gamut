60eee59072b600adfa7e3c065315467d57dbe4fb
Hailey <hailey@codecademy.com>
2022-04-06 16:32:06 -0400

    Broke out dropdown from component and mobile sub menu logic

--- packages/gamut-labs/src/AppHeader/AppHeaderElements/AppHeaderCatalogDropdown/index.tsx
+++ packages/gamut-labs/src/AppHeader/AppHeaderElements/AppHeaderCatalogDropdown/index.tsx
@@ -1,14 +1,3 @@
-import {
-  Anchor,
-  Box,
-  Column,
-  FlexBox,
-  LayoutGrid,
-  Text,
-  TextButton,
-} from '@codecademy/gamut';
-import { pxRem, theme } from '@codecademy/gamut-styles';
-import styled from '@emotion/styled';
 import cx from 'classnames';
 import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { useIsomorphicLayoutEffect } from 'react-use';
@@ -19,44 +8,14 @@ import {
   StyledDropdown,
   StyledText,
 } from '../../shared';
+import { AppHeaderCatalogSection } from '../AppHeaderCatalogSection';
 import { AppHeaderCatalogDropdownItem, AppHeaderClickHandler } from '../types';
 
-type AppHeaderCatalogDropdownProps = {
+export type AppHeaderCatalogDropdownProps = {
   action: AppHeaderClickHandler;
   item: AppHeaderCatalogDropdownItem;
 };
 
-const StyledTitle = styled(Text)`
-  font-size: ${pxRem(12)};
-  font-family: ${theme.fontFamily.accent};
-  font-weight: ${theme.fontWeight.title};
-  margin-bottom: ${pxRem(6)};
-`;
-
-const StyledDescription = styled(Text)`
-  font-size: ${pxRem(11)};
-  font-family: ${theme.fontFamily.base};
-  line-height: ${pxRem(19)};
-`;
-
-const StyledSubheader = styled(Text)`
-  font-size: ${pxRem(12)};
-  font-family: ${theme.fontFamily.base};
-  font-weight: ${theme.fontWeight.title};
-  color: ${theme.colors['navy-500']};
-  line-height: ${pxRem(24)};
-  margin-bottom: ${pxRem(2)};
-`;
-
-const StyledAnchor = styled(Anchor)`
-  font-size: ${pxRem(14)};
-  font-family: ${theme.fontFamily.base};
-  line-height: ${pxRem(32)};
-`;
-
-const StyledColumn = styled(Column)`
-  border-bottom: ${theme.borders[1]};
-`;
 export const AppHeaderCatalogDropdown: React.FC<AppHeaderCatalogDropdownProps> = ({
   action,
   item,
@@ -141,7 +100,7 @@ export const AppHeaderCatalogDropdown: React.FC<AppHeaderCatalogDropdownProps> =
       <StyledDropdown
         style={{
           top: '2.50rem',
-          width: pxRem(586),
+          width: '38rem',
         }}
         initial={{ borderWidth: 0, height: 0 }}
         animate={{
@@ -151,48 +110,13 @@ export const AppHeaderCatalogDropdown: React.FC<AppHeaderCatalogDropdownProps> =
         transition={{ duration: 0.175 }}
         aria-hidden={!isOpen}
       >
-        <LayoutGrid ref={containerRef}>
-          {item.popover.map((section) => (
-            <StyledColumn size={12}>
-              <LayoutGrid>
-                <Column size={4}>
-                  <Box background={theme.colors['gray-100']} p={16}>
-                    <StyledTitle>{section.title}</StyledTitle>
-                    <StyledDescription>{section.description}</StyledDescription>
-                  </Box>
-                </Column>
-                <Column size={8} p={16}>
-                  <FlexBox
-                    maxHeight={
-                      // This is so columns without subheaders stay grouped in threes
-                      section.title !== 'Popular courses' ? '7rem' : '8rem'
-                    }
-                    flexDirection="column"
-                    flexWrap="wrap"
-                  >
-                    {section.links.map((link) =>
-                      link.type === 'subheader' ? (
-                        <StyledSubheader>{link.text}</StyledSubheader>
-                      ) : (
-                        <Box minWidth="200px">
-                          <StyledAnchor variant="interface" href={link.href}>
-                            {link.text}
-                          </StyledAnchor>
-                        </Box>
-                      )
-                    )}
-                  </FlexBox>
-                </Column>
-              </LayoutGrid>
-            </StyledColumn>
-          ))}
-
-          <Column size={12} py={12}>
-            <TextButton href="/catalog" width="min-content">
-              Explore full catalog
-            </TextButton>
-          </Column>
-        </LayoutGrid>
+        <AppHeaderCatalogSection
+          action={action}
+          item={item}
+          role="menu"
+          ref={containerRef}
+          id={`menu-container${item.text}`}
+        />
       </StyledDropdown>
     </>
   );
---
+++ packages/gamut-labs/src/AppHeader/AppHeaderElements/AppHeaderCatalogSection/index.tsx
@@ -0,0 +1,111 @@
+import {
+  Anchor,
+  Box,
+  Column,
+  FlexBox,
+  LayoutGrid,
+  Text,
+} from '@codecademy/gamut';
+import { pxRem, theme } from '@codecademy/gamut-styles';
+import styled from '@emotion/styled';
+import React from 'react';
+
+import { AppHeaderCatalogDropdownItem, AppHeaderClickHandler } from '../types';
+
+export type AppHeaderCatalogSectionProps = {
+  action: AppHeaderClickHandler;
+  item: AppHeaderCatalogDropdownItem;
+  ref?: React.RefObject<HTMLUListElement>;
+  role?: string;
+  id?: string;
+};
+
+const StyledTitle = styled(Text)`
+  font-size: ${pxRem(12)};
+  font-family: ${theme.fontFamily.accent};
+  font-weight: ${theme.fontWeight.title};
+  margin-bottom: ${pxRem(6)};
+`;
+
+const StyledDescription = styled(Text)`
+  font-size: ${pxRem(11)};
+  font-family: ${theme.fontFamily.base};
+  line-height: ${pxRem(19)};
+`;
+
+const StyledSubheader = styled(Text)`
+  font-size: ${pxRem(12)};
+  font-family: ${theme.fontFamily.base};
+  font-weight: ${theme.fontWeight.title};
+  color: ${theme.colors['navy-500']};
+  line-height: ${pxRem(24)};
+  margin-bottom: ${pxRem(2)};
+`;
+
+const StyledLinkAnchor = styled(Anchor)`
+  font-size: ${pxRem(14)};
+  font-family: ${theme.fontFamily.base};
+  line-height: ${pxRem(32)};
+`;
+
+const StyledColumn = styled(Column)`
+  border-bottom: ${theme.borders[1]};
+`;
+
+export const AppHeaderCatalogSection = React.forwardRef<
+  HTMLDivElement,
+  AppHeaderCatalogSectionProps
+>(({ action, item }, ref) => (
+  <LayoutGrid ref={ref}>
+    {item.popover.map((section) => (
+      <StyledColumn size={12}>
+        <LayoutGrid>
+          <Column size={4}>
+            <Box background={theme.colors['gray-100']} p={16}>
+              <StyledTitle>{section.title}</StyledTitle>
+              <StyledDescription>{section.description}</StyledDescription>
+            </Box>
+          </Column>
+          <Column size={8} p={16}>
+            <FlexBox
+              maxHeight={
+                // This is so columns without subheaders stay grouped in threes on desktop
+                section.title !== 'Popular courses' ? '7rem' : '8rem'
+              }
+              flexDirection="column"
+              flexWrap="wrap"
+            >
+              {section.links.map((link) =>
+                link.type === 'subheader' ? (
+                  <StyledSubheader>{link.text}</StyledSubheader>
+                ) : (
+                  <Box minWidth="200px">
+                    <StyledLinkAnchor
+                      variant="interface"
+                      href={link.href}
+                      onClick={(event) => action(event, item)}
+                    >
+                      {link.text}
+                    </StyledLinkAnchor>
+                  </Box>
+                )
+              )}
+            </FlexBox>
+          </Column>
+        </LayoutGrid>
+      </StyledColumn>
+    ))}
+
+    {/* TODO: add onClick event for this */}
+    <Column size={12} p={16}>
+      <Anchor
+        variant="standard"
+        href="/catalog"
+        fontSize={16}
+        fontWeight="bold"
+      >
+        Explore full catalog
+      </Anchor>
+    </Column>
+  </LayoutGrid>
+));
