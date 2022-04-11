70be862a1a0415f8bc7c5bd6db6bc2f367c40c89
cass spain <cass@codecademy.com>
2022-04-11 14:07:05 -0400

    more templates

--- packages/gamut/src/Aside/index.tsx
+++ packages/gamut/src/Aside/index.tsx
@@ -1,15 +1,60 @@
+import { GamutIconProps } from '@codecademy/gamut-icons';
 import React from 'react';
 
-// Atoms to use
-// FloatingCard
-// List
-// Buttons
+import { ButtonProps, FillButton, StrokeButton } from '../Button';
+import { FloatingCard, FloatingCardProps } from '../FloatingCard/FloatingCard';
+import { List, ListCol, ListRow } from '../List';
+import { Text } from '../Typography';
 
-// TODO: add to FloatingCardDocs
-export interface AsideProps {
+interface AsideListItemProps {
+  /** Gamut icon component */
+  icon: React.ComponentType<GamutIconProps>;
+  /** Number of items for list item */
+  numberOfItems: number;
+  /** Each item needs to have a unique ID */
+  id: string;
+}
+interface AsideButtonProps extends Pick<ButtonProps, 'href' | 'onClick'> {
+  children: React.ReactNode;
+}
+export interface AsideProps extends Pick<FloatingCardProps, 'pattern'> {
   header: string;
+  headerLevel: 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+  subtitle: string;
+  listItems: AsideListItemProps[];
+  primaryButton: AsideButtonProps; // FillButton
+  secondaryButton: AsideButtonProps; // StrokeButton
 }
 
-export const Aside: React.FC<AsideProps> = (...props) => {
-  return <></>;
+// update FloatingCard docs
+export const Aside: React.FC<AsideProps> = ({
+  header,
+  headerLevel,
+  listItems,
+  pattern,
+  primaryButton,
+  secondaryButton,
+  subtitle,
+  ...rest
+}) => {
+  return (
+    <FloatingCard as="aside" pattern={pattern} {...rest}>
+      <Text as={headerLevel}>{header}</Text>
+      <Text>{subtitle}</Text>
+      <List variant="plain">
+        {listItems.map(({ icon: Icon, numberOfItems, id }) => (
+          <ListRow key={id}>
+            <ListCol>
+              <Icon size={24} />
+            </ListCol>
+            <ListCol size="lg" fill>
+              {numberOfItems} items
+            </ListCol>
+          </ListRow>
+        ))}
+      </List>
+      <FillButton {...primaryButton} />
+      <StrokeButton {...secondaryButton} />
+    </FloatingCard>
+  );
 };
