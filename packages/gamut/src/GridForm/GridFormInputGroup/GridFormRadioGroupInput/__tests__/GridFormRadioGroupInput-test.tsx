eade0fc97a8911756f8f7c53c13f35a3b065535a
cass spain <cass@codecademy.com>
2022-05-11 17:59:52 -0400

    delete radiogroup thing

--- packages/gamut/src/GridForm/GridFormInputGroup/GridFormRadioGroupInput/__tests__/GridFormRadioGroupInput-test.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import { getComponent } from '../../__fixtures__/renderers';
-
-const { renderField, defaultFieldProps } = getComponent(
-  'GridFormRadioGroupInput'
-);
