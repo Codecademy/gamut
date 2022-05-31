546e33e53961405280480effc2149398cbfba70f
cass spain <cass@codecademy.com>
2022-05-31 16:51:32 -0400

    rearranging cc imports

--- packages/gamut-labs/src/index.ts
+++ packages/gamut-labs/src/index.ts
@@ -14,8 +14,6 @@ export * from './Banner';
 export * from './BetaSticker';
 export * from './Byline';
 export * from './CurriculumCard';
-export * from './CurriculumCard/Difficulty';
-export * from './CurriculumCard/Difficulty/types';
 export * from './DropdownButton';
 export * from './EditorialImage';
 export * from './EmptySection';
