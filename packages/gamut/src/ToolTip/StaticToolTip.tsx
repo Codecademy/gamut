f8f4ef175eac7f357926c8eb5f98bdb3e6628d96
cass spain <cass@codecademy.com>
2022-06-14 17:25:50 -0400

    on the up + up

--- packages/gamut/src/ToolTip/StaticToolTip.tsx
+++ packages/gamut/src/ToolTip/StaticToolTip.tsx
@@ -11,178 +11,12 @@ import styled from '@emotion/styled';
 import React from 'react';
 
 import { ToolTipProps } from '.';
-import { TargetContainer, TooltipWrapper } from './elements';
-
-export type ToolTipAlignment =
-  | 'bottom-center'
-  | 'bottom-left'
-  | 'bottom-right'
-  | 'top-center'
-  | 'top-left'
-  | 'top-right';
-
-const arrowHeight = `1rem`;
-const containerOffsetVertical = `0.75rem`;
-
-type ToolTipBodyProps = {
-  /**
-   * How to align the tooltip relative to the target.
-   */
-  alignment?: ToolTipAlignment;
-
-  mode?: ColorModes;
-
-  widthMode?: 'standard' | 'unlimited';
-};
-
-type ToolTipContainerProps = ToolTipBodyProps;
-
-const getToolTipVisibilityCSS = (visibility: boolean) =>
-  visibility
-    ? `
-  opacity: 1;
-  visibility: visible;
-`
-    : `
-  opacity: 0;
-  visibility: hidden;
-`;
-
-const ToolTipContainer = styled.div<Required<ToolTipContainerProps>>`
-  ${fontSmoothPixel}
-  display: flex;
-  transition: opacity ${timing.fast};
-  transition-delay: ${timing.fast};
-  position: absolute;
-  max-width: ${({ alignment }) =>
-    alignment.includes('center') ? '8rem' : '16rem'};
-  width: 70vw;
-  z-index: 1;
-
-  ${getToolTipVisibilityCSS(false)}
-
-  &::after {
-    content: '';
-    display: block;
-    height: ${arrowHeight};
-    position: absolute;
-    transform: rotate(45deg);
-    width: ${arrowHeight};
-  }
-
-  &::after {
-    border-style: solid;
-
-    ${variant({
-      prop: 'mode',
-      variants: {
-        dark: { backgroundColor: 'black', borderColor: 'white' },
-        light: { backgroundColor: 'white', borderColor: 'black' },
-      },
-    })}
-  }
-
-  ${TargetContainer}:hover + &,
-  ${TargetContainer}:focus-within + &,
-  &:hover {
-    ${getToolTipVisibilityCSS(true)}
-  }
-
-  ${({ alignment }) =>
-    alignment.includes('top') &&
-    `
-      bottom: 100%;
-      padding-bottom: ${containerOffsetVertical};
-
-      &::after {
-        border-bottom-right-radius: 4px;
-        border-width: 0 1px 1px 0;
-        bottom: 0.25rem;
-      }
-    `}
-
-  ${({ alignment }) =>
-    alignment.includes('bottom') &&
-    `
-      top: 100%;
-      padding-top: ${containerOffsetVertical};
-
-      &::after {
-        border-top-left-radius: 4px;
-        border-width: 1px 0 0 1px;
-        top: 0.25rem;
-      }
-    `}
-
-${({ alignment }) =>
-    alignment.includes('center') &&
-    `
-      left: calc(50% - 4rem);
-
-      &::after {
-        left: calc(50% - 0.5rem);
-      }
-    `}
-
-  ${({ alignment }) =>
-    alignment.includes('left') &&
-    `
-      justify-content: flex-end;
-
-      &::after {
-        right: 1.5rem;
-      }
-
-      left: calc(50% - 14rem)
-    `}
-
-  ${({ alignment }) =>
-    alignment.includes('right') &&
-    `
-      &::after {
-        left: 1.5rem;
-      }
-
-      left: calc(50% - 2rem);
-    `}
-`;
-
-const ToolTipBody = styled.div<Required<ToolTipBodyProps>>`
-  border: 1px solid;
-  border-radius: 3px;
-  display: inline-block;
-  font-size: ${pxRem(14)};
-  line-height: ${lineHeight.base};
-
-  ${({ alignment }) =>
-    alignment.includes('center')
-      ? `
-      margin: auto;
-      padding: 0.5rem;
-      text-align: center;
-    `
-      : `
-      padding: 1rem;
-    `}
-
-  ${variant({
-    prop: 'mode',
-    variants: {
-      dark: {
-        bg: 'black',
-        borderColor: 'beige',
-        textColor: 'beige',
-      },
-      light: {
-        bg: 'white',
-        borderColor: 'black',
-        textColor: 'black',
-      },
-    },
-  })}
-
-  ${({ widthMode }) => (widthMode === 'unlimited' ? {} : { minWidth: '4rem' })}
-`;
+import {
+  TargetContainer,
+  ToolTipBody,
+  ToolTipContainer,
+  TooltipWrapper,
+} from './elements';
 
 export const StaticToolTip: React.FC<ToolTipProps> = ({
   alignment = 'top-right',
@@ -213,12 +47,13 @@ export const StaticToolTip: React.FC<ToolTipProps> = ({
         {target}
       </TargetContainer>
       <ToolTipContainer
+        as="div"
+        bg={mode === 'light' ? 'white' : 'black'}
+        color={mode === 'light' ? 'text' : 'secondary'}
         alignment={alignment}
         className={className}
         id={id}
         role="tooltip"
-        mode={mode}
-        widthMode={widthMode}
         aria-live="polite"
       >
         <ToolTipBody alignment={alignment} mode={mode} widthMode={widthMode}>
--- packages/gamut/src/ToolTip/elements.tsx
+++ packages/gamut/src/ToolTip/elements.tsx
@@ -1,4 +1,5 @@
 import {
+  Background,
   ColorModes,
   css,
   fontSmoothPixel,
@@ -9,6 +10,7 @@ import {
   timing,
   variant,
 } from '@codecademy/gamut-styles';
+import { StyleProps } from '@codecademy/variance';
 import styled from '@emotion/styled';
 
 export type ToolTipAlignment =
@@ -50,231 +52,154 @@ type ToolTipBodyProps = {
   widthMode?: 'standard' | 'unlimited';
 };
 
-type ToolTipContainerProps = ToolTipBodyProps;
-
-const getToolTipVisibilityCSS = (visibility: boolean) =>
-  visibility
-    ? `
-  opacity: 1;
-  visibility: visible;
-`
-    : `
-  opacity: 0;
-  visibility: hidden;
-`;
-
-const getToolTipVisibility = (boolean: boolean) =>
-  boolean
-    ? ({ opacity: 1, visibility: 'visible' } as const)
-    : ({ opacity: 0, visibility: 'hidden' } as const);
-
 const centerMaxWidth = { maxWidth: '8rem' } as const;
 const alignedMaxWidth = { maxWidth: '16rem' } as const;
 
 const topStyles = {
   bottom: '100%',
   pb: containerOffsetVertical,
-  '&::after': {
-    borderBottomRightRadius: '4px',
-    borderWidth: '0 1px 1px 0',
-    bottom: '0.25rem',
-  },
+} as const;
+
+const topStylesAfter = {
+  borderColor: 'currentColor',
+  borderBottomRightRadius: '4px',
+  borderWidth: '0 1px 1px 0',
+  bottom: '0.25rem',
 } as const;
 
 const bottomStyles = {
   top: '100%',
   pt: containerOffsetVertical,
-  '&::after': {
-    borderTopightRadius: '4px',
-    borderWidth: '1px 0 0 1px',
-    top: '0.25rem',
-  },
+} as const;
+
+const bottomStylesAfter = {
+  borderColor: 'currentColor',
+  borderTopRightRadius: '4px',
+  borderWidth: '1px 0 0 1px',
+  top: '0.25rem',
 } as const;
 
 const centerStyles = {
+  ...centerMaxWidth,
   left: 'calc(50% - 4rem)',
-  '&::after': {
-    left: 'calc(50% - 0.5rem)',
-  },
 } as const;
+
+const centerStylesAfter = { left: 'calc(50% - 0.5rem)' } as const;
 
 const leftStyles = {
+  ...alignedMaxWidth,
   justifyContent: 'flex-end',
   left: 'calc(50% - 14rem)',
-  '&::after': {
-    right: '1.5rem',
-  },
 } as const;
 
+const leftStylesAfter = { right: '1.5rem' } as const;
+
 const rightStyles = {
+  ...alignedMaxWidth,
   left: 'calc(50% - 2rem)',
-  '&::after': {
-    left: '1.5rem',
-  },
+} as const;
+
+const rightStylesAfter = {
+  left: '1.5rem',
 } as const;
 
 const toolTipAlignmentVariants = variant({
   prop: 'alignment',
+  base: {
+    bg: 'transparent',
+    fontSmoothPixel,
+    display: 'flex',
+    transition: `opacity ${timing.fast}`,
+    transitionDelay: `${timing.fast}`,
+    position: 'absolute',
+    width: '70vw',
+    zIndex: 1,
+    opacity: 0,
+    visibility: 'hidden',
+    '&::after': {
+      bg: 'background-current',
+      content: '""',
+      display: 'block',
+      height: `${arrowHeight}`,
+      position: 'absolute',
+      transform: 'rotate(45deg)',
+      width: `${arrowHeight}`,
+      borderStyle: 'solid',
+    },
+  },
   variants: {
     'bottom-center': {
-      centerMaxWidth,
+      ...bottomStyles,
+      ...centerStyles,
+      '&::after': { ...bottomStylesAfter, ...centerStylesAfter },
     },
     'bottom-left': {
-      alignedMaxWidth,
+      ...bottomStyles,
+      ...leftStyles,
+      '&::after': { ...bottomStylesAfter, ...leftStylesAfter },
     },
     'bottom-right': {
-      alignedMaxWidth,
+      ...bottomStyles,
+      ...rightStyles,
+      '&::after': { ...bottomStylesAfter, ...rightStylesAfter },
     },
     'top-center': {
-      centerMaxWidth,
-      topStyles,
+      ...topStyles,
+      ...centerStyles,
+      '&::after': { ...topStylesAfter, ...centerStylesAfter },
     },
     'top-left': {
-      alignedMaxWidth,
-      topStyles,
+      ...topStyles,
+      ...leftStyles,
+      '&::after': { ...topStylesAfter, ...leftStylesAfter },
     },
     'top-right': {
-      alignedMaxWidth,
-      topStyles,
+      ...topStyles,
+      ...rightStyles,
+      '&::after': { ...topStylesAfter, ...rightStylesAfter },
     },
   },
 });
 
-const toolTipContainerModeVariants = variant({
-  prop: 'mode',
-  variants: {
-    dark: { backgroundColor: 'black', borderColor: 'white' },
-    light: { backgroundColor: 'white', borderColor: 'black' },
-  },
-});
+export interface ToolTipContainerProps
+  extends StyleProps<typeof toolTipAlignmentVariants> {}
 
-const toolTipVisibilityStates = states({
-  visible: getToolTipVisibility(true),
-});
-
-const toolTipBaseCss = css({
-  fontSmoothPixel,
-  display: 'flex',
-  transition: `opacity ${timing.fast}`,
-  transitionDelay: `${timing.fast}`,
-  position: 'absolute',
-  width: '70vw',
-  zIndex: 1,
-  opacity: 1,
-  visibility: 'visible',
-  '&::after': {
-    content: '',
-    display: 'block',
-    height: `${arrowHeight}`,
-    position: 'absolute',
-    transform: 'rotate(45deg)',
-    width: `${arrowHeight}`,
-    borderStyle: 'solid',
-  },
-});
-
-export const ToolTipContainer = styled.div`
+export const ToolTipContainer = styled(Background)<ToolTipContainerProps>`
   ${TargetContainer}:hover + &,
   ${TargetContainer}:focus-within + &,
   &:hover {
-    ${getToolTipVisibilityCSS(true)}
+    opacity: 1;
+    visibility: visible;
+  }
+  ${toolTipAlignmentVariants}s
+`;
+
+const toolTipBodyStates = states({
+  center: {
+    m: 'auto',
+    p: 8,
+    textAlign: 'center',
   },
-  ${toolTipBaseCss},
-`;
+  unlimitedWidth: {
+    minWidth: 'initial',
+  },
+});
 
-const ToolTipContainer_1 = styled.div<Required<ToolTipContainerProps>>`
-  max-width: ${({ alignment }) =>
-    alignment.includes('center') ? '8rem' : '16rem'};
+export const ToolTipBody = styled.div(
+  css({
+    bg: 'background-current',
+    border: 1,
+    borderRadius: '3px',
+    display: 'inline-block',
+    fontSize: 14,
+    lineHeight: 'base',
+    p: 16,
+    minWidth: '4rem',
+  }),
+  toolTipBodyStates
+);
 
-  ${getToolTipVisibilityCSS(false)}
-
-  &::after {
-    content: '';
-    display: block;
-    height: ${arrowHeight};
-    position: absolute;
-    transform: rotate(45deg);
-    width: ${arrowHeight};
-  }
-
-  &::after {
-    border-style: solid;
-
-    ${variant({
-      prop: 'mode',
-      variants: {
-        dark: { backgroundColor: 'black', borderColor: 'white' },
-        light: { backgroundColor: 'white', borderColor: 'black' },
-      },
-    })}
-  }
-
-  ${TargetContainer}:hover + &,
-  ${TargetContainer}:focus-within + &,
-  &:hover {
-    ${getToolTipVisibilityCSS(true)}
-  }
-
-  ${({ alignment }) =>
-    alignment.includes('top') &&
-    `
-      bottom: 100%;
-      padding-bottom: ${containerOffsetVertical};
-
-      &::after {
-        border-bottom-right-radius: 4px;
-        border-width: 0 1px 1px 0;
-        bottom: 0.25rem;
-      }
-    `}
-
-  ${({ alignment }) =>
-    alignment.includes('bottom') &&
-    `
-      top: 100%;
-      padding-top: ${containerOffsetVertical};
-
-      &::after {
-        border-top-left-radius: 4px;
-        border-width: 1px 0 0 1px;
-        top: 0.25rem;
-      }
-    `}
-
-${({ alignment }) =>
-    alignment.includes('center') &&
-    `
-      left: calc(50% - 4rem);
-
-      &::after {
-        left: calc(50% - 0.5rem);
-      }
-    `}
-
-  ${({ alignment }) =>
-    alignment.includes('left') &&
-    `
-      justify-content: flex-end;
-
-      &::after {
-        right: 1.5rem;
-      }
-
-      left: calc(50% - 14rem)
-    `}
-
-  ${({ alignment }) =>
-    alignment.includes('right') &&
-    `
-      &::after {
-        left: 1.5rem;
-      }
-
-      left: calc(50% - 2rem);
-    `}
-`;
-
-const ToolTipBody = styled.div<Required<ToolTipBodyProps>>`
+const ToolTipBody2 = styled.div<Required<ToolTipBodyProps>>`
   border: 1px solid;
   border-radius: 3px;
   display: inline-block;
