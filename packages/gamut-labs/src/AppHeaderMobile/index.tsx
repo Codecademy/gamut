f1aa51463c47acf4d03c313a97effb221fe2401e
Hailey <hailey@codecademy.com>
2022-06-30 18:31:48 -0400

    Make scrollable

--- packages/gamut-labs/src/AppHeaderMobile/index.tsx
+++ packages/gamut-labs/src/AppHeaderMobile/index.tsx
@@ -96,7 +96,7 @@ export const AppHeaderMobile: React.FC<AppHeaderMobileProps> = ({
   ];
 
   const onItemType = (type: string) => {
-    if (type === 'catalog-dropdown') {
+    if (type === 'catalog-dropdown' || type === 'new-resources-dropdown') {
       setAllowScroll(true);
     } else {
       setAllowScroll(false);
