34dd1250a679f9d0b5c9079b31e9f71aa962eb3e
cass spain <cass@codecademy.com>
2022-07-07 14:25:03 -0400

    real tag test + compile

--- packages/gamut/src/Tag/__tests__/Tag-test.tsx
+++ packages/gamut/src/Tag/__tests__/Tag-test.tsx
@@ -1,13 +1,15 @@
-import { mount } from 'enzyme';
+import { setupRtl } from '@codecademy/gamut-tests';
 import React from 'react';
 
 import { Tag } from '..';
 
-describe('Tag', () => {
-  it('renders tah text', () => {
-    const badgeText = 'I am a badge';
-    const wrapped = mount(<Tag>{badgeText}</Tag>);
+const tagText = 'Tag Text';
+const renderTag = setupRtl(Tag, { children: <>{tagText}</> });
 
-    expect(wrapped.text()).toEqual(badgeText);
+describe('Tag', () => {
+  it('renders correct text', () => {
+    const { view } = renderTag();
+
+    expect(view.getByText(tagText));
   });
 });
