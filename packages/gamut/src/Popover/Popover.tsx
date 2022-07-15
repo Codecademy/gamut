8bb047c61f783b919b04b7c0cc08c5e88fbed00b
cassie spain <58054751+dreamwasp@users.noreply.github.com>
2022-07-15 11:57:28 -0400

    feat(Tooltip): Add tooltip placements
    * Adds 'sml' Tooltip-esque size variant to Popover
    * Add `animation` and `skipFocusTrap` to popovers to allow more flexibility (partially to better mimic ToolTips)
    * Add 'background-contrast' to our semantic colors (black for DarkMode, white for Light)
    * Adds 'placement' prop to allow Tooltips to break out of their context if needed
    * Refactors ToolTips + Popovers to use newer variant syntax + clean up file structure
    * Adds related tests
    * I also ending up adding a VS Code extension setting here for[ Story Explorer ](https://marketplace.visualstudio.com/items?itemName=joshbolduc.story-explorer) but can move that to a different PR 😅

---
+++ packages/gamut/src/Popover/Popover.tsx
@@ -0,0 +1,139 @@
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import { useWindowScroll, useWindowSize } from 'react-use';
+
+import { FocusTrap } from '../FocusTrap';
+import {
+  Beak,
+  PatternContainer,
+  PopoverContainer,
+  PopoverPortal,
+  RaisedDiv,
+} from './elements';
+import { PopoverProps } from './types';
+
+export const Popover: React.FC<PopoverProps> = ({
+  animation,
+  align = 'left',
+  beak,
+  children,
+  className,
+  horizontalOffset = 0,
+  isOpen,
+  onRequestClose,
+  outline = false,
+  skipFocusTrap,
+  pattern: Pattern,
+  position = 'below',
+  role,
+  variant,
+  targetRef,
+  verticalOffset = variant === 'secondary' ? 15 : 20,
+  widthRestricted,
+}) => {
+  const [targetRect, setTargetRect] = useState<DOMRect>();
+  const [isInViewport, setIsInViewport] = useState(true);
+  const { width, height } = useWindowSize();
+  const { x, y } = useWindowScroll();
+
+  const getPopoverPosition = useCallback(() => {
+    if (!targetRect) return {};
+
+    const positions = {
+      above: Math.round(targetRect.top - verticalOffset),
+      below: Math.round(targetRect.top + targetRect.height + verticalOffset),
+    };
+    const alignments = {
+      right: Math.round(window.scrollX + targetRect.right + horizontalOffset),
+      left: Math.round(window.scrollX + targetRect.left - horizontalOffset),
+    };
+    return {
+      top: positions[position],
+      left: alignments[align],
+    };
+  }, [targetRect, verticalOffset, horizontalOffset, align, position]);
+
+  useEffect(() => {
+    setTargetRect(targetRef?.current?.getBoundingClientRect());
+  }, [targetRef, isOpen, width, height, x, y]);
+
+  useEffect(() => {
+    if (targetRect) {
+      const inView =
+        targetRect.top >= 0 &&
+        targetRect.left >= 0 &&
+        targetRect.bottom <=
+          (window.innerHeight || document.documentElement.clientHeight) &&
+        targetRect.right <=
+          (window.innerWidth || document.documentElement.clientWidth);
+      setIsInViewport(inView);
+    }
+    if (!isInViewport) onRequestClose?.();
+  }, [targetRect, isInViewport, onRequestClose]);
+
+  const handleClickOutside = useCallback(
+    (e) => {
+      /**
+       * Allows targetRef to be or contain a button that toggles the popover open and closed.
+       * Without this check it would toggle closed then back open immediately.
+       */
+      if (targetRef.current?.contains(e.target as Node)) return;
+
+      onRequestClose?.();
+    },
+    [onRequestClose, targetRef]
+  );
+
+  const popoverRef = useRef<HTMLDivElement>(null);
+  if ((!isOpen || !targetRef) && !animation) return null;
+
+  const contents = (
+    <PopoverContainer
+      align={align}
+      className={className}
+      data-testid="popover-content-container"
+      position={position}
+      ref={popoverRef}
+      role={role}
+      style={getPopoverPosition()}
+      tabIndex={-1}
+    >
+      <RaisedDiv
+        alignment={variant === 'primary' || beak ? 'aligned' : undefined}
+        outline={outline ? 'outline' : 'boxShadow'}
+        variant={variant}
+        widthRestricted={widthRestricted}
+      >
+        {beak && (
+          <Beak
+            outline={outline ? 'outline' : 'boxShadow'}
+            beak={`${position}-${beak}${variant === 'secondary' ? '-sml' : ''}`}
+            data-testid="popover-beak"
+            size={variant === 'secondary' ? 'sml' : 'lrg'}
+          />
+        )}
+        {children}
+      </RaisedDiv>
+      {Pattern && (
+        <PatternContainer variant={`${position}-${align}`}>
+          <Pattern data-testid="popover-pattern" />
+        </PatternContainer>
+      )}
+    </PopoverContainer>
+  );
+
+  return (
+    <PopoverPortal animation={animation} isOpen={Boolean(isOpen && targetRef)}>
+      {skipFocusTrap ? (
+        <>{contents}</>
+      ) : (
+        <FocusTrap
+          allowPageInteraction
+          onClickOutside={handleClickOutside}
+          onEscapeKey={onRequestClose}
+        >
+          {contents}
+        </FocusTrap>
+      )}
+    </PopoverPortal>
+  );
+};
---
+++ packages/gamut/src/ToolTip/FloatingToolTip.tsx
@@ -0,0 +1,95 @@
+import React, { useLayoutEffect, useRef, useState } from 'react';
+
+import { Box, FlexBox } from '../Box';
+import { Popover } from '../Popover';
+import { TargetContainer } from './elements';
+import { tooltipDefaultProps, ToolTipPlacementComponentProps } from './types';
+import {
+  escapeKeyPressHandler,
+  getAccessibilityProps,
+  getPopoverAlignment,
+} from './utils';
+
+export const FloatingToolTip: React.FC<ToolTipPlacementComponentProps> = ({
+  alignment = tooltipDefaultProps.alignment,
+  children,
+  focusable,
+  id,
+  target,
+  widthMode = tooltipDefaultProps.widthMode,
+}) => {
+  const ref = useRef<HTMLDivElement>(null);
+  const [offset, setOffset] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
+  const [isFocused, setIsFocused] = useState(false);
+
+  useLayoutEffect(() => {
+    if (ref?.current) {
+      setOffset(-ref.current.clientWidth / 2 + 32);
+    }
+  }, []);
+
+  const accessibilityProps = getAccessibilityProps({ focusable, id });
+  const popoverAlignments = getPopoverAlignment({ alignment });
+
+  const handleShowHideAction = ({
+    type,
+  }:
+    | React.FocusEvent<HTMLDivElement, Element>
+    | React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (type === 'focus' && !isOpen) {
+      setIsOpen(true);
+      setIsFocused(true);
+    }
+    if (type === 'blur' && isOpen) {
+      setIsOpen(false);
+      setIsFocused(false);
+    }
+    if (type === 'mouseenter' && !isOpen) {
+      setIsOpen(true);
+    }
+    if (type === 'mouseleave' && isOpen && !isFocused) {
+      setIsOpen(false);
+    }
+  };
+
+  return (
+    <Box
+      position="relative"
+      display="inline-flex"
+      onMouseLeave={(e) => handleShowHideAction(e)}
+    >
+      <TargetContainer
+        onKeyDown={(e) => escapeKeyPressHandler(e)}
+        onFocus={(e) => {
+          handleShowHideAction(e);
+        }}
+        onBlur={(e) => {
+          handleShowHideAction(e);
+        }}
+        onMouseEnter={(e) => handleShowHideAction(e)}
+        ref={ref}
+        {...accessibilityProps}
+      >
+        {target}
+      </TargetContainer>
+      <Popover
+        {...popoverAlignments}
+        animation="fade"
+        aria-live="polite"
+        horizontalOffset={offset}
+        isOpen={isOpen}
+        outline
+        role="tooltip"
+        variant="secondary"
+        skipFocusTrap
+        targetRef={ref}
+        widthRestricted={widthMode === 'standard'}
+      >
+        <FlexBox id={id} alignItems="flex-start" flexDirection="column">
+          {children}
+        </FlexBox>
+      </Popover>
+    </Box>
+  );
+};
