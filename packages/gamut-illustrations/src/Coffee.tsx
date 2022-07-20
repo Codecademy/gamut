1408db5cdc967d8ab22e3ab399a7e0ebde69c4b3
Bach Diep <bdiep@codecademy.com>
2022-07-20 13:00:55 -0700

    add viewbox

--- packages/gamut-illustrations/src/Coffee.tsx
+++ packages/gamut-illustrations/src/Coffee.tsx
@@ -13,6 +13,7 @@ export const Coffee: React.FC<IllustrationProps> = ({
     className={className}
     width={width}
     height={height}
+    viewBox="0 0 48 73"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
     xmlnsXlink="http://www.w3.org/1999/xlink"
