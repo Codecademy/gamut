d79eac6fd40571f34428446609b529ca93ffd890
cass spain <cass@codecademy.com>
2022-08-01 12:46:38 -0400

    test new tooltip

--- packages/gamut/src/ToolTip/utils.tsx
+++ packages/gamut/src/ToolTip/utils.tsx
@@ -79,14 +79,23 @@ export const escapeKeyPressHandler = (
   }
 };
 
+export type ToolTipAccessibiltyProps = Pick<
+  ToolTipProps,
+  'focusable' | 'id'
+> & {
+  anotherThing?: boolean;
+};
+
 export const getAccessibilityProps = ({
   focusable,
   id,
-}: Pick<ToolTipProps, 'focusable' | 'id'>) => {
+  anotherThing,
+}: ToolTipAccessibiltyProps) => {
   // ToolTips sometimes contain actual <button>s, which cannot be a child of a button.
   // This element still needs tab focus so we must use the `tabIndex=0` hack.
   return {
-    'aria-labelledby': id,
+    'aria-labelledby':
+      anotherThing === undefined || anotherThing ? id : undefined,
     role: focusable ? 'button' : undefined,
     tabIndex: focusable ? 0 : undefined,
   };
