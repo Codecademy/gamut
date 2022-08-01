45daaf7fa7785bba390b221834ecf007392f5866
Jake Hiller <jakemhiller@gmail.com>
2022-08-01 11:56:55 -0400

    macros tests

---
+++ packages/macros/jest.config.ts
@@ -0,0 +1,8 @@
+import base from '../../jest.config.base';
+
+export default base('macros', {
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+});
