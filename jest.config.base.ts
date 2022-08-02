424f8500dfc77ad542c44226128950b8813d3d65
Jake Hiller <jakemhiller@gmail.com>
2022-08-02 16:34:40 -0400

    add test coverage directory

--- jest.config.base.ts
+++ jest.config.base.ts
@@ -3,7 +3,11 @@ const baseConfig = (packageName: string, overrides: Object) => ({
   preset: '../../jest.preset.js',
   clearMocks: true,
   verbose: true,
-  coverageDirectory: `../../coverage/packages/${packageName}`,
+  coverageDirectory: process.env.CI
+    ? `/tmp/test-results/jest/${packageName}`
+    : `../../coverage/packages/${packageName}`,
+  reporters: process.env.CI ? ['default', 'jest-junit'] : ['default'],
+  coverageReporters: ['json', 'text', 'clover'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|md)$':
       '<rootDir>/../../script/jest/fileMock',
