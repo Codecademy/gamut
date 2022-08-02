dcddc8b75dc8482155daa91a6176ff3d357a2378
Jake Hiller <jakemhiller@gmail.com>
2022-08-02 15:32:51 -0400

    strictFunctionTypes breaking builds

--- packages/gamut-patterns/babel.config.js
+++ packages/gamut-patterns/babel.config.js
@@ -1,4 +1,14 @@
 module.exports = {
   extends: '../../babel.defaults.js',
   presets: ['codecademy', '@babel/preset-typescript'],
+  plugins: [
+    [
+      '@emotion/babel-plugin',
+      {
+        sourceMap: true,
+        autoLabel: 'always',
+        labelFormat: '[local]',
+      },
+    ],
+  ],
 };
