8aded876a22b906838675ba88a7060174b99d531
cassie spain <58054751+dreamwasp@users.noreply.github.com>
2022-10-03 15:19:32 -0400

    feat(Markdown): add indentation to details
    Adds indentation to the post-summary details text

--- packages/gamut/src/Markdown/libs/overrides/Details/index.tsx
+++ packages/gamut/src/Markdown/libs/overrides/Details/index.tsx
@@ -1,5 +1,6 @@
 import React, { HTMLAttributes, useMemo } from 'react';
 
+import { Box } from '../../../../Box';
 import { HTMLToReactNode } from '..';
 // import styles from '../../../styles/_'
 export interface MarkdownDetailsProps
@@ -8,23 +9,54 @@ export interface MarkdownDetailsProps
   children?: HTMLToReactNode[];
 }
 
+interface GetDetailChildrenProps extends MarkdownDetailsProps {
+  summaryIndex?: number;
+  hasSummary: boolean;
+}
+
+const getStyledDetailChildren = ({
+  summaryIndex,
+  hasSummary,
+  children,
+}: GetDetailChildrenProps) => {
+  if (children && summaryIndex) {
+    const copiedChildren = hasSummary
+      ? children.slice(0, summaryIndex).concat(children.slice(summaryIndex + 1))
+      : children;
+
+    const summary = hasSummary ? (
+      children[summaryIndex]
+    ) : (
+      <summary>Details</summary>
+    );
+
+    return {
+      summary,
+      children: [<Box ml={24}>{copiedChildren.map((elem) => elem)}</Box>],
+    };
+  }
+};
+
 /**
  * Details element
  * By default, the <summary> element is not required, but the default "details" text is not easily styled
- * This ensures we always have a summary element to style.
+ * This ensures we always have a summary element to style and that the details are indented
  */
 export const Details: React.FC<MarkdownDetailsProps> = ({
   children,
   ...props
 }) => {
-  const isMissingSummary = useMemo(() => {
-    return children?.every((e) => e.type !== 'summary');
+  const editedDetails = useMemo(() => {
+    const summaryIndex = children?.findIndex((e) => e.type === 'summary');
+    const hasSummary = Boolean(summaryIndex && summaryIndex > 0 && children);
+
+    return getStyledDetailChildren({ summaryIndex, hasSummary, children });
   }, [children]);
-  const summary = isMissingSummary ? <summary>Details</summary> : null;
+
   return (
     <details data-testid="gamut-md-details" {...props}>
-      {summary}
-      {children}
+      {editedDetails?.summary}
+      {editedDetails?.children}
     </details>
   );
 };
