2307e50d86121ac18a599aae669b4cdf80414af8
cass <cass@codecademy.com>
2022-10-19 14:36:28 -0400

    lol

--- packages/gamut-styles/src/cache/stylisPlugins/focusVisible.ts
+++ packages/gamut-styles/src/cache/stylisPlugins/focusVisible.ts
@@ -2,12 +2,15 @@ import { StylisPlugin } from '@emotion/cache';
 
 export const focusVisible: StylisPlugin = (element) => {
   if (element.type === 'rule' && element.value.includes(':focus-visible')) {
-    console.log('ele 2', element);
+    console.log('ele', element);
     element.props = element.props.map((prop) => {
       const selector = process.env.NODE_ENV === 'dev' ? `${prop}, ` : '';
-      return (
-        selector + prop.replace(/:focus-visible/g, '[data-focus-visible-added]')
-      );
+      console.log(selector, 'selector');
+      const hey =
+        selector +
+        prop.replace(/:focus-visible/g, '[data-focus-visible-added]');
+      console.log(hey, 'returning');
+      return hey;
     });
   }
   return undefined;
