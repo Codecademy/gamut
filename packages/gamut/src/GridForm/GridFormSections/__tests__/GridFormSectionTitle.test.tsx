56f31c25706a7c76f943b75a88f9307707093ea8
cass <cass@codecademy.com>
2022-10-25 17:13:43 -0400

    more

--- packages/gamut/src/GridForm/GridFormSections/__tests__/GridFormSectionTitle.test.tsx
+++ packages/gamut/src/GridForm/GridFormSections/__tests__/GridFormSectionTitle.test.tsx
@@ -1,46 +1,47 @@
-import { setupEnzyme } from '@codecademy/gamut-tests';
+import { setupRtl } from '@codecademy/gamut-tests';
 
 import { GridFormSectionTitle } from '../GridFormSectionTitle';
 
-const renderWrapper = setupEnzyme(GridFormSectionTitle, {
+const renderView = setupRtl(GridFormSectionTitle, {
   title: 'Updog',
   numberOfFields: 3,
 });
 
 describe('GridFormSectionTitle', () => {
   it('renders a title that defaults to h2', () => {
-    const { wrapper } = renderWrapper();
-    const title = wrapper.find('h2');
+    const { view } = renderView();
 
-    expect(title.text()).toEqual('Updog');
+    expect(view.getByRole('heading', { level: 2 })).toHaveTextContent('Updog');
   });
 
   it('renders a title that can be set to other header tags', () => {
-    const { wrapper } = renderWrapper({ as: 'h3' });
-    const title = wrapper.find('h3');
+    const { view } = renderView({ as: 'h3' });
 
-    expect(title.text()).toEqual('Updog');
+    expect(view.getByRole('header', { level: 3 })).toHaveTextContent('Updog');
   });
 
   it('renders a title that can be styled with Text props', () => {
-    const { wrapper } = renderWrapper({ as: 'h3', variant: 'title-xxl' });
-    const titleNode = wrapper.find('h3').getDOMNode();
+    const { view } = renderView({ as: 'h3', variant: 'title-xxl' });
+    const titleNode = view.getByRole('header', { level: 3 });
+    const titleNodeStyle = getComputedStyle(titleNode);
 
-    expect(titleNode).toHaveStyle('font-size: 4rem');
+    expect(titleNodeStyle.fontSize).toHaveStyle('4rem');
   });
 
   it('renders the proper Column size when layout is set to center', () => {
-    const { wrapper } = renderWrapper();
-    const column = wrapper.find('Column') as any;
-    const { size, gridRowEnd } = column.props();
+    const { view } = renderView();
 
-    expect(size).toEqual({ _: 12, md: 12 });
-    expect(gridRowEnd).toEqual({ _: 'span 1', md: 'span 1' });
+    console.log(view.debug());
+    // const column = view.find('Column') as any;
+    // const { size, gridRowEnd } = column.props();
+
+    // expect(size).toEqual({ _: 12, md: 12 });
+    // expect(gridRowEnd).toEqual({ _: 'span 1', md: 'span 1' });
   });
 
   it('renders the proper Column size according to number of fields when layout is set to left', () => {
-    const { wrapper } = renderWrapper({ layout: 'left' });
-    const column = wrapper.find('Column') as any;
+    const { view } = renderView({ layout: 'left' });
+    const column = view.find('Column') as any;
     const { size, gridRowEnd } = column.props();
 
     expect({ gridRowEnd, size }).toEqual({
