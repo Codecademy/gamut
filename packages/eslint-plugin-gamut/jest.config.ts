7dca45ad3183425d82121e24cbda340ec545faed
Jake Hiller <jakemhiller@gmail.com>
2023-01-07 10:53:00 -0500

    add ts-jest config

--- packages/eslint-plugin-gamut/jest.config.ts
+++ packages/eslint-plugin-gamut/jest.config.ts
@@ -2,6 +2,14 @@ import base from '../../jest.config.base';
 
 export default base('eslint-plugin-gamut', {
   testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
+  },
   setupFiles: [],
   setupFilesAfterEnv: [],
 });
