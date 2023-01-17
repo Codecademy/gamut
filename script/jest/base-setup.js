5531c4da62310bf2ab8fd76231412a7b609dbaf0
Jake Hiller <jakemhiller@gmail.com>
2023-01-17 14:50:35 -0500

    mock scrollTo

--- script/jest/base-setup.js
+++ script/jest/base-setup.js
@@ -1,4 +1,9 @@
 // Set fixed date
 Date.now = jest.fn(() => new Date(2011, 6, 1).valueOf());
 
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+function noop() {}
+
+Object.defineProperty(window, 'scrollTo', { value: noop, writable: true });
+
 require('mutationobserver-shim');
