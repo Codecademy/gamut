c86330e7b42852ba767458d558d8a7a28dec9914
Jake Hiller <jakemhiller@gmail.com>
2023-01-18 14:35:42 -0500

    fix linting issue

--- packages/gamut-labs/src/PausableImage/index.tsx
+++ packages/gamut-labs/src/PausableImage/index.tsx
@@ -1,7 +1,6 @@
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import * as React from 'react';
-import { useMountedState, useRendersCount } from 'react-use';
 
 const BaseImage = React.lazy(() => import('./BaseImage'));
 
