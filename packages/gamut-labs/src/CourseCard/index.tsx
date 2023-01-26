d89f2f4f65a02353264c6db151fe8039c5abc79d
Hailey <hailey@codecademy.com>
2023-01-26 14:27:14 -0500

    broke out props in content and more storybook

--- packages/gamut-labs/src/CourseCard/index.tsx
+++ packages/gamut-labs/src/CourseCard/index.tsx
@@ -1,41 +1,47 @@
 import { CardProps } from '@codecademy/gamut';
 
-// TODO: create type for these content data
-import { CourseDataFragment } from '~/typings/generated/monolith';
-
 import { ContentGroupBaseCard } from '../ContentGroupBaseCard';
 import { CertificateComponent, Divider } from '../ContentGroupBaseCard/shared';
+import {
+  CourseDifficulty,
+  EnrollmentStatus,
+} from '../ContentGroupBaseCard/types';
 // TODO: handle moduleBasedGatingExperiment logic
-import { moduleBasedGatingExperimentTrackList } from '../CoursePages/CourseLandingPages';
+// import { moduleBasedGatingExperimentTrackList } from '../CoursePages/CourseLandingPages';
 
 type CourseCardProps = {
-  content: CourseDataFragment;
+  title: string;
+  lessonCount: number;
+  grantsCertificate: boolean;
+  shortDescription?: string | null;
+  difficulty?: CourseDifficulty | null;
+  enrollmentStatus: EnrollmentStatus;
+  pro: boolean;
   isFullSize?: boolean;
   minHeight?: CardProps['minHeight'];
   minWidth?: CardProps['minWidth'];
 };
 
 export const CourseCard: React.FC<CourseCardProps> = ({
-  content,
+  title,
+  lessonCount,
+  grantsCertificate,
+  shortDescription,
+  difficulty,
+  enrollmentStatus,
+  pro,
   isFullSize,
   minHeight,
   minWidth,
 }) => {
-  const {
-    title,
-    grantsCertificate,
-    shortDescription,
-    difficulty,
-    lessonCount,
-    pro,
-    enrollmentStatus,
-  } = content;
   // only show certificates for pro exclusive courses, since only pro users can do pro courses & only pro users can gain certificates
   // edge case here: pro users who are logged in wont be able to tell - from the card - that a free course can grant them a cert as well
 
-  const inModuleBasedGatingExp = moduleBasedGatingExperimentTrackList.includes(
-    content.slug
-  );
+  // const inModuleBasedGatingExp = moduleBasedGatingExperimentTrackList.includes(
+  //   content.slug
+  // );
+
+  const inModuleBasedGatingExp = true;
 
   const showCertificate = grantsCertificate && pro;
   return (
