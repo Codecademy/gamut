1b1e3c6200b951a008f33ca8f73abf5512f2863f
Vinnie Olsen <vinnie.olsen@gmail.com>
2023-01-26 13:10:07 -0600

    feat(GlobalFooter): Change 'Paid Memberships' URL to '/pages/paid-plans'

--- packages/gamut-labs/src/GlobalFooter/FooterNavLinks/CompanyLinks.tsx
+++ packages/gamut-labs/src/GlobalFooter/FooterNavLinks/CompanyLinks.tsx
@@ -116,7 +116,7 @@ export const CompanyLinks: React.FC<CompanyLinksProps> = ({
       <FooterLinkItems>
         <FooterLinkItem>
           <Anchor
-            href="/pages/pro"
+            href="/pages/paid-plans"
             onClick={(event) => onClick({ event, target: 'pro_membership' })}
             variant="interface"
           >
