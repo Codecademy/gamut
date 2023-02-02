7cf1ad0f4aed837110d85a6bcc3f171a1dfb8d60
Nar Medhat <nar@codecademy.com>
2023-02-02 16:03:33 -0500

    feat(CareerPathCard): New CareerPathCard

---
+++ packages/gamut-labs/src/CareerPathCard/index.tsx
@@ -0,0 +1,62 @@
+import { CardProps } from '@codecademy/gamut';
+
+import { getPathImageUrl } from '../ContentGroupBaseCard/helpers';
+import { ContentGroupBaseCard } from '../ContentGroupBaseCard/index';
+import {
+  CertificateComponent,
+  CourseCountComponent,
+  Divider,
+} from '../ContentGroupBaseCard/shared';
+import {
+  CourseDifficulty,
+  EnrollmentStatus,
+} from '../ContentGroupBaseCard/types';
+
+type CareerPathCardProps = {
+  title: string;
+  lessonCount: number;
+  enrollmentStatus: EnrollmentStatus;
+  shortDescription?: string | null;
+  difficulty?: CourseDifficulty | null;
+  imageUrl: string;
+  courseCount: number;
+  isFullSize?: boolean;
+  minHeight?: CardProps['minHeight'];
+  minWidth?: CardProps['minWidth'];
+};
+
+export const CareerPathCard: React.FC<CareerPathCardProps> = ({
+  imageUrl,
+  courseCount,
+  shortDescription,
+  difficulty,
+  enrollmentStatus,
+  lessonCount,
+  title,
+  isFullSize,
+  minHeight,
+  minWidth,
+}) => {
+  return (
+    <ContentGroupBaseCard
+      headerBackgroundColor="navy"
+      headerText="Career path"
+      title={title}
+      headingLevel="h3"
+      description={shortDescription}
+      difficulty={difficulty}
+      numLessons={lessonCount}
+      shadow="outline"
+      imageSrc={getPathImageUrl(enrollmentStatus, imageUrl)}
+      isFullSize={isFullSize}
+      enrollmentStatus={enrollmentStatus}
+      minHeight={minHeight}
+      minWidth={minWidth}
+    >
+      <Divider />
+      <CourseCountComponent count={courseCount} />
+      <Divider />
+      <CertificateComponent />
+    </ContentGroupBaseCard>
+  );
+};
