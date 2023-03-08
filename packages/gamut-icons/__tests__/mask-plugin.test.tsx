f6b6f9bf736eee23c8f193ca78a59d2c20b9b1b2
cass <cass@codecademy.com>
2023-03-08 16:13:59 -0500

    add mask plugin text

--- packages/gamut-icons/__tests__/mask-plugin.test.tsx
+++ packages/gamut-icons/__tests__/mask-plugin.test.tsx
@@ -2,8 +2,18 @@ import { transform } from '@babel/core';
 
 import plugin from '../mask-plugin';
 
+const maskIdString = '`${maskId}`';
+const urlIdString = '`url(#${maskId})`';
+const size = '`100%`';
+const currentColor = '`currentColor`';
+
 const menuIcon =
-  '<svg viewBox="0 0 24 24"><title>navigation-menu</title><line class="a" x1="2.25" y1="18.003" x2="21.75" y2="18.003"/><line class="a" x1="2.25" y1="12.003" x2="21.75" y2="12.003"/><line class="a" x1="2.25" y1="6.003" x2="21.75" y2="6.003"/></svg>';
+  "<svg viewBox='0 0 16 16' height='16' width='16'><title>mini-bold-interface-arrow-up</title><g transform='matrix(1.6,0,0,1.6,0,0)'><path d='M8.03,2.72,5.53.22a.749.749,0,0,0-1.06,0l-2.5,2.5A.75.75,0,0,0,3.03,3.78L4.25,2.561V9.25a.75.75,0,0,0,1.5,0V2.561L6.97,3.78A.75.75,0,1,0,8.03,2.72Z' fill='#000000' stroke='none' stroke-linecap='round' stroke-linejoin='round' stroke-width='0'></path></g></svg>";
+
+const menuIconTitleEnd =
+  "<svg viewBox='0 0 16 16' height='16' width='16'><g transform='matrix(1.6,0,0,1.6,0,0)'><path d='M8.03,2.72,5.53.22a.749.749,0,0,0-1.06,0l-2.5,2.5A.75.75,0,0,0,3.03,3.78L4.25,2.561V9.25a.75.75,0,0,0,1.5,0V2.561L6.97,3.78A.75.75,0,1,0,8.03,2.72Z' fill='#000000' stroke='none' stroke-linecap='round' stroke-linejoin='round' stroke-width='0'></path></g><title>mini-bold-interface-arrow-up</title></svg>";
+
+const postTransformIcon = `"<svg viewBox='0 0 16 16' height='16' width='16'><title>mini-bold-interface-arrow-up</title><mask id={${maskIdString}}><g transform='matrix(1.6,0,0,1.6,0,0)'><path d='M8.03,2.72,5.53.22a.749.749,0,0,0-1.06,0l-2.5,2.5A.75.75,0,0,0,3.03,3.78L4.25,2.561V9.25a.75.75,0,0,0,1.5,0V2.561L6.97,3.78A.75.75,0,1,0,8.03,2.72Z' fill='#000000' stroke='none' stroke-linecap='round' stroke-linejoin='round' stroke-width='0'></path></g></mask><g mask={${urlIdString}}><rect width={${size}} height={${size}} fill={${currentColor}} /></g></svg>;"`;
 
 const testPlugin = (code: string) => {
   const result = transform(code, {
@@ -15,14 +25,12 @@ const testPlugin = (code: string) => {
 };
 
 describe('title plugin', () => {
-  it('should add title attribute if not present', () => {
-    expect(testPlugin(menuIcon)).toMatchInlineSnapshot(
-      `"<svg><mask id={'updogId'}><title>updog</title><div /></mask></svg>;"`
-    );
+  it('should add a mask wrapper, add a mask id, and applies currentColor to the rect tag', () => {
+    expect(testPlugin(menuIcon)).toMatchInlineSnapshot(postTransformIcon);
   });
-  it('also', () => {
-    expect(testPlugin(menuIcon)).toMatchInlineSnapshot(
-      `"<svg><mask id={'updogId'}><title>updog</title><div /></mask></svg>;"`
+  it('sets title outside of mask regardless of placement', () => {
+    expect(testPlugin(menuIconTitleEnd)).toMatchInlineSnapshot(
+      postTransformIcon
     );
   });
 });
