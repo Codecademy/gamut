ac73fddcbd19afe858c168a3b14ba79f0e1e2897
sooinc <sooin@codecademy.com>
2023-03-24 14:15:44 -0400

    add warning illustration

---
+++ packages/gamut-illustrations/src/Warning.tsx
@@ -0,0 +1,115 @@
+import * as React from 'react';
+
+import { IllustrationProps } from './types';
+
+export const Warning: React.FC<IllustrationProps> = ({
+  'aria-hidden': ariaHidden,
+  className,
+  height,
+  width,
+}) => (
+  <svg
+    aria-hidden={ariaHidden}
+    className={className}
+    height={height}
+    width={width}
+    viewBox="0 0 275 275"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g clipPath="url(#clip0_1192_39433)">
+      <path
+        d="M100.551 156.308C140.62 156.308 173.103 123.854 173.103 83.8199C173.103 43.7857 140.62 11.3315 100.551 11.3315C60.4824 11.3315 28 43.7857 28 83.8199C28 123.854 60.4824 156.308 100.551 156.308Z"
+        fill="#1D2340"
+        stroke="#141C3A"
+        strokeWidth="0.663264"
+      />
+      <path
+        d="M100.552 132.269C127.332 132.269 149.042 110.578 149.042 83.82C149.042 57.0624 127.332 35.3711 100.552 35.3711C73.7707 35.3711 52.0605 57.0624 52.0605 83.82C52.0605 110.578 73.7707 132.269 100.552 132.269Z"
+        fill="white"
+        stroke="#141C3A"
+        strokeWidth="0.663264"
+      />
+      <path
+        d="M100.551 110.079C115.066 110.079 126.833 98.3223 126.833 83.8201C126.833 69.3179 115.066 57.5615 100.551 57.5615C86.0366 57.5615 74.27 69.3179 74.27 83.8201C74.27 98.3223 86.0366 110.079 100.551 110.079Z"
+        fill="#E91C11"
+        stroke="#141C3A"
+        strokeWidth="0.663264"
+      />
+      <path
+        d="M100.551 95.285C106.889 95.285 112.026 90.1519 112.026 83.82C112.026 77.488 106.889 72.355 100.551 72.355C94.2137 72.355 89.0762 77.488 89.0762 83.82C89.0762 90.1519 94.2137 95.285 100.551 95.285Z"
+        fill="#FFD300"
+        stroke="#141C3A"
+        strokeWidth="0.663264"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M111.889 68.635L118.736 88.4949L102.724 82.8943L111.889 68.635Z"
+        fill="#141C3A"
+        stroke="#141C3A"
+        strokeWidth="0.663264"
+      />
+      <path
+        d="M116.042 78.4165L212.499 45.2325"
+        stroke="#141C3A"
+        strokeWidth="0.663264"
+        strokeLinecap="square"
+      />
+      <path
+        d="M204.512 47.6357L208.872 38.7007"
+        stroke="#141C3A"
+        strokeWidth="0.663264"
+        strokeLinecap="square"
+      />
+      <path
+        d="M198.649 49.5884L203.008 40.6534"
+        stroke="#141C3A"
+        strokeWidth="0.663264"
+        strokeLinecap="square"
+      />
+      <path
+        d="M192.786 51.541L197.145 42.606"
+        stroke="#141C3A"
+        strokeWidth="0.663264"
+        strokeLinecap="square"
+      />
+      <path
+        d="M204.663 47.8223L213.606 52.1825"
+        stroke="#141C3A"
+        strokeWidth="0.663264"
+        strokeLinecap="square"
+      />
+      <path
+        d="M198.8 50.2629L207.743 54.6232"
+        stroke="#141C3A"
+        strokeWidth="0.663264"
+        strokeLinecap="square"
+      />
+      <path
+        d="M192.938 52.2158L201.88 56.576"
+        stroke="#141C3A"
+        strokeWidth="0.663264"
+        strokeLinecap="square"
+      />
+      <path
+        d="M220.02 44.0231C238.096 51.5746 256.68 32.8106 248.213 21.0109C243.502 14.4452 233.409 15.5012 234.923 24.6314C236.803 35.9701 258.201 38.2025 264.512 19.7506C265.499 16.8632 266.524 14.7798 266.396 11.6867"
+        stroke="#10162F"
+        strokeWidth="0.663264"
+        strokeLinecap="square"
+        strokeLinejoin="round"
+        strokeDasharray="1.33 1.99"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_1192_39433">
+        <rect
+          width="243.086"
+          height="145.918"
+          fill="white"
+          transform="translate(28 11)"
+        />
+      </clipPath>
+    </defs>
+  </svg>
+);
