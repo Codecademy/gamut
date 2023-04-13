96ac2f1181d9c31b06e72ab1fd14ccaa66c1daf3
Hailey <hailey@codecademy.com>
2023-04-13 13:40:35 -0400

    new illustration

--- packages/gamut-illustrations/src/index.ts
+++ packages/gamut-illustrations/src/index.ts
@@ -5,6 +5,7 @@ export * from './BinaryBlocks';
 export * from './BinaryStairs';
 export * from './Blocks';
 export * from './Blueprint';
+export * from './BlueprintWhite';
 export * from './Browser';
 export * from './BrowserLock';
 export * from './BrowserPuzzle';
