c574ed70ba7bcb9ee33829256af6dda42ae84d09
Hailey <hailey@codecademy.com>
2023-04-14 12:00:43 -0400

    feat(Illustration): Add new illustrations for upcoming dashboard component

---
+++ packages/gamut-illustrations/src/BookAndNotepad.tsx
@@ -0,0 +1,107 @@
+import * as React from 'react';
+
+import { IllustrationProps } from './types';
+
+export const BookAndNotepad: React.FC<IllustrationProps> = ({
+  'aria-hidden': ariaHidden,
+  className,
+  height,
+  width,
+}) => (
+  <svg
+    aria-hidden={ariaHidden}
+    className={className}
+    width={width}
+    height={height}
+    viewBox="0 0 236 174"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+  >
+    <title>BookAndNotepad Illustration</title>
+    <path
+      fill="#CCA900"
+      stroke="#10162F"
+      strokeMiterlimit="10"
+      d="M73.928 27.83H11c-5.523 0-10 4.477-10 10v100.002c0 5.523 4.477 10 10 10h62.928c5.523 0 10-4.477 10-10V37.83c0-5.523-4.477-10-10-10Z"
+    />
+    <path
+      fill="#FFD300"
+      stroke="#10162F"
+      strokeMiterlimit="10"
+      d="M93.686 27.83H10.757v99.514h82.929V27.83Z"
+    />
+    <path
+      fill="#fff"
+      stroke="#10162F"
+      strokeMiterlimit="10"
+      d="M11.427 127.342h82.258v25.367H11.427c-2.765 0-5.418-1.336-7.373-3.715C2.099 146.615 1 143.389 1 140.026c0-3.364 1.099-6.59 3.054-8.969 1.955-2.378 4.608-3.715 7.373-3.715Z"
+    />
+    <path
+      stroke="#10162F"
+      strokeMiterlimit="10"
+      d="M93.814 132.624h-76.12M93.815 139.689H16.91M93.816 145.968H18.481"
+    />
+    <path fill="#fff" d="M85.826 37.116h-66.87v40.375h66.87V37.116Z" />
+    <path
+      fill="#10162F"
+      stroke="#10162F"
+      strokeMiterlimit="10"
+      d="M14.955 21h8.06c.856 0 1.676.307 2.28.853.605.546.945 1.286.945 2.059v3.396H11.73v-3.396c0-.773.34-1.513.945-2.06a3.408 3.408 0 0 1 2.28-.852ZM48.806 148.418H32.442a5.423 5.423 0 0 1-2.027-.393 5.298 5.298 0 0 1-1.718-1.116 5.156 5.156 0 0 1-1.148-1.672 5.027 5.027 0 0 1-.403-1.971v-15.924H12.707v21.076c0 1.844.373 3.671 1.099 5.375a14.038 14.038 0 0 0 3.13 4.558 14.48 14.48 0 0 0 4.684 3.045 14.79 14.79 0 0 0 5.526 1.069h21.66l-8.035-7.022 8.035-7.025Z"
+    />
+    <path fill="#CCA900" d="M182.345 68.026h-78.226V173.38h78.226V68.026Z" />
+    <path
+      fill="#fff"
+      stroke="#10162F"
+      strokeMiterlimit="10"
+      d="M188.022 62.982h-78.857v105.353h78.857V62.982Z"
+    />
+    <path
+      fill="#CCA900"
+      stroke="#10162F"
+      strokeMiterlimit="10"
+      d="M80 42H24v31h56V42Z"
+    />
+    <path
+      fill="#FFD300"
+      stroke="#10162F"
+      strokeMiterlimit="10"
+      d="m173.411 135.137 2.5-1.592-31.106-111.377-2.484.723 31.09 112.246Z"
+    />
+    <path
+      fill="#FFD300"
+      stroke="#10162F"
+      strokeMiterlimit="10"
+      d="m171.331 136.111 2.487-.726-31.129-112.386-2.488.721-2.487.726 31.128 112.386 2.489-.721Z"
+    />
+    <path
+      fill="#FFD300"
+      stroke="#10162F"
+      strokeMiterlimit="10"
+      d="m166.038 137.321 2.799-.814-31.09-112.246-2.798.814 31.089 112.246Z"
+    />
+    <path
+      fill="#FFFAE5"
+      stroke="#10162F"
+      strokeMiterlimit="10"
+      d="M174.563 149.75a6.379 6.379 0 0 1 2.084-.306l-.654-15.498-2.983-1.205-1.988 2.802-2.981-1.208-1.991 2.801 6.589 13.609a6.852 6.852 0 0 1 1.924-.995Z"
+    />
+    <path
+      fill="#10162F"
+      stroke="#10162F"
+      strokeLinejoin="round"
+      strokeMiterlimit="10"
+      d="m172.883 150.969 4.929 8.554-1.099-9.456c-1.38-.119-2.726.199-3.83.902Z"
+    />
+    <path
+      fill="#FFFAE5"
+      stroke="#10162F"
+      strokeMiterlimit="10"
+      d="m144.804 22.167-9.923 2.886-3.781-13.651a5.232 5.232 0 0 1 .478-3.944 5.237 5.237 0 0 1 3.104-2.478 5.1 5.1 0 0 1 3.912.438 5.103 5.103 0 0 1 2.429 3.098l3.781 13.65Z"
+    />
+    <path
+      stroke="#10162F"
+      strokeMiterlimit="10"
+      d="m144.052 18.515-10.099 1.966"
+    />
+  </svg>
+);
