4cd949d2feba554a2fb31a643240f8a3b900b065
vonbarown <voniel@codecademy.com>
2023-05-11 11:32:15 -0400

    update story

--- packages/gamut/src/Markdown/libs/overrides/index.tsx
+++ packages/gamut/src/Markdown/libs/overrides/index.tsx
@@ -202,7 +202,7 @@ export const standardOverrides = [
   },
 ];
 
-// img tag override
+// Allows for img tag override, which is separate because it doesn't have children
 export const createImgOverride = (
   tagName: string,
   Override: OverrideSettings
