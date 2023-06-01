f7cd5ca5d18960139caf89692cec3c9211809597
cassie spain <58054751+dreamwasp@users.noreply.github.com>
2023-06-01 17:08:33 -0400

    first mass delete

--- packages/gamut-labs/src/LayoutMenuVariant/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './LayoutMenu';
-export * from './SectionItemLink';
--- packages/gamut-labs/src/Notifications/constants.ts
+++
@@ -1,4 +0,0 @@
-/**
- * How many notifications are visible in the list at once by default.
- */
-export const defaultDisplayLimit = 3;
