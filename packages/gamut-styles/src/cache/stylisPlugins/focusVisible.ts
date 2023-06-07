c4ff54683e9351c905303ba387a0d55cdf5daa13
John Rood <mr.john.rood@gmail.com>
2023-06-07 14:28:17 -0500

    fix(focusVisible): enable always for both native and polyfill

--- packages/gamut-styles/src/cache/stylisPlugins/focusVisible.ts
+++ packages/gamut-styles/src/cache/stylisPlugins/focusVisible.ts
@@ -3,7 +3,7 @@ import { StylisPlugin } from '@emotion/cache';
 export const focusVisible: StylisPlugin = (element) => {
   if (element.type === 'rule' && element.value.includes(':focus-visible')) {
     element.props = element.props.map((prop) => {
-      const selector = process.env.NODE_ENV === 'dev' ? `${prop}, ` : '';
+      const selector = `${prop}, `;
       return (
         selector + prop.replace(/:focus-visible/g, '[data-focus-visible-added]')
       );
