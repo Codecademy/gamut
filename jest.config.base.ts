b1467520b66c12dc892b751e1d5934aa105641b7
Jake Hiller <jakemhiller@gmail.com>
2023-09-08 11:24:55 -0400

    fix jest warnings

--- jest.config.base.ts
+++ jest.config.base.ts
@@ -5,10 +5,6 @@ const baseConfig = (packageName: string, overrides: Object) => ({
   coverageDirectory: process.env.CI
     ? `/tmp/test-results/jest/${packageName}`
     : `../../coverage/packages/${packageName}`,
-  reporters: process.env.CI
-    ? ['default', ['jest-junit', { outputName: `${packageName}-report.xml` }]]
-    : ['default'],
-  coverageReporters: ['json', 'text', 'clover'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|md)$':
       '<rootDir>/../../script/jest/fileMock',
--- jest.preset.js
+++ jest.preset.js
@@ -2,4 +2,8 @@ const nxPreset = require('@nrwl/jest/preset').default;
 
 module.exports = {
   ...nxPreset,
+  coverageReporters: ['json', 'text', 'clover'],
+  reporters: process.env.CI
+    ? ['default', ['jest-junit', { outputName: `${packageName}-report.xml` }]]
+    : ['default'],
 };
