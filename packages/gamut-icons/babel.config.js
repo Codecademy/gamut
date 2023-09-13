76820fdc0bded1ef7ea6ba05d3625c76e4e512bb
Jake Hiller <jakemhiller@gmail.com>
2023-09-13 15:49:50 -0400

    remove usage of outdated babel-preset-codecademy

--- packages/gamut-icons/babel.config.js
+++ packages/gamut-icons/babel.config.js
@@ -1,4 +1,19 @@
 module.exports = {
   extends: '../../babel.defaults.js',
-  presets: ['codecademy', '@babel/preset-typescript'],
+  presets: [
+    [
+      '@babel/env',
+      {
+        modules: process.env.NODE_ENV === 'test' ? 'commonjs' : false,
+        targets: 'defaults',
+      },
+    ],
+    [
+      '@babel/react',
+      {
+        runtime: 'automatic',
+      },
+    ],
+    '@babel/preset-typescript',
+  ],
 };
--- packages/gamut-patterns/babel.config.js
+++ packages/gamut-patterns/babel.config.js
@@ -1,4 +1,29 @@
 module.exports = {
   extends: '../../babel.defaults.js',
-  presets: ['codecademy', '@babel/preset-typescript'],
+  presets: [
+    [
+      '@babel/env',
+      {
+        modules: process.env.NODE_ENV === 'test' ? 'commonjs' : false,
+        targets: 'defaults',
+      },
+    ],
+    [
+      '@babel/react',
+      {
+        runtime: 'automatic',
+      },
+    ],
+    '@babel/preset-typescript',
+  ],
+  plugins: [
+    [
+      '@emotion/babel-plugin',
+      {
+        sourceMap: true,
+        autoLabel: 'always',
+        labelFormat: '[local]',
+      },
+    ],
+  ],
 };
