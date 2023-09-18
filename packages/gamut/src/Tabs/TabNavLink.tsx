adaf79d048ca072c22e66fdab120029470a29ac9
Cass Spain <cass.spain@skillsoft.com>
2023-09-18 17:23:06 -0400

    form group + tab link

--- packages/gamut/src/Tabs/TabNavLink.tsx
+++ packages/gamut/src/Tabs/TabNavLink.tsx
@@ -2,9 +2,18 @@ import styled from '@emotion/styled';
 
 import { TabButton } from './TabButton';
 
-export const TabNavLink = styled(TabButton)();
+const StyledTabNavLink = styled(TabButton)();
 
+<<<<<<< HEAD
 TabNavLink.defaultProps = {
   variant: 'standard',
   role: 'tab',
+=======
+export const TabNavLink: React.FC<ComponentProps<typeof StyledTabNavLink>> = ({
+  variant = 'standard',
+  roles = 'tab',
+  ...rest
+}) => {
+  return <StyledTabNavLink roles={roles} variant={variant} {...rest} />;
+>>>>>>> 678a6f54d (form group)
 };
