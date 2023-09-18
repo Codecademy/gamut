a6e151688df6de02bf6e9a44b4a4fbb14ab425f5
Cass Spain <cass.spain@skillsoft.com>
2023-09-18 17:00:46 -0400

    chore: remove js defaults

--- packages/gamut/src/Tabs/TabNavLink.tsx
+++ packages/gamut/src/Tabs/TabNavLink.tsx
@@ -1,10 +1,14 @@
 import styled from '@emotion/styled';
+import { ComponentProps } from 'react';
 
 import { TabButton } from './TabButton';
 
 export const TabNavLink = styled(TabButton)();
 
-TabNavLink.defaultProps = {
-  variant: 'standard',
-  role: 'tab',
+export const TabNavLink2: React.FC<ComponentProps<typeof TabNavLink>> = ({
+  variant = 'standard',
+  roles = 'tab',
+  ...rest
+}) => {
+  return <TabNavLink roles={roles} variant={variant} {...rest} />;
 };
