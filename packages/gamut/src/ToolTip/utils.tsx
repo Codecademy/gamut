6caabd8969ef3d4b32d3d25ecc86490be25b1eca
cassie spain <58054751+dreamwasp@users.noreply.github.com>
2023-09-25 10:01:11 -0400

    feat(Tooltip): add aria-described by to floating tooltip
    Swaps `aria-label` to `aria-describedby` for our floating ToolTip

--- packages/gamut/src/ToolTip/utils.tsx
+++ packages/gamut/src/ToolTip/utils.tsx
@@ -92,15 +92,16 @@ export const getAccessibilityProps = ({
   isOpenPopoverToolTip,
 }: ToolTipAccessibiltyProps) => {
   // Since PopoverToolTips are removed from the DOM, when they are inactive they need an aria-label instead of aria-labelledby
-  const ariaLabel =
+  const labeling =
     isOpenPopoverToolTip === undefined || isOpenPopoverToolTip
       ? { 'aria-labelledby': id }
-      : { 'aria-label': id };
+      : { 'aria-describedby': id };
   // ToolTips sometimes contain actual <button>s, which cannot be a child of a button.
   // This element still needs tab focus so we must use the `tabIndex=0` hack.
   return {
-    ...ariaLabel,
+    ...labeling,
     role: focusable ? 'button' : undefined,
     tabIndex: focusable ? 0 : undefined,
+    'aria-label': focusable ? 'tooltip' : undefined,
   };
 };
