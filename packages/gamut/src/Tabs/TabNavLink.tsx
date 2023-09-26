37fe0f51168db40adb1125bb3fa84daf1f6eb1cc
dreamwasp <cass.spain@skillsoft.com>
2023-09-26 11:24:52 -0400

    tab link merge conf fix

--- packages/gamut/src/Tabs/TabNavLink.tsx
+++ packages/gamut/src/Tabs/TabNavLink.tsx
@@ -1,19 +1,14 @@
 import styled from '@emotion/styled';
+import { ComponentProps } from 'react';
 
 import { TabButton } from './TabButton';
 
 const StyledTabNavLink = styled(TabButton)();
 
-<<<<<<< HEAD
-TabNavLink.defaultProps = {
-  variant: 'standard',
-  role: 'tab',
-=======
 export const TabNavLink: React.FC<ComponentProps<typeof StyledTabNavLink>> = ({
   variant = 'standard',
   roles = 'tab',
   ...rest
 }) => {
   return <StyledTabNavLink roles={roles} variant={variant} {...rest} />;
->>>>>>> 678a6f54d (form group)
 };
