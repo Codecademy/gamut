42972efedfd6d06923ba03225ae923e0525c18a7
dreamwasp <cass.spain@skillsoft.com>
2023-12-11 16:54:01 -0500

    remove test types

--- packages/gamut/src/Button/TextButton.tsx
+++ packages/gamut/src/Button/TextButton.tsx
@@ -1,12 +1,8 @@
-import { createButtonComponent, createButtonComponent2 } from './shared';
+import { createButtonComponent } from './shared';
 import { sizeVariants, textButtonVariants } from './variants';
 
 export const TextButton = createButtonComponent(
   'Text',
-  textButtonVariants,
-  sizeVariants
-);
-export const TextButtonAgain = createButtonComponent2(
   textButtonVariants,
   sizeVariants
 );
