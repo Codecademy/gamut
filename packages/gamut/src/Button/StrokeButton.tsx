eeafd480bb9fa99e7b08c32fe776e1bd8dd9d995
dreamwasp <cass.spain@skillsoft.com>
2023-12-19 11:19:02 -0500

    fix button types

--- packages/gamut/src/Button/StrokeButton.tsx
+++ packages/gamut/src/Button/StrokeButton.tsx
@@ -1,3 +1,7 @@
+import { Box, Box } from 'framer-motion';
+
+import { CTAButton } from './CTAButton';
+import { IconButton } from './IconButton';
 import { createButtonComponent } from './shared';
 import { sizeVariants, strokeButtonVariants } from './variants';
 
@@ -5,3 +9,23 @@ export const StrokeButton = createButtonComponent(
   sizeVariants,
   strokeButtonVariants
 );
+
+const Hey = () => {
+  return (
+    <>
+      <Box anything="seventeen">Hey</Box>
+      <CTAButton anything="seventeen" mb={4}>
+        hey
+      </CTAButton>
+      <StrokeButton variant="no" anything="seventeen" mb={54}>
+        hey
+      </StrokeButton>
+      <IconButton
+        variant="primary"
+        anything="seventeen"
+        tabIndex="seventeen"
+        mb={4}
+      />
+    </>
+  );
+};
--- packages/gamut/src/ButtonBase/ButtonBase.tsx
+++ packages/gamut/src/ButtonBase/ButtonBase.tsx
@@ -1,6 +1,6 @@
 import { css, styledOptions } from '@codecademy/gamut-styles';
 import styled from '@emotion/styled';
-import { forwardRef, HTMLProps, MutableRefObject } from 'react';
+import { ComponentProps, forwardRef, HTMLProps, MutableRefObject } from 'react';
 
 export type ButtonBaseElements = HTMLAnchorElement | HTMLButtonElement;
 export type ButtonBaseRef =
@@ -55,12 +55,18 @@ export const resetStyles = css({
 });
 
 const ResetElement = styled('button', styledOptions<'button'>())(resetStyles);
+const ResetElementAnchor = styled('a', styledOptions<'a'>())(resetStyles);
+
+type ButtonBaseProps =
+  | ComponentProps<typeof ResetElement>
+  | (Exclude<ComponentProps<typeof ResetElement>, 'ref'> &
+      ComponentProps<typeof ResetElementAnchor>);
 
 export const ButtonBase = forwardRef<
   HTMLButtonElement | HTMLAnchorElement,
-  any
->(({ href, disabled, children, role, type = 'button', ...rest }, ref) => {
-  if (href == null || disabled) {
+  ButtonBaseProps
+>(({ disabled, children, role, type = 'button', ...rest }, ref) => {
+  if (!('href' in rest) || rest?.href === null || disabled) {
     return (
       <ResetElement
         {...rest}
@@ -76,15 +82,15 @@ export const ButtonBase = forwardRef<
   }
 
   return (
-    <ResetElement
+    <ResetElementAnchor
       {...rest}
       ref={ref as MutableRefObject<HTMLAnchorElement>}
       as="a"
-      href={href}
+      href={rest?.href}
       role={role}
       {...rest}
     >
       {children}
-    </ResetElement>
+    </ResetElementAnchor>
   );
 });
