f21f49767b966b4171a5b1f9d4634c26334bb887
dreamwasp <cass.spain@skillsoft.com>
2024-02-02 10:46:50 -0500

    revert unneeded alert change

--- packages/gamut/src/Alert/Alert.tsx
+++ packages/gamut/src/Alert/Alert.tsx
@@ -37,7 +37,7 @@ export type AlertProps = WithChildrenProp &
     /** Callback to be called when the close icon is clicked */
     onClose?: () => void;
     /** Call to Action Configuration */
-    cta?: Exclude<
+    cta?: Omit<
       React.ComponentProps<typeof FillButton>,
       'variant' | 'mode' | 'size'
     > & { text?: string };
