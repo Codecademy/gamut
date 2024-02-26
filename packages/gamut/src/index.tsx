ef0ddc2a2c4c1006085eab24971b742aa96e1099
dreamwasp <cass.spain@skillsoft.com>
2024-02-26 14:46:58 -0500

    fix index

--- packages/gamut/src/index.tsx
+++ packages/gamut/src/index.tsx
@@ -35,7 +35,6 @@ export * from './Markdown';
 export * from './Menu';
 export * from './ModalDeprecated';
 export * from './Modals';
-f;
 export * from './Overlay';
 export * from './Pagination';
 export * from './Popover';
