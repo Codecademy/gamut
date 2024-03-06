c4451912e0345e40ee1afb4d9de56fffe5dce302
dreamwasp <cass.spain@skillsoft.com>
2024-03-06 12:13:47 -0500

    test updates

--- packages/gamut/src/Tip/__tests__/ToolTip.test.tsx
+++ packages/gamut/src/Tip/__tests__/ToolTip.test.tsx
@@ -4,24 +4,42 @@ import userEvent from '@testing-library/user-event';
 import { NewToolTipMock } from './mocks';
 
 const info = 'I am information';
+const onClick = jest.fn();
 
 const renderView = setupRtl(NewToolTipMock, {
   info,
   id: 'info-id',
+  onClick,
 });
 
 describe('ToolTip', () => {
+  describe('inline placement', () => {
+    it('calls onClick when clicked', () => {
+      const { view } = renderView();
+
+      userEvent.click(view.getByRole('button'));
+
+      expect(onClick).toHaveBeenCalled();
+    });
+  });
   describe('floating placement', () => {
     it('shows the tip when it is hovered over', () => {
       const { view } = renderView({
         placement: 'floating',
       });
 
-      expect(view.queryByText(info)).toBeNull();
+      expect(view.queryByRole('tooltip')).toBeNull();
 
       userEvent.hover(view.getByRole('button'));
 
-      view.getByText(info);
+      view.getByRole('tooltip');
+    });
+    it('calls onClick when clicked', () => {
+      const { view } = renderView();
+
+      userEvent.click(view.getByRole('button'));
+
+      expect(onClick).toHaveBeenCalled();
     });
   });
 });
--- packages/gamut/src/Tip/__tests__/mocks.tsx
+++ packages/gamut/src/Tip/__tests__/mocks.tsx
@@ -1,13 +1,17 @@
+import { ComponentProps } from 'react';
+
 import { Box } from '../../Box';
 import { FillButton } from '../../Button';
 import { NewToolTip, NewToolTipProps } from '../ToolTip';
 
-export const NewToolTipMock: React.FC<NewToolTipProps> = (props) => {
+export const NewToolTipMock: React.FC<
+  NewToolTipProps & ComponentProps<typeof FillButton>
+> = ({ info, placement, onClick }) => {
   return (
     <Box width="500px">
       <Box width="200px">
-        <NewToolTip {...props}>
-          <FillButton aria-describedby={props?.id} aria-disabled>
+        <NewToolTip info={info} placement={placement} id="tip-id">
+          <FillButton aria-describedby="tip-id" onClick={onClick}>
             Click me
           </FillButton>
         </NewToolTip>
