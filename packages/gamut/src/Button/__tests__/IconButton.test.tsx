5def57932054dfe0a90cab067c8b76f5f4f57b85
dreamwasp <cass.spain@skillsoft.com>
2024-03-06 13:14:57 -0500

    more tests

---
+++ packages/gamut/src/Button/__tests__/IconButton.test.tsx
@@ -0,0 +1,49 @@
+import { StarIcon } from '@codecademy/gamut-icons';
+import userEvent from '@testing-library/user-event';
+import { setupRtl } from 'component-test-setup';
+
+import { IconButton } from '../IconButton';
+import { IconButtonFloatingMock } from './mocks';
+
+const onClick = jest.fn();
+
+const renderView = setupRtl(IconButton, {
+  icon: StarIcon,
+  onClick,
+  tip: 'Click me!',
+});
+
+const renderFloatingView = setupRtl(IconButtonFloatingMock, {
+  icon: StarIcon,
+  onClick,
+  tip: 'Click me!',
+});
+
+describe('IconButton', () => {
+  it('renders a clickable button', () => {
+    const { view } = renderView();
+
+    const cta = view.getByRole('button', { name: 'Click me!' });
+
+    userEvent.click(cta);
+
+    expect(onClick).toHaveBeenCalled();
+  });
+  it('renders an icon ', () => {
+    const { view } = renderView();
+
+    view.getByTitle('Star Icon');
+  });
+
+  it('renders a tip', async () => {
+    const { view } = renderFloatingView({});
+
+    expect(view.queryByRole('tooltip')).toBeNull();
+
+    const cta = view.getByRole('button');
+
+    userEvent.hover(cta);
+
+    view.getByRole('tooltip');
+  });
+});
--- packages/gamut/src/Tip/__tests__/mocks.tsx
+++ packages/gamut/src/Tip/__tests__/mocks.tsx
@@ -1,6 +1,5 @@
 import { ComponentProps } from 'react';
 
-import { Box } from '../../Box';
 import { FillButton } from '../../Button';
 import { NewToolTip, NewToolTipProps } from '../ToolTip';
 
@@ -8,14 +7,10 @@ export const NewToolTipMock: React.FC<
   NewToolTipProps & ComponentProps<typeof FillButton>
 > = ({ info, placement, onClick }) => {
   return (
-    <Box width="500px">
-      <Box width="200px">
-        <NewToolTip info={info} placement={placement} id="tip-id">
-          <FillButton aria-describedby="tip-id" onClick={onClick}>
-            Click me
-          </FillButton>
-        </NewToolTip>
-      </Box>
-    </Box>
+    <NewToolTip info={info} placement={placement} id="tip-id">
+      <FillButton aria-describedby="tip-id" onClick={onClick}>
+        Click me
+      </FillButton>
+    </NewToolTip>
   );
 };
