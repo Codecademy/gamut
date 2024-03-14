3aee3d1be6078cecebf1a7198e31ad9a787f6c7c
dreamwasp <cass.spain@skillsoft.com>
2024-03-14 13:53:48 -0400

    start

--- packages/gamut/src/Card/index.tsx
+++ packages/gamut/src/Card/index.tsx
@@ -1,4 +1,10 @@
-import { Background, system, theme, variant } from '@codecademy/gamut-styles';
+import {
+  Background,
+  system,
+  theme,
+  useColorModes,
+  variant,
+} from '@codecademy/gamut-styles';
 import { StyleProps } from '@codecademy/variance';
 import styled from '@emotion/styled';
 import { ComponentProps } from 'react';
@@ -44,14 +50,14 @@ const shadowVariants = variant({
   prop: 'shadow',
   base: {
     position: 'relative',
-    boxShadow: `0px 0px 0 ${theme.colors.navy}`,
+    boxShadow: `0px 0px 0 currentColor`,
     transition: 'box-shadow 200ms ease, transform 200ms ease',
   },
   variants: {
     small: {
       '&:hover': {
         transform: 'translate(2px, -2px)',
-        boxShadow: `-4px 4px 0 ${theme.colors.navy}`,
+        boxShadow: `-4px 4px 0 currentColor`,
       },
     },
     medium: {
@@ -85,6 +91,8 @@ const CardWrapper = styled(Background)<CardWrapperProps>(
 );
 
 export const Card: React.FC<CardProps> = ({ variant, ...rest }) => {
+  const [mode] = useColorModes();
+
   if (!variant) {
     return (
       <DynamicCardWrapper
@@ -103,6 +111,7 @@ export const Card: React.FC<CardProps> = ({ variant, ...rest }) => {
       bg={variant}
       border={1}
       borderColor="navy"
+      color={mode === 'light' ? 'text' : 'background'}
       p={16}
       outline={variant === 'navy'}
       {...rest}
