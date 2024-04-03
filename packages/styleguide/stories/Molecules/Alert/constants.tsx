e757a1fe085f6d36611ff5fdf7a4f6f180eee157
linkcoding <kenny.lin.91@gmail.com>
2024-04-03 09:59:59 -0400

    trailing commas

--- packages/styleguide/stories/Molecules/Alert/constants.tsx
+++ packages/styleguide/stories/Molecules/Alert/constants.tsx
@@ -8,7 +8,7 @@ export const ALERTS = {
   success: {
     type: 'success',
     cta: { children: '' },
-    children: <>Success! Your content has been assigned.</>
+    children: <>Success! Your content has been assigned.</>,
   },
   feature: {
     type: 'feature',
@@ -20,7 +20,7 @@ export const ALERTS = {
     type: 'notice',
     cta: { children: 'Learn more' },
     children: (
-      <>Maintenance: Codecademy will be offline between 2AM and 4AM EST.</>
+      <>Maintenance: Codecademy will be offline between 2AM and 4AM EST.</>,
     ),
   },
   error: {
