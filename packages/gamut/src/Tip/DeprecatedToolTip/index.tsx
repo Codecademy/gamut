25597230099cc138620036462b66982b0a56a458
dreamwasp <cass.spain@skillsoft.com>
2024-04-04 16:58:32 -0400

    deprecated, thanks jake :)

--- packages/gamut/src/Tip/DeprecatedToolTip/index.tsx
+++ packages/gamut/src/Tip/DeprecatedToolTip/index.tsx
@@ -6,6 +6,10 @@ import { DeprecatedInlineToolTip } from './DeprecatedInlineToolTip';
 import { deprecatedTooltipDefaultProps, DeprecatedToolTipProps } from './types';
 
 // This iteration of ToolTip is deprecated, parts of it will be used in the upcoming InfoTip, ToolTip, and PreviewTip components
+
+/**
+ * @deprecated Use `InfoTip`or `ToolTip` instead.
+ */
 export const DeprecatedToolTip: React.FC<DeprecatedToolTipProps> = ({
   alignment = deprecatedTooltipDefaultProps.alignment,
   placement = deprecatedTooltipDefaultProps.placement,
