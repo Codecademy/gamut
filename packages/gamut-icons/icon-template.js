2f851fb9de16b8d07aa0f01c90fd01d0626bfbf6
dreamwasp <cass.spain@skillsoft.com>
2024-04-05 15:08:07 -0400

    merge dev

--- packages/gamut-icons/icon-template.js
+++ packages/gamut-icons/icon-template.js
@@ -1,11 +1,7 @@
-const path = require('path');
-const { startCase } = require('lodash');
-
 function iconTemplate(api, opts, { jsx }) {
   const template = api.template.smart({ plugins: ['jsx', 'typescript'] });
-  const { componentName, filePath } = opts.state;
+  const { componentName } = opts.state;
   const exportName = componentName.replace('Svg', '');
-  const title = startCase(path.basename(filePath, '.svg'));
 
   return template.ast`
     import * as React from 'react';
