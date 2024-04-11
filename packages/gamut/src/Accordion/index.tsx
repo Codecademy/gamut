b2b6882e92ff0373847acce7cc3028a041b97ee6
linkcoding <kenny.lin.91@gmail.com>
2024-04-11 12:53:35 -0400

    added spacing

--- packages/gamut/src/Accordion/index.tsx
+++ packages/gamut/src/Accordion/index.tsx
@@ -1,4 +1,4 @@
-import { ArrowChevronDownIcon, ArrowChevronUpIcon,  MiniChevronDownIcon } from '@codecademy/gamut-icons';
+import { ArrowChevronDownIcon, MiniChevronDownIcon } from '@codecademy/gamut-icons';
 import { ColorMode, theme, variant } from '@codecademy/gamut-styles';
 import { css } from '@emotion/react';
 import styled from '@emotion/styled';
@@ -10,12 +10,6 @@ import { Anchor, Rotation, Text,TextButton, WithChildrenProp } from '..';
 import { AccordionArea } from '../AccordionArea';
 import { Box, FlexBox } from '../Box';
 
-const StyledRotation = styled(Rotation)(css({
-  '&:hover': {
-    background: 'beige'
-  }
-}))
-
 export type AccordionDisplayProps = {
   normal: {
     bg: 'background-current'
@@ -26,12 +20,14 @@ export type AccordionDisplayProps = {
   }
 }
 
+// TODO: BODY CAN BE ANYTHING, not just a string!
 export type AccordionProps = {
   header: string,
+  headingLevel: 'h1'|'h2'|'h3'|'h4'|'h5',
   body: string,
   initiallyExpanded: boolean,
   display: AccordionDisplayProps
-  lineSpacing: 'normal' | 'condensed' | 'compact',
+  size: 'normal' | 'condensed' | 'compact',
   colorMode: 'light' | 'dark'
   overline?: string
 }
@@ -56,13 +52,33 @@ const stylingVariants = variant({
   }
 })
 
+const determineVerticalPadding = (size) => {
+  const verticalPaddingSizeMapping = {
+    'normal': 16,
+    'condensed': 8,
+    'compact': 4
+  }
+  return verticalPaddingSizeMapping[size]
+}
+
+const determineHorizontalPadding = (size) => {
+  const horizontalPaddingSizeMapping = {
+    'normal': 16,
+    'condensed': 8,
+    'compact': 8
+  }
+  return horizontalPaddingSizeMapping[size]
+}
+
 export const Accordion: React.FC<AccordionProps> = ({
   header,
+  headingLevel='h3',
   body,
   initiallyExpanded = false,
   display='background-current',
   colorMode='light',
-  overline='overline is optional'
+  overline='overline is optional',
+  size='normal'
 }) => {
   const [isExpanded, setIsExpanded] = useState(initiallyExpanded)
 
@@ -71,7 +87,15 @@ export const Accordion: React.FC<AccordionProps> = ({
   }
   return (
     <ColorMode mode={colorMode}>
-      <FlexBox width="100%" bg={display} border='solid 1px' column>
+      <FlexBox
+        width="100%"
+        bg={display}
+        border='solid 1px'
+        column
+        py={determineVerticalPadding(size)}
+        px={determineHorizontalPadding(size)}
+
+      >
         <Text>
           {overline && overline}
         </Text>
@@ -80,25 +104,22 @@ export const Accordion: React.FC<AccordionProps> = ({
           aria-expanded={isExpanded}
           onClick={handleClick}
           width="100%"
-          // py={{ _: 16, sm: 32 }}
-          // px={{ _: 0, sm: 64, lg: 0 }}
-          p={4}
           variant="interface"
         >
           <FlexBox
             flexDirection={{ _: 'column', sm: 'row' }}
             justifyContent="space-between"
             center
+            columnGap={40}
           >
-            {header}
+            <Text alignSelf='start' as={headingLevel}>
+              {header}
+            </Text>
             <Rotation rotated={isExpanded}>
-              <ArrowChevronDownIcon />
+              { size === 'normal' ? <ArrowChevronDownIcon /> : <MiniChevronDownIcon/> }
             </Rotation>
           </FlexBox>
         </StyledAnchor>
-        {/* <StyledRotation rotated={isExpanded}>
-          <ArrowChevronDownIcon color="text-disabled" />
-        </StyledRotation> */}
         <Box>
           {isExpanded && <AccordionArea body={body}/>}
         </Box>
