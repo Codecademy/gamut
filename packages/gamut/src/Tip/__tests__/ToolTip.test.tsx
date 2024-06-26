ca12c0bd063df52c7dae215731b6333591348ced
cassie spain <58054751+dreamwasp@users.noreply.github.com>
2024-06-26 15:45:24 -0400

    feat(IconButton): aria-describedby override
    Add override to have IconButton tip optionally not be split into aria-label and described-by (non-breaking)

--- packages/gamut/src/Tip/__tests__/ToolTip.test.tsx
+++ packages/gamut/src/Tip/__tests__/ToolTip.test.tsx
@@ -41,12 +41,24 @@ describe('ToolTip', () => {
       view.getByRole('button', { name: 'Click' });
       expect(view.queryByRole('tooltip')).toBeNull();
     });
+
     it('calls onClick when clicked', () => {
       const { view } = renderView({});
 
       userEvent.click(view.getByRole('button'));
 
       expect(onClick).toHaveBeenCalled();
+    });
+
+    it('hides ariaTooltip when there is hideAriaToolTip is true', () => {
+      const { view } = renderView({
+        'aria-label': ariaLabel,
+        hideAriaToolTip: true,
+        info: `${ariaLabel}`,
+      });
+
+      view.getByRole('button', { name: 'Click' });
+      expect(view.queryByRole('tooltip')).toBeNull();
     });
   });
 });
