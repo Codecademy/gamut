8716a2479d3b00d7bd1a3c132519ba7b91a63967
Jake Hiller <jakemhiller@gmail.com>
2024-07-01 14:51:00 -0400

    remove styleguide

--- packages/styleguide/.storybook/addons/system/preset.ts
+++
@@ -1,9 +0,0 @@
-function managerEntries(entry: any[] = []) {
-  return [...entry, require.resolve('./register')];
-}
-
-function config(entry: any[] = []) {
-  return [...entry, require.resolve('./enhancers')];
-}
-
-module.exports = { config, managerEntries };
