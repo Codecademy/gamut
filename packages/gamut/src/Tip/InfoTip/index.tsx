4027d2c74738c34289f96048a04505de528fed61
dreamwasp <cass.spain@skillsoft.com>
2024-07-11 12:16:02 -0400

    remove useMemo

--- packages/gamut/src/Tip/InfoTip/index.tsx
+++ packages/gamut/src/Tip/InfoTip/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import { Text } from '../../Typography';
 import { FloatingTip } from '../shared/FloatingTip';
