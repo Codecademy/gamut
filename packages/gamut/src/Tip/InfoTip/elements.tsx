630ca0a4583af8fc0734268f1917f9d2252fd31f
dreamwasp <cass.spain@skillsoft.com>
2024-07-24 12:45:51 -0400

    whoops

--- packages/gamut/src/Tip/InfoTip/elements.tsx
+++ packages/gamut/src/Tip/InfoTip/elements.tsx
@@ -4,5 +4,5 @@ import styled from '@emotion/styled';
 import { Text } from '../../Typography';
 
 export const ScreenreaderNavigableText = styled(Text)(
-  css({ display: 'absolute', position: 'relative' })
+  css({ position: 'absolute' })
 );
