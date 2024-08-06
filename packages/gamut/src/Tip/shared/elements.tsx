976edaaa41e422610ceb86628b107a6dc9c9d69e
Kenny Lin <kenny.lin.91@gmail.com>
2024-08-06 16:50:04 -0400

    feat(Border Radius Token)!: Updating Molecules + Organism (#2922)
    * updated molecules + organism borderradii

--- packages/gamut/src/Tip/shared/elements.tsx
+++ packages/gamut/src/Tip/shared/elements.tsx
@@ -49,7 +49,7 @@ export const TargetContainer = styled(Box)(
       content: "''",
       position: 'absolute',
       inset: -2,
-      borderRadius: '4px',
+      borderRadius: 'm',
       border: 2,
       borderColor: 'primary',
       opacity: 0,
