f8e97d3018bc41a2ccccc0519b6c8d7d35f9a1ef
dreamwasp <cass.spain@skillsoft.com>
2024-08-27 17:21:36 -0400

    fix circular deps

--- packages/gamut/src/Tip/index.tsx
+++
@@ -1,5 +0,0 @@
-export * from './DeprecatedToolTip';
-export * from './InfoTip';
-export * from './PreviewTip';
-export * from './ToolTip';
-export type { DeprecatedToolTipProps } from './DeprecatedToolTip/types';
