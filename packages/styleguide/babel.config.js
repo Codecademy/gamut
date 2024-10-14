08e23c029320a555275497097b844827011e5d48
dreamwasp <cass.spain@skillsoft.com>
2024-10-14 14:13:00 -0400

    add old babel config

--- packages/styleguide/babel.config.js
+++ packages/styleguide/babel.config.js
@@ -1,29 +1,21 @@
 module.exports = {
-  extends: '../../babel.defaults.js',
-  presets: [
-    [
-      '@babel/env',
-      {
-        modules: process.env.NODE_ENV === 'test' ? 'commonjs' : false,
-        targets: 'defaults',
-      },
-    ],
-    [
-      '@babel/react',
-      {
-        runtime: 'automatic',
-      },
-    ],
-    '@babel/preset-typescript',
-  ],
+  presets: ['codecademy', '@babel/preset-typescript'],
   plugins: [
+    'macros',
     [
-      '@emotion/babel-plugin',
+      '@emotion',
       {
         sourceMap: true,
         autoLabel: 'always',
         labelFormat: '[local]',
+        cssPropOptimization: true,
       },
     ],
   ],
+  ignore: [],
+  env: {
+    test: {
+      plugins: ['require-context-hook'],
+    },
+  },
 };
