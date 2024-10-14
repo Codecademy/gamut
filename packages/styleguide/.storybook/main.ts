36f731ecae1a14f7f23f00b5aa6e2299383ac706
dreamwasp <cass.spain@skillsoft.com>
2024-10-14 13:26:12 -0400

    keep on trucking

--- packages/styleguide/.storybook/main.ts
+++ packages/styleguide/.storybook/main.ts
@@ -1,15 +1,26 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
 
+const workspaceRoot = process.env.NX_WORKSPACE_ROOT;
+
+if (!workspaceRoot) {
+  throw new Error('This script must be run from an NX task');
+}
+
 const config: StorybookConfig = {
-  stories: ['../src/lib/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
+  stories: ['../src/stories'],
   addons: [
     '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
     '@nx/react/plugins/storybook',
+    '@storybook/addon-links',
+    '@storybook/addon-designs',
   ],
   framework: {
     name: '@storybook/react-webpack5',
-    options: {},
+    options: {
+      builder: {
+        useSWC: false,
+      },
+    },
   },
 };
 
