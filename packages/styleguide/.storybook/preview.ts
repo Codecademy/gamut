004b55925b900b776ddb5b48dc0e87297a179685
dreamwasp <cass.spain@skillsoft.com>
2024-10-25 14:50:20 -0400

    color modes working in components

--- packages/styleguide/.storybook/preview.ts
+++ packages/styleguide/.storybook/preview.ts
@@ -1,6 +1,7 @@
 import { Preview } from '@storybook/react';
 
 import theme from './GamutTheme';
+import { withEmotion } from './theming/GamutThemeProvider';
 
 const preview: Preview = {
   parameters: {
@@ -9,5 +10,25 @@ const preview: Preview = {
     },
   },
 };
+
+export const globalTypes = {
+  colorMode: {
+    name: ' ColorMode',
+    description: 'Global color mode for components',
+    defaultValue: 'light',
+    toolbar: {
+      icon: 'circlehollow',
+      // Array of plain string values or MenuItem shape (see below)
+      items: [
+        { value: 'light', icon: 'circlehollow', title: 'light' },
+        { value: 'dark', icon: 'circle', title: 'dark' },
+      ],
+      // Property that specifies if the name of the item will be displayed
+      showName: true,
+    },
+  },
+};
+
+export const decorators = [withEmotion];
 
 export default preview;
