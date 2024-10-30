449172aa8dd04194f6005d6ab7661d32c9253dce
dreamwasp <cass.spain@skillsoft.com>
2024-10-30 14:53:25 -0400

    keep getError message internal

--- packages/gamut/src/ConnectedForm/index.ts
+++ packages/gamut/src/ConnectedForm/index.ts
@@ -2,4 +2,12 @@ export * from './ConnectedFormGroup';
 export * from './ConnectedForm';
 export * from './ConnectedInputs';
 export * from './SubmitButton';
-export * from './utils';
+export {
+  useConnectedForm,
+  submitSuccessStatus,
+  useFormState,
+  useField,
+  useSubmitState,
+  useGetInitialFormValue,
+  useDebouncedField,
+} from './utils';
