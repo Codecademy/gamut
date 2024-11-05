bcd7d91593d196e5797fdc2e71c47144657d809c
dreamwasp <cass.spain@skillsoft.com>
2024-11-05 11:46:33 -0500

    try fixing video test running

--- packages/gamut/src/Video/__tests__/Video.test.tsx
+++ packages/gamut/src/Video/__tests__/Video.test.tsx
@@ -1,25 +1,30 @@
 import { setupRtl } from '@codecademy/gamut-tests';
+import { act, waitFor } from '@testing-library/react';
 
 import { Video } from '..';
 
 const renderView = setupRtl(Video, {});
 
 describe('Video', () => {
-  it('loads a video with a vimeo URL', () => {
-    const { view } = renderView({
-      videoUrl: 'https://vimeo.com/145702525',
-      videoTitle: 'Super Science Friends',
-    });
+  it('loads a video with a vimeo URL', async () => {
+    await act(async () => {
+      const { view } = renderView({
+        videoUrl: 'https://vimeo.com/145702525',
+        videoTitle: 'Super Science Friends',
+      });
 
-    view.getByTitle('Super Science Friends');
+      await waitFor(() => view.getByTitle('Super Science Friends'));
+    });
   });
 
-  it('loads a video with a youtube ID', () => {
-    const { view } = renderView({
-      videoUrl: 'Yl8yy5tpVIM',
-      videoTitle: 'Workout with Rick Sanchez',
-    });
+  it('loads a video with a youtube ID', async () => {
+    await act(async () => {
+      const { view } = renderView({
+        videoUrl: 'Yl8yy5tpVIM',
+        videoTitle: 'Workout with Rick Sanchez',
+      });
 
-    view.getByTitle('Workout with Rick Sanchez');
+      await waitFor(() => view.getByTitle('Workout with Rick Sanchez'));
+    });
   });
 });
