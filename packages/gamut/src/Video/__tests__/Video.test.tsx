3ebe718f6182897f1b80ed23fb29003b414c577f
dreamwasp <cass.spain@skillsoft.com>
2024-11-07 12:53:18 -0500

    await video resolution

--- packages/gamut/src/Video/__tests__/Video.test.tsx
+++ packages/gamut/src/Video/__tests__/Video.test.tsx
@@ -5,21 +5,21 @@ import { Video } from '..';
 const renderView = setupRtl(Video, {});
 
 describe('Video', () => {
-  it('loads a video with a vimeo URL', () => {
+  it('loads a video with a vimeo URL', async () => {
     const { view } = renderView({
       videoUrl: 'https://vimeo.com/145702525',
       videoTitle: 'Super Science Friends',
     });
 
-    view.getByTitle('Super Science Friends');
+    await view.findByTitle('Super Science Friends');
   });
 
-  it('loads a video with a youtube ID', () => {
+  it('loads a video with a youtube ID', async () => {
     const { view } = renderView({
       videoUrl: 'Yl8yy5tpVIM',
       videoTitle: 'Workout with Rick Sanchez',
     });
 
-    view.getByTitle('Workout with Rick Sanchez');
+    await view.findByTitle('Workout with Rick Sanchez');
   });
 });
