39ffa925696f8291c06653dd7e9e462443654eea
Smith <smith.gajjar@skillsoft.com>
2024-11-21 20:19:26 +0530

    custom fonts removed

--- packages/gamut-styles/src/remoteAssets/fonts.ts
+++ packages/gamut-styles/src/remoteAssets/fonts.ts
@@ -35,10 +35,5 @@ export const webFonts = [
     filePath: `${FONT_ASSET_PATH}/SuisseIntlMono-Regular-WebS`,
     extensions: ['woff', 'woff2'],
     name: 'Suisse',
-  },
-  {
-    filePath: `https://static-assets-staging.codecademy.com/test-locales/VideoJS`,
-    extensions: ['woff'],
-    name: 'VideoJS',
-  },
+  }
 ];
