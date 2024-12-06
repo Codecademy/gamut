3ebf1590721856753bfd206981e509221348c501
Kenny Lin <kenny.lin.91@gmail.com>
2024-12-06 09:59:19 -0500

    feat(Atoms): adding in the remaining Atoms (#2982)
    * finished badges
    
    * added buttons
    
    * added formelements
    
    * updated about page
    
    * minor touchups
    
    * fixed minor ts error
    
    * updated usage for CTAButton and IconButton

---
+++ packages/styleguide/src/lib/Atoms/Buttons/StrokeButton/StrokeButton.stories.tsx
@@ -0,0 +1,42 @@
+import { StrokeButton } from '@codecademy/gamut';
+import { SearchIcon } from '@codecademy/gamut-icons';
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof StrokeButton> = {
+  component: StrokeButton,
+  args: {
+    children: 'Click Me',
+    disabled: false,
+    size: 'normal',
+  },
+  argTypes: {
+    href: {
+      description: 'If defined, component will use an anchor tag',
+    },
+    mode: {
+      control: {
+        type: 'select',
+        options: ['dark', 'light'],
+      },
+    },
+    size: {
+      control: {
+        type: 'select',
+        options: ['normal', 'small', 'large'],
+      },
+    },
+    icon: {
+      control: {
+        options: [SearchIcon],
+      },
+    },
+  }
+};
+
+export default meta;
+type Story = StoryObj<typeof StrokeButton>;
+
+export const Default: Story = {
+  args: {},
+};
+
