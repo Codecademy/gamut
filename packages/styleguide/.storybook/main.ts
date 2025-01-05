2219eb6524a85f6977b3b982ae7a72314bd61b36
Smith <smith.gajjar@skillsoft.com>
2025-01-05 18:36:00 +0530

    cleanups & jest ignore module

--- packages/styleguide/.storybook/main.ts
+++ packages/styleguide/.storybook/main.ts
@@ -1,5 +1,5 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
-import { dirname, join, resolve } from 'path';
+import { resolve, dirname, join } from 'path';
 
 const config: StorybookConfig = {
   stories: [
