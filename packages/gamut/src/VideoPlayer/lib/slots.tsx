397050166131e2fbc6a4f3a1182b4ebbe8fe6ef0
Smith <smith.gajjar@skillsoft.com>
2025-01-07 19:01:19 +0530

    feat: custom caption toggle in settings

--- packages/gamut/src/VideoPlayer/lib/slots.tsx
+++ packages/gamut/src/VideoPlayer/lib/slots.tsx
@@ -1,21 +1,54 @@
 import { ArrowLeftIcon } from '@codecademy/gamut-icons';
-import { SeekButton } from '@vidstack/react';
+import { theme } from '@codecademy/gamut-styles';
+import { isTrackCaptionKind, SeekButton, useMediaRemote, useMediaState } from '@vidstack/react';
+import { DefaultVideoLayoutProps, DefaultVideoLayoutSlots } from '@vidstack/react/types/vidstack-react';
 
+import { Toggle } from '../../Toggle';
 
-export const customLayoutSlots = {
-    smallLayout: {
-        beforeCaptionButton: (
-          <SeekButton className="vds-button" seconds={-10}>
-            <ArrowLeftIcon className="vds-icon" />
-          </SeekButton>
-        )
-      },
-      largeLayout: {
-        afterPlayButton: (
-          <SeekButton className="vds-button" seconds={-10}>
-            <ArrowLeftIcon className="vds-icon" />
-          </SeekButton>
-        ),
-      },
-      googleCastButton: null,
+const CaptionToggle = () => { 
+  const playerRemote = useMediaRemote()
+  const $track = useMediaState('textTrack');
+  const isOn = $track && isTrackCaptionKind($track);
+ 
+  return (
+    <Toggle
+      style={{
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: 'var(--item-padding)',
+        color: theme.colors.text,
+      }}
+      label="Captions"
+      labelSide="left"
+      size='small'
+      checked={isOn || false}
+      onChange={() => {
+        playerRemote.toggleCaptions()
+      }} 
+    />
+  )
+}
+
+export const customLayoutSlots: DefaultVideoLayoutSlots | DefaultVideoLayoutProps = {
+  smallLayout: {
+    beforeCaptionButton: (
+      <SeekButton className="vds-button" seconds={-10}>
+      <ArrowLeftIcon className="vds-icon" />
+      </SeekButton>
+    )
+  },
+  largeLayout: {
+    afterPlayButton: (
+      <SeekButton className="vds-button" seconds={-10}>
+      <ArrowLeftIcon className="vds-icon" />
+      </SeekButton>
+    ),
+  },
+  googleCastButton: null,
+  beforeSettingsMenuStartItems: (
+    <CaptionToggle />
+  )
 }
