7bc15a563f297becfc2f4d5f3ebfc38b9e529deb
Smith <smith.gajjar@skillsoft.com>
2025-01-24 14:58:18 +0530

    fix(Markdown): vidstack player test mocks

--- packages/gamut/src/Markdown/__tests__/Markdown.test.tsx
+++ packages/gamut/src/Markdown/__tests__/Markdown.test.tsx
@@ -13,6 +13,13 @@ jest.mock('react-player', () => ({
   __esModule: true,
   default: () => <iframe title={mockTitle} />,
 }));
+jest.mock('@vidstack/react', () => ({
+  __esModule: true,
+  MediaPlayer: () => <iframe title={mockTitle} />,
+  MediaProvider: () => <div />,
+  Poster: jest.fn(),
+  Track: jest.fn(),
+}));
 
 const basicMarkdown = `
 # Heading 1
