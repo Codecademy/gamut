881a068e77a732c50e94e1d8a3937c47c01a6d1d
Kenny Lin <kenny.lin.91@gmail.com>
2025-02-07 13:09:52 -0500

    formatted

--- packages/styleguide/src/lib/Atoms/Animations/ExpandInCollapseOut/ExpandInCollapseOut.stories.tsx
+++ packages/styleguide/src/lib/Atoms/Animations/ExpandInCollapseOut/ExpandInCollapseOut.stories.tsx
@@ -1,15 +1,20 @@
-import { Box, ExpandInCollapseOut, FillButton, FlexBox, Text } from '@codecademy/gamut';
+import {
+  Box,
+  ExpandInCollapseOut,
+  FillButton,
+  FlexBox,
+  Text,
+} from '@codecademy/gamut';
 import type { Meta } from '@storybook/react';
 import { AnimatePresence } from 'framer-motion';
 import { useState } from 'react';
 
 const meta: Meta<typeof ExpandInCollapseOut> = {
- component: ExpandInCollapseOut,
- args: {},
+  component: ExpandInCollapseOut,
+  args: {},
 };
 
 export default meta;
-
 
 export const Default: React.FC = () => {
   const [isExpanded, setExpanded] = useState(false);
@@ -17,17 +22,26 @@ export const Default: React.FC = () => {
   return (
     <FlexBox column>
       <Box>
-        <FillButton onClick={() => setExpanded(!isExpanded)}>Look, the text expands in and collapses out!</FillButton>
+        <FillButton onClick={() => setExpanded(!isExpanded)}>
+          Look, the text expands in and collapses out!
+        </FillButton>
       </Box>
       <Box mt={8}>
         <AnimatePresence>
           {isExpanded && (
             <ExpandInCollapseOut>
-              <Text>I got bored one day - and I put everything on a bagel. Everything. All my hopes and dreams, my old report cards, every breed of dog, every last personal ad on craigslist. Sesame. Poppy seed. Salt. And it collapsed in on itself. &apos;Cause, you see, when you really put everything on a bagel, it becomes this.</Text>
+              <Text>
+                I got bored one day - and I put everything on a bagel.
+                Everything. All my hopes and dreams, my old report cards, every
+                breed of dog, every last personal ad on craigslist. Sesame.
+                Poppy seed. Salt. And it collapsed in on itself. &apos;Cause,
+                you see, when you really put everything on a bagel, it becomes
+                this.
+              </Text>
             </ExpandInCollapseOut>
           )}
         </AnimatePresence>
       </Box>
     </FlexBox>
-  )
-}
+  );
+};
