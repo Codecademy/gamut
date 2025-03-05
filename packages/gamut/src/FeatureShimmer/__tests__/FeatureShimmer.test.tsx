0bbba9753475656eaa47f4ca6d58fd22f21c314e
Amy Resnik <aresnik@codecademy.com>
2025-03-05 16:00:51 -0500

    refactor tests

--- packages/gamut/src/FeatureShimmer/__tests__/FeatureShimmer.test.tsx
+++ packages/gamut/src/FeatureShimmer/__tests__/FeatureShimmer.test.tsx
@@ -21,7 +21,7 @@ describe('FeatureShimmer', () => {
     });
     window.IntersectionObserver = mockIntersectionObserver;
   });
-  it('renders animation when reduced motion is false', () => {
+  it('renders shimmer when reduced motion is false', () => {
     mockUseReducedMotion.mockReturnValue(false);
 
     const { view } = renderView();
@@ -30,7 +30,7 @@ describe('FeatureShimmer', () => {
     view.getByTestId('feature-shimmer');
   });
 
-  it('does not render animation when reduced motion is true', () => {
+  it('does not render shimmer when reduced motion is true', () => {
     mockUseReducedMotion.mockReturnValue(true);
 
     const { view } = renderView();
@@ -39,16 +39,7 @@ describe('FeatureShimmer', () => {
     expect(view.queryByTestId('feature-shimmer')).toBeNull();
   });
 
-  it('does not render animation when element is out of view', () => {
-    mockUseReducedMotion.mockReturnValue(false);
-
-    const { view } = renderView();
-
-    view.getByText('Testing');
-    expect(view.queryByTestId('feature-shimmer')).toBeNull();
-  });
-
-  it('renders animation when element is in view', () => {
+  it('renders shimmer', () => {
     mockUseReducedMotion.mockReturnValue(false);
 
     const { view } = renderView();
