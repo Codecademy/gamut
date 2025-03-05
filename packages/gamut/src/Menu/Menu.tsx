85fdd43093df401642898ca4b43a947f2a6b043e
dreamwasp <cass.spain@skillsoft.com>
2025-03-05 15:08:23 -0500

    menu default border radius

--- packages/gamut/src/Menu/Menu.tsx
+++ packages/gamut/src/Menu/Menu.tsx
@@ -8,13 +8,20 @@ export const Menu = forwardRef<
   Omit<ComponentProps<typeof List>, 'root'>
 >(
   (
-    { children, variant = 'popover', spacing = 'normal', role, ...rest },
+    {
+      children,
+      variant = 'popover',
+      spacing = 'normal',
+      role,
+      borderRadius = 'sm',
+      ...rest
+    },
     ref
   ) => {
     const currentContext = useMenu({ variant, role, spacing });
 
     return (
-      <List {...rest} {...currentContext} ref={ref}>
+      <List {...rest} {...currentContext} borderRadius={borderRadius} ref={ref}>
         <MenuProvider value={currentContext}>{children}</MenuProvider>
       </List>
     );
