5fec6b6bef17edeac7c0dc25d67f4c0138e8e7c2
Kenny Lin <kenny.lin.91@gmail.com>
2025-03-10 12:14:32 -0400

    updated patternwrapper to remove state and rename

--- packages/gamut/src/Card/index.tsx
+++ packages/gamut/src/Card/index.tsx
@@ -2,11 +2,7 @@ import { CheckerDense } from '@codecademy/gamut-patterns';
 import { Colors } from '@codecademy/gamut-styles';
 import * as React from 'react';
 
-import {
-  DynamicCardWrapper,
-  MotionBox,
-  StaticCardWrapper,
-} from './elements';
+import { DynamicCardWrapper, MotionBox, StaticCardWrapper } from './elements';
 import { hoverShadowLeft, hoverShadowRight, patternFadeInOut } from './styles';
 import { CardProps } from './types';
 
@@ -36,23 +32,18 @@ export const Card: React.FC<CardProps> = ({
       position="relative"
       whileHover={isInteractive ? 'animate' : ''}
     >
-      {
-        hasPattern && (
-          <MotionBox
-            variants={patternFadeInOut}
-            borderRadius={trueBorderRadius}
-          >
-            <Pattern
-              dimensions={1}
-              position="absolute"
-              top=".5rem"
-              left={shadow === 'patternLeft' ? '-0.5rem' : undefined}
-              right={shadow === 'patternRight' ? '-0.5rem' : undefined}
-              style={{ borderRadius: 'inherit' }}
-            />
-          </MotionBox>
-        )
-      }
+      {hasPattern && (
+        <MotionBox variants={patternFadeInOut} borderRadius={trueBorderRadius}>
+          <Pattern
+            dimensions={1}
+            position="absolute"
+            top=".5rem"
+            left={shadow === 'patternLeft' ? '-0.5rem' : undefined}
+            right={shadow === 'patternRight' ? '-0.5rem' : undefined}
+            style={{ borderRadius: 'inherit' }}
+          />
+        </MotionBox>
+      )}
       <SelectedWrapper
         // setting bg here since Background requires a bg prop
         // the 'white' color doesn't actually get set since the variant overrides it
