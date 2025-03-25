6468a73347126bb503f5c1ee56c1da3aacc59295
Kenny Lin <kenny.lin.91@gmail.com>
2025-03-25 11:31:25 -0400

    provide background as a prop

--- packages/gamut/src/Badge/index.tsx
+++ packages/gamut/src/Badge/index.tsx
@@ -71,7 +71,7 @@ const badgeProps = variance.compose(
   system.space,
   system.layout,
   system.typography,
-  system.color
+  system.background
 );
 export interface BadgeBaseProps
   extends StyleProps<typeof badgeProps>,
