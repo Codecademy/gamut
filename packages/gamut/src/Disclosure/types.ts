b9d903e13d179eed246d3180d4d882153ebb8f4c
Kenny Lin <kenny.lin.91@gmail.com>
2025-04-01 15:22:00 -0400

    omit id from DisclosureProps

--- packages/gamut/src/Disclosure/types.ts
+++ packages/gamut/src/Disclosure/types.ts
@@ -64,7 +64,7 @@ export interface DisclosureBodyProps extends DisclosureBodyWrapperStyles {
 }
 
 export interface DisclosureProps
-  extends DisclosureButtonProps,
+  extends Omit<DisclosureButtonProps, 'id'>,
     DisclosureBodyProps,
     DisclosureWrapperStyles {
   /**
