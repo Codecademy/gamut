f66d2f8090b438130b479deb15d5e81f777c4c21
Amy Resnik <aresnik@codecademy.com>
2025-04-01 14:01:21 -0400

    update select dropdown border color

--- packages/gamut/src/Form/styles/shared-system-props.ts
+++ packages/gamut/src/Form/styles/shared-system-props.ts
@@ -73,13 +73,13 @@ export const formBaseFieldStylesObject = {
     'ease-in-out'
   ),
   border: 1,
-  borderColor: 'border-disabled',
+  borderColor: 'border-primary',
   borderRadius: 'md',
   [InputSelectors.HOVER]: {
     borderColor: 'primary',
   },
   [InputSelectors.PLACEHOLDER]: {
-    borderColor: 'border-disabled',
+    borderColor: 'border-primary',
     fontStyle: 'italic',
   },
   [InputSelectors.DISABLED]: {
