4f0e3b018d9102dcfcd2d9aeee1bc453538f48cd
Troy Chryssos <troychryssos@gmail.com>
2025-05-08 10:47:08 -0700

    commit for alpha comment

--- packages/gamut/src/ConnectedForm/__tests__/ConnectedForm.test.tsx
+++ packages/gamut/src/ConnectedForm/__tests__/ConnectedForm.test.tsx
@@ -277,6 +277,8 @@ describe('ConnectedForm', () => {
       expect(radioOption).not.toBeDisabled();
     });
 
+    // commit for alpha
+
     describe('resetOnSubmit', () => {
       it('resets fields when form is successfully submitted', async () => {
         let submitCount = 0;
