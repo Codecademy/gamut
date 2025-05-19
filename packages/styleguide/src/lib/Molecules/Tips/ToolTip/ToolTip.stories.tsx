dcf23b0b6711a38855be92dc0d36fd61aeb75d39
Kenny Lin <kenny.lin.91@gmail.com>
2025-05-19 15:12:21 -0400

    fix up stories

--- packages/styleguide/src/lib/Molecules/Tips/ToolTip/ToolTip.stories.tsx
+++ packages/styleguide/src/lib/Molecules/Tips/ToolTip/ToolTip.stories.tsx
@@ -1,6 +1,7 @@
-import { FillButton, FlexBox, IconButton, ToolTip } from '@codecademy/gamut';
+import { FillButton, FlexBox, IconButton, StrokeButton, ToolTip } from '@codecademy/gamut';
 import {
   ArrowRightIcon,
+  DeleteIcon,
   SmileyStarEyesIcon,
   SparkleIcon,
 } from '@codecademy/gamut-icons';
@@ -51,5 +52,29 @@ export const Floating: Story = {
       icon={SmileyStarEyesIcon}
       tipProps={{ alignment: 'bottom-center', placement: 'floating' }}
     />
+  ),
+};
+
+export const InteractiveElement: Story = {
+  render: () => (
+    <FlexBox center m={32}>
+      <ToolTip id="stroke-button-ex" info="And here's a tooltip">
+        <StrokeButton aria-describedby="stroke-button-ex">
+          I&apos;ve got my own text
+        </StrokeButton>
+      </ToolTip>
+    </FlexBox>
+  ),
+};
+
+export const Disabled: Story = {
+  render: () => (
+    <FlexBox center m={24}>
+      <ToolTip id="fill-id" info="Tooltip still shows">
+        <FillButton aria-describedby="fill-id" aria-disabled icon={DeleteIcon}>
+          Using aria-disabled
+        </FillButton>
+      </ToolTip>
+    </FlexBox>
   ),
 };
