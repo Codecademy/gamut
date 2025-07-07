94496e48393e545958fda3c4c6fb0f9f895c2453
dreamwasp <cass.spain@skillsoft.com>
2025-07-07 15:09:55 -0400

    add indeterminate tests

--- packages/gamut/src/Form/__tests__/Checkbox.test.tsx
+++ packages/gamut/src/Form/__tests__/Checkbox.test.tsx
@@ -28,6 +28,19 @@ describe('<Checkbox>', () => {
 
     expect(onChange).toHaveBeenCalled();
   });
+  it('sets the input indeterminate state when the prop is passed', () => {
+    const { view } = renderView({ indeterminate: true });
+
+    const checkbox = view.getByRole('checkbox');
+    expect(checkbox).toHaveProperty('indeterminate', true);
+  });
+
+  it('does not set indeterminate state when the prop is false', () => {
+    const { view } = renderView({ indeterminate: false });
+
+    const checkbox = view.getByRole('checkbox');
+    expect(checkbox).toHaveProperty('indeterminate', false);
+  });
 
   it('accepts JSX in the label', () => {
     const { view } = renderView({
