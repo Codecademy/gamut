b66c3e42421db7412e0cc27ac6e983f9c59a34f3
Kenny Lin <kenny.lin.91@gmail.com>
2025-07-15 10:36:26 -0400

    somewhat working example

--- packages/styleguide/src/lib/Typography/Anchor/Anchor.stories.tsx
+++ packages/styleguide/src/lib/Typography/Anchor/Anchor.stories.tsx
@@ -1,9 +1,17 @@
-import { Anchor, GridBox, Text } from '@codecademy/gamut';
+import {
+  Anchor,
+  BaseTextProps,
+  Box,
+  GridBox,
+  Text,
+  ToolTip,
+} from '@codecademy/gamut';
 import {
   MiniArrowRightIcon,
   MiniInfoOutlineIcon,
 } from '@codecademy/gamut-icons';
 import type { Meta, StoryObj } from '@storybook/react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 
 import { PolymorphicAnchors, VariantsExample } from './Anchor.examples';
 
@@ -84,4 +92,71 @@ export const IconModes: Story = {
 
 export const PolymorphicAnchor: Story = {
   render: () => <PolymorphicAnchors />,
+};
+
+type AnchorAndTextProps = {
+  isTruncated: boolean;
+  truncateLines?: BaseTextProps['truncateLines'] | undefined;
+  children: React.ReactNode;
+};
+
+export const AnchorAndText = React.forwardRef<
+  HTMLDivElement,
+  AnchorAndTextProps
+>(({ children, isTruncated, truncateLines }, ref) => {
+  return (
+    <Anchor href="/" variant="inline">
+      {isTruncated && truncateLines ? (
+        <Text ref={ref} truncate="ellipsis" truncateLines={truncateLines}>
+          {children}
+        </Text>
+      ) : (
+        children
+      )}
+    </Anchor>
+  );
+});
+
+export const TruncateWithTooltip: React.FC<{ text: string, toolTipString: string }> = ({ text, toolTipString }) => {
+  const textRef = useRef<HTMLDivElement>(null);
+  const shouldTruncate = useCallback(() => text.length > 150, [text]);
+
+  const [isTruncated, setIsTruncated] = useState(shouldTruncate());
+
+  useEffect(() => {
+    const el = textRef.current;
+    if (el) {
+      const style = window.getComputedStyle(el);
+      const textOverStyle = style.getPropertyValue('text-overflow');
+      setIsTruncated(textOverStyle === 'ellipsis');
+    }
+  }, [shouldTruncate]);
+
+  return (
+    <Box width="500px">
+      {isTruncated ? (
+        <ToolTip info="Example tooltip" placement="floating">
+          <AnchorAndText
+            isTruncated={isTruncated}
+            ref={textRef}
+            truncateLines={2}
+          >
+            {text}
+          </AnchorAndText>
+        </ToolTip>
+      ) : (
+        <AnchorAndText isTruncated={false}>{text}</AnchorAndText>
+      )}
+    </Box>
+  );
+};
+
+export const TruncateWithToolTipExample: Story = {
+  render: () => (
+    <TruncateWithTooltip text="This is a looooooong text that will be truncated after two lines. Hover to see the tooltip. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." toolTipString="Example tooltip" />
+  ),
+};
+
+export const TruncateWithNoToolTipExample: Story = {
+  render: () => <TruncateWithTooltip text="No truncation, no tooltip" toolTipString="In case this truncates" />,
 };
