709520f56dfd01294f0faf314d9e6c7f45f09835
dreamwasp <cass.spain@skillsoft.com>
2025-08-05 10:32:50 -0400

    animations working + approved

--- packages/gamut/src/List/elements.tsx
+++ packages/gamut/src/List/elements.tsx
@@ -8,6 +8,7 @@ import {
 } from '@codecademy/gamut-styles';
 import { StyleProps, variance } from '@codecademy/variance';
 import styled from '@emotion/styled';
+import { motion } from 'framer-motion';
 
 import { Box } from '../Box';
 
@@ -430,16 +431,24 @@ export const StickyHeaderColWrapper = styled.th(
   })
 );
 
-export const ListWrapper = styled(Box)(
-  css({
-    containerType: 'inline-size',
-  }),
-  states({
-    scrollable: {
-      background: `linear-gradient(90deg, transparent 0%, transparent 40%, ${theme.colors['background-selected']} 50%, ${theme.colors['border-tertiary']} 100%)`,
-      backgroundSize: '124px 100%',
-      backgroundPosition: 'right',
-      backgroundRepeat: 'no-repeat',
-    },
-  })
-);
+const listStyles = css({
+  containerType: 'inline-size',
+});
+
+export const StaticListWrapper = styled(Box)(listStyles);
+
+export const AnimatedListWrapper = styled(motion(Box))(listStyles);
+
+export const hiddenVariant = {
+  background: `linear-gradient(90deg, transparent 0%, transparent 40%, ${theme.colors['background-selected']} 50%, ${theme.colors['border-tertiary']} 100%)`,
+  backgroundSize: '0px 100%',
+  backgroundPosition: 'right',
+  backgroundRepeat: 'no-repeat',
+} as const;
+
+export const shadowVariant = {
+  background: `linear-gradient(90deg, transparent 0%, transparent 40%, ${theme.colors['background-selected']} 50%, ${theme.colors['border-tertiary']} 100%)`,
+  backgroundSize: '124px 100%',
+  backgroundPosition: 'right',
+  backgroundRepeat: 'no-repeat',
+};
