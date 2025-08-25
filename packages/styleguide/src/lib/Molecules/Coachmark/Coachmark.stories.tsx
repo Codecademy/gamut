a1e2e2363d41df1c0302c8296311c510b22454a7
Amy Resnik <aresnik11@gmail.com>
2025-08-25 13:04:19 -0400

    another chunk of fixes

--- packages/styleguide/src/lib/Molecules/Coachmark/Coachmark.stories.tsx
+++ packages/styleguide/src/lib/Molecules/Coachmark/Coachmark.stories.tsx
@@ -1,55 +1,51 @@
-import {
-  Coachmark,
-  CoachmarkProps,
-  FillButton,
-  FlexBox,
-  Text,
-} from '@codecademy/gamut';
+import { Coachmark, FillButton, FlexBox, Text } from '@codecademy/gamut';
 import { CheckerDense } from '@codecademy/gamut-patterns';
 import type { Meta, StoryObj } from '@storybook/react';
-import { useState } from 'react';
+import { ComponentProps, useEffect, useState } from 'react';
 
 const meta: Meta<typeof Coachmark> = {
   component: Coachmark,
   args: {},
+  argTypes: {
+    skipFocusTrap: {
+      control: 'boolean',
+    },
+  },
 };
 
 export default meta;
 type Story = StoryObj<typeof Coachmark>;
 
-export const CoachmarkExample = (args: CoachmarkProps) => {
-  const [shouldShow, setShouldShow] = useState(
-    args.shouldShow === undefined ? true : args.shouldShow
-  );
+export const CoachmarkExample: React.FC<ComponentProps<typeof Coachmark>> = (
+  args
+) => {
+  const [shouldShow, setShouldShow] = useState(args.shouldShow);
 
-  const renderPopover = () => (
-    <FlexBox alignItems="flex-start" flexDirection="column" p={16}>
-      <Text mb={8}>You should click the button.</Text>
-      <FillButton
-        size="small"
-        onClick={() => {
-          setShouldShow(false);
-        }}
-      >
-        Got it
-      </FillButton>
-    </FlexBox>
-  );
+  useEffect(() => {
+    setShouldShow(args.shouldShow);
+  }, [args.shouldShow]);
 
   return (
     <Coachmark
       {...args}
-      delay={args.delay ?? 0}
-      renderPopover={renderPopover}
+      renderPopover={() => (
+        <FlexBox alignItems="flex-start" flexDirection="column" p={16}>
+          <Text mb={8}>You should click the button.</Text>
+          <FillButton
+            size="small"
+            onClick={() => {
+              setShouldShow(false);
+            }}
+          >
+            Got it
+          </FillButton>
+        </FlexBox>
+      )}
       shouldShow={shouldShow}
     >
       <FillButton onClick={() => setShouldShow(true)}>A Button</FillButton>
     </Coachmark>
   );
-};
-
-export const Default: Story = {
-  render: (props) => <CoachmarkExample {...props} />,
 };
 
 export const Customized: Story = {
