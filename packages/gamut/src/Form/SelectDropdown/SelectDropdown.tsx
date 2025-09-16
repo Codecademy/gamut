6d984d8858be028a70e380b976980545a6a8fb11
dreamwasp <cass.spain@skillsoft.com>
2025-09-16 15:37:21 -0400

    build kit

--- packages/gamut/src/Form/SelectDropdown/SelectDropdown.tsx
+++ packages/gamut/src/Form/SelectDropdown/SelectDropdown.tsx
@@ -92,6 +92,7 @@ const onChangeAction = 'select-option';
  * // Grouped options with extended features
  * <SelectDropdown
  *   name="category"
+ *  placeholder="Default placeholder text"
  *   options={[
  *     {
  *       label: 'Frontend',
