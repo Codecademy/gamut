e2e3cc62d1b3a75de0ef15bf96a61b999bbe46b0
Amy Resnik <aresnik11@gmail.com>
2025-09-24 17:31:01 -0400

    connectedform default value

--- packages/gamut/src/ConnectedForm/ConnectedInputs/ConnectedNestedCheckboxes/index.tsx
+++ packages/gamut/src/ConnectedForm/ConnectedInputs/ConnectedNestedCheckboxes/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useMemo } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { Controller } from 'react-hook-form';
 
 import { Box } from '../../..';
@@ -8,6 +8,7 @@ import { ConnectedNestedCheckboxesProps } from '../types';
 import {
   calculateStates,
   flattenOptions,
+  getAllDescendants,
   handleCheckboxChange,
   renderCheckbox,
 } from './utils';
@@ -15,10 +16,13 @@ import {
 export const ConnectedNestedCheckboxes: React.FC<
   ConnectedNestedCheckboxesProps
 > = ({ name, options, disabled, onUpdate, spacing }) => {
-  const { isDisabled, control, validation, isRequired } = useField({
-    name,
-    disabled,
-  });
+  const { isDisabled, control, validation, isRequired, getValues, setValue } =
+    useField({
+      name,
+      disabled,
+    });
+
+  const defaultValue: string[] = getValues()[name];
 
   const optionsWithSpacing = options.map((option) => ({
     ...option,
@@ -30,10 +34,32 @@ export const ConnectedNestedCheckboxes: React.FC<
     [optionsWithSpacing]
   );
 
+  const [hasExpandedInitially, setHasExpandedInitially] = useState(false);
+
+  useEffect(() => {
+    if (hasExpandedInitially || !defaultValue || defaultValue.length === 0)
+      return;
+
+    const expandedValues = [...defaultValue];
+    defaultValue.forEach((value) =>
+      expandedValues.push(...getAllDescendants(value, flatOptions))
+    );
+
+    setValue(name, expandedValues);
+    onUpdate?.(expandedValues); // do we want to do this?
+    setHasExpandedInitially(true);
+  }, [
+    hasExpandedInitially,
+    flatOptions,
+    setValue,
+    defaultValue,
+    name,
+    onUpdate,
+  ]);
+
   return (
     <Controller
       control={control}
-      defaultValue={[]}
       name={name}
       render={({ field: { value = [], onChange, onBlur, ref } }) => (
         <Box as="ul" m={0} p={0}>
