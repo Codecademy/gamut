8c65ca0a5bcc5c4092f7e3a3573f8286fa5877c2
Amy Resnik <aresnik11@gmail.com>
2025-09-25 15:44:11 -0400

    update connectedform

--- packages/gamut/src/ConnectedForm/ConnectedInputs/ConnectedNestedCheckboxes/index.tsx
+++ packages/gamut/src/ConnectedForm/ConnectedInputs/ConnectedNestedCheckboxes/index.tsx
@@ -61,33 +61,35 @@ export const ConnectedNestedCheckboxes: React.FC<
     <Controller
       control={control}
       name={name}
-      render={({ field: { value = [], onChange, onBlur, ref } }) => (
-        <Box as="ul" m={0} p={0}>
-          {flatOptions.map((option) => {
-            const states = calculateStates(value, flatOptions);
-            const state = states.get(option.value)!;
-            return renderCheckbox(
-              option,
-              state,
-              `${name}-${option.value}`,
-              isRequired,
-              isDisabled,
-              onBlur,
-              (event) => {
-                handleCheckboxChange(
-                  option,
-                  event.target.checked,
-                  value,
-                  flatOptions,
-                  onChange,
-                  onUpdate
-                );
-              },
-              ref
-            );
-          })}
-        </Box>
-      )}
+      render={({ field: { value = [], onChange, onBlur, ref } }) => {
+        const states = calculateStates(value, flatOptions);
+        return (
+          <Box as="ul" m={0} p={0}>
+            {flatOptions.map((option) => {
+              const state = states.get(option.value)!;
+              return renderCheckbox(
+                option,
+                state,
+                `${name}-${option.value}`,
+                isRequired,
+                isDisabled,
+                onBlur,
+                (event) => {
+                  handleCheckboxChange(
+                    option,
+                    event.target.checked,
+                    value,
+                    flatOptions,
+                    onChange,
+                    onUpdate
+                  );
+                },
+                ref
+              );
+            })}
+          </Box>
+        );
+      }}
       rules={validation}
     />
   );
