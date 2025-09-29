bf87c5cf7e701588000351c373ea3fe39985a687
Kenny Lin <kenny.lin.91@gmail.com>
2025-09-29 12:14:02 -0400

    updates to dialog to reflect modal

--- packages/gamut/src/Modals/Dialog.tsx
+++ packages/gamut/src/Modals/Dialog.tsx
@@ -8,7 +8,7 @@ import { Overlay } from '../Overlay';
 import { Text } from '../Typography';
 import { ModalContainer, ModalContainerProps } from './elements';
 import { ImageContainer } from './ImageContainer';
-import { ModalBaseProps } from './types';
+import { CloseButtonProps, ModalBaseProps } from './types';
 
 interface DialogButtonProps {
   children: React.ReactNode;
@@ -16,7 +16,7 @@ interface DialogButtonProps {
   onClick?: ComponentProps<typeof FillButton>['onClick'];
 }
 
-export interface DialogProps extends ModalBaseProps {
+export interface DialogProps extends ModalBaseProps, CloseButtonProps {
   title: ModalBaseProps['title'];
   size?: Exclude<ModalContainerProps['size'], 'fluid' | false>;
   variant?: Extract<
@@ -25,7 +25,6 @@ export interface DialogProps extends ModalBaseProps {
   >;
   confirmCta: DialogButtonProps;
   cancelCta?: DialogButtonProps;
-  closeButtonTipText?: string;
   /**
    * TEMPORARY: a stopgap solution to avoid zIndex conflicts -
    * will be reworked with: GM-624
@@ -39,10 +38,16 @@ export const Dialog: React.FC<DialogProps> = ({
   children,
   confirmCta,
   cancelCta,
+  closeButtonProps: {
+    disabled: disableCloseButton,
+    hidden: hideCloseButton,
+    ref: closeButtonRef,
+    tip: closeButtonTip = 'Close dialog',
+    tipAlignment = 'top-center' as const,
+  } = {},
   onRequestClose,
   image,
   size = 'small',
-  closeButtonTipText = 'Close dialog',
   ...rest
 }) => {
   const onConfirm: DialogButtonProps['onClick'] = (
@@ -75,15 +80,20 @@ export const Dialog: React.FC<DialogProps> = ({
         <Text as="h2" fontSize={20} gridArea="title" lineHeight="base">
           {title}
         </Text>
-        <Box gridArea="close">
-          <IconButton
-            alignSelf="start"
-            icon={MiniDeleteIcon}
-            size="small"
-            tip={closeButtonTipText}
-            onClick={onCancel}
-          />
-        </Box>
+        {!hideCloseButton && (
+          <Box gridArea="close">
+            <IconButton
+              alignSelf="start"
+              disabled={disableCloseButton}
+              icon={MiniDeleteIcon}
+              ref={closeButtonRef}
+              size="small"
+              tip={closeButtonTip}
+              tipProps={{ alignment: tipAlignment }}
+              onClick={onCancel}
+            />
+          </Box>
+        )}
         <Box as="div" data-testid="dialog-content" gridArea="content">
           {image && <ImageContainer image={image} size={size} />}
           {children}
--- packages/styleguide/src/lib/Molecules/Modals/Dialog/Dialog.stories.tsx
+++ packages/styleguide/src/lib/Molecules/Modals/Dialog/Dialog.stories.tsx
@@ -1,7 +1,7 @@
-import { Dialog, FillButton } from '@codecademy/gamut';
+import { Dialog, FillButton, FlexBox, Text } from '@codecademy/gamut';
 import { ColorMode } from '@codecademy/gamut-styles';
 import type { Meta } from '@storybook/react';
-import { ComponentProps, useEffect, useState } from 'react';
+import { ComponentProps, useEffect, useState, useRef } from 'react';
 
 const meta: Meta<typeof Dialog> = {
   component: Dialog,
@@ -76,5 +76,54 @@ export const DarkMode: React.FC<ComponentProps<typeof Dialog>> = (args) => {
         onRequestClose={() => setIsOpen(false)}
       />
     </ColorMode>
+  );
+};
+
+export const CloseButtonDialogCustomization: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isDisabled, setIsDisabled] = useState(false);
+  const closeButtonRef = useRef<HTMLButtonElement>(null);
+
+  const handleFocusCloseButton = () => {
+    closeButtonRef.current?.focus();
+  };
+
+  return (
+    <>
+      <FlexBox flexDirection="column" gap={16}>
+        <FlexBox gap={8}>
+          <FillButton onClick={() => setIsOpen(true)}>
+            Open Dialog with Custom Close Button
+          </FillButton>
+        </FlexBox>
+      </FlexBox>
+      <Dialog
+        closeButtonProps={{
+          ref: closeButtonRef,
+          tip: 'Close this very important Dialog',
+          disabled: isDisabled,
+        }}
+        isOpen={isOpen}
+        size="medium"
+        title="Close Button Customization Demo"
+        onRequestClose={() => setIsOpen(false)}
+        confirmCta={{ children: 'Confirm' }}
+        cancelCta={{ children: 'Cancel' }}
+      >
+        <FlexBox flexDirection="column" gap={16} p={16}>
+          <Text>
+            This dialog has a customized close button with a ref for
+            programmatic focus management, a custom tooltip, and a disabled
+            state.
+          </Text>
+          <FillButton disabled={isDisabled} onClick={handleFocusCloseButton}>
+            Focus Close Button
+          </FillButton>
+          <FillButton onClick={() => setIsDisabled(!isDisabled)}>
+            {isDisabled ? 'Enable' : 'Disable'} Focus Close Button
+          </FillButton>
+        </FlexBox>
+      </Dialog>
+    </>
   );
 };
