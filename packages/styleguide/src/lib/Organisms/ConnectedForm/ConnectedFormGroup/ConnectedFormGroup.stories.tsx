f80cdb33df6a9cb40168543a6c4f4a8827360c05
Amy Resnik <aresnik11@gmail.com>
2025-10-03 14:58:49 -0400

    form story updates

--- packages/styleguide/src/lib/Organisms/ConnectedForm/ConnectedFormGroup/ConnectedFormGroup.stories.tsx
+++ packages/styleguide/src/lib/Organisms/ConnectedForm/ConnectedFormGroup/ConnectedFormGroup.stories.tsx
@@ -7,8 +7,9 @@ import {
 } from '@codecademy/gamut';
 import { action } from '@storybook/addon-actions';
 import type { Meta, StoryObj } from '@storybook/react';
+import type { TypeWithDeepControls } from 'storybook-addon-deep-controls';
 
-const meta: Meta<typeof ConnectedFormGroup> = {
+const meta: TypeWithDeepControls<Meta<typeof ConnectedFormGroup>> = {
   component: ConnectedFormGroup,
   args: {
     field: {
@@ -28,6 +29,58 @@ const meta: Meta<typeof ConnectedFormGroup> = {
     },
     disabled: {
       control: 'boolean',
+    },
+    infotip: {
+      table: {
+        disable: true,
+      },
+    },
+    'infotip.emphasis': {
+      control: 'radio',
+      options: ['high', 'low'],
+      table: {
+        defaultValue: { summary: 'low' },
+        type: { summary: 'high | low' },
+      },
+    },
+    'infotip.alignment': {
+      control: 'radio',
+      options: ['bottom-left', 'bottom-right', 'top-left', 'top-right'],
+      table: {
+        defaultValue: { summary: 'top-right' },
+        type: { summary: 'bottom-left | bottom-right | top-left | top-right' },
+      },
+    },
+    'infotip.placement': {
+      control: 'radio',
+      options: ['floating', 'inline'],
+      table: {
+        defaultValue: { summary: 'inline' },
+        type: { summary: 'floating | inline' },
+      },
+    },
+    'infotip.narrow': {
+      control: 'boolean',
+      table: {
+        type: { summary: 'boolean' },
+      },
+      description:
+        'Forces the tooltip to be its narrowest width. For use along the edges of the page or other tight spaces.',
+    },
+    'infotip.inheritDims': {
+      control: 'boolean',
+      table: {
+        type: { summary: 'boolean' },
+      },
+      description:
+        'If ToolTipWrapper should inherit the dimensions of the parent element. Can only be used for inline tips.',
+    },
+    'infotip.info': {
+      control: 'text',
+      table: {
+        type: { summary: 'string' },
+      },
+      description: 'The text for the infotip.',
     },
   },
 };
