0c3f479b0dc63476ad6e4821983d40da48017948
dreamwasp <cass.spain@skillsoft.com>
2025-10-10 16:07:24 -0400

    new snapshots

--- packages/gamut-icons/__tests__/icons-exports.test.ts
+++ packages/gamut-icons/__tests__/icons-exports.test.ts
@@ -1,6 +1,6 @@
 import * as icons from '../src/index';
 
-test('Icons Exported Keys', () => {
+test('Icons Exported Keys 1', () => {
   const exportedNames = Object.keys(icons).sort((a, b) =>
     a.toLowerCase().localeCompare(b.toLowerCase())
   );
--- packages/gamut-patterns/tests/patterns-exports.test.ts
+++ packages/gamut-patterns/tests/patterns-exports.test.ts
@@ -1,6 +1,6 @@
 import * as patterns from '../src/index';
 
-test('Patterns Exported Keys', () => {
+test('Patterns Exported Keys 1', () => {
   const exportedNames = Object.keys(patterns).sort((a, b) =>
     a.toLowerCase().localeCompare(b.toLowerCase())
   );
