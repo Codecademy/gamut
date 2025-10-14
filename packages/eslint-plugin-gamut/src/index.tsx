562bad713d774120d29ce11cca8c8a1b7a9648d8
dreamwasp <cass.spain@skillsoft.com>
2025-10-14 10:15:38 -0400

    test lint build

--- packages/eslint-plugin-gamut/src/index.tsx
+++ packages/eslint-plugin-gamut/src/index.tsx
@@ -1,12 +1,11 @@
 import gamutImportPaths from './gamut-import-paths';
 import noCssStandalone from './no-css-standalone';
-import preferThemed from './prefer-themed';
 import recommended from './recommended';
 
 const rules = {
   'import-paths': gamutImportPaths,
   'no-css-standalone': noCssStandalone,
-  'prefer-themed': preferThemed,
+  // 'prefer-themed': preferThemed,
 };
 
 export { rules, recommended };
