60bf3ef03a67330c03afabf6ff089d29deaf0001
dreamwasp <cass.spain@skillsoft.com>
2025-10-20 16:15:51 -0400

    foremat + coverage fix

--- jest.config.base.ts
+++ jest.config.base.ts
@@ -16,10 +16,10 @@ const baseConfig = (packageName: string, overrides: any) => {
     displayName: packageName,
     preset: '../../jest.preset.js',
     clearMocks: true,
-    coverageDirectory: process.env.CI,
-    reporters: process.env.CI ? ['github-actions', 'summary'] : ['default'],
+    coverageDirectory: process.env.CI
       ? `/tmp/test-results/jest/${packageName}`
       : `../../coverage/packages/${packageName}`,
+    reporters: process.env.CI ? ['github-actions', 'summary'] : ['default'],
     moduleNameMapper: {
       '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga|md)$':
         '<rootDir>/../../script/jest/fileMock',
