3513a21c425815fe4e977673229604ae54823565
Amy Resnik <aresnik11@gmail.com>
2025-11-06 10:43:56 -0500

    change back

--- packages/gamut/src/Tip/InfoTip/index.tsx
+++ packages/gamut/src/Tip/InfoTip/index.tsx
@@ -135,7 +135,7 @@ export const InfoTip: React.FC<InfoTipProps> = ({
         setTimeout(() => {
           console.log('in setTimeout');
           buttonRef.current?.focus();
-        }, 1000);
+        }, 0);
       };
 
       // Wait for the popover ref to be set before attaching the listener
