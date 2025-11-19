d2537cd1ea855bf0cc88a6a746089e50a650a777
Kenny Lin <kenny.lin.91@gmail.com>
2025-11-19 16:37:35 -0500

    add ESLint page to docs

--- packages/styleguide/.storybook/preview.ts
+++ packages/styleguide/.storybook/preview.ts
@@ -23,7 +23,7 @@ const preview: Preview = {
         order: [
           'Gamut',
           'Meta',
-          ['About', 'Best Practices', 'Contributing', 'FAQs', 'Stories'],
+          ['About', 'Best Practices', 'ESLint rules', 'Contributing', 'FAQs', 'Stories'],
           'Foundations',
           'Layouts',
           'Typography',
