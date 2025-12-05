d28d48a9c39fed493a43ad2a98aaeb489fb1a6bf
Kenny Lin <kenny.lin.91@gmail.com>
2025-12-05 15:59:22 -0500

    updated styling

--- packages/styleguide/.storybook/components/Elements/KeyboardKey.tsx
+++ packages/styleguide/.storybook/components/Elements/KeyboardKey.tsx
@@ -5,12 +5,13 @@ export const KeyboardKey = styled.kbd(
   css({
     mx: 2 as any,
     fontFamily: 'monospace',
+    fontSize: 12 as any,
     bg: 'background-selected',
     border: 1,
     borderColor: 'border-tertiary',
     borderRadius: 'sm',
     px: 4,
     py: 2 as any,
-    boxShadow: '0 1px 2px rgba(0, 0, 0, 0.2)',
+    boxShadow: 'inset 0 -1px 0 rgba(0, 0, 0, 0.2)',
   })
 );
