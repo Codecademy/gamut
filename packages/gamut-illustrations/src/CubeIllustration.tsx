d351ef271e51c85bc5db67002af2cf07e1aa0450
Hailey <hailey.robledo@skillsoft.com>
2026-01-08 16:26:17 -0500

    Add CubeIllustration component
    Introduced a new CubeIllustration React component and exported it from the illustrations package index for use in other parts of the application.

---
+++ packages/gamut-illustrations/src/CubeIllustration.tsx
@@ -0,0 +1,218 @@
+import * as React from 'react';
+
+import { IllustrationProps } from './types';
+
+export const CubeIllustration: React.FC<IllustrationProps> = ({
+  'aria-hidden': ariaHidden,
+  className,
+  height,
+  width,
+}) => (
+  <svg
+    aria-hidden={ariaHidden}
+    className={className}
+    fill="none"
+    height={height}
+    viewBox="0 0 48 48"
+    width={width}
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g clipPath="url(#clip0_2438_2846)">
+      <rect width="48" height="48" fill="white" />
+      <path
+        d="M45.2622 12.6602V36.0468L25.3724 48L3 37.1994V13.4076L22.8898 1.85815L45.2622 12.6602Z"
+        fill="#10162F"
+        stroke="#10162F"
+        strokeLinejoin="round"
+        strokeWidth="0.27941"
+      />
+      <path
+        d="M3 35.341V11.5464L25.3752 22.3484V46.1416L3 35.341Z"
+        fill="#FFD300"
+        stroke="black"
+        strokeMiterlimit="10"
+        strokeWidth="0.27941"
+      />
+      <path
+        d="M25.375 22.3486L45.2634 10.8005V34.1872L25.375 46.1417V22.3486Z"
+        fill="#FFF0E5"
+        stroke="#10162F"
+        strokeMiterlimit="10"
+        strokeWidth="0.27941"
+      />
+      <path
+        d="M3 11.5466L22.8884 0L45.2636 10.8006L25.3752 22.3486L3 11.5466Z"
+        fill="#FFD300"
+        stroke="black"
+        strokeMiterlimit="10"
+        strokeWidth="0.27941"
+      />
+      <path
+        d="M7.97223 19.2346L20.4032 25.2364V38.363L7.97223 32.4535V19.2346Z"
+        fill="#FFF0E5"
+        stroke="#10162F"
+        strokeLinejoin="round"
+        strokeWidth="0.27941"
+      />
+      <path
+        d="M7.97223 19.2346L20.4032 25.2364V38.363L7.97223 32.4535V19.2346Z"
+        stroke="#10162F"
+        strokeLinejoin="round"
+        strokeWidth="0.27941"
+      />
+      <path
+        d="M7.97223 32.4535L20.4032 25.2363V38.363L7.97223 32.4535Z"
+        fill="#10162F"
+        stroke="#10162F"
+        strokeLinejoin="round"
+        strokeWidth="0.27941"
+      />
+      <path
+        d="M8.44479 8.52954L29.6478 19.6864"
+        stroke="#10162F"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="0.27941"
+      />
+      <path
+        d="M18.2312 2.75854L39.4343 13.9154"
+        stroke="#10162F"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="0.27941"
+      />
+      <path
+        d="M13.3371 5.75952L34.5402 16.9164"
+        stroke="#10162F"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="0.27941"
+      />
+      <path
+        d="M36.0208 6.2478L17.0852 18.2513"
+        stroke="#10162F"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="0.27941"
+      />
+      <path
+        d="M40.3168 8.66772L21.7388 20.498"
+        stroke="#10162F"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="0.27941"
+      />
+      <path
+        d="M27.0017 1.88208L8.15823 13.8199"
+        stroke="#10162F"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="0.27941"
+      />
+      <path
+        d="M31.6548 4.12817L12.8994 16.0744"
+        stroke="#10162F"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="0.27941"
+      />
+      <path
+        d="M19.3032 17.783C19.8303 17.7269 20.2242 17.3679 20.183 16.9812C20.1418 16.5945 19.6811 16.3266 19.154 16.3827C18.6269 16.4389 18.233 16.7978 18.2742 17.1845C18.3154 17.5712 18.7761 17.8392 19.3032 17.783Z"
+        fill="#3A10E5"
+        stroke="#10162F"
+        strokeMiterlimit="10"
+        strokeWidth="0.27941"
+      />
+      <path
+        d="M32.791 13.6502C33.3181 13.5941 33.712 13.2351 33.6708 12.8484C33.6296 12.4617 33.169 12.1938 32.6419 12.2499C32.1148 12.306 31.7209 12.665 31.7621 13.0517C31.8033 13.4384 32.2639 13.7064 32.791 13.6502Z"
+        fill="#3A10E5"
+        stroke="#10162F"
+        strokeMiterlimit="10"
+        strokeWidth="0.27941"
+      />
+      <path
+        d="M40.311 9.20588C40.8381 9.14973 41.232 8.79075 41.1908 8.40407C41.1496 8.01738 40.6889 7.74943 40.1618 7.80557C39.6347 7.86171 39.2408 8.22069 39.282 8.60738C39.3232 8.99406 39.7839 9.26202 40.311 9.20588Z"
+        fill="#3A10E5"
+        stroke="#10162F"
+        strokeMiterlimit="10"
+        strokeWidth="0.27941"
+      />
+      <path
+        d="M19.6591 12.366C20.1862 12.3099 20.5801 11.9509 20.5389 11.5642C20.4977 11.1775 20.037 10.9096 19.5099 10.9657C18.9828 11.0219 18.5889 11.3809 18.6301 11.7675C18.6713 12.1542 19.132 12.4222 19.6591 12.366Z"
+        fill="#3A10E5"
+        stroke="#10162F"
+        strokeMiterlimit="10"
+        strokeWidth="0.27941"
+      />
+      <path
+        d="M19.7059 7.20466C20.233 7.14851 20.6269 6.78953 20.5857 6.40285C20.5446 6.01616 20.0839 5.74821 19.5568 5.80435C19.0297 5.86049 18.6358 6.21947 18.677 6.60616C18.7182 6.99284 19.1789 7.2608 19.7059 7.20466Z"
+        fill="#3A10E5"
+        stroke="#10162F"
+        strokeMiterlimit="10"
+        strokeWidth="0.27941"
+      />
+      <path
+        d="M29.502 19.9681C30.0291 19.9119 30.423 19.553 30.3818 19.1663C30.3406 18.7796 29.8799 18.5116 29.3528 18.5678C28.8257 18.6239 28.4318 18.9829 28.473 19.3696C28.5142 19.7563 28.9749 20.0242 29.502 19.9681Z"
+        fill="#3A10E5"
+        stroke="#10162F"
+        strokeMiterlimit="10"
+        strokeWidth="0.27941"
+      />
+      <path
+        d="M26.4881 7.99494C27.0151 7.9388 27.409 7.57981 27.3678 7.19313C27.3267 6.80645 26.866 6.53849 26.3389 6.59463C25.8118 6.65078 25.4179 7.00976 25.4591 7.39644C25.5003 7.78313 25.961 8.05108 26.4881 7.99494Z"
+        fill="#3A10E5"
+        stroke="#10162F"
+        strokeMiterlimit="10"
+        strokeWidth="0.27941"
+      />
+      <path
+        d="M8.88997 9.07306C9.41706 9.01692 9.81096 8.65794 9.76977 8.27125C9.72858 7.88457 9.2679 7.61661 8.74081 7.67276C8.21373 7.7289 7.81983 8.08788 7.86102 8.47457C7.9022 8.86125 8.36288 9.12921 8.88997 9.07306Z"
+        fill="#3A10E5"
+        stroke="#10162F"
+        strokeMiterlimit="10"
+        strokeWidth="0.27941"
+      />
+      <path
+        d="M31.3038 25.945V30.9897L33.7249 29.6891V24.5396L31.3038 25.945Z"
+        fill="#10162F"
+      />
+      <path
+        d="M31.3038 25.2983V30.343L33.7249 29.0424V23.8928L31.3038 25.2983Z"
+        fill="#3A10E5"
+        stroke="#10162F"
+        strokeMiterlimit="10"
+        strokeWidth="0.27941"
+      />
+      <path
+        d="M40.6897 20.9099V26.0594L38.3455 27.4202V22.3531L40.6897 20.9099Z"
+        fill="#10162F"
+      />
+      <path
+        d="M40.6897 20.0269V25.1764L38.3455 26.5385V21.4714L40.6897 20.0269Z"
+        fill="#3A10E5"
+        stroke="#10162F"
+        strokeMiterlimit="10"
+        strokeWidth="0.27941"
+      />
+      <path
+        d="M40.9896 28.7488L31.3038 34.3719V36.9467L40.9896 31.3235V28.7488Z"
+        fill="#10162F"
+        stroke="#10162F"
+        strokeLinejoin="round"
+        strokeWidth="0.27941"
+      />
+      <path
+        d="M40.9896 28.0239L31.3038 33.6471V36.2218L40.9896 30.5987V28.0239Z"
+        fill="#3A10E5"
+        stroke="#10162F"
+        strokeMiterlimit="10"
+        strokeWidth="0.27941"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_2438_2846">
+        <rect width="48" height="48" fill="white" />
+      </clipPath>
+    </defs>
+  </svg>
+);
