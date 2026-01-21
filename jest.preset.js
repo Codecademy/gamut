fcc2a344bef776acfe4448854021ab181919fab1
Evan Yamanishi <sh0ji@users.noreply.github.com>
2026-01-21 14:24:00 -0500

    style: nx format:write

--- jest.preset.js
+++ jest.preset.js
@@ -8,6 +8,10 @@ const targetProject = kebabCase(
 module.exports = {
   ...nxPreset,
   reporters: process.env.CI
-    ? ['default', 'github-actions', ['jest-junit', { outputName: `${targetProject}-report.xml` }]]
+    ? [
+        'default',
+        'github-actions',
+        ['jest-junit', { outputName: `${targetProject}-report.xml` }],
+      ]
     : ['default'],
 };
