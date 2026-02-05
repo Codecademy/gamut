cd465e05831ead167ef4830f2f46c4be02609cee
Kenny Lin <kenny.lin.91@gmail.com>
2026-02-05 16:10:18 -0500

    fix codecov coverage

--- packages/gamut/src/Pagination/__tests__/Pagination.test.tsx
+++ packages/gamut/src/Pagination/__tests__/Pagination.test.tsx
@@ -13,7 +13,7 @@ const renderView = setupRtl(Pagination, {
 interface TestHelpersType {
   view: RenderResult<typeof queries, HTMLElement>;
   pageNumber?: number;
-  direction?: 'forward' | 'back';
+  buttonDirection?: 'forward' | 'back';
 }
 
 const getPage = ({ view, pageNumber }: TestHelpersType) => {
@@ -31,8 +31,8 @@ const getForwardButton = ({ view }: TestHelpersType) => {
 const getJumpButtonCount = ({ view }: TestHelpersType) =>
   view.getAllByLabelText(/(Jump )+/).length;
 
-const getJumpButton = ({ view, pageNumber, direction }: TestHelpersType) => {
-  return view.getByLabelText(`Jump ${direction} to page ${pageNumber}`);
+const getJumpButton = ({ view, pageNumber, buttonDirection }: TestHelpersType) => {
+  return view.getByLabelText(`Jump ${buttonDirection} to page ${pageNumber}`);
 };
 
 describe('Pagination', () => {
@@ -181,7 +181,7 @@ describe('Pagination', () => {
       const forwardButton = getJumpButton({
         view,
         pageNumber: 6,
-        direction: 'forward',
+        buttonDirection: 'forward',
       });
       fireEvent.click(forwardButton);
 
@@ -201,7 +201,7 @@ describe('Pagination', () => {
       const backButton = getJumpButton({
         view,
         pageNumber: 6,
-        direction: 'back',
+        buttonDirection: 'back',
       });
 
       fireEvent.click(backButton);
@@ -217,7 +217,7 @@ describe('Pagination', () => {
       const backButton = getJumpButton({
         view,
         pageNumber: 3,
-        direction: 'back',
+        buttonDirection: 'back',
       });
 
       fireEvent.click(backButton);
@@ -226,7 +226,7 @@ describe('Pagination', () => {
       const forwardButton = getJumpButton({
         view,
         pageNumber: 8,
-        direction: 'forward',
+        buttonDirection: 'forward',
       });
 
       fireEvent.click(forwardButton);
