4e56ec4190950bb1212fd886f9fa14feef984e38
dreamwasp <cass.spain@skillsoft.com>
2026-02-09 16:16:17 -0500

    build

--- packages/gamut/src/Tip/__tests__/helpers.tsx
+++ packages/gamut/src/Tip/__tests__/helpers.tsx
@@ -1,5 +1,5 @@
 import { setupRtl } from '@codecademy/gamut-tests';
-import { act, screen, waitFor } from '@testing-library/react';
+import { screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { createRef, RefObject } from 'react';
 
