4ff2b761badb3b0c6987a7c0a502ca71349d6e88
Jake Hiller <Jake.Hiller@skillsoft.com>
2026-03-18 16:07:59 -0400

    Add test utility setup for AssetProvider and GamutProvider tests
    - Introduced a new testUtils.tsx file to encapsulate the setupRtl function with a mock GamutProvider.
    - Updated AssetProvider.test.tsx and GamutProvider.test.tsx to use the new setupRtl from testUtils, improving test consistency.

---
+++ packages/gamut-styles/src/__tests__/testUtils.tsx
@@ -0,0 +1,25 @@
+import { setupRtl as setupRtlBase } from 'component-test-setup';
+import overArgs from 'lodash/overArgs';
+import * as React from 'react';
+
+import { GamutProvider } from '../GamutProvider';
+import { coreTheme as theme } from '../themes';
+
+// This is effectively a duplicate of the `MockGamutProvider` in `gamut-tests`
+// Because gamut-tests depends on gamut-styles, it creates a circular dependency.
+function withMockGamutProvider<Props extends JSX.IntrinsicAttributes>(
+  WrappedComponent: React.ComponentType<Props>
+) {
+  const WithBoundaryComponent: React.FC<Props> = (props) => (
+    <GamutProvider theme={theme} useCache={false} useGlobals={false}>
+      <WrappedComponent {...props} />
+    </GamutProvider>
+  );
+
+  return WithBoundaryComponent;
+}
+
+export const setupRtl = overArgs(
+  setupRtlBase,
+  withMockGamutProvider
+) as typeof setupRtlBase;
