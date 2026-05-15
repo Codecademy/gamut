b77825e58df2f13dbdedd682e56a41c2ea0fff18
Kenny Lin <kenny.lin.91@gmail.com>
2026-05-15 09:44:17 -0400

    removed import warning

--- packages/gamut-styles/utils/variables/index.js
+++
@@ -1,8 +0,0 @@
-if (process.env.NODE_ENV === 'development') {
-  // eslint-disable-next-line no-console
-  console.warn(
-    'Importing from `gamut-styles/utils/variables` is now deprecated, please import directly from the gamut-styles package'
-  );
-}
-
-export * from '../../dist/variables';
