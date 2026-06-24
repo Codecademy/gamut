7f522b9b66de94ddb8c66196a7ea7e7c92b81d38
Kenny Lin <kenny.lin.91@gmail.com>
2026-06-24 10:31:35 -0400

    remove test code and update version plan

--- packages/gamut/src/DataList/DataList.tsx
+++ packages/gamut/src/DataList/DataList.tsx
@@ -16,24 +16,3 @@ export const DataList: DataList = ({ variant = 'default', ...rest }) => {
     <DataGrid variant={variant} {...rest} scrollable={false} shadow={false} />
   );
 };
-
-// USE FOR TESTING ONLY
-// UNCOMMENT AND RUN THE FOLLOWING CODE TO TEST THAT size: 'content' errors
-
-// const TestingDataList = () => {
-//   return (
-//     <DataList variant="default" columns={[
-//       { header: 'Name', key: 'name', size: 'lg' },
-//       { header: 'Age', key: 'age', size: 'sm' },
-//       { header: 'Species', key: 'species', size: 'md' },
-//       { header: 'Ship', key: 'ship', size: 'content' },
-//     ]}
-//       id="test"
-//       idKey="id"
-//       rows={[
-//         { id: 1, name: 'John Doe', age: 30, species: 'Human', ship: 'USS Enterprise' },
-//         { id: 2, name: 'Jane Doe', age: 25, species: 'Human', ship: 'USS Enterprise' },
-//         { id: 3, name: 'John Smith', age: 35, species: 'Human', ship: 'USS Enterprise' },
-//       ]} />
-//   )
-// }
--- packages/gamut/src/DataList/DataTable.tsx
+++ packages/gamut/src/DataList/DataTable.tsx
@@ -28,26 +28,3 @@ export const DataTable: DataTable = (props) => {
     />
   );
 };
-
-// USE FOR TESTING ONLY
-// UNCOMMENT AND RUN THE FOLLOWING CODE TO TEST THAT size: 'content' errors
-
-// const TestingDataTable = () => {
-//   return (
-//     <DataTable
-//       columns={[
-//         { header: 'Name', key: 'name', size: 'lg' },
-//         { header: 'Age', key: 'age', size: 'sm' },
-//         { header: 'Species', key: 'species', size: 'md' },
-//         { header: 'Ship', key: 'ship', size: 'content' },
-//       ]}
-//       id="test"
-//       idKey="id"
-//       rows={[
-//         { id: 1, name: 'John Doe', age: 30, species: 'Human', ship: 'USS Enterprise' },
-//         { id: 2, name: 'Jane Doe', age: 25, species: 'Human', ship: 'USS Enterprise' },
-//         { id: 3, name: 'John Smith', age: 35, species: 'Human', ship: 'USS Enterprise' },
-//       ]}
-//     />
-//   )
-// }
