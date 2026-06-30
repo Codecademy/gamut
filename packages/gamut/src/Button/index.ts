fb7a4906797d190850d147491556b7cbf9dba042
dreamwasp <cass.spain@skillsoft.com>
2026-06-30 13:06:36 -0400

    feat: promote internal symbols to public API for module migration
    Adds missing exports needed by consumers migrating away from /dist deep imports:
    - variance: Scale type
    - gamut/Button: ButtonBaseProps
    - gamut/ButtonBase: ButtonSelectors
    - gamut/Form: FormValues, formFieldPaddingStyles, formFieldStyles, conditionalStyles
    - gamut/Markdown: HTMLToReactNode
    - gamut/Menu: ListItem
    - gamut-styles/variance: propConfig namespace

--- packages/gamut/src/Button/index.ts
+++ packages/gamut/src/Button/index.ts
@@ -1,4 +1,4 @@
-export type { ButtonProps } from './shared';
+export type { ButtonProps, ButtonBaseProps } from './shared';
 export * from './CTAButton';
 export * from './FillButton';
 export * from './IconButton';
--- packages/variance/src/index.ts
+++ packages/variance/src/index.ts
@@ -1,6 +1,7 @@
 export { variance } from './core';
 export * from './createTheme';
 export * from './types/props';
+export type { Scale } from './types/config';
 export * from './transforms';
 export * from './scales/createScale';
 export * from './getPropertyMode';
