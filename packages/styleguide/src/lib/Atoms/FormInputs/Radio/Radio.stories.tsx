74c6a8006a271442a16cc7a541942c5056b2044e
Amy Resnik <aresnik11@gmail.com>
2026-08-24 18:42:56 -0400

    fix

--- packages/styleguide/src/lib/Atoms/FormInputs/Radio/Radio.stories.tsx
+++ packages/styleguide/src/lib/Atoms/FormInputs/Radio/Radio.stories.tsx
@@ -1,6 +1,6 @@
 import { FormGroup, Radio, RadioGroup } from '@codecademy/gamut';
 import type { Meta, StoryObj } from '@storybook/react';
-import { ChangeEvent, useState } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import type { TypeWithDeepControls } from 'storybook-addon-deep-controls';
 
 import { infotipNestedArgTypes } from '~styleguide/argTypes';
@@ -24,33 +24,31 @@ export const Default: Story = {
   args: {},
 };
 
-export const RadioGroupComponent: Story = {
-  render: () => {
-    const [selected, setSelected] = useState('option-1');
+export const RadioGroupComponent: React.FC = () => {
+  const [selected, setSelected] = useState('option-1');
 
-    const changeHandler = (event: ChangeEvent<HTMLInputElement>) => {
-      setSelected(event.target.value);
-    };
+  const changeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setSelected(event.target.value);
+  };
 
-    return (
-      <RadioGroup
-        htmlForPrefix="example-radio"
-        name="example-radio"
-        onChange={changeHandler}
-      >
-        <Radio
-          label="Option 1"
-          value="option-1"
-          checked={selected === 'option-1'}
-        />
-        <Radio
-          label="Option 2"
-          value="option-2"
-          checked={selected === 'option-2'}
-        />
-      </RadioGroup>
-    );
-  },
+  return (
+    <RadioGroup
+      htmlForPrefix="example-radio"
+      name="example-radio"
+      onChange={changeHandler}
+    >
+      <Radio
+        checked={selected === 'option-1'}
+        label="Option 1"
+        value="option-1"
+      />
+      <Radio
+        checked={selected === 'option-2'}
+        label="Option 2"
+        value="option-2"
+      />
+    </RadioGroup>
+  );
 };
 
 export const RadioGroupWithInfoTips: Story = {
